444e44617ed54c8f37076ae989ff1f95805a893b
Aki Kaivola <aki.kaivola@gmail.com>
2016-03-23 11:08:32 +0200

    Base project

--- src/cljs/lomake_editori/core.cljs
+++ src/cljs/lomake_editori/core.cljs
@@ -6,7 +6,7 @@
               [lomake-editori.routes :as routes]
               [lomake-editori.views :as views]
               [lomake-editori.config :as config]
-              [taoensso.timbre :refer-macros [spy]]))
+              [taoensso.timbre :refer-macros [spy info]]))
 
 (when config/debug?
   (info "dev mode"))
@@ -15,7 +15,7 @@
   (reagent/render [views/main-panel]
                   (.getElementById js/document "app")))
 
-(defn ^:export init [] 
+(defn ^:export init []
   (routes/app-routes)
   (re-frame/dispatch-sync [:initialize-db])
   (mount-root))
