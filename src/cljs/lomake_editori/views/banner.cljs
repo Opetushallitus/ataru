4224608699d517bbad37d3f6f6b891990aaa0d9f
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-12 13:39:39 +0300

    Build more layout

--- src/cljs/lomake_editori/views/banner.cljs
+++ src/cljs/lomake_editori/views/banner.cljs
@@ -3,19 +3,24 @@
             [reagent.core :as r]
             [re-com.core :as c]))
 
-(def logo [:div "logo"])
-(def title [:div "LOMAKE-EDITORI | HAKEMUKSET"])
-(def profile [:div "Testi Käyttäjä"
+(def logo [:div.logo
+           [:img {:src "/images/opintopolku_large-fi.png"
+                  :height "40px"}]])
+
+(def title [:div.title "LOMAKE-EDITORI | HAKEMUKSET"])
+
+(def profile [:div.profile "Testi Käyttäjä"
               [:a {:href "#"} "Kirjaudu ulos"]])
 
 (def top-banner
   [c/h-box
+   :class "top-banner"
    :width  "100%"
    :height "100px"
    :align :center
    :children [[c/box
                :child logo
-               :size "1"]
+               :size "300px"]
               [c/box
                :child title
                :size "1"]
