1b74e0adb94e448e9b694b3b10fbe5ca7e1c9cf9
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-12 14:50:02 +0300

    Create initial opintopolku.fi banner

--- src/cljs/lomake_editori/views/banner.cljs
+++ src/cljs/lomake_editori/views/banner.cljs
@@ -3,14 +3,36 @@
             [reagent.core :as r]
             [re-com.core :as c]))
 
-(def logo [:div.logo
-           [:img {:src "/images/opintopolku_large-fi.png"
-                  :height "40px"}]])
+(def logo
+  [:div.logo
+    [:img {:src "/images/opintopolku_large-fi.png"
+           :height "40px"}]])
 
-(def title [:div.title "LOMAKE-EDITORI | HAKEMUKSET"])
+(def title
+  [c/h-box
+   :class "title"
+   :width "100%"
+   :align :center
+   :gap "1em"
+   :children
+   [[c/box
+     :child [:span "Virkailijan Työpöytä"]
+     :class "section-name"]
+    [c/box
+     :child [:span ""]
+     :class "divider"
+     :size "1px"]
+    [c/box
+     :child [:span "Hakemukset"]]]
+   ])
 
-(def profile [:div.profile "Testi Käyttäjä"
-              [:a {:href "#"} "Kirjaudu ulos"]])
+(def profile
+  [:div.profile
+   [:div
+    [:p "Testi Käyttäjä"]
+    [:p "Stadin Aikuisopisto"]]
+   [:div
+    [:a {:href "#"} "Kirjaudu ulos"]]])
 
 (def top-banner
   [c/h-box
@@ -18,6 +40,7 @@
    :width  "100%"
    :height "100px"
    :align :center
+   :justify :center
    :children [[c/box
                :child logo
                :size "300px"]
@@ -26,5 +49,5 @@
                :size "1"]
               [c/box
                :child profile
-               :size "1"]]])
+               :size "300px"]]])
 
