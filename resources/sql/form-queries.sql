875f2294728244d5ccc3bcc219a683379f684573
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-25 13:49:55 +0300

    Remove id from insert

--- resources/sql/form-queries.sql
+++ resources/sql/form-queries.sql
@@ -1,15 +1,15 @@
 -- name: get-forms-query
 -- Get all stored forms
-select id, name, modified_time from forms
+select id, name, modified_time from forms;
 
 -- name: add-form-query<!
 -- Add form
-insert into forms (id, name) values (:id, :name)
+insert into forms (name) values (:name);
 
 -- name: form-exists-query
 -- Get single form
-select id from forms where id = :id
+select id from forms where id = :id;
 
 -- name: update-form-query!
 -- Update form
-update forms set name = :name where id = :id
+update forms set name = :name where id = :id;
