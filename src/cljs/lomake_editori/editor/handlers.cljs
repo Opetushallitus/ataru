dfd766b9977c616a3f0af422db80dac9e6a5e6c4
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-26 11:39:40 +0300

    Move handlers to own namespace

--- src/cljs/lomake_editori/editor/handlers.cljs
+++ src/cljs/lomake_editori/editor/handlers.cljs
@@ -1,6 +1,10 @@
 (ns lomake-editori.editor.handlers
   (:require [re-frame.core :refer [register-handler dispatch]]
             [lomake-editori.handlers :refer [http]]
+            [lomake-editori.temporal :refer [coerce-timestamp]]
+            [lomake-editori.handlers :refer [post]]
+            [cljs.core.match :refer-macros [match]]
+            [goog.date :as gd]
             [taoensso.timbre :refer-macros [spy debug]]))
 
 (defn refresh-forms []
@@ -14,3 +18,37 @@
   (fn [db _]
     (refresh-forms)
     db))
+
+(register-handler
+  :editor/select-form
+  (fn [db [_ clicked-form]]
+    (assoc-in db [:editor :selected-form]
+              clicked-form)))
+
+(register-handler
+  :editor/add-form
+  (fn [db _]
+    (post "/lomake-editori/api/form"
+          {:name   "Uusi lomake"
+           :author {:last  "Testaaja" ;; placeholder
+                    :first "Teppo"}}
+          (fn [db new-form]
+            (let [form-with-time (-> ((coerce-timestamp :modified-time) new-form)
+                                     (assoc :author {:last  "Testaaja" ;; placeholder
+                                                     :first "Teppo"}))]
+              (-> db
+                  (assoc-in [:editor :selected-form] form-with-time)
+                  (assoc-in [:editor :forms (:id form-with-time)] form-with-time)))))
+    db))
+
+(register-handler
+  :editor/change-form-name
+  (fn [db [_ new-form-name]]
+    (let [selected-form (-> db :editor :selected-form)
+          name-before-edit (:name selected-form)]
+      (update-in db [:editor :forms (:id selected-form)]
+                 assoc :name
+                 (if (empty? new-form-name)
+                   name-before-edit
+                   new-form-name)))))
+
--- src/cljs/lomake_editori/editor/view.cljs
+++ src/cljs/lomake_editori/editor/view.cljs
@@ -3,47 +3,11 @@
   (:require [re-frame.core :refer [subscribe dispatch dispatch-sync register-handler]]
             [reagent.core :as r]
             [lomake-editori.soresu.component :as component]
-            [lomake-editori.temporal :refer [time->str coerce-timestamp]]
-            [lomake-editori.handlers :refer [post]]
-            [goog.date :as gd]
+            [lomake-editori.temporal :refer [time->str]]
             [re-com.core :as re-com]
-            [cljs.core.match :refer-macros [match]]
             [lomake-editori.dev.lomake :as l]
             [taoensso.timbre :refer-macros [spy debug]]))
 
-
-(register-handler
-  :editor/select-form
-  (fn [db [_ clicked-form]]
-    (assoc-in db [:editor :selected-form]
-              clicked-form)))
-
-(register-handler
-  :editor/add-form
-  (fn [db _]
-    (post "/lomake-editori/api/form"
-          {:name   "Uusi lomake"
-           :author {:last  "Testaaja" ;; placeholder
-                    :first "Teppo"}}
-          (fn [db new-form]
-            (let [form-with-time (-> ((coerce-timestamp :modified-time) new-form)
-                                     (assoc :author {:last  "Testaaja" ;; placeholder
-                                                     :first "Teppo"}))]
-              (-> db
-                  (assoc-in [:editor :selected-form] form-with-time)
-                  (assoc-in [:editor :forms (:id form-with-time)] form-with-time)))))
-    db))
-
-(register-handler
-  :editor/change-form-name
-  (fn [db [_ new-form-name]]
-    (let [selected-form (-> db :editor :selected-form)
-          name-before-edit (:name selected-form)]
-      (update-in db [:editor :forms (:id selected-form)]
-                 assoc :name
-                 (if (empty? new-form-name)
-                   name-before-edit
-                   new-form-name)))))
 
 (defn form-list []
   (let [forms            (subscribe [:state-query [:editor :forms]])
