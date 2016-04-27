178f3f45514cdeb18de84df107d61a1ba1eb91f5
Aki Kaivola <aki.kaivola@gmail.com>
2016-04-27 21:28:13 +0300

    No need to touch :selected-form all the time

--- src/cljs/lomake_editori/editor/handlers.cljs
+++ src/cljs/lomake_editori/editor/handlers.cljs
@@ -43,12 +43,7 @@
   (let [form-with-time (-> ((coerce-timestamp :modified-time) new-or-updated-form)
                            (assoc :author {:last  "Testaaja" ;; placeholder
                                            :first "Teppo"}))]
-    (-> db
-        ; take note :selected-form is not watched/subscribed by autosave or the ui
-        ; it is only changed here to keep it in sync with [:editor :forms]
-        ; changes to selected-form should be done via (dispatch [:editor/select-form form])
-        (assoc-in [:editor :selected-form] form-with-time)
-        (assoc-in [:editor :forms (:id form-with-time)] form-with-time))))
+    (assoc-in db [:editor :forms (:id form-with-time)] form-with-time)))
 
 (register-handler
   :editor/save-form
@@ -66,6 +61,7 @@
            :author {:last  "Testaaja" ;; placeholder
                     :first "Teppo"}}
           (fn [db new-or-updated-form]
+            (autosave/stop-autosave! (-> db :editor :autosave))
             (refresh-forms)
             db))
     db))
@@ -73,10 +69,7 @@
 (register-handler
   :editor/change-form-name
   (fn [db [_ new-form-name]]
-    (let [selected-form (-> db :editor :selected-form)
-          name-before-edit (:name selected-form)]
+    (let [selected-form (-> db :editor :selected-form)]
       (update-in db [:editor :forms (:id selected-form)]
                  assoc :name
-                 (if (empty? new-form-name)
-                   name-before-edit
-                   new-form-name)))))
+                 new-form-name))))
