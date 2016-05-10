05bf776b8dbdedbea9fe41859ade9f82bde44d48
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-05-10 08:27:53 +0300

    Use ordinary routing utilities instead of compojure-api
    This is not api-stuff, all happening in browser

--- src/clj/lomake_editori/authentication/auth_routes.clj
+++ src/clj/lomake_editori/authentication/auth_routes.clj
@@ -1,6 +1,6 @@
 (ns lomake-editori.authentication.auth-routes
   (:require [lomake-editori.authentication.login :refer [login]]
-            [compojure.api.sweet :as api]
+            [compojure.core :refer [GET routes context]]
             [taoensso.timbre :refer [info error]]
             [ring.util.http-response :refer [ok]]
             [ring.util.response :as resp]))
@@ -13,22 +13,20 @@
 (def ataru-login-success-url "http://localhost:8350/lomake-editori/auth/cas")
 
 (defn auth-routes []
-  (api/api
-    (api/context "/lomake-editori/auth" []
-                 (api/GET "/cas" [ticket :as request]
-                          :summary "Handle login ticket and logout callback from cas"
-                          (try
-                            (if ticket
-                              (if-let [username (login ticket "http://localhost:8350/lomake-editori/auth/cas")]
-                                (do
-                                  (info "username" username "logged in")
-                                  (-> (resp/redirect "http://localhost:8350/lomake-editori")
-                                      (assoc :session {:identity {:username username :ticket ticket}})))
-                                (redirect-to-loggged-out-page))
-                              (redirect-to-loggged-out-page))
-                            (catch Exception e
-                              (error "Error in login ticket handling" e)
-                              (redirect-to-loggged-out-page))))
-                 (api/GET "/logout" {session :session}
-                          (-> (resp/redirect (str opintopolku-logout-url ataru-login-success-url))
-                           (assoc :session {:identity nil}))))))
+  (context "/lomake-editori/auth" []
+           (GET "/cas" [ticket]
+                (try
+                  (if ticket
+                    (if-let [username (login ticket "http://localhost:8350/lomake-editori/auth/cas")]
+                      (do
+                        (info "username" username "logged in")
+                        (-> (resp/redirect "http://localhost:8350/lomake-editori")
+                            (assoc :session {:identity {:username username :ticket ticket}})))
+                      (redirect-to-loggged-out-page))
+                    (redirect-to-loggged-out-page))
+                  (catch Exception e
+                    (error "Error in login ticket handling" e)
+                    (redirect-to-loggged-out-page))))
+           (GET "/logout" []
+                (-> (resp/redirect (str opintopolku-logout-url ataru-login-success-url))
+                    (assoc :session {:identity nil})))))
