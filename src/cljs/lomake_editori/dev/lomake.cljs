40522ccd14846fef42583947150dbcb3882c2785
Esko Lahti <esko.lahti@reaktor.com>
2016-05-10 15:36:54 +0300

    Just show the mocked-up name fields

--- src/cljs/lomake_editori/dev/lomake.cljs
+++ src/cljs/lomake_editori/dev/lomake.cljs
@@ -22,75 +22,20 @@
 
 (def placeholder-content
   {:content
-   [{:fieldClass "infoElement"
-     :id         (str (gensym))
-     :fieldType  "p"
-     :params     {:preview false}
-     :text       {:fi "Hakemusta on mahdollista muokata hakuajan loppuun asti. Hakemukset käsitellään hakuajan jälkeen."
-                  :sv "Ansökan kan bearbetas ända till ansökningstidens slut. Ansökningarna behandlas efter ansökningstiden."}}
-
-    {:fieldClass "infoElement"
-     :id         (str (gensym))
-     :fieldType  "link"
-     :params     {:href {:fi "http://oph.fi/download/173879_ohje_othk_2016_hakijoille.pdf"
-                         :sv "http://oph.fi/download/173895_anvisingar_for_ansokan_personalfortbildning_2016.pdf"}}
-     :text       {:fi "Lataa ohje hakijoille."
-                  :sv "Ladda ner anvisningarna för ansökan "}}
-
-    {:fieldClass "wrapperElement",
-     :id         "applicant-fieldset",
-     :fieldType  "fieldset",
+   [{:fieldClass "wrapperElement"
+     :id         "applicant-fieldset"
      :children
-     [{:params     {:size "large", :maxlength 80},
-       :fieldClass "formField",
+     [{:fieldClass "formField"
        :helpText
-       {:fi "Yhteyshenkilöllä tarkoitetaan hankkeen vastuuhenkilöä.",
-        :sv "Med kontaktperson avses den projektansvariga i sökandeorganisationen."},
-       :label      {:fi "Yhteyshenkilö", :sv "Kontaktperson"},
-       :id         "applicant-name",
-       :required   true,
+                   {:fi "Yhteyshenkilöllä tarkoitetaan hankkeen vastuuhenkilöä."
+                    :sv "Med kontaktperson avses den projektansvariga i sökandeorganisationen."}
+       :label      {:fi "Sukunimi", :sv "Efternamn"}
+       :id         "applicant-firstname"
+       :required   true
        :fieldType  "textField"}
-      {:fieldClass "wrapperElement",
-       :id         "postal-stuff",
-       :fieldType  "fieldset",
-       :children
-       [{:params     {:size "large", :maxlength 100},
-         :fieldClass "formField",
-         :helpText   {:fi "", :sv ""},
-         :label      {:fi "Postiosoite", :sv "Postadress"},
-         :id         "textField-2",
-         :required   true,
-         :fieldType  "textField"}
-        {:params     {:size "extra-small", :maxlength 8},
-         :fieldClass "formField",
-         :helpText   {:fi "", :sv ""},
-         :label      {:fi "Postinumero", :sv "Postnummer"},
-         :id         "textField-3",
-         :required   true,
-         :fieldType  "textField"}
-        {:params     {:size "small", :maxlength 100},
-         :fieldClass "formField",
-         :helpText   {:fi "", :sv ""},
-         :label      {:fi "Postitoimipaikka", :sv "Postanstalt"},
-         :id         "textField-4",
-         :required   true,
-         :fieldType  "textField"}]}
-      {:params     {},
-       :fieldClass "formField",
-       :helpText   {:fi "", :sv ""},
-       :label      {:fi "Omistajatyyppi", :sv "Ägartyp"},
-       :id         "radioButton-0",
-       :options
-       [{:value "kunta-kuntayhtymae", :label {:fi "Kunta/Kuntayhtymä", :sv "Kommun/Samkommun"}}
-        {:value "rekisteroeity-yhteisoe-tai-saeaetioe",
-         :label {:fi "rekisteröity yhteisö tai säätiö", :sv "registrerad sammanslutning eller stiftelse"}}
-        {:value "yliopisto", :label {:fi "Yliopisto", :sv "Universitet"}}],
-       :required   true,
-       :fieldType  "radioButton"}
-      {:params     {:size "small", :maxlength 100},
-       :fieldClass "formField",
-       :helpText   {:fi "", :sv ""},
-       :label      {:fi "Yritysmuoto", :sv "Företagsform"},
-       :id         "textField-0",
-       :required   true,
-       :fieldType  "textField"}]}]})
+      {:fieldClass "formField"
+       :label      {:fi "Etunimi", :sv "Förnamn"}
+       :id         "applicant-surname"
+       :required   true
+       :fieldType  "textField"}
+      ]}]})
