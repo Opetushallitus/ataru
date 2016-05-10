9fac4e1986735f8a2c04c1ebf5db22d68ff86c28
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-05-10 11:12:56 +0300

    Fix logged out url
    It's now the same as original cas login page
    Fix typo also

--- src/clj/lomake_editori/authentication/auth_routes.clj
+++ src/clj/lomake_editori/authentication/auth_routes.clj
@@ -6,28 +6,28 @@
             [ring.util.http-response :refer [ok]]
             [ring.util.response :as resp]))
 
-(defn- redirect-to-loggged-out-page []
-  ;; TODO implement logged-out page
-  (resp/redirect (str "/lomake-editori/logged-out")))
-
+(def opintopolku-login-url (-> config :authentication :opintopolku-login-url))
 (def opintopolku-logout-url (-> config :authentication :opintopolku-logout-url))
 (def ataru-login-success-url (-> config :authentication :ataru-login-success-url))
+
+(defn- redirect-to-logged-out-page []
+  (resp/redirect (str opintopolku-login-url ataru-login-success-url)))
 
 (defn auth-routes []
   (context "/lomake-editori/auth" []
            (GET "/cas" [ticket]
-                (try
-                  (if ticket
-                    (if-let [username (login ticket ataru-login-success-url)]
-                      (do
+             (try
+               (if ticket
+                 (if-let [username (login ticket ataru-login-success-url)]
+                   (do
                         (info "username" username "logged in")
                         (-> (resp/redirect "/lomake-editori")
                             (assoc :session {:identity {:username username :ticket ticket}})))
-                      (redirect-to-loggged-out-page))
-                    (redirect-to-loggged-out-page))
-                  (catch Exception e
-                    (error "Error in login ticket handling" e)
-                    (redirect-to-loggged-out-page))))
+                   (redirect-to-logged-out-page))
+                 (redirect-to-logged-out-page))
+               (catch Exception e
+                 (error "Error in login ticket handling" e)
+                 (redirect-to-logged-out-page))))
            (GET "/logout" {session :session}
                 (info "username" (-> session :identity :username) "logged out")
                 (-> (resp/redirect (str opintopolku-logout-url ataru-login-success-url))
