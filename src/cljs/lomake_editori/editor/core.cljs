3805960056c749db9a662d04d90bcbcce448881f
Esko Lahti <esko.lahti@reaktor.com>
2016-05-11 10:05:08 +0300

    Don't remove path from add-component setup fn

--- src/cljs/lomake_editori/editor/core.cljs
+++ src/cljs/lomake_editori/editor/core.cljs
@@ -121,7 +121,7 @@
            (reset! local-latch handle)
            (reset! latch handle))))]))
 
-(defn add-component []
+(defn add-component [path]
   (let [show-bar? (reaction nil)
         [toolbar-abort-trigger
          toolbar-delayed-trigger] (delayed-trigger 1000 #(reset! show-bar? false))
