6eeec78c274f4fc07bf01083cc36596fd783570a
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-05-12 12:01:59 +0300

    Use the right parsing of cas logout
    https://wiki.jasig.org/display/casum/single+sign+out#SingleSignOut-Howitworks

--- src/clj/lomake_editori/authentication/auth.clj
+++ src/clj/lomake_editori/authentication/auth.clj
@@ -1,7 +1,9 @@
 (ns lomake-editori.authentication.auth
   (:require [oph.soresu.common.config :refer [config]]
+            [taoensso.timbre :refer [info error]]
             [lomake-editori.authentication.cas-ticketstore :as cas-store]
-            [clj-util.cas :as cas]))
+            [clj-util.cas :as cas])
+  (:import (fi.vm.sade.utils.cas CasLogout)))
 
 (def cas-client-url (-> config :authentication :cas-client-url))
 
@@ -16,7 +18,12 @@
     (cas-store/logout ticket)))
 
 (defn cas-initiated-logout [logout-request]
-  (cas-store/logout logout-request))
+  (info "cas-initiated logout")
+  (let [ticket (CasLogout/parseTicketFromLogoutRequest logout-request)]
+    (info "logging out ticket" ticket)
+    (if (.isEmpty ticket)
+      (error "Could not parse ticket from CAS request" logout-request)
+      (cas-store/logout (.get ticket)))))
 
 (defn logged-in? [request]
   (let [ticket (-> request :session :identity :ticket)]
