d0ffafed92b5189a39a6326d8e90e8eeb0927006
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-05-12 14:57:07 +0300

    Move implementation totally to auth-module.
    Earlier part of the impl was in auth and part in routes.
    Routes gets pretty bare, but is simple now.

--- src/clj/lomake_editori/authentication/auth.clj
+++ src/clj/lomake_editori/authentication/auth.clj
@@ -2,20 +2,44 @@
   (:require [oph.soresu.common.config :refer [config]]
             [taoensso.timbre :refer [info error]]
             [lomake-editori.authentication.cas-ticketstore :as cas-store]
-            [clj-util.cas :as cas])
+            [clj-util.cas :as cas]
+            [ring.util.http-response :refer [ok]]
+            [ring.util.response :as resp])
   (:import (fi.vm.sade.utils.cas CasLogout)))
 
 (def cas-client-url (-> config :authentication :cas-client-url))
+(def opintopolku-login-url (-> config :authentication :opintopolku-login-url))
+(def opintopolku-logout-url (-> config :authentication :opintopolku-logout-url))
+(def ataru-login-success-url (-> config :authentication :ataru-login-success-url))
 
-(defn login [ticket virkailija-login-url]
+(defn- redirect-to-logged-out-page []
+  (resp/redirect (str opintopolku-login-url ataru-login-success-url)))
+
+(defn- cas-login [ticket virkailija-login-url]
   (let [cas-client (cas/cas-client cas-client-url)
         username (.run (.validateServiceTicket cas-client virkailija-login-url ticket))]
     (cas-store/login ticket)
     username))
 
+(defn login [ticket]
+  (try
+    (if ticket
+      (if-let [username (cas-login ticket ataru-login-success-url)]
+        (do
+          (info "username" username "logged in")
+          (-> (resp/redirect "/lomake-editori")
+              (assoc :session {:identity {:username username :ticket ticket}})))
+        (redirect-to-logged-out-page))
+      (redirect-to-logged-out-page))
+    (catch Exception e
+      (error "Error in login ticket handling" e)
+      (redirect-to-logged-out-page))))
+
 (defn logout [session]
-  (let [ticket (-> session :identity :ticket)]
-    (cas-store/logout ticket)))
+  (info "username" (-> session :identity :username) "logged out")
+  (cas-store/logout (-> session :identity :ticket))
+  (-> (resp/redirect (str opintopolku-logout-url ataru-login-success-url))
+      (assoc :session {:identity nil})))
 
 (defn cas-initiated-logout [logout-request]
   (info "cas-initiated logout")
@@ -23,7 +47,8 @@
     (info "logging out ticket" ticket)
     (if (.isEmpty ticket)
       (error "Could not parse ticket from CAS request" logout-request)
-      (cas-store/logout (.get ticket)))))
+      (cas-store/logout (.get ticket)))
+    (ok)))
 
 (defn logged-in? [request]
   (let [ticket (-> request :session :identity :ticket)]
--- src/clj/lomake_editori/authentication/auth_routes.clj
+++ src/clj/lomake_editori/authentication/auth_routes.clj
@@ -1,38 +1,11 @@
 (ns lomake-editori.authentication.auth-routes
   (:require [lomake-editori.authentication.auth :refer [login logout cas-initiated-logout]]
-            [compojure.core :refer [GET POST routes context]]
-            [oph.soresu.common.config :refer [config]]
-            [taoensso.timbre :refer [info error]]
-            [ring.util.http-response :refer [ok]]
-            [ring.util.response :as resp]))
-
-(def opintopolku-login-url (-> config :authentication :opintopolku-login-url))
-(def opintopolku-logout-url (-> config :authentication :opintopolku-logout-url))
-(def ataru-login-success-url (-> config :authentication :ataru-login-success-url))
-
-(defn- redirect-to-logged-out-page []
-  (resp/redirect (str opintopolku-login-url ataru-login-success-url)))
-
+            [compojure.core :refer [GET POST routes context]]))
 (defn auth-routes []
   (context "/lomake-editori/auth" []
            (GET "/cas" [ticket]
-             (try
-               (if ticket
-                 (if-let [username (login ticket ataru-login-success-url)]
-                   (do
-                        (info "username" username "logged in")
-                        (-> (resp/redirect "/lomake-editori")
-                            (assoc :session {:identity {:username username :ticket ticket}})))
-                   (redirect-to-logged-out-page))
-                 (redirect-to-logged-out-page))
-               (catch Exception e
-                 (error "Error in login ticket handling" e)
-                 (redirect-to-logged-out-page))))
+             (login ticket))
            (POST "/cas" [logoutRequest]
-                 (cas-initiated-logout logoutRequest)
-                 (ok))
+                 (cas-initiated-logout logoutRequest))
            (GET "/logout" {session :session}
-                (info "username" (-> session :identity :username) "logged out")
-                (logout session)
-                (-> (resp/redirect (str opintopolku-logout-url ataru-login-success-url))
-                    (assoc :session {:identity nil})))))
+                (logout session))))
