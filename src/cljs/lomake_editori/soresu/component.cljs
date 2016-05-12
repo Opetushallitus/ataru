bdf2ea1294656b02cc3a9c7e90854591c5683a31
Esko Lahti <esko.lahti@reaktor.com>
2016-05-12 14:42:48 +0300

    Removed unused defs

--- src/cljs/lomake_editori/soresu/component.cljs
+++ src/cljs/lomake_editori/soresu/component.cljs
@@ -2,9 +2,6 @@
   (:require [lomake-editori.soresu.components :refer [component]]
             [taoensso.timbre :refer-macros [spy]]))
 
-(defonce form-component (:form-component component))
-(defonce info-element (:info-element component))
-
 (defn text-field
   []
   {:fieldClass "formField"
