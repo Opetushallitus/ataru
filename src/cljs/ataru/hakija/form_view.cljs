bef324d04e7540d46c0e20822f6b820fcbf1dd59
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-05-24 10:57:55 +0300

    Skeleton for rendering

--- src/cljs/ataru/hakija/form_view.cljs
+++ src/cljs/ataru/hakija/form_view.cljs
@@ -1,10 +1,25 @@
 (ns ataru.hakija.form-view
   (:require [ataru.hakija.banner :refer [banner]]
-            [re-frame.core :refer [subscribe]]))
+            [re-frame.core :refer [subscribe]]
+            [cljs.core.match :refer-macros [match]]))
+
+(defn render-field
+  ([content] (render-field content [:div]))
+  ([content result]
+   (match [content]
+          [{:fieldClass "wrapperElement"
+            :children   children}] (into result (mapv #(render-field % [:div]) children))
+          [{:fieldClass "formField" :fieldType "textField"}] [:label (-> content :label :fi)])))
+
+(defn render-fields [form-data]
+  (if form-data
+    (let [rendered (mapv render-field (:content form-data))]
+      (into [:div] rendered))
+    nil))
 
 (defn application-contents []
   (let [form (subscribe [:state-query [:form]])]
-    [:div "form contents" (str form)]))
+    [:div "form contents" (render-fields @form)]))
 
 (defn form-view []
   [:div
