c2f9ebbc25ba0a76e492ec329afee8321c7259df
Aki Kaivola <aki.kaivola@gmail.com>
2016-05-30 12:01:56 +0300

    Move hacky stuff to soresu

--- src/clj/ataru/schema/clj_schema.clj
+++ src/clj/ataru/schema/clj_schema.clj
@@ -4,12 +4,6 @@
             [schema-tools.core :as st]
             [oph.soresu.form.schema :as soresu]))
 
-(soresu/create-form-schema [] [] [])
-
-(s/defschema OptionalHelpText
-  {(s/optional-key :helpText) soresu/LocalizedString})
-
-
 ;        __.,,------.._
 ;     ,'"   _      _   "`.
 ;    /.__, ._  -=- _ "`    Y
@@ -34,31 +28,16 @@
 ; it overwrites some of soresus schemas with little changes
 ((memoize
    (fn []
-
      (intern 'oph.soresu.form.schema
              'LocalizedString
              {:fi           s/Str
               (s/optional-key :sv) s/Str
               (s/optional-key :en) s/Str})
-
-     (intern 'oph.soresu.form.schema
-             'FormField
-             (-> soresu/FormField
-                 (st/dissoc :helpText)
-                 (st/merge OptionalHelpText)))
-
      nil)))
 
-(s/defschema Form schema/Form)
+(soresu/create-form-schema [] [] [])
 
-(intern 'oph.soresu.form.schema
-        'FormField
-        (st/assoc soresu/WrapperElement
-                  :children
-                  [(s/conditional #(= "wrapperElement" (:fieldClass %))
-                                  (s/recursive #'soresu/WrapperElement)
-                                  :else
-                                  soresu/BasicElement)]))
+(s/defschema Form schema/Form)
 
 (s/defschema FormWithContent
   (merge Form
