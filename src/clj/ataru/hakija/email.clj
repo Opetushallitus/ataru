3aaf831121cc492585108175ad02818bceeae832
Esko Lahti <esko.lahti@reaktor.com>
2016-06-22 09:42:16 +0300

    Display questions & answers on the confirmation email

--- src/clj/ataru/hakija/email.clj
+++ src/clj/ataru/hakija/email.clj
@@ -5,12 +5,19 @@
 
 (defn send-email-verification
   [application]
-  (let [url (str
-              (get-in config [:email :email_service_url])
-              "/ryhmasahkoposti-service/email/firewall")]
+  (let [url  (str
+               (get-in config [:email :email_service_url])
+               "/ryhmasahkoposti-service/email/firewall")
+        body (clojure.string/join "<br/>" (map
+                                            (fn [element]
+                                              (str
+                                                (get-in element [:label :fi])
+                                                ": "
+                                                (:value element)))
+                                            (:answers application)))]
     (http/post url {:headers {"content-type" "application/json"}
                     :body (json/generate-string {:email {:from "no-reply@opintopolku.fi"
                                                          :subject "Hakemus vastaanotettu"
                                                          :isHtml false
-                                                         :body "Hakemuksesi on vastaanotettu!"}
+                                                         :body body}
                                                  :recipient [{:email "hakija@opintopolku.fi"}]})})))
