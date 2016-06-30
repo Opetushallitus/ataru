35ea6736c8336d57998312a599c49b7a72a50796
Esko Lahti <esko.lahti@reaktor.com>
2016-06-30 16:13:59 +0300

    Initial person service namespace and component

--- src/clj/ataru/virkailija/virkailija_system.clj
+++ src/clj/ataru/virkailija/virkailija_system.clj
@@ -2,6 +2,7 @@
   (:require [com.stuartsierra.component :as component]
             [ataru.db.migrations :as migrations]
             [ataru.http.server :as server]
+            [ataru.person-service.client :as person-service]
             [ataru.virkailija.virkailija-routes :refer [clerk-routes]]))
 
 (defn new-system
@@ -9,6 +10,7 @@
    (component/system-map
      :server-setup {:routes clerk-routes :port http-port :repl-port 3333}
      :migration (migrations/new-migration)
+     :person-service (person-service/new-client)
      :server (component/using (server/new-server) [:server-setup])))
   ([]
    (new-system 8350)))
