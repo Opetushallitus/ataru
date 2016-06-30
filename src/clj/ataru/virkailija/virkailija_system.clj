3986c8c3beaa28faeb249711ff63cd4ce6f0b0ea
Esko Lahti <esko.lahti@reaktor.com>
2016-06-30 17:32:13 +0300

    Handler as component
    That way future dependencies are easily available.

--- src/clj/ataru/virkailija/virkailija_system.clj
+++ src/clj/ataru/virkailija/virkailija_system.clj
@@ -3,12 +3,12 @@
             [ataru.db.migrations :as migrations]
             [ataru.http.server :as server]
             [ataru.person-service.client :as person-service]
-            [ataru.virkailija.virkailija-routes :refer [clerk-routes]]))
+            [ataru.virkailija.virkailija-routes :as handler]))
 
 (defn new-system
   ([http-port]
    (component/system-map
-     :handler {:routes clerk-routes}
+     :handler (handler/new-handler)
      :server-setup {:port http-port :repl-port 3333}
      :migration (migrations/new-migration)
      :person-service (person-service/new-client)
