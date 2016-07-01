07f2a55d87c5120ce27c970c5d9dd405f143b96a
Esko Lahti <esko.lahti@reaktor.com>
2016-07-01 12:40:39 +0300

    Initialize CAS client inside the PersonServiceClient component

--- src/clj/ataru/person_service/client.clj
+++ src/clj/ataru/person_service/client.clj
@@ -1,14 +1,26 @@
 (ns ataru.person-service.client
-  (:require [com.stuartsierra.component :as component]))
+  (:require [clj-util.cas :as cas]
+            [com.stuartsierra.component :as component]
+            [oph.soresu.common.config :refer [config]]))
 
 (defrecord PersonServiceClient []
   component/Lifecycle
 
   (start [this]
-    this)
+    (let [person-service-url (str (get-in config [:person-service :person-service-url]) "/authentication-service")
+          username           (get-in config [:cas :username])
+          password           (get-in config [:cas :password])
+          cas-url            (get-in config [:authentication :cas-client-url])
+          cas-params         (cas/cas-params person-service-url username password)
+          cas-client         (cas/cas-client cas-url)]
+      (-> this
+          (assoc :cas-client cas-client)
+          (assoc :cas-params cas-params))))
 
   (stop [this]
-    this))
+    (-> this
+        (assoc :cas-client nil)
+        (assoc :cas-params nil))))
 
 (defn new-client []
   (->PersonServiceClient))
