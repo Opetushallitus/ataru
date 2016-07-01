456e2222038692a9ae5d568d93cfd46b30c5f723
Esko Lahti <esko.lahti@reaktor.com>
2016-07-01 14:01:11 +0300

    Use correct key when getting person service URL from conf

--- src/clj/ataru/cas/client.clj
+++ src/clj/ataru/cas/client.clj
@@ -3,7 +3,7 @@
             [oph.soresu.common.config :refer [config]]))
 
 (defn new-client []
-  (let [person-service-url (str (get-in config [:person-service :person-service-url]) "/authentication-service")
+  (let [person-service-url (str (get-in config [:person-service :url]) "/authentication-service")
         username           (get-in config [:cas :username])
         password           (get-in config [:cas :password])
         cas-url            (get-in config [:authentication :cas-client-url])
