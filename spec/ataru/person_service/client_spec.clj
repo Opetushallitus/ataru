5f151552f16f6248764c6f81702666ee5aacaf39
Esko Lahti <esko.lahti@reaktor.com>
2016-07-04 14:47:34 +0300

    Change CAS session ID after first request

--- spec/ataru/person_service/client_spec.clj
+++ spec/ataru/person_service/client_spec.clj
@@ -25,11 +25,17 @@
 
 (def cas-url (get-in config [:authentication :cas-client-url]))
 
-(def cas-session-id "173219B0D0FDB30124AD4D4696BC1336")
+(def cas-session-id-1 "173219B0D0FDB30124AD4D4696BC1336")
+(def cas-session-id-2 "173219B0D0FDB30124AD4D4696BC1337")
+(def current-session-id (atom nil))
 
 (def mock-client (proxy [CasClient] [cas-url client-util/client]
                    (fetchCasSession [_]
-                     (Task/now cas-session-id))))
+                     (let [session-id (if
+                                        (nil? @current-session-id)
+                                        (reset! current-session-id cas-session-id-1)
+                                        (reset! current-session-id cas-session-id-2))]
+                       (Task/now session-id)))))
 
 (def person-service-url (get-in config [:person-service :url]))
 
@@ -63,7 +69,7 @@
     (with-mock-api [client (fn [url {:keys [query-params headers]}]
                              (should= (str person-service-url "/authentication-service/resources/henkilo") url)
                              (should= {"q" username} query-params)
-                             (should= {"Cookie" (str "JSESSIONID=" cas-session-id)} headers)
+                             (should= {"Cookie" (str "JSESSIONID=" cas-session-id-1)} headers)
                              (response/ok person-search-response))]
       (let [oid-resp (.resolve-person-oids client "test-user")]
         (should= person-search-response oid-resp)))))
