0b286f6b85b306fd55a30cf653ef908c4a00851f
Esko Lahti <esko.lahti@reaktor.com>
2016-07-05 13:57:15 +0300

    :unit tags for excel spec tests

--- spec/ataru/applications/excel_export_spec.clj
+++ spec/ataru/applications/excel_export_spec.clj
@@ -6,16 +6,19 @@
             [speclj.core :refer :all]))
 
 (describe "writing form"
-    (it "writes the form"
-        (let [book (#'j2ee/application-workbook)]
-          (should= 3 (#'j2ee/write-form! (#'j2ee/make-writer (.getSheetAt book 0) 0) fixtures/form)))))
+  (tags :unit)
+
+  (it "writes the form"
+    (let [book (#'j2ee/application-workbook)]
+      (should= 3 (#'j2ee/write-form! (#'j2ee/make-writer (.getSheetAt book 0) 0) fixtures/form)))))
 
 (describe "writing excel"
-    (it "writes excel"
-        (tags :enterprise :architecture :paradigm :shift :dispruption :big :data :deep :learning :yolo)
+  (tags :unit)
 
-      (with-redefs [application-store/fetch-applications (fn [& _] fixtures/applications)
-                    form-store/fetch-form                (fn [& _] fixtures/form)]
+  (it "writes excel"
 
-        (->> (j2ee/export-all-applications 99999999)
-             (.write (java.io.FileOutputStream. "/tmp/foo.xlsx"))))))
+    (with-redefs [application-store/fetch-applications (fn [& _] fixtures/applications)
+                  form-store/fetch-form                (fn [& _] fixtures/form)]
+
+      (->> (j2ee/export-all-applications 99999999)
+        (.write (java.io.FileOutputStream. "/tmp/foo.xlsx"))))))
