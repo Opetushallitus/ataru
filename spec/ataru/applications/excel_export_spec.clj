f6fd1505a24cca31367dbe45b3dfd0ddb8cf1f22
Esko Lahti <esko.lahti@reaktor.com>
2016-07-07 13:53:44 +0300

    Verify headers of the Excel

--- spec/ataru/applications/excel_export_spec.clj
+++ spec/ataru/applications/excel_export_spec.clj
@@ -5,7 +5,8 @@
             [ataru.fixtures.application :as fixtures]
             [speclj.core :refer :all])
   (:import (java.io FileOutputStream File)
-           (java.util UUID)))
+           (java.util UUID)
+           (org.apache.poi.ss.usermodel WorkbookFactory)))
 
 (describe "writing form"
   (tags :unit)
@@ -15,11 +16,11 @@
       (should= 3 (#'j2ee/write-form! (#'j2ee/make-writer (.getSheetAt book 0) 0) fixtures/form)))))
 
 (describe "writing excel"
-  (tags :unit)
+  (tags :excel)
 
   (around [spec]
-    (with-redefs [application-store/fetch-applications (fn [& _] fixtures/applications)
-                  form-store/fetch-form                (fn [& _] fixtures/form)]
+    (with-redefs [application-store/exec-db (fn [& _] fixtures/applications)
+                  form-store/fetch-form (fn [& _] fixtures/form)]
       (spec)))
 
   (it "writes excel"
@@ -28,5 +29,15 @@
           (with-open [output (FileOutputStream. (.getPath file))]
             (->> (j2ee/export-all-applications 99999999)
                  (.write output)))
+          (let [sheet      (-> file WorkbookFactory/create (.getSheetAt 0))
+                header-row (iterator-seq (.cellIterator (.getRow sheet 8)))
+                headers    (map #(.getStringCellValue %) (take 6 header-row))]
+            (should= (nth headers 0) "Eka kysymys")
+            (should= (nth headers 1) "Toka kysymys")
+            (should= (nth headers 2) "Kolmas kysymys")
+            (should= (nth headers 3) "Neljas kysymys")
+            (should= (nth headers 4) "Viides kysymys")
+            (should= (nth headers 5) "Kuudes kysymys")
+            (should (every? nil? (nthrest headers 6))))
           (finally
             (.delete file))))))
