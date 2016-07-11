b708eb97807dc22e2470b4c06765206078446c03
Esko Lahti <esko.lahti@reaktor.com>
2016-07-11 10:02:02 +0300

    Draggable handle as a background image

--- src/cljs/ataru/virkailija/editor/component.cljs
+++ src/cljs/ataru/virkailija/editor/component.cljs
@@ -55,7 +55,6 @@
     :on-drag-start (on-drag-start path)
     :on-drag-over prevent-default}
    [:header.editor-form__component-header label]
-   [:img.editor-form__draggable-indicator {:src "images/drag_handle.png"}]
    [:a.editor-form__component-header-link
     {:on-click (fn [event]
                  (dispatch [:remove-component path
