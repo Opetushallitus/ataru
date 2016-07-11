755613cc37e46bb1e74e2cea2fd160090d41c1d6
Esko Lahti <esko.lahti@reaktor.com>
2016-07-11 09:53:00 +0300

    Use custom draggable handle img

--- src/cljs/ataru/virkailija/editor/component.cljs
+++ src/cljs/ataru/virkailija/editor/component.cljs
@@ -55,7 +55,7 @@
     :on-drag-start (on-drag-start path)
     :on-drag-over prevent-default}
    [:header.editor-form__component-header label]
-   [:i.zmdi.zmdi-menu.editor-form__draggable-indicator]
+   [:img.editor-form__draggable-indicator {:src "images/drag_handle.png"}]
    [:a.editor-form__component-header-link
     {:on-click (fn [event]
                  (dispatch [:remove-component path
