bd0099cbf2dde1f75cfcb5e6f1e41ccdd1921a4d
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-07-14 15:59:44 +0300

    Remove currently unused, dead code for application handling in virkailija UI
    This can be reverted later when we work on it

--- src/cljs/ataru/virkailija/application/view.cljs
+++ src/cljs/ataru/virkailija/application/view.cljs
@@ -7,40 +7,6 @@
             [ataru.cljs-util :refer [wrap-scroll-to]]
             [taoensso.timbre :refer-macros [spy debug]]))
 
-(defn applications []
-  (let [applications (subscribe [:state-query [:application :applications]])
-        selected-key (subscribe [:state-query [:application :selected]])]
-    (into [:div.application-list
-           [:div.application-list__header.application-list__row
-            [:span.application-list__row--applicant "Hakija"]
-            [:span.application-list__row--time "Saapunut"]
-            [:span.application-list__row--state "Tila"]]]
-          (for [application @applications
-                :let        [key       (:key application)
-                             time      (t/time->str (:modified-time application))
-                             applicant (:applicant application)
-                             state     (:state application)]]
-            [:div.application-list__row
-             {:on-click #(dispatch [:application/select-application (:key application)])
-              :class    (when (= @selected-key key)
-                          "application-list__row--selected")}
-             [:span.application-list__row--applicant
-              (or applicant [:span.application-list__row--applicant-unknown "Tuntematon"])]
-             [:span.application-list__row--time time]
-             [:span.application-list__row--state
-              (case (:state application)
-                "received" "Saapunut"
-                "Tuntematon")]]))))
-
-(defn application-list []
-  (let [form (subscribe [:state-query [:application :form]])]
-    [:div
-     [:h1.application__name
-      (:name @form)]
-     ; Show when we have "henkilötiedot-moduuli", when we can rely on the applicant's name to be there
-     ;[applications]
-     ]))
-
 (defn form-list-arrow-up [open?-atom]
   [:i.zmdi.zmdi-chevron-up.application-handling__form-list-arrow
    {:on-click #(reset! open?-atom false)}])
