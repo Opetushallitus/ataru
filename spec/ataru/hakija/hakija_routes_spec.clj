470593964695eecff19ab5fcfafa9c2451505d3c
Esko Lahti <esko.lahti@reaktor.com>
2016-07-27 13:57:16 +0300

    Macro to share the glue code involved in invoking reqs

--- spec/ataru/hakija/hakija_routes_spec.clj
+++ spec/ataru/hakija/hakija_routes_spec.clj
@@ -14,6 +14,14 @@
 (def person-info-form-application {:form 15, :lang "fi", :answers [{:key "address", :value "Paratiisitie 13", :fieldType "textField", :label {:fi "Katuosoite", :sv "Adress"}} {:key "email", :value "aku@ankkalinna.com", :fieldType "textField", :label {:fi "Sähköpostiosoite", :sv "E-postadress"}} {:key "preferred-name", :value "Aku", :fieldType "textField", :label {:fi "Kutsumanimi", :sv "Smeknamn"}} {:key "last-name", :value "Ankka", :fieldType "textField", :label {:fi "Sukunimi", :sv "Efternamn"}} {:key "phone", :value "050123", :fieldType "textField", :label {:fi "Matkapuhelin", :sv "Mobiltelefonnummer"}} {:key "nationality", :value "Suomi", :fieldType "dropdown", :label {:fi "Kansalaisuus", :sv "Nationalitet"}} {:key "ssn", :value "010101-123N", :fieldType "textField", :label {:fi "Henkilötunnus", :sv "Personnummer"}} {:key "municipality", :value "Ankkalinna", :fieldType "textField", :label {:fi "Kotikunta", :sv "Bostadsort"}} {:key "first-name", :value "Aku Petteri", :fieldType "textField", :label {:fi "Etunimet", :sv "Förnamn"}} {:key "postal-code", :value "00013", :fieldType "textField", :label {:fi "Postinumero", :sv "Postnummer"}} {:key "language", :value "suomi", :fieldType "dropdown", :label {:fi "Äidinkieli", :sv "Modersmål"}} {:key "gender", :value "Mies", :fieldType "dropdown", :label {:fi "Sukupuoli", :sv "Kön"}}]})
 (def handler (-> (routes/new-handler) .start :routes))
 
+(defmacro with-response
+  [resp application & body]
+  `(let [~resp (-> (mock/request :post "/hakemus/api/application" (json/generate-string ~application))
+                   (mock/content-type "application/json")
+                   handler
+                   (update :body (comp #(json/parse-string % true) slurp)))]
+     ~@body))
+
 (defn- have-application-in-db
   [application-id]
   (when-let [actual (first (soresu-db/exec :db yesql-get-application-by-id {:application_id application-id}))]
@@ -21,9 +29,6 @@
 
 (describe "POST /application"
   (tags :hakija)
-
-  (with req (-> (mock/request :post "/hakemus/api/application" (json/generate-string person-info-form-application))
-                (mock/content-type "application/json")))
 
   (around [spec]
     (with-redefs [;store/add-new-application (fn [_] application-id)
@@ -35,7 +40,6 @@
   (after-all (db/clear-database))
 
   (it "should validate application"
-    (let [resp (-> (handler @req)
-                   (update :body (comp #(json/parse-string % true) slurp)))]
+    (with-response resp person-info-form-application
       (should= 200 (:status resp))
       (should (have-application-in-db (get-in resp [:body :id]))))))
