b90fe8665f1760ada778b0980a6a6ad9d90e31ff
Aki Kaivola <aki.kaivola@gmail.com>
2016-08-03 13:09:15 +0300

    Allow overriding http/repl port from an env variable

--- src/clj/ataru/hakija/hakija_system.clj
+++ src/clj/ataru/hakija/hakija_system.clj
@@ -2,16 +2,22 @@
   (:require [com.stuartsierra.component :as component]
             [ataru.db.migrations :as migrations]
             [ataru.hakija.hakija-routes :as handler]
-            [ataru.http.server :as server]))
+            [ataru.http.server :as server]
+            [environ.core :refer [env]]))
 
 (defn new-system
-  []
-  (component/system-map
-    :handler      (handler/new-handler)
+  ([]
+   (new-system
+     (Integer/parseInt (get env :ataru-http-port "8351"))
+     (Integer/parseInt (get env :ataru-repl-port "3335"))))
+  ([http-port repl-port]
+   (component/system-map
+     :handler      (handler/new-handler)
 
-    :server-setup {:port 8351 :repl-port 3335}
+     :server-setup {:port http-port
+                    :repl-port repl-port}
 
-    :migration    (migrations/new-migration)
-    :server       (component/using
-                    (server/new-server)
-                    [:server-setup :handler])))
+     :migration    (migrations/new-migration)
+     :server       (component/using
+                     (server/new-server)
+                     [:server-setup :handler]))))
