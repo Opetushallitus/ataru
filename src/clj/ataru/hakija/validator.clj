82063dda0583c61a488c8f4719ba158ebe7da652
Aki Kaivola <aki.kaivola@gmail.com>
2016-08-05 11:38:27 +0300

    Validate application on server side
    - Checks for extra keys not in original form
    - Looks up answers based on original form, not the other way around
    - Validates answers using validators defined in original form

--- src/clj/ataru/hakija/validator.clj
+++ src/clj/ataru/hakija/validator.clj
@@ -2,33 +2,56 @@
   (:require [ataru.forms.form-store :as form-store]
             [ataru.hakija.application-validators :as validator]
             [ataru.util :as util]
-            [yesql.core :as sql]))
+            [clojure.set :refer [difference]]
+            [yesql.core :as sql]
+            [taoensso.timbre :refer [spy debug]]))
 
-(defn valid-application
+(defn allowed-values [options]
+  (set
+    (reduce
+      (fn [values option]
+        (when-not (clojure.string/blank? (:value option))
+          (concat values (vals (:label option)))))
+      []
+      options)))
+
+(defn form-validators [form-content]
+  (reduce (fn [m field]
+            (assoc m
+                   (keyword (:id field))
+                   (cond-> (select-keys field [:validators])
+                     (= (:fieldType field) "dropdown")
+                     (assoc :allowed-values (allowed-values (:options field))))))
+          {}
+          (util/flatten-form-fields form-content)))
+
+(defn extra-answers-not-in-original-form [form-keys answer-keys]
+  (apply disj (set answer-keys) form-keys))
+
+(defn validation-failures [form-validators answers-by-key]
+  (into {}
+    (for [[form-key {:keys [validators allowed-values]}] form-validators
+          :let  [answer           (first (get answers-by-key form-key))
+                 valid-answer?    (fn [validator]
+                                    (validator/validate validator (:value answer)))]
+          :when (not
+                  (and
+                    (every? valid-answer? validators)
+                    (or
+                      (nil? (:allowed-values validators))
+                      (some? ((:allowed-values validators) (:value answer))))))]
+      ; this form-key has failed validation against the given answer
+      [form-key answer])))
+
+(defn valid-application?
   "Verifies that given application is valid by validating each answer
    against their associated validators."
-  [application]
-  (when-let [form (form-store/fetch-form (:form application))]
-    (let [allowed-values (partial reduce
-                                  (fn [values option]
-                                    (when-not (clojure.string/blank? (:value option))
-                                      (concat values (vals (:label option)))))
-                                  [])
-          validators     (reduce (fn [m field]
-                                   (assoc m (:id field) (cond-> (select-keys field [:validators])
-                                                          (= (:fieldType field) "dropdown") (assoc :allowed-values (allowed-values (:options field))))))
-                                 {}
-                                 (util/flatten-form-fields (:content form)))
-          valid-field?   (fn [answer]
-                           (let [validators    (or
-                                                 (get validators (:key answer))
-                                                 [])
-                                 valid-answer? (fn [validator]
-                                                 (validator/validate validator (:value answer)))]
-                             (and
-                               (every? true? (map valid-answer? (:validators validators)))
-                               (or
-                                 (nil? (:allowed-values validators))
-                                 (some #(= (:value answer) %) (:allowed-values validators))))))
-          answers        (:answers application)]
-      (every? valid-field? answers))))
+  ([application]
+   (valid-application? application (form-store/fetch-form (:form application))))
+  ([application form]
+   {:pre [(not-empty form)]}
+   (let [form-validators (form-validators (:content form))
+         answers-by-key  (group-by (comp keyword :key) (:answers application))]
+     (and
+       (empty? (extra-answers-not-in-original-form (keys form-validators) (keys answers-by-key)))
+       (empty? (validation-failures form-validators answers-by-key))))))
