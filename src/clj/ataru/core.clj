9047f28293e2cf3a7deaf25b98660103db47b60d
Aki Kaivola <aki.kaivola@gmail.com>
2016-08-24 13:21:27 +0300

    Fix stop and start to work in repl and reload routes correctly

--- src/clj/ataru/core.clj
+++ src/clj/ataru/core.clj
@@ -14,13 +14,25 @@
 (def ^:private app-systems {:virkailija virkailija-system/new-system
                             :hakija hakija-system/new-system})
 
-(def system nil)
+(def system (atom {:system nil
+                   :system-fn nil}))
 
 (defn start []
-  (alter-var-root #'system component/start))
+  (swap! system (fn [{:keys [app-id system system-fn] :as old-system}]
+                  (if system
+                    (do
+                      (info "System already started")
+                      old-system)
+                    (do
+                      (info "Starting system" app-id)
+                      (assoc old-system :system (component/start (system-fn))))))))
 
 (defn stop []
-  (alter-var-root #'system (fn [s] (when s (component/stop s)))))
+  (swap! system (fn [{:keys [system system-fn] :as old-system}]
+                   (do
+                     (when system
+                       (component/stop system))
+                     (dissoc old-system :system)))))
 
 (defn- get-app-id [[app-id & _]]
   (keyword
@@ -32,15 +44,15 @@
   (start))
 
 (defn -main [& args]
-  (let [app-id (get-app-id args)
-        system ((get app-systems app-id (constantly nil)))]
+  (let [app-id         (get-app-id args)
+        system-fn      (get app-systems app-id)]
     (info "Starting application" app-id (if (:dev? env) "dev" ""))
-    (when-not system
+    (when-not system-fn
       (println "ERROR: No system map found for application" app-id "exiting. Valid keys: "
                (apply str (interpose ", " (map name (keys app-systems)))))
       (System/exit 1))
     (do
-      (alter-var-root #'system
-                      (constantly system))
+      (reset! system {:app-id app-id
+                      :system-fn system-fn})
       (start)
       (wait-forever))))
