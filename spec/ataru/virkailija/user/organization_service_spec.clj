09574d35f3298b35e70187e9f10139911cd3ddfa
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-09-05 12:06:44 +0300

    Add organization service spec

---
+++ spec/ataru/virkailija/user/organization_service_spec.clj
@@ -0,0 +1,19 @@
+(ns ataru.virkailija.user.organization-service-spec
+  (:require [ataru.virkailija.user.organization-service :as org-service]
+            [ataru.virkailija.user.ldap-client-spec :refer [test-user1 test-user1-organization-oid]]
+            [speclj.core :refer [describe it should= tags]]
+            [clj-ldap.client :as ldap]
+            [ataru.virkailija.user.ldap-client :as ataru-ldap]))
+
+(defn fake-ldap-search [connection path props] [test-user1])
+(defn fake-create-connection [] :fake-conn)
+
+(defn create-org-service-instance [] (.start (org-service/->IntegratedOrganizationService)))
+
+(describe "OrganizationService"
+  (tags :unit)
+  (it "should use ldap module to fetch organization oids"
+      (with-redefs [ldap/search fake-ldap-search
+                    ataru-ldap/create-ldap-connection fake-create-connection]
+        (let [org-service-instance (create-org-service-instance)]
+          (should= [test-user1-organization-oid] (.get-direct-organization-oids org-service-instance "testi2editori"))))))
