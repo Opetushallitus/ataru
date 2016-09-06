bbae0f9341c7e4065ff900e38521cc468ba5a594
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-09-06 10:21:16 +0300

    Test get-all-organizations

--- spec/ataru/virkailija/user/organization_service_spec.clj
+++ spec/ataru/virkailija/user/organization_service_spec.clj
@@ -1,12 +1,16 @@
 (ns ataru.virkailija.user.organization-service-spec
   (:require [ataru.virkailija.user.organization-service :as org-service]
             [ataru.virkailija.user.ldap-client-spec :refer [test-user1 test-user1-organization-oid]]
+            [ataru.virkailija.user.organization-client-spec :refer [expected-flat-organizations]]
             [speclj.core :refer [describe it should= tags]]
             [clj-ldap.client :as ldap]
-            [ataru.virkailija.user.ldap-client :as ataru-ldap]))
+            [ataru.virkailija.user.ldap-client :as ataru-ldap]
+            [ataru.cas.client :as cas-client]
+            [clojure.java.io :as io]))
 
 (defn fake-ldap-search [connection path props] [test-user1])
 (defn fake-create-connection [] :fake-conn)
+(defn fake-cas-authenticated-get [cas-client url] {:status 200 :body (io/resource "organisaatio_service/organization-hierarchy1.json")})
 
 (defn create-org-service-instance [] (.start (org-service/->IntegratedOrganizationService)))
 
@@ -19,6 +23,8 @@
           (should= [test-user1-organization-oid] (.get-direct-organization-oids org-service-instance "testi2editori")))))
   (it "Should get organizations from organization client and cache the result"
       (with-redefs [ldap/search fake-ldap-search
-                    ataru-ldap/create-ldap-connection fake-create-connection]
+                    ataru-ldap/create-ldap-connection fake-create-connection
+                    cas-client/cas-authenticated-get fake-cas-authenticated-get]
         (let [org-service-instance (create-org-service-instance)]
-          (println "all orgs cache " @(:all-orgs-cache org-service-instance))))))
+          (should= expected-flat-organizations (.get-all-organizations org-service-instance test-user1-organization-oid))
+          (should= {test-user1-organization-oid  expected-flat-organizations} (into {} @(:all-orgs-cache org-service-instance)))))))
