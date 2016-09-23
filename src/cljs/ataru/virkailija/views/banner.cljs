b3167a9d6fe18cdf85762485f3d6deb5b84558ac
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-09-23 15:30:32 +0300

    Change error-color to something that eye can separate from the background

--- src/cljs/ataru/virkailija/views/banner.cljs
+++ src/cljs/ataru/virkailija/views/banner.cljs
@@ -70,7 +70,7 @@
 
                 [false _ {:detail detailed-error
                         :message message}]
-                [:div.flasher {:style {"color" "crimson"}}
+                [:div.flasher {:style {"color" "#f89a9a"}}
                  [:span message]]
 
                 [_ false {:message (message :guard some?)}]
