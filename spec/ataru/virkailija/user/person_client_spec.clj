b120420398dabe1c06f56ca800c071c5b596d72a
Esko Lahti <esko.lahti@reaktor.com>
2016-09-28 15:58:04 +0300

    Return nil when no results are produced by person client

--- spec/ataru/virkailija/user/person_client_spec.clj
+++ spec/ataru/virkailija/user/person_client_spec.clj
@@ -4,25 +4,40 @@
             [cheshire.core :as json]
             [oph.soresu.common.config :refer [config]]
             [ring.util.http-response :as response]
-            [speclj.core :refer [describe it tags should=]]))
+            [speclj.core :refer [describe it tags should= should-be-nil]]))
 
 (def fake-config {:authentication-service {:base-address "dummy"} :cas {}})
 
-(def person {:etunimet   "Aku"
-             :hetu       "010101-123N"
-             :sukunimi   "Ankka"
-             :oidHenkilo "1.2.246.562.24.96282369159"})
+(def person1 {:etunimet   "Aku"
+              :hetu       "010101-123N"
+              :sukunimi   "Ankka"
+              :oidHenkilo "1.2.246.562.24.96282369159"})
 
-(defn- fake-cas-get [client url]
-  (-> {:totalCount 1 :results [person]}
-      json/generate-string
-      response/ok))
+(def person2 {:etunimet   "Iines"
+              :hetu       "010101+123N"
+              :sukunimi   "Ankka"
+              :oidHenkilo "1.2.246.562.24.96282369160"})
+
+(def person-response {:totalCount 2 :results [person1 person2]}) ; just verify that first one is returned
+
+(def empty-response {:totalCount 0 :results []})
+
+(defn- fake-cas-get [resp]
+  (fn [client url]
+    (-> resp
+        json/generate-string
+        response/ok)))
 
 (describe "person client"
   (tags :unit)
 
   (it "searches person by a search parameter"
     (with-redefs [config fake-config
-                  cas-client/cas-authenticated-get fake-cas-get]
-      (should= person
-               (person-client/list-persons nil "010101-123N")))))
+                  cas-client/cas-authenticated-get (fake-cas-get person-response)]
+      (should= person1
+               (person-client/list-persons nil "010101-123N"))))
+
+  (it "returns nil when search produces no results"
+    (with-redefs [config fake-config
+                  cas-client/cas-authenticated-get (fake-cas-get empty-response)]
+      (should-be-nil (person-client/list-persons nil "010101-123N")))))
