75388baf1a035d21290dd0fe46d5f113af2ea907
Aki Kaivola <aki.kaivola@gmail.com>
2016-09-29 13:07:28 +0300

    Add rotating logs when not in dev

---
+++ src/clj/ataru/timbre_config.clj
@@ -0,0 +1,12 @@
+(ns ataru.timbre-config
+  (:require [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.3rd-party.rotor :as rotor]
+            [environ.core :refer [env]]))
+
+(defn configure-logging! [logname]
+  (when-not (:dev? env)
+    (timbre/merge-config! {:appenders
+                           {:rotor (rotor/rotor-appender
+                                     {:max-size (* 10 1024 1024)
+                                      :backlog  10
+                                      :path     (str "./" logname ".log")})}})))
