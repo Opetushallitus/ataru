4928512a72f6b7c4f38b776bb1ec9e4498cf32d6
Esko Lahti <esko.lahti@reaktor.com>
2016-09-29 15:45:02 +0300

    Initialize person service in the hakija system

--- src/clj/ataru/hakija/hakija_system.clj
+++ src/clj/ataru/hakija/hakija_system.clj
@@ -4,6 +4,7 @@
             [ataru.hakija.hakija-routes :as handler]
             [ataru.http.server :as server]
             [ataru.hakija.email :as email]
+            [ataru.person-service.person-service :as person-service]
             [environ.core :refer [env]]))
 
 (defn new-system
@@ -13,13 +14,15 @@
      (Integer/parseInt (get env :ataru-repl-port "3335"))))
   ([http-port repl-port]
    (component/system-map
-     :handler (handler/new-handler)
+     :handler        (handler/new-handler)
 
-     :server-setup {:port http-port
-                    :repl-port repl-port}
+     :server-setup   {:port      http-port
+                      :repl-port repl-port}
 
-     :email        (email/new-emailer)
+     :email          (email/new-emailer)
 
-     :server       (component/using
-                     (server/new-server)
-                     [:server-setup :handler]))))
+     :server         (component/using
+                       (server/new-server)
+                       [:server-setup :handler])
+
+     :person-service (person-service/new-person-service))))
