1fbb0faf3f6551dd287e13a60e0d50c27526e0e0
Esa Virtanen <esa.virtanen@reaktor.fi>
2016-09-29 15:59:20 +0300

    Basic tests for application structure

--- resources/spec/hakijaSpec.js
+++ resources/spec/hakijaSpec.js
@@ -18,14 +18,43 @@
     expect(window.uiError || null).to.be.null
   })
 
+  function formHeader() {
+    return testFrame().find('.application__header')
+  }
+
+  function submitButton() {
+    return testFrame().find('.application__send-application-button')
+  }
+
+  function formSections() {
+    return testFrame().find('.application__form-content-area .application__wrapper-element')
+  }
+
+  function formFields() {
+    return testFrame().find('.application__form-content-area .application__form-field')
+  }
+
+  function invalidFieldsStatus() {
+    return testFrame().find('.application__invalid-field-status-title')
+  }
+
+  function invalidSections() {
+    return testFrame().find('.application__banner-wrapper-sections-content')
+  }
+
   describe('hakemus', function() {
 
-    describe('page loads', function () {
+    describe('form loads', function () {
       before(
-        wait.until(function() { return true; })
+        wait.until(function() { return formSections().length == 2 })
       )
-      it('with complete form', function () {
-        expect(function() { return true; })
+      it('with complete form', function() {
+        expect(formFields().length).to.equal(20)
+        expect(submitButton().prop('disabled')).to.equal(true)
+        expect(formHeader().text()).to.equal('Testilomake')
+        expect(invalidFieldsStatus().text()).to.equal('12 pakollista tietoa puuttuu')
+        expect(invalidSections().find('a').length).to.equal(2)
+        expect(invalidSections().find('a.application__banner-wrapper-section-link-not-valid').length).to.equal(2)
       })
     })
   })
