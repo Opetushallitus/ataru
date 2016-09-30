a53ff62d16634990ab7e557d29d4751778e72c68
Esko Lahti <esko.lahti@reaktor.com>
2016-09-30 09:04:23 +0300

    Just print the lein version when running script

--- bin/cibuild.sh
+++ bin/cibuild.sh
@@ -8,13 +8,10 @@ echo $OLD_CWD
 export JAVA_HOME=/data00/oph/java/jdk1.8.0_60
 export PATH=/data00/oph/java/jdk1.8.0_60/bin:$PATH
 
-exec-lein-cmd() {
-    ./bin/lein version
-    $1
-}
+./bin/lein version
 
 compile-less() {
-    exec-lein-cmd "./bin/lein less once"
+    ./bin/lein less once
 }
 
 npm-dependencies() {
@@ -22,35 +19,35 @@ npm-dependencies() {
 }
 
 process-resources() {
-    exec-lein-cmd "./bin/lein resource"
+    ./bin/lein resource
 }
 
 build-clojurescript-virkailija() {
-    exec-lein-cmd "./bin/lein cljsbuild once virkailija-min"
+    ./bin/lein cljsbuild once virkailija-min
 }
 
 build-clojurescript-hakija() {
-    exec-lein-cmd "./bin/lein cljsbuild once hakija-min"
+    ./bin/lein cljsbuild once hakija-min
 }
 
 create-uberjar() {
-    exec-lein-cmd "./bin/lein uberjar"
+    ./bin/lein uberjar
 }
 
 test-clojure() {
-    exec-lein-cmd "./bin/lein spec -t ~ui"
+    ./bin/lein spec -t ~ui
 }
 
 test-clojurescript() {
-    exec-lein-cmd "./bin/lein doo phantom test once"
+    ./bin/lein doo phantom test once
 }
 
 test-integration() {
-    exec-lein-cmd "./bin/lein spec -t ui"
+    ./bin/lein spec -t ui
 }
 
 run-migrations() {
-    exec-lein-cmd "./bin/lein with-profile dev run -m manual-migrations/migrate"
+    ./bin/lein with-profile dev run -m manual-migrations/migrate
 }
 
 clean() {
