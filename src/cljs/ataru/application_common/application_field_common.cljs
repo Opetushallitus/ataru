8b34e21b69d9ee792b11d7bbfe5ed65b72c2bd49
Esko Lahti <esko.lahti@reaktor.com>
2016-10-11 16:34:05 +0300

    Render koodisto label instead of URIs in applicant side

--- src/cljs/ataru/application_common/application_field_common.cljs
+++ src/cljs/ataru/application_common/application_field_common.cljs
@@ -5,8 +5,24 @@
 
 (defn required-hint [field-descriptor] (if (some #(= % "required") (:validators field-descriptor)) " *" ""))
 
+(defn- extract-value-from-koodisto [{:keys [options]} koodi-uri]
+  (if-some [koodi-value (->> options
+                             (filter (comp (partial = koodi-uri)
+                                           :value))
+                             first)]
+    (get-in koodi-value [:label :fi])
+    ; If there isn't a suitable value in field-descriptor
+    ; we might be in the officer side - there the backend
+    ; already has replaced the koodi URI with a human-readable
+    ; label -> just return the koodi-uri if koodi-value is nil
+    koodi-uri))
+
 (defn textual-field-value [field-descriptor application]
-  (:value ((answer-key field-descriptor) (:answers application))))
+  (let [key                (answer-key field-descriptor)
+        value-or-koodi-uri (:value (get (:answers application) key))]
+    (cond->> value-or-koodi-uri
+      (contains? field-descriptor :koodisto-source)
+      (extract-value-from-koodisto field-descriptor))))
 
 (defn scroll-to-anchor
   [field-descriptor]
