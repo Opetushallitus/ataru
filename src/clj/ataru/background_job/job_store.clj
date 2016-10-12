e7bb99b5416cee58aa0a5ea79d307990cadd49dd
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-10-12 08:30:33 +0300

    Remove obsolete crap

--- src/clj/ataru/background_job/job_store.clj
+++ src/clj/ataru/background_job/job_store.clj
@@ -8,20 +8,6 @@
    [clojure.java.jdbc :as jdbc]
    [oph.soresu.common.db :as db]))
 
-(def in-memory-store (atom {}))
-(def id-seq (atom 0))
-
-(defn x_store-new [job-type state]
-  (let [new-job-id (swap! id-seq inc)]
-    (swap! in-memory-store assoc new-job-id {:id new-job-id :job-type job-type :state state :next-step :initial :status :running})))
-
-(defn x_store [job]
-  (swap! in-memory-store assoc (:id job) job))
-
-(defn get-due-jobs []
-  (filter #(= :running (:status %)) (vals @in-memory-store)))
-
-
 (defqueries "sql/background-job-queries.sql")
 
 (defn store-new [job-type state]
