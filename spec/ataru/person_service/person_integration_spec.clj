c9c94b7c2f59647e28875f33a9cd2c97fa948ae0
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-10-18 15:06:16 +0300

    Make more specific tests for person extraction

--- spec/ataru/person_service/person_integration_spec.clj
+++ spec/ataru/person_service/person_integration_spec.clj
@@ -14,37 +14,68 @@
 
 (def application application-fixtures/application-with-person-info-module)
 (def fake-config {:authentication-service {:base-address "dummy"}})
-(def person {:email          "aku@ankkalinna.com",
-             :personId       "130580-327L",
-             :nativeLanguage nil,
-             :nationality    "fi",
-             :birthDate      nil,
-             :firstName      "Roope",
-             :lastName       "Ankka",
-             :gender         nil,
-             :idpEntitys     [],
-             :personOid      "1.2.246.562.24.56818753409"})
+(def finnish-person {:email          "aku@ankkalinna.com",
+                     :personId       "120496-924J",
+                     :nativeLanguage "FI",
+                     :nationality    "246",
+                     :firstName      "Aku",
+                     :lastName       "Ankka",
+                     :gender         "2",
+                     :idpEntitys     []})
 
-(describe "person-integration/upsert-person"
-  (tags :unit)
+(def finnish-person-with-oid (assoc finnish-person :personOid "1.2.246.562.24.56818753409"))
 
-  (it "updates person oid to application in local database"
-    (let [oid-in-db? (atom false)]
-      (with-redefs [application-store/add-person-oid (fn [application-id person-oid]
-                                                       (should= (:id application) application-id)
-                                                       (should= (:personOid person) person-oid)
-                                                       (reset! oid-in-db? true))
-                    application-store/get-application (fn [id]
-                                                        (should= (:id application) id)
-                                                        application)
-                    config fake-config
-                    cas-client/cas-authenticated-post (fn [client url body]
-                                                        {:status 200
-                                                         :body
-                                                         (-> person
-                                                             json/generate-string)})]
-        (should= {:transition {:id :final}}
-                 (person-integration/upsert-person
+(describe
+ "person-integration/upsert-person"
+ (tags :unit)
+
+ (it "updates person oid to application in local database"
+     (let [oid-in-db? (atom false)]
+       (with-redefs [application-store/add-person-oid (fn [application-id person-oid]
+                                                        (should= (:id application) application-id)
+                                                        (should= (:personOid finnish-person-with-oid) person-oid)
+                                                        (reset! oid-in-db? true))
+                     application-store/get-application (fn [id]
+                                                         (should= (:id application) id)
+                                                         application)
+                     config fake-config
+                     cas-client/cas-authenticated-post (fn [client url body]
+                                                         {:status 200
+                                                          :body
+                                                          (-> finnish-person-with-oid
+                                                              json/generate-string)})]
+         (should= {:transition {:id :final}}
+                  (person-integration/upsert-person
                    {:application-id (:id application)}
                    {:person-service person-service}))
-        (should @oid-in-db?)))))
+         (should @oid-in-db?)))))
+
+;; Only relevant fields here
+(def foreign-application {:answers [{:key "email",:value "roger.moore@ankkalinna.com"}
+                                    {:key "first-name" :value "Roger"}
+                                    {:key "last-name" :value "Moore"}
+                                    {:key "birth-date" :value "29.10.1984"}
+                                    {:key "language" :value "SV"}
+                                    {:key "nationality" :value "247"}
+                                    {:key "gender" :value "2"}]}) ;; TODO use 1 when it's possible to do so
+
+(def expected-foreign-person {:email          "roger.moore@ankkalinna.com"
+                              :nativeLanguage "SV"
+                              :nationality    "247"
+                              :firstName      "Roger"
+                              :lastName       "Moore"
+                              :birthDate      "1984-10-29"
+                              :gender         "2"
+                              :idpEntitys     []})
+
+(describe
+ "extract person"
+ (tags :unit :extract-person)
+ (it "extracts finnish person correctly"
+     (should=
+      finnish-person
+      (person-integration/extract-person  application-fixtures/application-with-person-info-module)))
+ (it "extracts foreign person correctly"
+     (should=
+      expected-foreign-person
+      (person-integration/extract-person  foreign-application))))
--- src/clj/ataru/person_service/person_integration.clj
+++ src/clj/ataru/person_service/person_integration.clj
@@ -9,13 +9,31 @@
             value))
         answers))
 
-(defn- extract-person [application]
-  {:email          (extract-field application "email")
-   :firstName      (extract-field application "first-name")
-   :personId       (extract-field application "ssn")
-   :lastName       (extract-field application "last-name")
-   :nativeLanguage (extract-field application "language")
-   :idpEntitys     []})
+(def finnish-date-regex #"(\d{2})\.(\d{2})\.(\d{4})")
+
+(defn- convert-birth-date [finnish-format-date]
+  {:post [(not= % "--")]} ;; When no match for finnish date, this would result in "--"
+  (let [[_ day month year] (re-find finnish-date-regex finnish-format-date)]
+    (str year "-" month "-" day)))
+
+(defn- extract-birth-date [application]
+  (let [finnish-format-date (extract-field application "birth-date")]
+    (if-not finnish-format-date (throw (Exception. "Expected a birth-date in application")))
+    (convert-birth-date finnish-format-date)))
+
+(defn extract-person [application]
+  (let [basic-fields
+        {:email          (extract-field application "email")
+         :firstName      (extract-field application "first-name")
+         :lastName       (extract-field application "last-name")
+         :gender         "2" ;; TODO FIX
+         :nativeLanguage (extract-field application "language")
+         :nationality    (extract-field application "nationality")
+         :idpEntitys     []}
+        person-id (extract-field application "ssn")]
+    (if person-id
+      (assoc basic-fields :personId person-id)
+      (assoc basic-fields :birthDate (extract-birth-date application)))))
 
 (defn upsert-and-log-person [person-service application-id]
   (let [person-to-send (extract-person (application-store/get-application application-id))]
