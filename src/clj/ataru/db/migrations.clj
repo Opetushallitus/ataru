1677e663e3e52b7505ab5d0bc2a993ff3a1c5415
Aki Kaivola <aki.kaivola@gmail.com>
2016-10-20 12:43:01 +0300

    Migrate person-info modules to all existing forms

--- src/clj/ataru/db/migrations.clj
+++ src/clj/ataru/db/migrations.clj
@@ -2,28 +2,40 @@
   (:require
     [ataru.forms.form-store :as store]
     [ataru.virkailija.component-data.person-info-module :as person-info-module]
-    [oph.soresu.common.db.migrations :as migrations])
+    [oph.soresu.common.db.migrations :as migrations]
+    [clojure.core.match :refer [match]]
+    [taoensso.timbre :refer [spy debug]])
   (:use [oph.soresu.common.config :only [config]]))
 
 (defn- update-person-info-module
-  [form new-person-info-module]
-  (let [content-without-person-info-module (remove #(= (:module %) "person-info") (:content form))
-        content-with-new-person-info-module (into [new-person-info-module] content-without-person-info-module)]
-    (assoc-in form [:content] content-with-new-person-info-module)))
+  [new-person-info-module form]
+  (clojure.walk/postwalk
+    (fn [expr]
+      (match expr
+        {:module (:or :person-info "person-info")}
+        new-person-info-module
+        :else expr))
+    form))
+
+(defn refresh-person-info-modules []
+  (let [new-person-module (person-info-module/person-info-module)
+        existing-forms    (try
+                            (map #(store/fetch-by-id (:id %)) (store/get-all-forms))
+                            (catch Exception _ []))]
+    (doseq [form existing-forms]
+      (let [changed-form (update-person-info-module new-person-module form)]
+        (store/create-form-or-increment-version! changed-form (:organization-oid form))))))
 
 (migrations/defmigration
   migrate-person-info-module "1.13"
   "Update person info module structure in existing forms"
-  (let [new-person-module (person-info-module/person-info-module)
-        existing-forms    (try (store/get-all-forms) (catch Exception _ []))]
-    (doseq [form existing-forms]
-      (let [changed-form (update-person-info-module form new-person-module)]
-        ; Form versioning deprecates this migration which made it into production
-        ; before form versioning. No harm done for empty databases, for existing development databases
-        ; this may or may not work. comment below expression if it doesn't :)
-        (store/create-form-or-increment-version! changed-form nil)
-        ))))
+  (refresh-person-info-modules))
+
+(migrations/defmigration
+  migrate-person-info-module "1.18"
+  "Update person info module structure in existing forms"
+  (refresh-person-info-modules))
 
 (defn migrate
   []
-  (migrations/migrate :db "db.migration" "ataru.db.migrations"))
+ (migrations/migrate :db "db.migration" "ataru.db.migrations"))
