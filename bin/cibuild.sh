710f3e2870e847dc78295aa8028893109a8b471a
Perttu Auramo <perttu.auramo@reaktor.fi>
2016-10-21 11:31:11 +0300

    Refactor build script
    Run all the tests and their preparation phases in run-tests instead
    of fragmented parts as tasks in Bamboo

--- bin/cibuild.sh
+++ bin/cibuild.sh
@@ -8,53 +8,83 @@ echo $OLD_CWD
 export JAVA_HOME=/data00/oph/java/jdk1.8.0_60
 export PATH=/data00/oph/java/jdk1.8.0_60/bin:$PATH
 
+echo "Lein version:"
 ./bin/lein version
 
 compile-less() {
+    echo "Compiling less"
     ./bin/lein less once
 }
 
 npm-dependencies() {
+    echo "Installing npm dependencies"
     npm install
 }
 
 process-resources() {
+    echo "Processing resources"
     ./bin/lein resource
 }
 
 build-clojurescript-virkailija() {
+    echo "Building virkailija clojurescript"
     ./bin/lein cljsbuild once virkailija-min
 }
 
 build-clojurescript-hakija() {
+    echo "Building hakija clojurescript"
     ./bin/lein cljsbuild once hakija-min
 }
 
 create-uberjar() {
+    echo "Creating uberjar"
     ./bin/lein uberjar
 }
 
 test-clojure() {
-    ./bin/lein clean
+    echo "Running clojure tests"
     ./bin/lein spec -t ~ui
 }
 
 test-clojurescript() {
-    ./bin/lein clean
+    echo "Testing clojurescript"
     ./bin/lein doo phantom test once
 }
 
-test-integration() {
+test-browser() {
     ./bin/lein spec -t ui
 }
 
 run-migrations() {
+    echo "Running migrations"
     ./bin/lein with-profile dev run -m manual-migrations/migrate
 }
 
+nuke-test-db() {
+    echo "Nuking test database"
+    ./bin/lein with-profile dev run -m ataru.fixtures.db.unit-test-db/clear-database
+}
+
+run-tests() {
+    echo "Starting test run"
+    ./bin/lein clean
+    rm -rf out # phantom tests fail randomly without this
+    npm-dependencies
+    test-clojurescript
+    nuke-test-db
+    run-migrations
+    test-clojure
+    compile-less
+    build-clojurescript-virkailija
+    build-clojurescript-hakija
+    test-browser
+}
+
 clean() {
+    echo "Cleaning everything"
     ./bin/lein clean
     rm -rf target
+    rm -rf out
     rm -rf node_modules
 }
 
@@ -84,6 +114,9 @@ case "$command" in
         ;;
     "test-clojurescript" )
         test-clojurescript
+        ;;
+    "run-tests" )
+        run-tests
         ;;
     "test-integration" )
         test-integration
