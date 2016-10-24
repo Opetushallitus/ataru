59b099336f43e9300bcc4b54389394a423943a27
Aki Kaivola <aki.kaivola@gmail.com>
2016-10-24 15:10:58 +0300

    Bump migration version numbers

--- src/clj/ataru/db/migrations.clj
+++ src/clj/ataru/db/migrations.clj
@@ -32,7 +32,7 @@
   (refresh-person-info-modules))
 
 (migrations/defmigration
-  migrate-person-info-module "1.20"
+  migrate-person-info-module "1.21"
   "Update person info module structure in existing forms"
   (refresh-person-info-modules))
 
