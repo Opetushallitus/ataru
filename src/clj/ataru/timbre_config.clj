56d7ee7ae25ccb016762f565e9321973d29caf30
Esko Lahti <esko.lahti@reaktor.com>
2016-10-24 15:27:06 +0300

    Log path as conf var

--- src/clj/ataru/timbre_config.clj
+++ src/clj/ataru/timbre_config.clj
@@ -1,12 +1,14 @@
 (ns ataru.timbre-config
   (:require [taoensso.timbre :as timbre]
             [taoensso.timbre.appenders.3rd-party.rotor :as rotor]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [oph.soresu.common.config :refer [config]]))
 
 (defn configure-logging! [logname]
   (when-not (env :dev?)
-    (timbre/merge-config! {:appenders
-                           {:rotor (rotor/rotor-appender
-                                     {:max-size (* 10 1024 1024)
-                                      :backlog  10
-                                      :path     (str "./logs" logname ".log")})}})))
+    (let [path (or (:log-path config) ".")]
+      (timbre/merge-config! {:appenders
+                             {:rotor (rotor/rotor-appender
+                                       {:max-size (* 10 1024 1024)
+                                        :backlog  10
+                                        :path     (str path "/logs" logname ".log")})}}))))
