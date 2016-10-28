8ae73c5ec947b217236a562e43d308dfd19220bf
Esko Lahti <esko.lahti@reaktor.com>
2016-10-28 10:50:30 +0300

    Make application_key columns unique only after V1.24 migration

--- resources/db/migration/V1_23__Add_application_versioning.sql
+++ resources/db/migration/V1_23__Add_application_versioning.sql
@@ -1,7 +1,7 @@
 alter table applications drop constraint "applications_key_key";
-alter table application_events add column application_key varchar(40) not null;
-alter table application_confirmation_emails add column application_key varchar(40) not null;
-alter table application_reviews add column application_key varchar(40) not null;
+alter table application_events add column application_key varchar(40);
+alter table application_confirmation_emails add column application_key varchar(40);
+alter table application_reviews add column application_key varchar(40);
 
 alter table applications add column created_time timestamp with time zone default now();
 update applications set created_time = modified_time;
---
+++ resources/db/migration/V1_25__Application_key_as_not_null.sql
@@ -0,0 +1,3 @@
+alter table application_events add constraint application_events_application_key unique (application_key);
+alter table application_confirmation_emails add constraint application_confirmation_emails_application_key unique (application_key);
+alter table application_reviews add constraint application_reviews_application_key unique (application_key);
