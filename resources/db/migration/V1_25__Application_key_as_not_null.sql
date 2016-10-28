d7386f003693014667e870d0e443cab395507d11
Esko Lahti <esko.lahti@reaktor.com>
2016-10-28 13:40:06 +0300

    Properly create keys for application_* tables

--- resources/db/migration/V1_25__Application_key_as_not_null.sql
+++ resources/db/migration/V1_25__Application_key_as_not_null.sql
@@ -1,3 +1,3 @@
-alter table application_events add constraint application_events_application_key unique (application_key);
-alter table application_confirmation_emails add constraint application_confirmation_emails_application_key unique (application_key);
+create index application_events_application_key on application_events (application_key);
+create index application_confirmation_emails_application_key on application_confirmation_emails (application_key);
 alter table application_reviews add constraint application_reviews_application_key unique (application_key);
