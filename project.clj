84cd11de191a7f9d182211ccab0ff48ae7fd86f0
Esko Lahti <esko.lahti@reaktor.com>
2016-11-21 14:36:17 +0200

    http-kit update

--- project.clj
+++ project.clj
@@ -30,7 +30,7 @@
                  [metosin/compojure-api "1.1.9"]
                  [com.stuartsierra/component "0.3.1"]
                  [aleph "0.4.1"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.2.0"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.2.0"]
                  [ring/ring-json "0.4.0"]
