65541d808cf6aac086eaa519f55fc035ca21e24e
Esko Lahti <esko.lahti@reaktor.com>
2016-11-21 16:44:22 +0200

    poi-ooxml update

--- project.clj
+++ project.clj
@@ -52,7 +52,7 @@
                  [oph/clj-util "0.1.0"]
                  [ring.middleware.logger "0.5.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.apache.poi/poi-ooxml "3.15-beta1"]
+                 [org.apache.poi/poi-ooxml "3.15"]
                  [org.clojars.pntblnk/clj-ldap "0.0.12"]
                  [org.clojure/core.cache "0.6.5"]
                  [org.clojure/tools.nrepl "0.2.12"]]
