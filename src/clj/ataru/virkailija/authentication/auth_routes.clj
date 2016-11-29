e9fc8603580126115c827003a55d8b889ba6792e
Esko Lahti <esko.lahti@reaktor.com>
2016-11-29 14:39:18 +0200

    Extract function

--- src/clj/ataru/virkailija/authentication/auth_routes.clj
+++ src/clj/ataru/virkailija/authentication/auth_routes.clj
@@ -7,15 +7,18 @@
             [taoensso.timbre :refer [spy debug]]
             [clojure.core.match :refer [match]]))
 
+(defn- url->relative [url-from-session]
+  (let [{:keys [path query anchor]} (url/url url-from-session)]
+    (str path
+         (when (some? query) (url/map->query query))
+         (when (some? anchor) (str "#" anchor)))))
+
 (defn auth-routes [organization-service]
   (api/context "/auth" []
     (api/undocumented
       (api/GET "/cas" [ticket :as request]
                (let [redirect-url (if-let [url-from-session (get-in request [:session :original-url])]
-                                    (let [{:keys [path query anchor]} (url/url url-from-session)]
-                                      (str path
-                                           (when (some? query) (url/map->query query))
-                                           (when (some? anchor) (str "#" anchor))))
+                                    (url->relative url-from-session)
                                     "/lomake-editori")]
                  (login (if (-> config :dev :fake-dependencies)
                           (str (System/currentTimeMillis))
