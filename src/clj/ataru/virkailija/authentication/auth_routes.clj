ca1d8427535286a43d9c84181b3f0cb4469deee4
Esa Virtanen <esa.virtanen@reaktor.fi>
2016-12-01 15:07:18 +0200

    Fixes to CAS redirect urls

--- src/clj/ataru/virkailija/authentication/auth_routes.clj
+++ src/clj/ataru/virkailija/authentication/auth_routes.clj
@@ -1,22 +1,17 @@
 (ns ataru.virkailija.authentication.auth-routes
   (:require [ataru.virkailija.authentication.auth :refer [login logout cas-initiated-logout]]
-            [cemerick.url :as url]
             [oph.soresu.common.config :refer [config]]
             [compojure.api.sweet :as api]
             [environ.core :refer [env]]
             [taoensso.timbre :refer [spy debug info]]
-            [clojure.core.match :refer [match]]))
+            [clojure.core.match :refer [match]]
+            [clojure.string :as string]))
 
 (defn- rewrite-url-for-environment [url-from-session]
   "Ensure that https is used when available (due to https termination on non-development environments)"
-  (let [{:keys [host path query anchor]} (url/url url-from-session)]
-    (if (:dev? env)
-      url-from-session
-      (str "https://"
-           host
-           path
-           (when (some? query) (url/map->query query))
-           (when (some? anchor) (str "#" anchor))))))
+  (if (:dev? env)
+    url-from-session
+    (string/replace url-from-session #"^http://" "https://")))
 
 (defn auth-routes [organization-service]
   (api/context "/auth" []
@@ -24,7 +19,7 @@
       (api/GET "/cas" [ticket :as request]
                (let [redirect-url (if-let [url-from-session (get-in request [:session :original-url])]
                                     (rewrite-url-for-environment url-from-session)
-                                    (get-in config [:public-config :virkailija :service-url]))]
+                                    (get-in config [:public-config :virkailija :service_url]))]
                  (login (if (-> config :dev :fake-dependencies)
                           (str (System/currentTimeMillis))
                           ticket)
