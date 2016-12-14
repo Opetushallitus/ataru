239a06224e1ec24589511ce0bde101fb0e5fa89b
Esa Virtanen <esa.virtanen@reaktor.fi>
2016-12-14 14:24:42 +0200

    Remove possibility for followup questions in radio button component

--- src/cljs/ataru/virkailija/editor/component.cljs
+++ src/cljs/ataru/virkailija/editor/component.cljs
@@ -206,9 +206,10 @@
                    (dispatch [:editor/remove-dropdown-option path :options option-index]))}
    [:i.zmdi.zmdi-close.zmdi-hc-lg]])
 
-(defn- dropdown-option [option-index path languages & {:keys [header?] :or {header? false}}]
+(defn- dropdown-option [option-index path languages & {:keys [header? include-followup?] :or {header? false include-followup? true} :as opts}]
   (let [multiple-languages? (< 1 (count languages))
         option-path         [path :options option-index]]
+    (println "dd-option" opts path include-followup?)
     ^{:key (str "options-" option-index)}
     [:div
      [:div.editor-form__multi-options-wrapper-outer
@@ -221,8 +222,10 @@
            [input-field option-path lang #(dispatch [:editor/set-dropdown-option-value (-> % .-target .-value) option-path :label lang])])
          languages)]
       [remove-dropdown-option-button path option-index]
-      [followup-question option-path]]
-     [followup-question-overlay option-path]]))
+      (when include-followup?
+        [followup-question option-path])]
+     (when include-followup?
+       [followup-question-overlay option-path])]))
 
 (defn- dropdown-multi-options [path options-koodisto]
   (let [dropdown-id                (util/new-uuid)
@@ -319,7 +322,7 @@
                   ^{:key "options-input"}
                   [:div.editor-form__multi-options-container
                    (map-indexed (fn [idx _]
-                                  (dropdown-option idx path languages))
+                                  (dropdown-option idx path languages :include-followup? (not= field-type "radioButton")))
                      (:options @value))]
                   ^{:key "options-input-add"}
                   [:div.editor-form__add-dropdown-item
