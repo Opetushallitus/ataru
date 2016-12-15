5e2f97a31141f94b5df18dc6121801b6d5814864
Esa Virtanen <esa.virtanen@reaktor.fi>
2016-12-15 15:36:58 +0200

    Pass salient organization OIDs to tarjonta forms-in-use listing query

--- src/clj/ataru/tarjonta_service/tarjonta_client.clj
+++ src/clj/ataru/tarjonta_service/tarjonta_client.clj
@@ -3,7 +3,8 @@
     [cheshire.core :as json]
     [oph.soresu.common.config :refer [config]]
     [org.httpkit.client :as http]
-    [taoensso.timbre :refer [warn]]))
+    [taoensso.timbre :refer [warn]]
+    [clojure.string :as string]))
 
 (defn get-hakukohde
   [hakukohde-oid]
@@ -17,9 +18,11 @@
       (warn "could not retrieve hakukohde details" url status))))
 
 (defn get-forms-in-use
-  []
+  [organization-oids]
   (let [url      (get-in config [:tarjonta-service :forms-in-use-url])
-        response @(http/get url)]
+        query    (when (< 0 (count organization-oids))
+                   (str "?" (string/join "&" (map #(str "oid=" %) organization-oids))))
+        response @(http/get (str url query))]
     (-> response :body (json/parse-string true) :result)))
 
 (defn get-form-key-for-hakukohde [hakukohde-oid]
