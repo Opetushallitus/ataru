afd44f4eb1d0f041157933e514be09ea6fbe33af
Perttu Auramo <perttu.auramo@reaktor.com>
2016-12-22 15:58:16 +0200

    Remove duplication

--- src/cljs/ataru/virkailija/application_sorting.cljs
+++ src/cljs/ataru/virkailija/application_sorting.cljs
@@ -11,13 +11,16 @@
   (compare-fn (t/time->long (:created-time x))
               (t/time->long (:created-time y))))
 
+(defn- applicant-sort [order-fn x y]
+  (order-fn (compare (clojure.string/lower-case (:applicant-name x)) (clojure.string/lower-case (:applicant-name y)))))
+
 (def application-sort-column-fns
   {:score
    {:ascending (partial score-sort <)
     :descending (partial score-sort >)}
    :applicant-name
-   {:ascending (fn [x y] (compare (clojure.string/lower-case (:applicant-name x)) (clojure.string/lower-case (:applicant-name y))))
-    :descending (fn [x y] (- (compare (clojure.string/lower-case (:applicant-name x)) (clojure.string/lower-case (:applicant-name y)))))}
+   {:ascending (partial applicant-sort +)
+    :descending (partial applicant-sort -)}
    :created-time
    {:ascending (partial date-sort <)
     :descending (partial date-sort >)}})
