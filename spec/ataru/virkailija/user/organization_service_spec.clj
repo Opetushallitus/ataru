cd37af91596868323d43fd4b9ef53caaba74da73
Perttu Auramo <perttu.auramo@reaktor.com>
2016-12-30 15:03:03 +0200

    Refactor

--- spec/ataru/virkailija/user/organization_service_spec.clj
+++ spec/ataru/virkailija/user/organization_service_spec.clj
@@ -2,7 +2,7 @@
   (:require [ataru.virkailija.user.organization-service :as org-service]
             [ataru.virkailija.user.ldap-client-spec :refer [test-user1 test-user1-organization-oid]]
             [ataru.virkailija.user.organization-client-spec :refer [expected-flat-organizations]]
-            [speclj.core :refer [describe it should= tags]]
+            [speclj.core :refer [describe it should= tags around]]
             [clj-ldap.client :as ldap]
             [ataru.virkailija.user.ldap-client :as ataru-ldap]
             [oph.soresu.common.config :refer [config]]
@@ -20,35 +20,35 @@
 (defn create-org-service-instance [] (.start (org-service/->IntegratedOrganizationService)))
 
 (describe "OrganizationService"
-  (tags :unit :organization)
-  (it "should use ldap module to fetch organization oids"
-      (with-redefs [ldap/search                       fake-ldap-search
-                    ataru-ldap/create-ldap-connection fake-create-connection]
-        (let [org-service-instance (create-org-service-instance)]
-          (should= [test-user1-organization-oid] (.get-direct-organization-oids org-service-instance "testi2editori")))))
-  (it "Should get all organizations from organization client and cache the result"
-      (let [cas-get-call-count (atom 0)]
-        (with-redefs [ldap/search                       fake-ldap-search
-                      ataru-ldap/create-ldap-connection fake-create-connection
-                      cas-client/new-client             {}
-                      cas-client/cas-authenticated-get  (partial fake-cas-auth-organization-hierarchy cas-get-call-count)
-                      config                            fake-config]
-          (let [org-service-instance (create-org-service-instance)]
-            (should= expected-flat-organizations
-                     (.get-all-organizations org-service-instance
-                                             [{:oid test-user1-organization-oid :name {:fi "org1"}}]))
-            (should= {test-user1-organization-oid  expected-flat-organizations}
-                     (into {} (for [[k v] @(:all-orgs-cache org-service-instance)] [k v])))
-            (should= 1 @cas-get-call-count)))))
-  (it "Should get direct organizatons from organization client"
-      (with-redefs [ldap/search                       fake-ldap-search
-                    ataru-ldap/create-ldap-connection fake-create-connection
-                    cas-client/new-client             {}
-                    cas-client/cas-authenticated-get  fake-cas-auth-organization
-                    config                            fake-config]
-        (let [org-service-instance (create-org-service-instance)]
-          (should= [{:name {:fi "Telajärven seudun koulutuskuntayhtymä"}
-                     :oid "1.2.246.562.10.3242342"
-                     :type :organization}]
-                   (.get-direct-organizations org-service-instance "testi2editori"))))))
+          (tags :unit :organization)
+
+          (around [spec]
+                  (with-redefs [ldap/search                       fake-ldap-search
+                                ataru-ldap/create-ldap-connection fake-create-connection
+                                config                            fake-config
+                                cas-client/new-client             {}]
+                    (spec)))
+
+          (it "should use ldap module to fetch organization oids"
+              (let [org-service-instance (create-org-service-instance)]
+                (should= [test-user1-organization-oid] (.get-direct-organization-oids org-service-instance "testi2editori"))))
+          (it "Should get all organizations from organization client and cache the result"
+              (let [cas-get-call-count (atom 0)]
+                (with-redefs [cas-client/cas-authenticated-get  (partial fake-cas-auth-organization-hierarchy cas-get-call-count)
+                              config                            fake-config]
+                  (let [org-service-instance (create-org-service-instance)]
+                    (should= expected-flat-organizations
+                             (.get-all-organizations org-service-instance
+                                                     [{:oid test-user1-organization-oid :name {:fi "org1"}}]))
+                    (should= {test-user1-organization-oid expected-flat-organizations}
+                             (into {} (for [[k v] @(:all-orgs-cache org-service-instance)] [k v])))
+                    (should= 1 @cas-get-call-count)))))
+          (it "Should get direct organizatons from organization client"
+              (with-redefs [cas-client/cas-authenticated-get  fake-cas-auth-organization
+                            config                            fake-config]
+                (let [org-service-instance (create-org-service-instance)]
+                  (should= [{:name {:fi "Telajärven seudun koulutuskuntayhtymä"}
+                             :oid  "1.2.246.562.10.3242342"
+                             :type :organization}]
+                           (.get-direct-organizations org-service-instance "testi2editori"))))))
 
