8621357aec5a1dac46b987e4f2e98a69785c046a
Esko Lahti <esko.lahti@reaktor.com>
2017-01-12 10:33:39 +0200

    temp

--- spec/ataru/applications/excel_export_spec.clj
+++ spec/ataru/applications/excel_export_spec.clj
@@ -1,13 +1,11 @@
 (ns ataru.applications.excel-export-spec
   (:require [ataru.applications.excel-export :as j2ee]
-            [ataru.applications.application-store :as application-store]
-            [ataru.forms.form-store :as form-store]
-            [ataru.fixtures.application :as fixtures]
+            [ataru.fixtures.excel-fixtures :as fixtures]
             [speclj.core :refer :all]
-            [taoensso.timbre :refer [spy debug]])
-  (:import (java.io FileOutputStream File)
-           (java.util UUID)
-           (org.apache.poi.ss.usermodel WorkbookFactory)))
+            [ataru.applications.application-store :as application-store])
+  (:import [java.io FileOutputStream File]
+           [java.util UUID]
+           [org.apache.poi.ss.usermodel WorkbookFactory]))
 
 (defn- verify-row
   [sheet row-num expected-values]
@@ -20,51 +18,73 @@
           (should= (nth expected-values col-idx) (.getStringCellValue cell))
           (should-be-nil cell))))))
 
-(defn- verify-pane-information
-  [sheet]
-  (let [info (.getPaneInformation sheet)]
-    (should (.isFreezePane info))
-    (should= 1 (.getHorizontalSplitPosition info))
-    (should= 0 (.getVerticalSplitPosition info))
-    (should= 1 (.getHorizontalSplitTopRow info))
-    (should= 0 (.getVerticalSplitLeftColumn info))))
+;(defn- verify-pane-information
+;  [sheet]
+;  (let [info (.getPaneInformation sheet)]
+;    (should (.isFreezePane info))
+;    (should= 1 (.getHorizontalSplitPosition info))
+;    (should= 0 (.getVerticalSplitPosition info))
+;    (should= 1 (.getHorizontalSplitTopRow info))
+;    (should= 0 (.getVerticalSplitLeftColumn info))))
+
+(defn- temp-file []
+  (File/createTempFile (str "excel-" (UUID/randomUUID)) ".xlsx"))
 
 (describe "excel export"
   (tags :unit :excel)
 
   (around [spec]
-    (with-redefs [application-store/exec-db (fn [& _] (filter #(nil? (:hakukohde %)) fixtures/applications))
-                  form-store/fetch-by-key (fn [& _] fixtures/form)
-                  form-store/fetch-by-id (fn [& _] fixtures/form)
-                  application-store/get-application-review (fn [application-key]
-                                                             (when (= "9d24af7d-f672-4c0e-870f-3c6999f105e0" application-key)
-                                                               fixtures/application-review))]
+    (with-redefs [application-store/get-application-review (fn [& _] fixtures/application-review)]
       (spec)))
 
-  (it "has expected values"
-      (let [file  (File/createTempFile (str "excel-" (UUID/randomUUID)) ".xlsx")
-            applications (->> fixtures/applications
-                              (map application-store/unwrap-application)
-                              (filter (comp nil? :hakukohde)))]
-        (try
-          (with-open [output (FileOutputStream. (.getPath file))]
-            (->> (j2ee/export-applications applications)
-                 (.write output)))
-          (let [workbook           (WorkbookFactory/create file)
-                metadata-sheet     (.getSheetAt workbook 0)
-                applications-sheet (.getSheetAt workbook 1)]
-            (verify-row metadata-sheet 0
-              ["Nimi" "Id" "Tunniste" "Viimeksi muokattu" "Viimeinen muokkaaja"])
-            (verify-row metadata-sheet 1
-              ["Test fixture what is this" "703" "abcdefghjkl" "2016-06-14 15:34:56" "DEVELOPER"])
-            (verify-row applications-sheet 0
-              ["Id" "Lähetysaika" "Tila" "Eka kysymys" "Toka kysymys" "Kolmas kysymys" "Neljas kysymys" "Viides kysymys" "Kuudes kysymys" "Seitsemas kysymys" "Muistiinpanot"])
-            (verify-row applications-sheet 1
-              ["9d24af7d-f672-4c0e-870f-3c6999f105e0" "2016-06-16 09:00:00" "Käsittelemättä" "a" "b" "d" "e" nil "g" "f" "Some notes about the applicant"])
-            (verify-row applications-sheet 2
-              ["956ae57b-8bd2-42c5-90ac-82bd0a4fd31f" "2016-06-15 17:30:55" "Käsittelemättä" "Vastaus" "lomakkeeseen" "asiallinen" "vastaus" nil "jee" nil])
-            (verify-row applications-sheet 3
-              ["c58df586-fdb9-4ee1-b4c4-030d4cfe9f81" "2016-06-15 15:30:55" "Käsittelemättä" "1" "2" "3" "4" "5" "6" nil nil])
-            (verify-pane-information applications-sheet))
-          (finally
-            (.delete file))))))
+  (it "should export applications for a form without hakukohde or haku"
+    (let [file (temp-file)]
+      (try
+        (with-open [output (FileOutputStream. (.getPath file))]
+          (->> (j2ee/export-applications [fixtures/application-for-form])
+               (.write output)))
+        (let [workbook          (WorkbookFactory/create file)
+              metadata-sheet    (.getSheetAt workbook 0)
+              application-sheet (.getSheetAt workbook 1)])
+        (finally
+          (.delete file))))))
+
+;(describe "excel export"
+;  (tags :unit :excel)
+;
+;  (around [spec]
+;    (with-redefs [application-store/exec-db (fn [& _] (filter #(nil? (:hakukohde %)) fixtures/applications))
+;                  form-store/fetch-by-key (fn [& _] fixtures/form)
+;                  form-store/fetch-by-id (fn [& _] fixtures/form)
+;                  application-store/get-application-review (fn [application-key]
+;                                                             (when (= "9d24af7d-f672-4c0e-870f-3c6999f105e0" application-key)
+;                                                               fixtures/application-review))]
+;      (spec)))
+;
+;  (it "has expected values"
+;      (let [file  (File/createTempFile (str "excel-" (UUID/randomUUID)) ".xlsx")
+;            applications (->> fixtures/applications
+;                              (map application-store/unwrap-application)
+;                              (filter (comp nil? :hakukohde)))]
+;        (try
+;          (with-open [output (FileOutputStream. (.getPath file))]
+;            (->> (j2ee/export-applications applications)
+;                 (.write output)))
+;          (let [workbook           (WorkbookFactory/create file)
+;                metadata-sheet     (.getSheetAt workbook 0)
+;                applications-sheet (.getSheetAt workbook 1)]
+;            (verify-row metadata-sheet 0
+;              ["Nimi" "Id" "Tunniste" "Viimeksi muokattu" "Viimeinen muokkaaja"])
+;            (verify-row metadata-sheet 1
+;              ["Test fixture what is this" "703" "abcdefghjkl" "2016-06-14 15:34:56" "DEVELOPER"])
+;            (verify-row applications-sheet 0
+;              ["Id" "Lähetysaika" "Tila" "Eka kysymys" "Toka kysymys" "Kolmas kysymys" "Neljas kysymys" "Viides kysymys" "Kuudes kysymys" "Seitsemas kysymys" "Muistiinpanot"])
+;            (verify-row applications-sheet 1
+;              ["9d24af7d-f672-4c0e-870f-3c6999f105e0" "2016-06-16 09:00:00" "Käsittelemättä" "a" "b" "d" "e" nil "g" "f" "Some notes about the applicant"])
+;            (verify-row applications-sheet 2
+;              ["956ae57b-8bd2-42c5-90ac-82bd0a4fd31f" "2016-06-15 17:30:55" "Käsittelemättä" "Vastaus" "lomakkeeseen" "asiallinen" "vastaus" nil "jee" nil])
+;            (verify-row applications-sheet 3
+;              ["c58df586-fdb9-4ee1-b4c4-030d4cfe9f81" "2016-06-15 15:30:55" "Käsittelemättä" "1" "2" "3" "4" "5" "6" nil nil])
+;            (verify-pane-information applications-sheet))
+;          (finally
+;            (.delete file))))))
---
+++ spec/ataru/fixtures/excel_fixtures.clj
@@ -0,0 +1,29 @@
+(ns ataru.fixtures.excel-fixtures
+  (:require [clj-time.core :as c]))
+
+(def form {:id           123
+           :key          "form_123_key"
+           :name         "Form name"
+           :created-by   "SEPPO PAPUNEN"
+           :created-time (c/date-time 2016 6 14 12 34 56)
+           :content      [{:id       "form_field_0"
+                           :label    "Lomakeosio"
+                           :children [{:id         "form_field_0_0"
+                                       :label      "Kysymys 1"
+                                       :fieldType  "textField"
+                                       :fieldClass "formField"}]}]})
+
+(def application-for-form {:id           9432
+                           :key          "application_9432_key"
+                           :created-time (c/date-time 2016 6 15 12 34 56)
+                           :state        "unprocessed"
+                           :form-id      123
+                           :answers      [{:key       "form_field_0_0"
+                                           :label     "Kysymys 1"
+                                           :value     "Vastaus 9432"
+                                           :fieldType "textfield"}]})
+
+(def application-review {:id              1
+                         :application_key "c58df586-fdb9-4ee1-b4c4-030d4cfe9f81"
+                         :state           "unprocessed"
+                         :notes           "Some notes about the applicant"})
