794360b20d2a319f51efdd6bf6f0d6d45b6f86d0
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-01-17 14:55:07 +0200

    Remove schema diagram generation & schema-viz

--- src/clj/ataru/scripts/generate_schema_diagram.clj
+++ src/clj/ataru/scripts/generate_schema_diagram.clj
@@ -4,9 +4,7 @@
     [ataru.schema.form-schema]
     [clojure.java.shell :refer [sh]]
     [environ.core :refer [env]]
-    [oph.soresu.common.config :refer [config]]
-    ;[schema-viz.core :as svc]
-    ))
+    [oph.soresu.common.config :refer [config]]))
 
 (defn generate-db-schema-diagram
   []
@@ -21,14 +19,7 @@
     (println return-value)
     (:exit return-value)))
 
-;(defn generate-form-schema-diagram
-;  []
-;  (svc/save-schemas (str "target/db-schema/ataru-form-" (:ataru-version env) ".png") {:ns 'ataru.schema.form-schema}))
-
 (defn -main
   []
   (migrations/migrate)
-  (generate-db-schema-diagram)
-  ; disabled since graphviz is not installed on CI server:
-  ; (generate-form-schema-diagram)
-  )
+  (generate-db-schema-diagram))
