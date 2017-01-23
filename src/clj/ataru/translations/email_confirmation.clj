e6f3794926585e07fb607494d67bd27fc0914ca7
Antti Viljakainen <antti.viljakainen@reaktor.fi>
2017-01-23 15:44:54 +0200

    Remove information about modifying application

--- src/clj/ataru/translations/email_confirmation.clj
+++ src/clj/ataru/translations/email_confirmation.clj
@@ -13,9 +13,9 @@
    :application-edited-text       {:fi "Hakemuksesi on päivitetty."
                                    :en "Your application has been updated."
                                    :sv "Din ansökan har uppdaterats."}
-   :modify-link-text              {:fi "Allaolevan linkin kautta löydät hakemuksesi. Voit muokata tietojasi hakuajan päättymiseen asti."
-                                   :en "You can view and modify your application using the link below. Application can be modified till the end of the application period."
-                                   :sv "Via länken nedan hittar du din ansökan. Du kan granska och bearbeta din ansökningsblankett under ansökningstiden "}
+   :modify-link-text              {:fi "Allaolevan linkin kautta voit katsella ja muokata hakemustasi."
+                                   :en "You can view and modify your application using the link below."
+                                   :sv "Via länken nedan hittar du din ansökan."}
    :best-regards                  {:fi "terveisin"
                                    :sv "Med vänliga hälsningar"
                                    :en "Best Regards"}
