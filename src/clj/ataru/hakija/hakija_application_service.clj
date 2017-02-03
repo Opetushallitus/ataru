9f022dfb96f5a7b25a943b80c26508f51c51d987
Perttu Auramo <perttu.auramo@reaktor.com>
2017-02-03 16:02:01 +0200

    Check hakuaika also on server-side

--- src/clj/ataru/hakija/hakija_application_service.clj
+++ src/clj/ataru/hakija/hakija_application_service.clj
@@ -5,6 +5,7 @@
    [ataru.hakija.background-jobs.hakija-jobs :as hakija-jobs]
    [ataru.hakija.application-email-confirmation :as application-email]
    [ataru.person-service.person-integration :as person-integration]
+   [ataru.tarjonta-service.hakuaika :as hakuaika]
    [ataru.forms.form-store :as form-store]
    [ataru.hakija.validator :as validator]
    [ataru.applications.application-store :as application-store]))
@@ -30,15 +31,28 @@
     (log/info "Started person creation job (to person service) with job id" person-service-job-id)
     {:passed? true :id application-id}))
 
-(defn handle-application-submit [application]
+(defn- allowed-to-apply?
+  "If there is a hakukohde the user is applying to, check that hakuaika is on"
+  [tarjonta-service application]
+  (if (not (:hakukohde application))
+    true ;; plain form, always allowed to apply
+    (let [hakukohde         (.get-hakukohde tarjonta-service (:hakukohde application))
+          haku-oid          (:hakuOid hakukohde)
+          haku              (when haku-oid (.get-haku tarjonta-service haku-oid))
+          {hakuaika-on :on} (hakuaika/get-hakuaika-info hakukohde haku)]
+      hakuaika-on)))
+
+(defn handle-application-submit [tarjonta-service application]
   (log/info "Application submitted:" application)
-  (let [{passed? :passed?
-         application-id :application-id
-         :as validation-result}
-        (validate-and-store application application-store/add-application)]
-    (if passed?
-      (start-submit-jobs application-id)
-      validation-result)))
+  (if (allowed-to-apply? tarjonta-service application)
+    (let [{passed? :passed?
+           application-id :application-id
+           :as result}
+          (validate-and-store application application-store/add-application)]
+      (if passed?
+        (start-submit-jobs application-id)
+        result))
+    {:passed? false :failures ["Not allowed to apply (probably hakuaika is not on)"]}))
 
 (defn handle-application-edit [application]
   (log/info "Application edited:" application)
--- src/clj/ataru/hakija/hakija_form_service.clj
+++ src/clj/ataru/hakija/hakija_form_service.clj
@@ -1,6 +1,7 @@
 (ns ataru.hakija.hakija-form-service
   (:require [ataru.forms.form-store :as form-store]
             [ataru.koodisto.koodisto :as koodisto]
+            [ataru.tarjonta-service.hakuaika :as hakuaika]
             [clj-time.core :as time]
             [clj-time.coerce :as time-coerce]
             [taoensso.timbre :refer [warn]]))
@@ -12,40 +13,6 @@
                (not (true? (:deleted form))))
       (-> form
           (koodisto/populate-form-koodisto-fields)))))
-
-(defn- time-within?
-  ([instant {:keys [alkuPvm loppuPvm]}]
-   (time-within? instant alkuPvm loppuPvm))
-  ([instant start end]
-   (time/within?
-    (time/interval (time-coerce/from-long start)
-                   (time-coerce/from-long end))
-    instant)))
-
-(defn- find-current-or-last-hakuaika
-  [hakuaikas]
-  (let [now              (time/now)
-        current-hakuaika (first (filter (partial time-within? now) hakuaikas))]
-    (or
-      current-hakuaika
-      (last hakuaikas))))
-
-(defn- parse-hakuaika
-  "Hakuaika from hakuaika can override hakuaika from haku. Haku may have multiple hakuaikas defined."
-  [hakukohde haku]
-  ; TODO need to check that hakukohde hakuaika is valid wrt. haku hakuaika?
-  (if (and (:hakuaikaAlkuPvm hakukohde) (:hakuaikaLoppuPvm hakukohde))
-    {:start (:hakuaikaAlkuPvm hakukohde)
-     :end   (:hakuaikaLoppuPvm hakukohde)}
-    (let [this-haku-hakuaika (find-current-or-last-hakuaika (:hakuaikas haku))]
-      {:start (:alkuPvm this-haku-hakuaika)
-       :end   (:loppuPvm this-haku-hakuaika)})))
-
-(defn- get-hakuaika-info [hakukohde haku]
-  (let [{start :start end :end :as interval} (parse-hakuaika hakukohde haku)]
-    (if (and start end (time-within? (time/now) start end))
-      (assoc interval :on true)
-      (assoc interval :on false))))
 
 (defn fetch-form-by-hakukohde-oid
   [tarjonta-service hakukohde-oid]
@@ -65,5 +32,5 @@
                :haku-tarjoaja-name (-> hakukohde :tarjoajaNimet :fi)
                :haku-oid           haku-oid
                :haku-name          (-> haku :nimi :kieli_fi)
-               :hakuaika-dates     (get-hakuaika-info hakukohde haku)}})
+               :hakuaika-dates     (hakuaika/get-hakuaika-info hakukohde haku)}})
       (warn "could not find local form for hakukohde" hakukohde-oid "with key" form-key))))
