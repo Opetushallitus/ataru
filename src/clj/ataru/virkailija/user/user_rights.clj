f1ddd8f9dbf318d2481ffaba338bfced2940a817
Perttu Auramo <perttu.auramo@reaktor.com>
2017-02-28 17:12:40 +0200

    Move user right management to a separate module
    More fine-grained user rights which are mapped to ldap rights

---
+++ src/clj/ataru/virkailija/user/user_rights.clj
@@ -0,0 +1,26 @@
+(ns ataru.virkailija.user.user-rights
+  (:require [schema.core :as s]
+            [oph.soresu.common.config :refer [config]]))
+
+(def ^:private
+  rights
+  {:form-edit         "APP_ATARU_EDITORI_CRUD"
+   :view-applications "APP_ATARU_HAKEMUS_READ"
+   :edit-applications "APP_ATARU_HAKEMUS_CRUD"})
+
+(def right-names (keys rights))
+
+
+(defn- ldap-right-name-from-config [right]
+  (let [name-from-config (-> config :ldap :user-right-names right)]
+    (if (< 0 (count (clojure.string/trim name-from-config)))
+      name-from-config
+      (right rights))))
+
+(s/defschema Right (apply s/enum right-names))
+
+(s/defn ^:always-validate ldap-right [right :- Right]
+  (let [name-from-config (-> config :ldap :user-right-names right)]
+    (if (< 0 (count (clojure.string/trim name-from-config)))
+      name-from-config
+      (right rights))))
