c79a9a64aa1666b44eda4ba08e0ae14ecae1e7fe
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-03-17 15:10:59 +0200

    Remove some unused args

--- src/cljs/ataru/hakija/hakija_readonly.cljs
+++ src/cljs/ataru/hakija/hakija_readonly.cljs
@@ -53,7 +53,7 @@
         :when (get-in ui [(keyword (:id child)) :visible?] true)]
     [field child application lang]))
 
-(defn- person-info-wrapper [content application lang]
+(defn- person-info-wrapper [content lang]
   [:div.application__wrapper-element.application__wrapper-element--border
    [:div.application__wrapper-heading
     [:h2 (-> content :label lang)]
@@ -136,7 +136,7 @@
 
 (defn field [content application lang]
   (match content
-         {:fieldClass "wrapperElement" :module "person-info"} [person-info-wrapper content application lang]
+         {:fieldClass "wrapperElement" :module "person-info"} [person-info-wrapper content lang]
          {:fieldClass "wrapperElement" :fieldType "fieldset" :children children} [wrapper content application lang children]
          {:fieldClass "wrapperElement" :fieldType "rowcontainer" :children children} [row-container application lang children]
          {:fieldClass "wrapperElement" :fieldType "adjacentfieldset" :children children} [fieldset content application lang children]
