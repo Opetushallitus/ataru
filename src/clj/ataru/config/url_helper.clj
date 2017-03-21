b76cb3a19163b8cd5034c5d484da6d151f872834
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-03-21 15:25:19 +0200

    Add minimal url config to soresu-cfg files

--- src/clj/ataru/config/url_helper.clj
+++ src/clj/ataru/config/url_helper.clj
@@ -1,19 +1,20 @@
 (ns ataru.config.url-helper
-  (:require [clojure.tools.logging :refer [spy]])
+  (:require [oph.soresu.common.config :refer [config]])
   (:import (fi.vm.sade.properties OphProperties)))
 
 ; TODO component if not too much work?
 (def ^fi.vm.sade.properties.OphProperties url-config (atom nil))
 
 (defn- load-config
-  []
+  [{:keys [virkailija-host hakija-host liiteri-url] :as url-config}]
   (reset! url-config
           (doto (OphProperties. (into-array String ["/ataru-oph.properties"]))
-            (.addDefault "host-virkailija" "itest-virkailija.oph.ware.fi")
-            (.addDefault "host-hakija" "itest-hakija.oph.ware.fi"))))
+            (.addDefault "host-virkailija" (or virkailija-host "itest-virkailija.oph.ware.fi"))
+            (.addDefault "host-hakija" (or hakija-host "itest-hakija.oph.ware.fi"))
+            (.addDefault "url-liiteri" liiteri-url))))
 
 (defn resolve-url
   [key & params]
   (when (nil? @url-config)
-    (load-config))
+    (load-config (:urls config)))
   (.url @url-config (name key) (to-array (or params []))))
