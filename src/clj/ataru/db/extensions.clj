67e7f3b93a64f74e68a2de3d7edb4f8e894aa935
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-03-23 15:24:38 +0200

    Inline soresu db extensions

--- src/clj/ataru/db/extensions.clj
+++ src/clj/ataru/db/extensions.clj
@@ -1,17 +1,34 @@
 (ns ataru.db.extensions
-  (:import [java.sql PreparedStatement])
-  (:require [oph.soresu.common.jdbc.extensions] ; don't remove! imports JSONB handling
-            [clojure.java.jdbc :as jdbc]
-            [clj-time.coerce :as c]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [clj-time.coerce :as c]
+            [cheshire.core :as json])
+  (:import (java.sql PreparedStatement)
+           (org.postgresql.util PGobject)))
+
+(extend-protocol jdbc/ISQLValue
+  clojure.lang.IPersistentCollection
+  (sql-value [value]
+    (doto (PGobject.)
+      (.setType "jsonb")
+      (.setValue (json/generate-string value)))))
 
 (extend-protocol jdbc/IResultSetReadColumn
-                 java.sql.Date
-                 (result-set-read-column [v _ _] (c/from-sql-date v))
+  PGobject
+  (result-set-read-column [pgobj _ _]
+    (let [type  (.getType pgobj)
+          value (.getValue pgobj)]
+      (case type
+        "jsonb" (json/parse-string value true)
+        :else value))))
 
-                 java.sql.Timestamp
-                 (result-set-read-column [v _ _] (c/from-sql-time v)))
+(extend-protocol jdbc/IResultSetReadColumn
+  java.sql.Date
+  (result-set-read-column [v _ _] (c/from-sql-date v))
+
+  java.sql.Timestamp
+  (result-set-read-column [v _ _] (c/from-sql-time v)))
 
 (extend-type org.joda.time.DateTime
-             jdbc/ISQLParameter
-             (set-parameter [v ^PreparedStatement stmt idx]
-                            (.setTimestamp stmt idx (c/to-sql-time v))))
+  jdbc/ISQLParameter
+  (set-parameter [v ^PreparedStatement stmt idx]
+    (.setTimestamp stmt idx (c/to-sql-time v))))
