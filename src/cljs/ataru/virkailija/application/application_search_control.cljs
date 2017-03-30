33b2997279399d5ea783a6e29c29825cecf71a86
Perttu Auramo <perttu.auramo@reaktor.com>
2017-03-30 10:00:31 +0300

    Add an opening/closing chevron which reacts to clicks
    Also some minor styling

--- src/cljs/ataru/virkailija/application/application_search_control.cljs
+++ src/cljs/ataru/virkailija/application/application_search_control.cljs
@@ -1,6 +1,7 @@
 (ns ataru.virkailija.application.application-search-control
   (:require
    [re-frame.core :refer [subscribe dispatch]]
+   [reagent.core :as r]
    [ataru.virkailija.application.application-search-control-handlers]))
 
 (defn tab-row []
@@ -11,19 +12,27 @@
    [:div.application__search-control-tab-selector "Käsitellyt haut"]])
 
 (defn tarjonta-haku [haku]
-  [:div
-   [:a {:href (str "/lomake-editori/applications/haku/" (:oid haku))}
-    (:name haku)
-    (str " (" (:application-count haku) ")")
-    (str " " (:unprocessed haku) " Käsittelemättä")]])
+  (let [hakukohteet-opened (r/atom false)]
+    (fn [haku]
+      (let [toggle-opened #(reset! hakukohteet-opened (not @hakukohteet-opened))]
+        ^{:key (:oid haku)} [:div.application__search-control-haku
+         (if @hakukohteet-opened
+            [:i.zmdi.zmdi-chevron-up.application__search-control-open-hakukohteet
+             {:on-click toggle-opened}]
+            [:i.zmdi.zmdi-chevron-down.application__search-control-open-hakukohteet
+             {:on-click toggle-opened}])
+         [:a {:href (str "/lomake-editori/applications/haku/" (:oid haku))}
+          " "
+          (:name haku)
+          (str " (" (:application-count haku) ")")
+          (str " " (:unprocessed haku) " Käsittelemättä")]]))))
 
 (defn incomplete-haut []
   (let [show (subscribe [:state-query [:application :search-control :show]])
         haut (subscribe [:state-query [:application :haut2]])]
-    (println "haut value" @haut)
     (when (= :incomplete @show)
       [:div
-       (map tarjonta-haku (:tarjonta-haut @haut))])))
+       (map (fn [haku] [tarjonta-haku haku]) (:tarjonta-haut @haut))])))
 
 (defn application-search-control []
   [:div.application-handling__content-wrapper
