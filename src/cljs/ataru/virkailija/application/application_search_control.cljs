0b0648f10a4f04bc8e853f93997a33ce3fd52d3e
Perttu Auramo <perttu.auramo@reaktor.com>
2017-03-30 10:42:53 +0300

    Details shown (no styling yet)

--- src/cljs/ataru/virkailija/application/application_search_control.cljs
+++ src/cljs/ataru/virkailija/application/application_search_control.cljs
@@ -11,11 +11,20 @@
     "Käsittelemättä olevat haut"]
    [:div.application__search-control-tab-selector "Käsitellyt haut"]])
 
+(defn hakukohde-list [hakukohteet]
+  [:div (map
+         (fn [hakukohde]
+           ^{:key (:oid hakukohde)} [:div
+            (:name hakukohde)
+            (str " (" (:application-count hakukohde) ")")
+            (str " " (:unprocessed hakukohde) " Käsittelemättä")])
+         hakukohteet)])
+
 (defn tarjonta-haku [haku]
   (let [hakukohteet-opened (r/atom false)]
     (fn [haku]
       (let [toggle-opened #(reset! hakukohteet-opened (not @hakukohteet-opened))]
-        ^{:key (:oid haku)} [:div.application__search-control-haku
+         [:div.application__search-control-haku
          (if @hakukohteet-opened
             [:i.zmdi.zmdi-chevron-up.application__search-control-open-hakukohteet
              {:on-click toggle-opened}]
@@ -25,14 +34,16 @@
           " "
           (:name haku)
           (str " (" (:application-count haku) ")")
-          (str " " (:unprocessed haku) " Käsittelemättä")]]))))
+          (str " " (:unprocessed haku) " Käsittelemättä")]
+          (when @hakukohteet-opened [hakukohde-list (:hakukohteet haku)])
+          ]))))
 
 (defn incomplete-haut []
   (let [show (subscribe [:state-query [:application :search-control :show]])
         haut (subscribe [:state-query [:application :haut2]])]
     (when (= :incomplete @show)
       [:div
-       (map (fn [haku] [tarjonta-haku haku]) (:tarjonta-haut @haut))])))
+       (map (fn [haku] ^{:key (:oid haku)} [tarjonta-haku haku]) (:tarjonta-haut @haut))])))
 
 (defn application-search-control []
   [:div.application-handling__content-wrapper
