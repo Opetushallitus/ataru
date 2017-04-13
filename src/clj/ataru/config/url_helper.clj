edfe5a2c7c94515003879ada41ca2a288e652adb
Ilmari Vacklin <ilmari.vacklin@icloud.com>
2017-04-13 10:42:51 +0300

    Revert "Simplify URL helper"

--- src/clj/ataru/config/url_helper.clj
+++ src/clj/ataru/config/url_helper.clj
@@ -2,15 +2,22 @@
   (:require [ataru.config.core :refer [config]])
   (:import (fi.vm.sade.properties OphProperties)))
 
-(defonce ^OphProperties url-properties
+; TODO component if not too much work?
+(def ^fi.vm.sade.properties.OphProperties url-properties (atom nil))
+
+(defn- load-config
+  []
   (let [{:keys [virkailija-host hakija-host editor-url liiteri-url] :or
                {virkailija-host "" hakija-host "" editor-url "" liiteri-url ""}} (:urls config)]
-    (doto (OphProperties. (into-array String ["/ataru-oph.properties"]))
-      (.addDefault "host-virkailija" virkailija-host)
-      (.addDefault "host-hakija" hakija-host)
-      (.addDefault "url-editor" editor-url)
-      (.addDefault "url-liiteri" liiteri-url))))
+    (reset! url-properties
+            (doto (OphProperties. (into-array String ["/ataru-oph.properties"]))
+              (.addDefault "host-virkailija" virkailija-host)
+              (.addDefault "host-hakija" hakija-host)
+              (.addDefault "url-editor" editor-url)
+              (.addDefault "url-liiteri" liiteri-url)))))
 
 (defn resolve-url
   [key & params]
-  (.url url-properties (name key) (to-array (or params []))))
+  (when (nil? @url-properties)
+    (load-config))
+  (.url @url-properties (name key) (to-array (or params []))))
