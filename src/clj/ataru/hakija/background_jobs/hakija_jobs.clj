89744ad2eb4309bc52a2958ddd9f85cba05baf65
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-04-21 09:10:47 +0300

    Clean up

--- src/clj/ataru/hakija/background_jobs/hakija_jobs.clj
+++ src/clj/ataru/hakija/background_jobs/hakija_jobs.clj
@@ -3,6 +3,6 @@
             [ataru.person-service.person-integration :as person-integration]
             [ataru.hakija.background-jobs.attachment-finalizer-job :as attachment-finalizer-job]))
 
-(def job-definitions {(:type email-job/job-definition) email-job/job-definition
-                      person-integration/job-type      person-integration/job-definition
+(def job-definitions {(:type email-job/job-definition)                email-job/job-definition
+                      person-integration/job-type                     person-integration/job-definition
                       (:type attachment-finalizer-job/job-definition) attachment-finalizer-job/job-definition})
