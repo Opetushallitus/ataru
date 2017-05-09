bd06bdd83dbb7b9cb16ae3da1c6dfdfeb8249669
Esko Lahti <esko.lahti@reaktor.com>
2017-05-09 11:29:23 +0300

    Test happy-path of finalizing attachments

---
+++ spec/ataru/hakija/background_jobs/attachment_finalizer_job_spec.clj
@@ -0,0 +1,29 @@
+(ns ataru.hakija.background-jobs.attachment-finalizer-job-spec
+  (:require [ataru.applications.application-store :as application-store]
+            [ataru.hakija.background-jobs.attachment-finalizer-job :as job]
+            [cheshire.core :as json]
+            [org.httpkit.client :as http]
+            [ring.util.http-response :as response]
+            [speclj.core :refer :all]))
+
+(describe "finalizing attachments"
+  (tags :unit)
+
+  (it "should finalize attachments belonging to an application"
+    (with-redefs [http/post                         (fn [url params]
+                                                      (should= "/api/files/finalize" url)
+                                                      (let [body (json/parse-string (:body params) true)]
+                                                        (should= {:keys ["attachment-key-1"
+                                                                         "attachment-key-2"
+                                                                         "attachment-key-3"]} body))
+                                                      (future (response/ok)))
+                  application-store/get-application (fn [application-id]
+                                                      (should= application-id 3)
+                                                      {:answers [{:fieldType "attachment"
+                                                                  :value     ["attachment-key-1"]}
+                                                                 {:fieldType "textField"
+                                                                  :value     "lolbal"}
+                                                                 {:fieldType "attachment"
+                                                                  :value     ["attachment-key-2" "attachment-key-3"]}]})]
+      (let [result (job/finalize-attachments {:application-id 3} nil)]
+        (should= {:transition {:id :final}} result)))))
