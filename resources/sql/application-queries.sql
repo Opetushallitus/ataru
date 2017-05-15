2898d6b777ae4f734aefddee07bf37adfa7323c7
Esko Lahti <esko.lahti@reaktor.com>
2017-05-15 15:05:35 +0300

    Return application count by email

--- resources/sql/application-queries.sql
+++ resources/sql/application-queries.sql
@@ -214,7 +214,12 @@ SELECT
                          FROM applications a2
                            JOIN forms f2 ON a2.form_id = f2.id
                          WHERE a2.ssn = a.ssn
-                               AND (:query_type = 'ALL' OR f2.organization_oid IN (:authorized_organization_oids))) AS temp) AS applications_count
+                               AND (:query_type = 'ALL' OR f2.organization_oid IN (:authorized_organization_oids))) AS temp) AS applications_count,
+  (SELECT COUNT(*) FROM (SELECT DISTINCT(a3.key)
+                         FROM applications a3
+                           JOIN forms f3 ON a3.form_id = f3.id
+                         WHERE a3.email = a.email
+                               AND (:query_type = 'ALL' OR f3.organization_oid IN (:authorized_organization_oids))) AS temp) AS applications_by_email_count
   FROM applications a
   JOIN latest_version lv ON a.created_time = lv.latest_time;
 
