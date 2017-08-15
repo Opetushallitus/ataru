5ed692c689696143a4a3e34a38d515dce54e0f7e
Esko Lahti <esko.lahti@reaktor.com>
2017-08-15 09:51:16 +0300

    Require with alias

--- src/clj/ataru/virkailija/authentication/virkailija_edit.clj
+++ src/clj/ataru/virkailija/authentication/virkailija_edit.clj
@@ -1,29 +1,29 @@
 (ns ataru.virkailija.authentication.virkailija-edit
-  (:require [ataru.db.db :refer [exec]]
-            [yesql.core :refer [defqueries]]
+  (:require [ataru.db.db :as db]
+            [yesql.core :as sql]
             [ataru.virkailija.user.ldap-client :as ldap])
   (:import (java.util UUID)))
 
-(defqueries "sql/virkailija-queries.sql")
-(defqueries "sql/virkailija-credentials-queries.sql")
+(sql/defqueries "sql/virkailija-queries.sql")
+(sql/defqueries "sql/virkailija-credentials-queries.sql")
 
 (defn- upsert-virkailija [session]
   (when-let [virkailija (ldap/get-virkailija-by-username (-> session :identity :username))]
-    (exec :db yesql-upsert-virkailija<! {:oid        (:employeeNumber virkailija)
-                                        :first_name (:givenName virkailija)
-                                        :last_name  (:sn virkailija)})))
+    (db/exec :db yesql-upsert-virkailija<! {:oid        (:employeeNumber virkailija)
+                                            :first_name (:givenName virkailija)
+                                            :last_name  (:sn virkailija)})))
 
 (defn create-virkailija-credentials [session application-key]
   (when-let [virkailija (upsert-virkailija session)]
     (let [secret (str (UUID/randomUUID))]
-      (exec :db yesql-upsert-virkailija-credentials<! {:oid             (:oid virkailija)
-                                                      :secret          secret
-                                                      :application_key application-key}))))
+      (db/exec :db yesql-upsert-virkailija-credentials<! {:oid             (:oid virkailija)
+                                                          :secret          secret
+                                                          :application_key application-key}))))
 
 (defn invalidate-virkailija-credentials [virkailija-secret]
-  (exec :db yesql-invalidate-virkailija-credentials! {:virkailija_secret virkailija-secret}))
+  (db/exec :db yesql-invalidate-virkailija-credentials! {:virkailija_secret virkailija-secret}))
 
 (defn virkailija-secret-valid? [virkailija-secret]
-  (-> (exec :db yesql-get-virkailija-secret-valid {:virkailija_secret virkailija-secret})
+  (-> (db/exec :db yesql-get-virkailija-secret-valid {:virkailija_secret virkailija-secret})
       first
       :valid))
