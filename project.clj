edf12121c1eadf17e645df68de19ae63e0b9346c
Esko Lahti <esko.lahti@reaktor.com>
2017-09-05 08:37:29 +0300

    Update lein-resource

--- project.clj
+++ project.clj
@@ -101,7 +101,7 @@
             [lein-less "1.7.5"]
             [lein-ancient "0.6.10"]
             [lein-environ "1.1.0"]
-            [lein-resource "16.11.1"]
+            [lein-resource "17.06.1"]
             [speclj "3.3.2"]]
 
   :doo {:paths {:phantom "./node_modules/phantomjs-prebuilt/bin/phantomjs"}}
