07cf8d9dc41c882e5bc78f454467ec9a2796d7d1
Esko Lahti <esko.lahti@reaktor.com>
2017-09-05 08:31:36 +0300

    Update ring

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
                  [fi.vm.sade/auditlogger "5.0.0-SNAPSHOT"]
                  [fi.vm.sade.java-utils/java-properties "0.1.0-SNAPSHOT"]
                  [http-kit "2.2.0"]
-                 [ring "1.5.1"]
+                 [ring "1.6.2"]
                  [ring/ring-defaults "0.2.3"]
                  [ring/ring-json "0.4.0"]
                  [ring-ratelimit "0.2.2"]
