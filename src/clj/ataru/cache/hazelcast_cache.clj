399fb9ed976d1af33484172e2cee35dc03eb2d70
Jani Rahkola <jani.rahkola@reaktor.com>
2017-09-11 14:34:42 +0300

    Periodically updating cache

--- src/clj/ataru/cache/hazelcast_cache.clj
+++ src/clj/ataru/cache/hazelcast_cache.clj
@@ -3,7 +3,9 @@
             [com.stuartsierra.component :as component]
             [ataru.cache.cache-service :refer [Cache get-from put-to]]
             [ataru.cache.hazelcast :refer [Configurator]])
-  (:import com.hazelcast.config.MapConfig))
+  (:import java.util.concurrent.ScheduledThreadPoolExecutor
+           com.hazelcast.config.MapConfig
+           com.hazelcast.map.listener.EntryEvictedListener))
 
 (defn- with-lock [m key f]
   (.lock m key)
@@ -46,3 +48,86 @@
     (.evict map key))
   (clear-all [_]
     (.evictAll map)))
+
+(deftype EvictionSynchonizer [other]
+  EntryEvictedListener
+  (entryEvicted [_ event]
+    (.evict other (.getKey event))))
+
+(defrecord UpdatingCache [hazelcast
+                          updated-map
+                          stable-map
+                          scheduler
+                          get-fns
+                          name
+                          max-size
+                          ttl
+                          period]
+  component/Lifecycle
+  (start [this]
+    (if (nil? scheduler)
+      (let [updated-map (.getMap (:instance hazelcast) (str "updated-" name))
+            stable-map (.getMap (:instance hazelcast) (str "stable-" name))
+            scheduler (ScheduledThreadPoolExecutor. 1)
+            get-fns (atom {})
+            [period timeunit] period]
+        (.addEntryListener updated-map (EvictionSynchonizer. stable-map) false)
+        (.addEntryListener stable-map (EvictionSynchonizer. updated-map) false)
+        (.scheduleAtFixedRate
+         scheduler
+         (fn [] (doseq [key (.keySet stable-map)]
+                  (when-let [get-fn (get @get-fns key)]
+                    (if-let [new-value (get-fn)]
+                      (.replace updated-map key new-value)
+                      (warn "Could not update value for cache" name key)))))
+         period period timeunit)
+        (-> this
+            (assoc :updated-map updated-map)
+            (assoc :stable-map stable-map)
+            (assoc :scheduler scheduler)
+            (assoc :get-fns get-fns)))
+      this))
+  (stop [this]
+    (when (some? scheduler)
+      (.shutdown scheduler))
+    (-> this
+        (assoc :updated-map nil)
+        (assoc :stable-map nil)
+        (assoc :scheduler nil)
+        (assoc :get-fns nil)))
+
+  Configurator
+  (configure [_ configuration]
+    (let [umc (MapConfig.)
+          smc (MapConfig.)
+          [duration timeunit] ttl]
+      (.setName umc (str "updated-" name))
+      (.setName smc (str "stable-" name))
+      (.setSize (.getMaxSizeConfig umc) max-size)
+      (.setTimeToLiveSeconds smc (.toSeconds timeunit duration))
+      (.addMapConfig configuration umc)
+      (.addMapConfig configuration smc)))
+
+  Cache
+  (get-from [_ key]
+    (when (.containsKey stable-map key)
+      (.get updated-map key)))
+  (put-to [_ key value]
+    (.put updated-map key value)
+    (.put stable-map key key))
+  (get-from [this key get-fn]
+    (if-let [value (get-from this key)]
+      value
+      (with-lock stable-map key
+        (fn []
+          (if-let [value (get-from this key)]
+            value
+            (if-let [new-value (get-fn)]
+              (do (swap! get-fns assoc key get-fn)
+                  (put-to this key new-value)
+                  new-value)
+              (warn "Could not fetch value for cache" name key)))))))
+  (remove-from [_ key]
+    (.evict stable-map key))
+  (clear-all [_]
+    (.evictAll stable-map)))
