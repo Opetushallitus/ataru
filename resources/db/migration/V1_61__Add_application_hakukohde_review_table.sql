86409601123486bc96496a0062f26c660fe540a5
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-09-14 14:22:36 +0300

    Null-proof unique index on application_hakukohde_reviews

--- resources/db/migration/V1_61__Add_application_hakukohde_review_table.sql
+++ resources/db/migration/V1_61__Add_application_hakukohde_review_table.sql
@@ -4,9 +4,16 @@ CREATE TABLE application_hakukohde_reviews (
   requirement     VARCHAR(40) NOT NULL,
   state           VARCHAR(40) NOT NULL,
   hakukohde       VARCHAR(40),
-  modified_time   TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  UNIQUE (application_key, hakukohde, requirement)
+  modified_time   TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+
+CREATE UNIQUE INDEX application_hakukohde_reviews_application_key_1_idx
+  ON application_hakukohde_reviews (application_key, hakukohde, requirement)
+  WHERE hakukohde IS NOT NULL;
+
+CREATE UNIQUE INDEX application_hakukohde_reviews_application_key_2_idx
+  ON application_hakukohde_reviews (application_key, requirement)
+  WHERE hakukohde IS NULL;
 
 CREATE INDEX application_hakukohde_reviews_application_key_idx
   ON application_hakukohde_reviews (application_key);
