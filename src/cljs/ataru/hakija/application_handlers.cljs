44eeab36f049eb9ca7e70c3548b37e3ea440bc5a
Esko Lahti <esko.lahti@reaktor.com>
2017-09-25 12:29:39 +0300

    Set initial row amount to 1 instead of zero

--- src/cljs/ataru/hakija/application_handlers.cljs
+++ src/cljs/ataru/hakija/application_handlers.cljs
@@ -247,6 +247,10 @@
           (assoc-in [:application :answers :hakukohteet :valid] true))
       db)))
 
+(defn- >0? [x]
+  (when (> x 0)
+    x))
+
 (defn set-question-group-row-amounts [db]
   (let [flattened-form-fields (cljc-util/flatten-form-fields (-> db :form :content))]
     (reduce-kv (fn [db answer-key {:keys [value]}]
@@ -256,7 +260,7 @@
                        question-group-id (-> field-descriptor :params :question-group-id)]
                    (cond-> db
                      question-group-id
-                     (update-in [:application :ui question-group-id :count] (fnil identity (-> value first count))))))
+                     (update-in [:application :ui question-group-id :count] (fnil identity ((some-fn >0?) (-> value first count) 1))))))
                db
                (-> db :application :answers))))
 
