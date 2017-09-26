266f51df3e2ac3e64af966cc1a5365b1a57de7a6
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-09-26 17:27:03 +0300

    Fix tests

--- src/clj/ataru/virkailija/authentication/virkailija_edit.clj
+++ src/clj/ataru/virkailija/authentication/virkailija_edit.clj
@@ -1,17 +1,26 @@
 (ns ataru.virkailija.authentication.virkailija-edit
   (:require [ataru.db.db :as db]
             [yesql.core :as sql]
-            [ataru.virkailija.user.ldap-client :as ldap])
+            [ataru.virkailija.user.ldap-client :as ldap]
+            [ataru.config.core :refer [config]])
   (:import (java.util UUID)))
 
 (sql/defqueries "sql/virkailija-queries.sql")
 (sql/defqueries "sql/virkailija-credentials-queries.sql")
 
-(defn upsert-virkailija [session]
-  (when-let [virkailija (ldap/get-virkailija-by-username (-> session :identity :username))]
-    (db/exec :db yesql-upsert-virkailija<! {:oid        (:employeeNumber virkailija)
-                                            :first_name (:givenName virkailija)
-                                            :last_name  (:sn virkailija)})))
+(def fake-virkailija-value {:oid        "1.2.246.562.11.11111111012"
+                            :first_name "Veijo"
+                            :last_name  "Virkailija"})
+
+(defn upsert-virkailija
+  [session]
+  ; TODO this function should probably be in a service with a mock version
+  (if (-> config :dev :fake-dependencies)
+    fake-virkailija-value
+    (when-let [virkailija (ldap/get-virkailija-by-username (-> session :identity :username))]
+      (db/exec :db yesql-upsert-virkailija<! {:oid        (:employeeNumber virkailija)
+                                              :first_name (:givenName virkailija)
+                                              :last_name  (:sn virkailija)}))))
 
 (defn create-virkailija-credentials [session application-key]
   (when-let [virkailija (upsert-virkailija session)]
