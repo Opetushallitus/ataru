a8f9933e861128b67818655fb7aa75c74dd3efa0
Esko Lahti <esko.lahti@reaktor.com>
2017-10-26 17:25:07 +0300

    Renamed information_request table as information_requests

---
+++ resources/db/migration/V1_69__Rename_information_request_to_information_requests.sql
@@ -0,0 +1,1 @@
+ALTER TABLE information_request RENAME TO information_requests;
--- resources/sql/information-request-queries.sql
+++ resources/sql/information-request-queries.sql
@@ -1,6 +1,6 @@
 -- name: yesql-add-information-request<!
 -- Add new information request in an unprocessed state
-INSERT INTO information_request (
+INSERT INTO information_requests (
   application_key,
   subject,
   message
@@ -12,4 +12,4 @@ INSERT INTO information_request (
 
 -- name: yesql-get-information-requests
 -- Get all information requests belonging to an application
-SELECT application_key, subject, message, created_time FROM information_request WHERE application_key = :application_key;
+SELECT application_key, subject, message, created_time FROM information_requests WHERE application_key = :application_key;
