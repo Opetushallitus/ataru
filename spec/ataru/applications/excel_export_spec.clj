1f8ea8ab28902218e7d09ade7ec50a7ffebf6a39
Esa Virtanen <esa.virtanen@reaktor.fi>
2017-11-15 13:59:15 +0200

    Fix tests

--- spec/ataru/applications/excel_export_spec.clj
+++ spec/ataru/applications/excel_export_spec.clj
@@ -37,7 +37,7 @@
   `(let [~(first bindings) (File/createTempFile (str "excel-" (UUID/randomUUID)) ".xlsx")]
      (try
        (with-open [output# (FileOutputStream. (.getPath ~(first bindings)))]
-         (->> (j2ee/export-applications ~(second bindings) (tarjonta-service/new-tarjonta-service))
+         (->> (j2ee/export-applications ~(second bindings) nil (tarjonta-service/new-tarjonta-service))
               (.write output#)))
        ~@body
        (finally
