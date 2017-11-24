8be0fdb8ebeb965ce4d62e3db50bdd408eaacc09
Esko Lahti <esko.lahti@reaktor.com>
2017-11-24 14:19:19 +0200

    Just return dates as long

--- spec/ohjausparametrit/ohjausparametrit_client_spec.clj
+++ spec/ohjausparametrit/ohjausparametrit_client_spec.clj
@@ -33,12 +33,21 @@
       (specs)))
 
   (it "should get ohjausparametri from ohjausparametrit-service"
-    (let [resp (oc/get-ohjausparametrit "1.2.246.562.29.75477542726")]
-      (should= "2017-10-02T05:00:00.000Z"
-               (-> resp :PH_SS :dateStart str))
-      (should= "2017-10-02T05:00:00.000Z"
-               (-> resp :PH_TJT :date str))
-      (should= "2017-10-19T08:14:29.203Z"
-               (-> resp :PH_YNH :dateEnd str))
-      (should= "2017-10-19T08:14:29.203Z"
-               (-> resp :__modified__ str)))))
+    (should= {:PH_SS          {:dateStart 1506920400000, :dateEnd nil},
+              :PH_OPVP        {:date nil},
+              :PH_HKMT        {:date nil},
+              :PH_HKLPT       {:date nil},
+              :PH_VSSAV       {:date nil},
+              :PH_HKP         {:date nil},
+              :PH_AHP         {:date nil},
+              :PH_TJT         {:date 1506920400000},
+              :PH_VTSSV       {:date nil},
+              :PH_EVR         {:date nil},
+              :PH_VSTP        {:date nil},
+              :PH_YNH         {:dateStart nil, :dateEnd 1508400869203},
+              :target         "1.2.246.562.29.75477542726",
+              :PH_VTJH        {:dateStart nil, :dateEnd nil},
+              :PH_IP          {:date nil},
+              :__modifiedBy__ "1.2.246.562.24.64667668834",
+              :__modified__   1508400869203}
+             (oc/get-ohjausparametrit "1.2.246.562.29.75477542726"))))
--- src/clj/ataru/ohjausparametrit/ohjausparametrit_client.clj
+++ src/clj/ataru/ohjausparametrit/ohjausparametrit_client.clj
@@ -4,12 +4,6 @@
             [clj-time.coerce :as t]))
 
 (defn get-ohjausparametrit [haku-oid]
-  (let [resp (-> :ohjausparametrit-service.parametri
-                  (url-helper/resolve-url haku-oid)
-                  (hu/do-request))]
-    (clojure.walk/prewalk (fn [x]
-                            (cond-> x
-                              (and (vector? x)
-                                   (some #{(first x)} [:dateStart :dateEnd :date :__modified__]))
-                              (update 1 t/from-long)))
-                          resp)))
+  (-> :ohjausparametrit-service.parametri
+      (url-helper/resolve-url haku-oid)
+      (hu/do-request)))
