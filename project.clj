27b832382550efeac1ab0899570c1c753b9aac1d
Esko Lahti <esko.lahti@reaktor.com>
2017-12-14 10:34:46 +0200

    Updated devtools

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [com.andrewmcveigh/cljs-time "0.5.2"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [cljs-ajax "0.7.3"]
-                 [binaryage/devtools "0.9.7"]
+                 [binaryage/devtools "0.9.8"]
                  [re-frisk "0.5.2"]
                  [venantius/accountant "0.2.3"]
                  [com.cemerick/url "0.1.1"]
