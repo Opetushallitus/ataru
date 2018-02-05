b08ce8133d944d1bc92dbd87169a3ec270680988
Jani Rahkola <jani.rahkola@reaktor.com>
2018-02-05 16:54:21 +0200

    Lisää pisteet excel testihakemukseen

--- spec/ataru/fixtures/excel_fixtures.clj
+++ spec/ataru/fixtures/excel_fixtures.clj
@@ -89,5 +89,6 @@
 (def application-review {:id              1
                          :application_key "c58df586-fdb9-4ee1-b4c4-030d4cfe9f81"
                          :state           "active"
+                         :score           12
                          :notes           [{:id    342
                                             :notes "Some notes about the applicant"}]})
