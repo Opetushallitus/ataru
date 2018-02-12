11bb561f29c94f32554d9c9245ae42bbb65ba6d5
Heikki Honkanen <heikki.honkanen@gofore.com>
2018-02-12 08:34:00 +0200

    Cleanup.

--- src/cljs/ataru/virkailija/application/handlers.cljs
+++ src/cljs/ataru/virkailija/application/handlers.cljs
@@ -717,13 +717,13 @@
 (reg-event-db
   :application/remove-field-highlight
   (fn [db [_ field-id]]
-    (let [highlighted-fields (-> db :application :selected-application-and-form :highligted-fields)
+    (let [highlighted-fields (-> db :application :selected-application-and-form :highlighted-fields)
           updated-fields     (remove #(= field-id %) highlighted-fields)]
-      (assoc-in db [:application :selected-application-and-form :highligted-fields] updated-fields))))
+      (assoc-in db [:application :selected-application-and-form :highlighted-fields] updated-fields))))
 
 (reg-event-fx
   :application/highlight-field
   (fn [{:keys [db]} [_ field-id]]
     (.scrollIntoView (.getElementById js/document (name field-id)) (js-obj "behavior" "smooth"))
-    {:db (update-in db [:application :selected-application-and-form :highligted-fields] conj field-id)
+    {:db (update-in db [:application :selected-application-and-form :highlighted-fields] conj field-id)
      :dispatch-later [{:ms 3000 :dispatch [:application/remove-field-highlight field-id]}]}))
