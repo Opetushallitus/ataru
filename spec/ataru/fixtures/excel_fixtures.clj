46e9b7bb7d3d4e4252d6328ebfa248a4455fd973
Esa Virtanen <esa.virtanen@reaktor.fi>
2018-02-19 14:48:56 +0200

    Fix tests + add person-info check

--- spec/ataru/fixtures/excel_fixtures.clj
+++ spec/ataru/fixtures/excel_fixtures.clj
@@ -38,53 +38,62 @@
                                                      :exclude-from-answers true
                                                      :fieldClass           "formField"}]}]})
 
-(def application-for-form {:id           9432
-                           :key          "application_9432_key"
-                           :created-time (c/date-time 2016 6 15 12 34 56)
-                           :state        "active"
-                           :form         123
-                           :name         {:fi "Standalone form"}
-                           :lang         "fi"
+(def application-for-form {:id                            9432
+                           :key                           "application_9432_key"
+                           :created-time                  (c/date-time 2016 6 15 12 34 56)
+                           :state                         "active"
+                           :form                          123
+                           :name                          {:fi "Standalone form"}
+                           :lang                          "fi"
+                           :person                        {:turvakielto false}
                            :application-hakukohde-reviews [{:requirement "language-requirement" :state "unreviewed" :hakukohde "form"}
                                                            {:requirement "degree-requirement" :state "fulfilled" :hakukohde "form"}
                                                            {:requirement "eligibility-state" :state "uneligible" :hakukohde "form"}]
-                           :answers      [{:key       "form_field_0_0"
-                                           :label     "Kysymys 1"
-                                           :value     "Vastaus 1"
-                                           :fieldType "textfield"}
-                                          {:key       "form_field_0_1"
-                                           :label     "Kysymys 2"
-                                           :value     "Vastaus 2"
-                                           :fieldType "textfield"}
-                                          {:key       "random_0"
-                                           :label     "Kysymys 3"
-                                           :value     "Vastaus 3"
-                                           :fieldType "textfield"}]})
+                           :answers                       [{:key   "first-name"
+                                                            :label "Etunimi"
+                                                            :value "Lomake-etunimi"}
+                                                           {:key       "form_field_0_0"
+                                                            :label     "Kysymys 1"
+                                                            :value     "Vastaus 1"
+                                                            :fieldType "textfield"}
+                                                           {:key       "form_field_0_1"
+                                                            :label     "Kysymys 2"
+                                                            :value     "Vastaus 2"
+                                                            :fieldType "textfield"}
+                                                           {:key       "random_0"
+                                                            :label     "Kysymys 3"
+                                                            :value     "Vastaus 3"
+                                                            :fieldType "textfield"}]})
 
-(def application-for-hakukohde {:id             3424
-                                :key            "application_3424_key"
-                                :created-time   (c/date-time 2016 6 15 12 34 56)
-                                :state          "active"
-                                :form           321
-                                :name           {:fi "Form with hakukohde and haku"}
-                                :lang           "fi"
-                                :hakukohde      ["hakukohde.oid"]
-                                :haku           "haku.oid"
-                                :person-oid     "1.123.345456567123"
+(def application-for-hakukohde {:id                            3424
+                                :key                           "application_3424_key"
+                                :created-time                  (c/date-time 2016 6 15 12 34 56)
+                                :state                         "active"
+                                :form                          321
+                                :name                          {:fi "Form with hakukohde and haku"}
+                                :lang                          "fi"
+                                :hakukohde                     ["hakukohde.oid"]
+                                :haku                          "haku.oid"
+                                :person-oid                    "1.123.345456567123"
+                                :person                        {:turvakielto true
+                                                                :first-name  "Person-etunimi"}
                                 :application-hakukohde-reviews [{:requirement "selection-state" :state "selected" :hakukohde "hakukohde.oid"}
                                                                 {:requirement "processing-state" :state "processing" :hakukohde "hakukohde.oid"}]
-                                :answers        [{:key       "form_field_1_0"
-                                                  :label     "Kysymys 4"
-                                                  :value     "Vastaus 4"
-                                                  :fieldType "textfield"}
-                                                 {:key       "random_0"
-                                                  :label     "Kysymys 5"
-                                                  :value     "Vastaus 5"
-                                                  :fieldType "textfield"}
-                                                 {:key       "should_not_be_shown"
-                                                  :label     "You should not see this"
-                                                  :value     "Really, no"
-                                                  :fieldType "textField"}]})
+                                :answers                       [{:key   "first-name"
+                                                                 :label "Etunimi"
+                                                                 :value "Lomake-etunimi"}
+                                                                {:key       "form_field_1_0"
+                                                                 :label     "Kysymys 4"
+                                                                 :value     "Vastaus 4"
+                                                                 :fieldType "textfield"}
+                                                                {:key       "random_0"
+                                                                 :label     "Kysymys 5"
+                                                                 :value     "Vastaus 5"
+                                                                 :fieldType "textfield"}
+                                                                {:key       "should_not_be_shown"
+                                                                 :label     "You should not see this"
+                                                                 :value     "Really, no"
+                                                                 :fieldType "textField"}]})
 
 (def application-review {:id              1
                          :application_key "c58df586-fdb9-4ee1-b4c4-030d4cfe9f81"
