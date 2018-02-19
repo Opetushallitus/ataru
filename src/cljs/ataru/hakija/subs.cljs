59a99aaf72fcd59d386367b6ea1518a5832b0b50
Heikki Honkanen <heikki.honkanen@gofore.com>
2018-02-19 11:49:52 +0200

    Fix show-more-hakukohdes? sub.

--- src/cljs/ataru/hakija/subs.cljs
+++ src/cljs/ataru/hakija/subs.cljs
@@ -126,9 +126,8 @@
 (re-frame/reg-sub
   :application/show-more-hakukohdes?
   (fn [db _]
-    (let [hakukohdes-visible          (-> db :application :hakukohde-hits count)
-          remaining-hakukohde-results (-> db :application :remaining-hakukohde-search-results count)]
-      (> remaining-hakukohde-results hakukohdes-visible))))
+    (let [remaining-hakukohde-results (-> db :application :remaining-hakukohde-search-results count)]
+      (> remaining-hakukohde-results 0))))
 
 (re-frame/reg-sub
   :application/hakukohde-hits
