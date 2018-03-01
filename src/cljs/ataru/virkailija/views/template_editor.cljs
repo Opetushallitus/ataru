d735b2fe95f4df0babfc3d474bf41e94415ed103
Esa Virtanen <esa.virtanen@reaktor.fi>
2018-03-01 14:37:15 +0200

    Add email header info to preview view

--- src/cljs/ataru/virkailija/views/template_editor.cljs
+++ src/cljs/ataru/virkailija/views/template_editor.cljs
@@ -3,19 +3,32 @@
 
 (defn email-template-editor
   []
-  (when @(subscribe [:state-query [:editor :ui :template-editor-visible?]])
-    [:div.virkailija-modal__container
-     [:div.virkailija-modal__content
-      [:a.virkailija-modal__close-link
-       {:on-click #(dispatch [:editor/toggle-email-template-editor])}
-       "Sulje"]
-      [:div.virkailija-modal__panels
-       [:div.virkailija-modal__editor-panel
-        [:h3 "Sisältö"]
-        [:textarea.virkailija-modal__editor
-         {:value     @(subscribe [:state-query [:editor :email-template :content]])
-          :on-change #(dispatch [:editor/update-email-template (.-value (.-target %))])}]]
-       [:div.virkailija-modal__preview-panel
-        [:h3 "Esikatselu"]
-        [:iframe.virkailija-modal__preview
-         {:srcDoc @(subscribe [:state-query [:editor :email-template :body]])}]]]]]))
+  (let [visible? @(subscribe [:state-query [:editor :ui :template-editor-visible?]])
+        {:keys [body subject content from]} @(subscribe [:state-query [:editor :email-template]])]
+    (when visible?
+      [:div.virkailija-modal__container
+       [:div.virkailija-modal__content
+        [:a.virkailija-modal__close-link
+         {:on-click #(dispatch [:editor/toggle-email-template-editor])}
+         "Sulje"]
+        [:div.virkailija-modal__panels
+         [:div.virkailija-modal__editor-panel
+          [:h3 "Sisältö"]
+          [:textarea.virkailija-modal__editor
+           {:value     content
+            :on-change #(dispatch [:editor/update-email-template (.-value (.-target %))])}]]
+         [:div.virkailija-modal__preview-panel
+          [:h3 "Esikatselu"]
+          [:div "Lähettäjä:" [:span from]]
+          [:div "Otsikko:" [:span subject]]
+          [:iframe.virkailija-modal__preview
+           {:srcDoc body}]
+          [:div.virkailija-modal__buttons-container
+           [:button.virkailija-modal__button
+            {:on-click (fn [_]
+                         (dispatch [:editor/toggle-email-template-editor])
+                         (dispatch [:editor/update-email-template nil]))}
+            "Peruuta"]
+           [:button.virkailija-modal__button
+            {:on-click #(dispatch [:editor/save-email-template])}
+            "Talleta"]]]]]])))
