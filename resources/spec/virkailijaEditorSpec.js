398f7aa8820907764fad9c7456366f1dcb738af9
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-03-07 15:44:10 +0200

    wait for autosave to trigger

--- resources/spec/virkailijaEditorSpec.js
+++ resources/spec/virkailijaEditorSpec.js
@@ -109,6 +109,7 @@
                   setTextFieldValue(function() { return formComponents().eq(0).find('.editor-form__text-field') }, 'Ensimmäinen kysymys'),
                   clickElement(function() { return formComponents().eq(0).find('.editor-form__info-addon-checkbox label') }),
                   setTextFieldValue(function() { return formComponents().eq(0).find('.editor-form__info-addon-inputs textarea') }, 'Ensimmäisen kysymyksen ohjeteksti'),
+                  wait.forMilliseconds(2500), // waiting for autosave to trigger (this is the only way)
                   clickElement(function() { return formListItems(0)})
               )
               it('has expected contents', function() {
