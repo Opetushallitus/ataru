a4393addca6eabf82b75005f94f6dd0f3296ed1b
Heikki Honkanen <heikki.honkanen@gofore.com>
2018-03-28 09:20:33 +0300

    Migrate all forms.

--- resources/sql/migration-1.88-queries.sql
+++ resources/sql/migration-1.88-queries.sql
@@ -1,8 +1,7 @@
 -- name: yesql-get-1_88-forms
 SELECT *
-FROM latest_forms
-WHERE deleted IS NULL OR NOT deleted
-ORDER BY created_time DESC;
+FROM forms
+ORDER BY created_time ASC;
 
 -- name: yesql-insert-1_88-form<!
 INSERT INTO forms (name, content, created_by, key, languages, organization_oid, deleted)
