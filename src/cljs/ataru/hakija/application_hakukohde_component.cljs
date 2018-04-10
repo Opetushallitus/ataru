bc7b063ecfd0801e9191e1d1925b04facc19ca25
Jani Rahkola <jani.rahkola@reaktor.com>
2018-04-10 09:56:57 +0300

    Katkoviivalaatikko takaisin

--- src/cljs/ataru/hakija/application_hakukohde_component.cljs
+++ src/cljs/ataru/hakija/application_hakukohde_component.cljs
@@ -248,16 +248,15 @@
 
 (defn- select-new-hakukohde-row []
   (when @(subscribe [:application/hakukohteet-editable?])
-    [:a.application__hakukohde-selection-open-search
-     {:class (str (when @(subscribe [:application/hakukohteet-full?])
-                    "application__hakukohde-selection-open-search--disabled")
-                  (when @(subscribe [:application/prioritize-hakukohteet?])
-                    " application__hakukohde-selection-open-search--left-margin"))
-      :on-click (when-not @(subscribe [:application/hakukohteet-full?])
-                  hakukohde-search-toggle-event-handler)}
-     (get-translation :add-application-option)
-     (when-let [max-hakukohteet @(subscribe [:application/max-hakukohteet])]
-       (str " " (get-translation :applications_at_most max-hakukohteet)))]))
+    [:div.application__hakukohde-selection-open-search-wrapper
+     [:a.application__hakukohde-selection-open-search
+      {:class (str (when @(subscribe [:application/hakukohteet-full?])
+                     "application__hakukohde-selection-open-search--disabled"))
+       :on-click (when-not @(subscribe [:application/hakukohteet-full?])
+                   hakukohde-search-toggle-event-handler)}
+      (get-translation :add-application-option)
+      (when-let [max-hakukohteet @(subscribe [:application/max-hakukohteet])]
+        (str " " (get-translation :applications_at_most max-hakukohteet)))]]))
 
 (defn hakukohteet
   [field-descriptor]
