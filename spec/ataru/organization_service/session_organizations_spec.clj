7d7ca57adc75fdc827abcfee91d4daca5b2497e5
Esa Virtanen <esa.virtanen@reaktor.fi>
2018-04-23 16:37:37 +0300

    Fix test arity

--- spec/ataru/organization_service/session_organizations_spec.clj
+++ spec/ataru/organization_service/session_organizations_spec.clj
@@ -1,21 +1,22 @@
 (ns ataru.organization-service.session-organizations-spec
   (:require
-   [speclj.core :refer [describe it should= tags around]]
-   [ataru.organization-service.session-organizations :refer [select-organizations-for-rights]]))
+    [speclj.core :refer [describe it should= tags around]]
+    [ataru.organization-service.session-organizations :refer [select-organizations-for-rights]]
+    [ataru.organization-service.organization-service :as organization-service]))
 
 (def dummy-session {:identity
                     {:user-right-organizations
-                     {:form-edit [{:name {:fi "Telajärven aikuislukio"}
-                                    :oid "1.2.246.562.10.1234334543"
-                                   :type :organization}
-                                  {:name {:fi "Omnia"}
-                                    :oid "1.2.246.562.10.22"
-                                   :type :organization}]
+                     {:form-edit         [{:name {:fi "Telajärven aikuislukio"}
+                                           :oid  "1.2.246.562.10.1234334543"
+                                           :type :organization}
+                                          {:name {:fi "Omnia"}
+                                           :oid  "1.2.246.562.10.22"
+                                           :type :organization}]
                       :view-applications [{:name {:fi "Omnia"}
-                                           :oid "1.2.246.562.10.22"
+                                           :oid  "1.2.246.562.10.22"
                                            :type :organization}
                                           {:name {:fi "Lasikoulu"}
-                                           :oid "1.2.246.562.10.11"
+                                           :oid  "1.2.246.562.10.11"
                                            :type :organization}]}}})
 
 (def expected-organizations-for-rights
@@ -34,7 +35,10 @@
  (tags :unit :session-organizations)
 
  (it "selects unique list of organizations for multiple rights (no duplicates)"
-     (let [results (select-organizations-for-rights dummy-session [:form-edit :view-applications :edit-applications])]
+     (let [results (select-organizations-for-rights
+                     (organization-service/new-organization-service)
+                     dummy-session
+                     [:form-edit :view-applications :edit-applications])]
        (should= expected-organizations-for-rights
                 results))))
 
