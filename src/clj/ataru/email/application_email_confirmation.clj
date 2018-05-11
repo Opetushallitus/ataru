bf77e92e94a56fc9912ba72cef0c62d9eb917eb0
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-05-11 14:49:40 +0300

    allow br element (new line)

--- src/clj/ataru/email/application_email_confirmation.clj
+++ src/clj/ataru/email/application_email_confirmation.clj
@@ -55,7 +55,7 @@
 
 (def html-policy
   (as-> (HtmlPolicyBuilder.) hpb
-        (.allowElements hpb (->string-array "p" "span" "div" "h1" "h2" "h3" "h4" "h5" "ul" "ol" "li"))
+        (.allowElements hpb (->string-array "p" "span" "div" "h1" "h2" "h3" "h4" "h5" "ul" "ol" "li" "br"))
         (.allowElements hpb add-style-to-links (->string-array "a"))
         (.allowUrlProtocols hpb (->string-array "http" "https"))
         (.onElements (.allowAttributes hpb (->string-array "href" "target")) (->string-array "a"))
