adc4a46856594556d68a593a125494c3d6e1e1f2
Jani Rahkola <jani.rahkola@reaktor.com>
2018-05-18 10:08:39 +0300

    Toistuva automaattinen hakukelpoisuus
    Toistuva tausta-ajo joka käynnistää automaattinen hakukelpoisuus jos
    ylioppilas -tarkistuksen hakemuksille joiden hakijoille löytyy muuttuneita
    ylioppilastutkintosuorituksia suoritusrekisteristä.

--- src/clj/ataru/suoritus/suoritus_service.clj
+++ src/clj/ataru/suoritus/suoritus_service.clj
@@ -2,6 +2,7 @@
   (:require [ataru.cas.client :as cas-client]
             [ataru.config.url-helper :as url]
             [cheshire.core :as json]
+            [clj-time.format :as format]
             [clojure.core.match :refer [match]]
             [com.stuartsierra.component :as component]))
 
@@ -15,19 +16,37 @@
     "KESKEYTYNYT" :keskeytynyt
     :else         (throw
                    (new RuntimeException
-                        (str "Unknown suorituksen tila " (:tila data))))))
+                        (str "Unknown suorituksen tila " (:tila data)
+                             " in suoritus " data)))))
+
+(defn- ->suoritus-person-oid
+  [data]
+  (when (clojure.string/blank? (:henkiloOid data))
+    (throw (new RuntimeException (str "No henkiloOid in suoritus " data))))
+  (:henkiloOid data))
 
 (defn- ->suoritus
   [data]
-  {:tila (->suoritus-tila data)})
+  {:tila       (->suoritus-tila data)
+   :person-oid (->suoritus-person-oid data)})
+
+(defn- format-modified-since
+  [modified-since]
+  (format/unparse (:date-time format/formatters)
+                  modified-since))
 
 (defn- ylioppilas-suoritukset
-  [cas-client person-oid]
+  [cas-client person-oid modified-since]
   (match [(cas-client/cas-authenticated-get
            cas-client
-           (url/resolve-url "suoritusrekisteri.suoritukset"
-                            {"henkilo" person-oid
-                             "komo"    yo-komo}))]
+           (url/resolve-url
+            "suoritusrekisteri.suoritukset"
+            (cond-> {"komo" yo-komo}
+                    (some? person-oid)
+                    (assoc "henkilo" person-oid)
+                    (some? modified-since)
+                    (assoc "muokattuJalkeen"
+                           (format-modified-since modified-since)))))]
     [{:status 200 :body s}]
     (map ->suoritus (json/parse-string s true))
     [r]
@@ -35,6 +54,7 @@
                 (str "Fetching ylioppilas suoritukset failed: " r)))))
 
 (defprotocol SuoritusService
+  (ylioppilas-suoritukset-modified-since [this modified-since])
   (ylioppilas? [this person-oid]))
 
 (defrecord HttpSuoritusService [cas-client]
@@ -45,8 +65,10 @@
     (assoc this :cas-client nil))
 
   SuoritusService
+  (ylioppilas-suoritukset-modified-since [this modified-since]
+    (ylioppilas-suoritukset cas-client nil modified-since))
   (ylioppilas? [this person-oid]
     (some #(= :valmis (:tila %))
-          (ylioppilas-suoritukset cas-client person-oid))))
+          (ylioppilas-suoritukset cas-client person-oid nil))))
 
 (defn new-suoritus-service [] (->HttpSuoritusService nil))
