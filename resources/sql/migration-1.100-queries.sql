0573fba0e751d0b95fe0f7432c73d74b47eacc79
Esa Virtanen <esa.virtanen@reaktor.fi>
2018-06-07 16:17:25 +0300

    Preserve created_time and id order in migration

--- resources/sql/migration-1.100-queries.sql
+++ resources/sql/migration-1.100-queries.sql
@@ -2,7 +2,7 @@
 SELECT id
 FROM forms
 WHERE deleted IS NULL OR NOT deleted
-ORDER BY created_time DESC;
+ORDER BY id ASC;
 
 -- name: yesql-get-1_100-form
 SELECT *
@@ -10,8 +10,8 @@ FROM forms
 WHERE id = :id;
 
 -- name: yesql-insert-1_100-form<!
-INSERT INTO forms (name, content, created_by, key, languages, organization_oid, deleted)
-VALUES (:name, :content, :created_by, :key, :languages, :organization_oid, :deleted);
+INSERT INTO forms (name, content, created_by, key, languages, organization_oid, deleted, created_time)
+VALUES (:name, :content, :created_by, :key, :languages, :organization_oid, :deleted, :created_time);
 
 -- name: yesql-get-1_100-applications
 SELECT *
@@ -20,9 +20,9 @@ WHERE form_id = :form_id;
 
 -- name: yesql-insert-1_100-application!
 INSERT INTO applications
-(key, lang, form_id, content, preferred_name, last_name, person_oid, hakukohde, haku, ssn, dob, email)
+(key, lang, form_id, content, preferred_name, last_name, person_oid, hakukohde, haku, ssn, dob, email, created_time)
 VALUES (:key, :lang, :form_id, :content, :preferred_name, :last_name, :person_oid,
-              ARRAY [:hakukohde] :: CHARACTER VARYING(127) [], :haku, :ssn, :dob, :email);
+              ARRAY [:hakukohde] :: CHARACTER VARYING(127) [], :haku, :ssn, :dob, :email, :created_time);
 
 -- name: yesql-insert-1_100-application-event<!
 INSERT INTO application_events (application_key, event_type, new_review_state, virkailija_oid, hakukohde, review_key)
