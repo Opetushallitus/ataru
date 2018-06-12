34bdbf62f99eea983412dd633b32b00aa8e921ba
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-06-12 14:21:03 +0300

    HLE-510 "Kysymys kuuluu hakukohteisiin"

--- resources/spec/hakijaHakukohteenHakuaikaSpec.js
+++ resources/spec/hakijaHakukohteenHakuaikaSpec.js
@@ -59,10 +59,10 @@
                     return testFrame().find(id).parent().siblings(".application__question_hakukohde_names_container").text()
                 };
 
-                expect(kysymysKoskeeHakukohteitaFinder("#hakuajat-ohi")).to.equal("Kysymys koskee hakukohteita (1)")
-                expect(kysymysKoskeeHakukohteitaFinder("#osa-hakuajoista-ohi")).to.equal("Kysymys koskee hakukohteita (2)")
-                expect(kysymysKoskeeHakukohteitaFinder("#kaikki-hakuajat-voimassa")).to.equal("Kysymys koskee hakukohteita (1)")
-                expect(kysymysKoskeeHakukohteitaFinder("#assosiaatio-hakukohderyhman-kautta")).to.equal("Kysymys koskee hakukohteita (1)")
+                expect(kysymysKoskeeHakukohteitaFinder("#hakuajat-ohi")).to.equal("Kysymys kuuluu hakukohteisiin (1)")
+                expect(kysymysKoskeeHakukohteitaFinder("#osa-hakuajoista-ohi")).to.equal("Kysymys kuuluu hakukohteisiin (2)")
+                expect(kysymysKoskeeHakukohteitaFinder("#kaikki-hakuajat-voimassa")).to.equal("Kysymys kuuluu hakukohteisiin (1)")
+                expect(kysymysKoskeeHakukohteitaFinder("#assosiaatio-hakukohderyhman-kautta")).to.equal("Kysymys kuuluu hakukohteisiin (1)")
             })
         })
 
