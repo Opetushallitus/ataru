e1f1b570ed4b3f4688e2d1fc56e82cfe3fd73ff1
Jani Rahkola <jani.rahkola@reaktor.com>
2018-06-27 15:59:51 +0300

    Päivitä henkilötiedot heti henkilönluonnin jälkeen

--- src/clj/ataru/person_service/person_integration.clj
+++ src/clj/ataru/person_service/person_integration.clj
@@ -5,13 +5,18 @@
    [clojure.java.jdbc :as jdbc]
    [taoensso.timbre :as log]
    [ataru.applications.application-store :as application-store]
+   [ataru.background-job.job :as job]
    [ataru.db.db :as db]
    [ataru.person-service.person-service :as person-service]
    [yesql.core :refer [defqueries]]))
 
 (defqueries "sql/person-integration-queries.sql")
 
-(defn upsert-and-log-person [person-service application-id]
+(defn start-update-person-info-job
+  [job-runner person-oid]
+  (job/start-job job-runner "update-person-info-job" {:person-oid person-oid}))
+
+(defn upsert-and-log-person [job-runner person-service application-id]
   (let [application (application-store/get-application application-id)]
     (try
       (let [{:keys [status oid]} (person-service/create-or-find-person
@@ -23,7 +28,9 @@
           :exists
           (log/info "Person" oid "already existed in oppijanumerorekisteri"))
         (application-store/add-person-oid application-id oid)
-        (log/info "Added person" oid "to application" application-id))
+        (log/info "Added person" oid "to application" application-id)
+        (start-update-person-info-job job-runner oid)
+        (log/info "Started person info update job for application" application-id))
       (catch IllegalArgumentException e
         (log/error e "Failed to create-or-find person for application"
                    application-id)))
@@ -31,13 +38,13 @@
 
 (defn upsert-person
   [{:keys [application-id]}
-   {:keys [person-service]}]
+   {:keys [person-service] :as job-runner}]
   {:pre [(not (nil? application-id))
          (not (nil? person-service))]}
   (log/info "Trying to add applicant from application"
             application-id
             "to oppijanumerorekisteri")
-  (upsert-and-log-person person-service application-id))
+  (upsert-and-log-person job-runner person-service application-id))
 
 (defn- update-person-info-as-in-person
   [person-oid person]
