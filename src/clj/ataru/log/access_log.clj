5e7c30a7efb633f415c1198dc1b473dd95fc6d71
Heikki Honkanen <heikki.honkanen@gofore.com>
2018-07-19 10:55:05 +0300

    Fix ring session fetch.

--- src/clj/ataru/log/access_log.clj
+++ src/clj/ataru/log/access_log.clj
@@ -2,6 +2,7 @@
   (:require [ataru.config.core :refer [config]]
             [ataru.util.app-utils :as app-utils]
             [clj-time.core :as t]
+            [clojure.string :as s]
             [environ.core :refer [env]]
             [taoensso.timbre :as timbre]
             [taoensso.timbre.appenders.3rd-party.rolling :refer [rolling-appender]]))
@@ -40,41 +41,32 @@
 
 (defn- extract-session
   [request]
-  (-> request
-      (extract-header "cookie")
-      (clojure.string/split #"=")
-      last))
+  (let [cookies      (s/split (extract-header request "cookie") #";")
+        ring-session (some #(when (s/starts-with? % "ring-session") %) cookies)]
+    (last (s/split ring-session #"="))))
 
 (defn log
   [{:keys [info]}
    {:keys [request-method uri remote-addr query-string] :as req}
    {:keys [status] :as resp}
    totaltime]
-  (let [method          (-> request-method name clojure.string/upper-case)
-        request-path    (str uri (when query-string (str "?" query-string)))
-        agent           (extract-header req "user-agent")
-        size            (extract-header resp "Content-Length")
-        caller-id       (extract-header req "caller-id")
-        client-code     (extract-header req "clientsubsystemcode")
-        x-forwarded-for (extract-header req "x-forwarded-for")
-        x-real-ip       (extract-header req "x-real-ip")
-        referer         (extract-header req "referer")
-        session         (extract-session req)
-        log-map         {:timestamp           (.toString (t/to-time-zone (t/now) (t/time-zone-for-id "Europe/Helsinki")))
-                         :responseCode        status
-                         :request             (str method " " request-path)
-                         :responseTime        totaltime
-                         :requestMethod       method
-                         :service             service-name
-                         :environment         (-> config :public-config :environment-name)
-                         :user-agent          agent
-                         :caller-id           caller-id
-                         :clientSubsystemCode client-code
-                         :x-forwarded-for     x-forwarded-for
-                         :x-real-ip           x-real-ip
-                         :remote-ip           remote-addr
-                         :session             session
-                         :response-size       size
-                         :referer             referer}
-        log-message     (cheshire.core/generate-string log-map)]
+  (let [method      (-> request-method name s/upper-case)
+        request     (str method " " uri (when query-string (str "?" query-string)))
+        log-map     {:timestamp           (.toString (t/to-time-zone (t/now) (t/time-zone-for-id "Europe/Helsinki")))
+                     :responseCode        status
+                     :request             request
+                     :responseTime        totaltime
+                     :requestMethod       method
+                     :service             service-name
+                     :environment         (-> config :public-config :environment-name)
+                     :user-agent          (extract-header req "user-agent")
+                     :caller-id           (extract-header req "caller-id")
+                     :clientSubsystemCode (extract-header req "clientsubsystemcode")
+                     :x-forwarded-for     (extract-header req "x-forwarded-for")
+                     :x-real-ip           (extract-header req "x-real-ip")
+                     :remote-ip           remote-addr
+                     :session             (extract-session req)
+                     :response-size       (extract-header resp "Content-Length")
+                     :referer             (extract-header req "referer")}
+        log-message (cheshire.core/generate-string log-map)]
     (info log-message)))
