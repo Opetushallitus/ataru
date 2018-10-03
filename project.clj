35b84591da67a16e78a06674407e9f8b66fffdcd
Jani Rahkola <jani.rahkola@reaktor.com>
2018-10-03 10:57:10 +0300

    Travis build-ohje

--- project.clj
+++ project.clj
@@ -77,15 +77,15 @@
 
   :min-lein-version "2.5.3"
 
-  :repositories [["releases" {:url           "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"
+  :repositories [["releases" {:url           "https://artifactory.opintopolku.fi/artifactory/oph-sade-release-local"
                               :sign-releases false
                               :snapshots     false
                               ;                             :creds :gpg
                               }]
-                 ["snapshots" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"
+                 ["snapshots" {:url "https://artifactory.opintopolku.fi/artifactory/oph-sade-snapshot-local"
                                ;                                   :creds :gpg
                                }]
-                 ["ext-snapshots" {:url "https://artifactory.oph.ware.fi/artifactory/ext-snapshot-local"}]
+                 ["ext-snapshots" {:url "https://artifactory.opintopolku.fi/artifactory/ext-snapshot-local"}]
                  ["Laughing Panda" {:url       "http://maven.laughingpanda.org/maven2"
                                     :snapshots false}]]
 
