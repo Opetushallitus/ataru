603f229c0e68a625aa181c428fc35889e7fc9f11
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-11-16 13:06:56 +0200

    too specific assert removed

--- resources/spec/virkailijaApplicationHandlingSpec.js
+++ resources/spec/virkailijaApplicationHandlingSpec.js
@@ -291,7 +291,7 @@
 
       it('shows link to all applications belonging to a given ssn', function(done) {
         wait.until(function() {
-          return searchApplicationsBySsnLink().textContent === 'Näytä oppijan hakemukset (2)'
+          return searchApplicationsBySsnLink()
         })()
         .then(clickElement(searchApplicationsBySsnLink))
         .then(wait.until(ssnSearchFieldHasValue('020202A0202')))
