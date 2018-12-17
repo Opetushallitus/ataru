967fb6a437a758f338de09dcf0f142057232f0bf
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-12-17 13:22:20 +0200

    HLE-799 Nationality ei näy virkailijan näkymässä

--- src/cljs/ataru/virkailija/views/virkailija_readonly.cljs
+++ src/cljs/ataru/virkailija/views/virkailija_readonly.cljs
@@ -316,7 +316,9 @@
   (let [field            (first children)
         id               (keyword (:id field-descriptor))
         use-onr-info?    (contains? (:person application) id)
-        values           (flatten (replace-with-option-label (-> application :person :nationality)
+        values           (flatten (replace-with-option-label (if use-onr-info?
+                                                               (-> application :person :nationality)
+                                                               (get-in application [:answers :nationality :value]))
                                                              (:options field)
                                                              lang))
         highlight-field? (subscribe [:application/field-highlighted? id])]
