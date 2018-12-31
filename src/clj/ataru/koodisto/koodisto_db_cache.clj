584236b74934b53e311d01daffcf13df9cb34ae1
Jani Rahkola <jani.rahkola@reaktor.com>
2018-12-31 10:22:18 +0200

    Poista kuollutta koodia

--- src/clj/ataru/koodisto/koodisto_db_cache.clj
+++ src/clj/ataru/koodisto/koodisto_db_cache.clj
@@ -1,45 +1,22 @@
 (ns ataru.koodisto.koodisto-db-cache
   (:require [ataru.config.url-helper :refer [resolve-url]]
-            [ataru.db.db :as db]
             [ataru.koodisto.koodisto-codes :refer [institution-type-codes]]
             [ataru.organization-service.organization-client :as organization-client]
             [ataru.util.http-util :as http-util]
             [cheshire.core :as cheshire]
             [clojure.string :as str]
-            [clojure.tools.logging :as log]
-            [pandect.algo.sha256 :refer :all]
-            [yesql.core :refer [defqueries]]))
-
-; TODO url.config
-(def koodisto-base-url "https://virkailija.opintopolku.fi:443/koodisto-service/rest/")
-(def all-koodisto-groups-path "codes")
-(def all-koodistos-group-uri "http://kaikkikoodistot")
-
-(defqueries "sql/koodisto.sql")
-
-(defn json->map [body] (cheshire/parse-string body true))
+            [clojure.tools.logging :as log]))
 
 (defn- do-get [url]
   (let [{:keys [status headers body error] :as resp} (http-util/do-get url)]
     (if (= 200 status)
-      (let [body (json->map body)]
-        (log/info (str "Fetched koodisto from URL: " url))
-        body)
+      (do (log/info (str "Fetched koodisto from URL: " url))
+          (cheshire/parse-string body true))
       (throw (ex-info "Error when fetching doing HTTP GET" {:status  status
                                                             :url     url
                                                             :body    body
                                                             :error   error
                                                             :headers headers})))))
-
-(defn- fetch-all-koodisto-groups []
-  (do-get (str koodisto-base-url all-koodisto-groups-path)))
-
-(defn- koodisto-groups->uris-and-latest [koodisto-groups]
-  (->> koodisto-groups
-       (filter #(= all-koodistos-group-uri (:koodistoRyhmaUri %)))
-       (first)
-       (:koodistos)
-       (mapv #(select-keys % [:koodistoUri :latestKoodistoVersio]))))
 
 (defn- nil-to-empty-string [x]
   (or x ""))
@@ -52,17 +29,6 @@
        (mapv :nimi)
        (first)
        nil-to-empty-string))
-
-(defn- extract-name [koodisto-version]
-  (->> koodisto-version
-       (:latestKoodistoVersio)
-       (:metadata)
-       (extract-name-with-language "FI")))
-
-(defn- koodisto-version->uri-and-name [koodisto-version]
-  {:uri     (:koodistoUri koodisto-version)
-   :name    (extract-name koodisto-version)
-   :version (-> koodisto-version :latestKoodistoVersio :versio)})
 
 (defn- compare-case-insensitively [s1 s2]
   (compare (str/upper-case s1) (str/upper-case s2)))
@@ -100,14 +66,14 @@
         :version version}
        add-within
        :within
-       (filter #(clojure.string/starts-with? (:uri %) "koulutus_"))))
+       (filter #(str/starts-with? (:uri %) "koulutus_"))))
 
 (defn- get-vocational-institutions-by-type [type version]
   (->> {:uri     (str "oppilaitostyyppi_" type)
         :version version}
        add-within
        :within
-       (filter #(clojure.string/starts-with? (:uri %) "oppilaitosnumero_"))
+       (filter #(str/starts-with? (:uri %) "oppilaitosnumero_"))
        (map #(assoc % :label (:nimi (organization-client/get-organization-by-oid-or-number (:value %)))))))
 
 (defn- get-vocational-institutions [version]
@@ -115,7 +81,7 @@
           institution-type-codes))
 
 (defn get-koodi-options [koodisto-uri]
-  (let [[uri version] (clojure.string/split koodisto-uri #"#")]
+  (let [[uri version] (str/split koodisto-uri #"#")]
     (condp = uri
 
            "AmmatillisetOPSperustaiset" (get-vocational-degree-options version)
@@ -135,9 +101,3 @@
              (->> (do-get url)
                   (mapv koodi-value->soresu-option)
                   (sort-by (comp :fi :label) compare-case-insensitively))))))
-
-(defn list-koodistos []
-  (->> (fetch-all-koodisto-groups)
-       (koodisto-groups->uris-and-latest)
-       (mapv koodisto-version->uri-and-name)
-       (sort compare-case-insensitively)))
