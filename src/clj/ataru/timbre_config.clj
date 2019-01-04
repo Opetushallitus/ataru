0ab60120f3f5faf9ea469b264cb4c5c03d199747
Jani Rahkola <jani.rahkola@reaktor.com>
2019-01-04 12:52:32 +0200

    Metriikkalogitus piiloon dev tilassa

--- src/clj/ataru/timbre_config.clj
+++ src/clj/ataru/timbre_config.clj
@@ -1,24 +1,34 @@
 (ns ataru.timbre-config
   (:require [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.core :as appenders]
             [taoensso.timbre.appenders.3rd-party.rotor :as rotor]
             [environ.core :refer [env]]
             [ataru.config.core :refer [config]])
   (:import [java.util TimeZone]))
 
 (defn configure-logging! [app-id]
-  (when-not (env :dev?)
-    (let [log-kwd  (case app-id
-                     :virkailija :virkailija-base-path
-                     :hakija :hakija-base-path)
-          path     (get-in config [:log log-kwd])
-          filename (str (name app-id) ".log")]
+  (let [timestamp-opts {:pattern  "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+                        :timezone (TimeZone/getTimeZone "Europe/Helsinki")}]
+    (if (env :dev?)
       (timbre/merge-config! {:appenders
-                                             {:println      {:enabled? false}
-                                              :standard-out {:enabled? false}
-                                              :spit         {:enabled? false}
-                                              :rotor        (rotor/rotor-appender
-                                                              {:max-size (* 10 1024 1024)
-                                                               :backlog  10
-                                                               :path     (str path "/" filename)})}
-                             :timestamp-opts {:pattern  "yyyy-MM-dd HH:mm:ss ZZ"
-                                              :timezone (TimeZone/getTimeZone "Europe/Helsinki")}}))))
+                             {:println
+                              {:ns-blacklist ["ataru.cache.redis-cache" "ataru.http.server"]}
+                              :cache-error-appender
+                              (assoc (appenders/println-appender)
+                                     :min-level :warn
+                                     :ns-whitelist ["ataru.cache.redis-cache" "ataru.http.server"])}
+                             :timestamp-opts timestamp-opts})
+      (let [log-kwd  (case app-id
+                       :virkailija :virkailija-base-path
+                       :hakija     :hakija-base-path)
+            path     (get-in config [:log log-kwd])
+            filename (str (name app-id) ".log")]
+        (timbre/merge-config! {:appenders
+                               {:println      {:enabled? false}
+                                :standard-out {:enabled? false}
+                                :spit         {:enabled? false}
+                                :rotor        (rotor/rotor-appender
+                                               {:max-size (* 10 1024 1024)
+                                                :backlog  10
+                                                :path     (str path "/" filename)})}
+                               :timestamp-opts timestamp-opts})))))
