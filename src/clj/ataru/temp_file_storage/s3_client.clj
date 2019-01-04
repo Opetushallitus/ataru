e88de6db1e1885faf30f8e654083b685e18e04a4
Jani Rahkola <jani.rahkola@reaktor.com>
2019-01-04 15:24:29 +0200

    CredentialsProvider komponentti

--- src/clj/ataru/temp_file_storage/s3_client.clj
+++ src/clj/ataru/temp_file_storage/s3_client.clj
@@ -2,32 +2,24 @@
   (:require [com.stuartsierra.component :as component]
             [ataru.config.core :refer [config]])
   (:import [com.amazonaws.services.s3 AmazonS3Client]
-           [com.amazonaws.regions Regions]
-           [com.amazonaws.auth InstanceProfileCredentialsProvider DefaultAWSCredentialsProviderChain]
-           (java.io Closeable)))
+           [com.amazonaws.regions Regions]))
 
-(defrecord S3Client []
+(defrecord S3Client [credentials-provider s3-client]
   component/Lifecycle
 
   (start [this]
-    (let [credentials-provider (DefaultAWSCredentialsProviderChain/getInstance)
-          client               (-> (AmazonS3Client/builder)
-                                   (.withRegion (Regions/fromName (get-in config [:aws :region] "eu-west-1")))
-                                   (.withCredentials credentials-provider)
-                                   (.build))]
-      (-> this
-          (assoc :s3-client client)
-          (assoc :credentials-provider credentials-provider))))
+    (if (nil? s3-client)
+      (assoc this :s3-client (-> (AmazonS3Client/builder)
+                                 (.withRegion (Regions/fromName (get-in config [:aws :region] "eu-west-1")))
+                                 (.withCredentials
+                                  (:credentials-provider credentials-provider))
+                                 (.build)))
+      this))
 
   (stop [this]
     (when-let [client (:s3-client this)]
       (.shutdown client))
-    (when-let [credentials-provider (:credentials-provider this)]
-      (when (instance? Closeable credentials-provider)
-        (.close credentials-provider)))
-    (-> this
-        (assoc :s3-client nil)
-        (assoc :credentials-provider nil))))
+    (assoc this :s3-client nil)))
 
 (defn new-client []
   (map->S3Client {}))
