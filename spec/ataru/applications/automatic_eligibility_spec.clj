6f0253cac87975a64fad87c7b2b4626c9d0835bc
Jani Rahkola <jani.rahkola@reaktor.com>
2019-01-08 15:42:59 +0200

    Refaktoroi automatic eligibility spec

--- spec/ataru/applications/automatic_eligibility_spec.clj
+++ spec/ataru/applications/automatic_eligibility_spec.clj
@@ -7,70 +7,41 @@
             [clojure.test.check.properties :as prop]
             [speclj.core :refer :all]))
 
-(defn input-gen
-  [haku-gen ph-ahp-gen raw-now-gen ylioppilas?-gen]
-  (gen/let [haku haku-gen
+(defn- ->ohjausparametrit-gen
+  [haku]
+  (if (nil? haku)
+    (gen/return nil)
+    (gen/one-of [(gen/return {})
+                 (gen/return {:PH_AHP nil})
+                 (gen/return {:PH_AHP {:date nil}})
+                 (gen/fmap #(hash-map :PH_AHP (hash-map :date %))
+                           (gen/choose 1 3))])))
+
+(def input-gen
+  (gen/let [haku (gen/one-of [(gen/return nil)
+                              (gen/hash-map
+                               :oid (gen/return "haku-oid")
+                               :ylioppilastutkintoAntaaHakukelpoisuuden gen/boolean)])
             hakukohde-oids (if (nil? haku)
                              (gen/return [])
                              (gen/fmap (fn [i]
                                          (map #(str "hakukohde-" % "-oid")
                                               (range i)))
-                                       (gen/choose 1 10)))]
+                                       (gen/choose 1 6)))
+            yah?           (gen/vector gen/boolean (count hakukohde-oids))]
     (gen/hash-map
-     :application (gen/hash-map :key (gen/return "application-key")
-                                :person-oid (gen/return "person-oid")
-                                :haku-oid (gen/return (:oid haku))
-                                :hakukohde-oids (gen/return hakukohde-oids))
-     :haku (gen/return haku)
-     :ohjausparametrit (if (nil? haku)
-                         (gen/return nil)
-                         (gen/hash-map :PH_AHP ph-ahp-gen))
-     :raw-now raw-now-gen
-     :hakukohteet      (apply gen/tuple (map #(gen/hash-map :oid (gen/return %)
-                                                            :ylioppilastutkintoAntaaHakukelpoisuuden gen/boolean)
-                                             hakukohde-oids))
-     :ylioppilas? ylioppilas?-gen)))
-
-(def no-haku-input-gen
-  (input-gen (gen/return nil)
-             (gen/hash-map :date gen/nat)
-             gen/nat
-             gen/boolean))
-
-(def haku-input-gen
-  (input-gen (gen/return {:oid                                     "haku-oid"
-                          :ylioppilastutkintoAntaaHakukelpoisuuden true})
-             (gen/return {:date 2})
-             (gen/return 1)
-             gen/boolean))
-
-(def automatic-eligibility-if-ylioppilas-not-in-use-input-gen
-  (input-gen (gen/return {:oid                                     "haku-oid"
-                          :ylioppilastutkintoAntaaHakukelpoisuuden false})
-             (gen/hash-map :date gen/nat)
-             gen/nat
-             gen/boolean))
-
-(def ph-ahp-passed-input-gen
-  (input-gen (gen/hash-map :oid (gen/return "haku-oid")
-                           :ylioppilastutkintoAntaaHakukelpoisuuden gen/boolean)
-             (gen/return {:date 2})
-             (gen/return 3)
-             gen/boolean))
-
-(def ylioppilas-input-gen
-  (input-gen (gen/return {:oid                                     "haku-oid"
-                          :ylioppilastutkintoAntaaHakukelpoisuuden true})
-             (gen/return {:date 2})
-             (gen/return 1)
-             (gen/return true)))
-
-(def not-ylioppilas-input-gen
-  (input-gen (gen/return {:oid                                     "haku-oid"
-                          :ylioppilastutkintoAntaaHakukelpoisuuden true})
-             (gen/return {:date 2})
-             (gen/return 1)
-             (gen/return false)))
+     :application      (gen/hash-map :key (gen/return "application-key")
+                                     :person-oid (gen/return "person-oid")
+                                     :haku-oid (gen/return (:oid haku))
+                                     :hakukohde-oids (gen/return hakukohde-oids))
+     :haku             (gen/return haku)
+     :ohjausparametrit (->ohjausparametrit-gen haku)
+     :now              (gen/fmap coerce/from-long (gen/choose 1 3))
+     :hakukohteet      (gen/return (map #(hash-map :oid %1
+                                                   :ylioppilastutkintoAntaaHakukelpoisuuden %2)
+                                        hakukohde-oids
+                                        yah?))
+     :suoritus?        gen/boolean)))
 
 (defn- call-ae
   [inputs]
@@ -78,9 +49,9 @@
    (:application inputs)
    (:haku inputs)
    (:ohjausparametrit inputs)
-   (coerce/from-long (:raw-now inputs))
+   (:now inputs)
    (:hakukohteet inputs)
-   (:ylioppilas? inputs)))
+   (:suoritus? inputs)))
 
 (defn- check [times prop]
   (let [result (tc/quick-check times prop)]
@@ -89,39 +60,51 @@
         (-fail (str (with-out-str (clojure.pprint/pprint (list 'call-ae input))) "\n"
                     (with-out-str (clojure.pprint/pprint (call-ae input)))))))))
 
-(describe "Automatic eligibility if ylioppilas"
+(describe "Automatic eligibility"
   (tags :unit)
 
   (it "returns no updates when application has no haku-oid"
-    (check 100 (prop/for-all [inputs no-haku-input-gen]
-                 (empty? (call-ae inputs)))))
+    (check 100 (prop/for-all [inputs input-gen]
+                 (if (nil? (:haku inputs))
+                   (empty? (call-ae inputs))
+                   true))))
 
   (it "returns no updates when automatic eligibility if ylioppilas not in use"
-    (check 100 (prop/for-all [inputs automatic-eligibility-if-ylioppilas-not-in-use-input-gen]
-                 (empty? (call-ae inputs)))))
-
-  (it "returns no updates when now is pass PH_AHP"
-    (check 100 (prop/for-all [inputs ph-ahp-passed-input-gen]
-                 (empty? (call-ae inputs)))))
-
-  (it "returns updates from unreviewed to eligible when ylioppilas"
-    (check 100 (prop/for-all [inputs ylioppilas-input-gen]
-                 (every? #(and (= "unreviewed" (:from %))
-                               (= "eligible" (:to %)))
-                         (call-ae inputs)))))
-
-  (it "returns updates from eligible to unreviewed when not ylioppilas"
-    (check 100 (prop/for-all [inputs not-ylioppilas-input-gen]
-                 (every? #(and (= "eligible" (:from %))
-                               (= "unreviewed" (:to %)))
-                         (call-ae inputs)))))
+    (check 100 (prop/for-all [inputs input-gen]
+                 (if (false? (get-in inputs [:haku :ylioppilastutkintoAntaaHakukelpoisuuden]))
+                   (empty? (call-ae inputs))
+                   true))))
 
   (it "returns updates only for hakukohteet where automatic eligibility if ylioppilas is in use"
-    (check 100 (prop/for-all [inputs haku-input-gen]
+    (check 100 (prop/for-all [inputs input-gen]
                  (every? #(:ylioppilastutkintoAntaaHakukelpoisuuden (:hakukohde %))
                          (call-ae inputs)))))
 
+  (it "returns no updates when now is pass PH_AHP"
+    (check 100 (prop/for-all [inputs input-gen]
+                 (if (some->> (get-in inputs [:ohjausparametrit :PH_AHP :date])
+                              coerce/from-long
+                              (time/after? (:now inputs)))
+                   (empty? (call-ae inputs))
+                   true))))
+
+  (it "returns updates from unreviewed to eligible when ylioppilas or ammatillinen"
+    (check 100 (prop/for-all [inputs input-gen]
+                 (if (:suoritus? inputs)
+                   (every? #(and (= "unreviewed" (:from %))
+                                 (= "eligible" (:to %)))
+                           (call-ae inputs))
+                   true))))
+
+  (it "returns updates from eligible to unreviewed when not ylioppilas or ammatillinen"
+    (check 100 (prop/for-all [inputs input-gen]
+                 (if (not (:suoritus? inputs))
+                   (every? #(and (= "eligible" (:from %))
+                                 (= "unreviewed" (:to %)))
+                           (call-ae inputs))
+                   true))))
+
   (it "returns application as given in updates"
-    (check 100 (prop/for-all [inputs haku-input-gen]
+    (check 100 (prop/for-all [inputs input-gen]
                  (every? #(= (:application inputs) (:application %))
                          (call-ae inputs))))))
