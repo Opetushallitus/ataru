02cc7385af03e8f05a75ddd54e576647bf55e1ba
Esa Virtanen <esa.virtanen@reaktor.com>
2019-01-28 16:59:14 +0200

    Refactor and fix list with orgs w/o valid names

--- src/cljs/ataru/virkailija/views/banner.cljs
+++ src/cljs/ataru/virkailija/views/banner.cljs
@@ -89,19 +89,19 @@
     label]])
 
 (defn profile []
-  (let [user-info           (subscribe [:state-query [:editor :user-info]])
-        org-select-visible? (reagent/atom false)
-        lang                (subscribe [:editor/virkailija-lang])
-        results-page        (subscribe [:state-query [:editor :organizations :results-page]])
-        page-size           20]
+  (let [user-info                 (subscribe [:state-query [:editor :user-info]])
+        org-select-visible?       (reagent/atom false)
+        lang                      (subscribe [:editor/virkailija-lang])
+        results-page              (subscribe [:state-query [:editor :organizations :results-page]])
+        search-results            (subscribe [:state-query [:editor :organizations :matches]])
+        selected-organization-sub (subscribe [:state-query [:editor :user-info :selected-organization]])
+        page-size                 20]
     (fn []
       (when @user-info
-        (let [organizations             (:organizations @user-info)
-              search-results            (subscribe [:state-query [:editor :organizations :matches]])
-              selected-organization-sub (subscribe [:state-query [:editor :user-info :selected-organization]])
-              selected-organization     (when @selected-organization-sub [@selected-organization-sub])
-              org-str                   (org-label organizations (first selected-organization))
-              num-results-to-show       (* page-size (inc @results-page))]
+        (let [organizations         (:organizations @user-info)
+              selected-organization (when @selected-organization-sub [@selected-organization-sub])
+              org-str               (org-label organizations (first selected-organization))
+              num-results-to-show   (* page-size (inc @results-page))]
           [:div.profile
            [:div.profile__organization
             [:a.profile__organization-link
@@ -119,10 +119,10 @@
                  [:i.zmdi.zmdi-close]]]
                [:div.profile__organization-select-content
                 (into
-                 [:ul.profile__organization-select-user-orgs.zmdi-hc-ul]
-                 (map
-                  (fn [org] [:li [:i.zmdi.zmdi-hc-li.zmdi-accounts] org])
-                  (create-org-labels (or selected-organization organizations) @lang)))
+                  [:ul.profile__organization-select-user-orgs.zmdi-hc-ul]
+                  (map
+                    (fn [org] [:li [:i.zmdi.zmdi-hc-li.zmdi-accounts] org])
+                    (create-org-labels (or selected-organization organizations) @lang)))
                 (when selected-organization
                   [:div
                    (when (:superuser? @user-info)
@@ -153,7 +153,10 @@
                           (if hakukohderyhma?
                             [:i.zmdi.zmdi-hc-li.zmdi-collection-text]
                             [:i.zmdi.zmdi-hc-li.zmdi-accounts])
-                          (get-label name)]])
+                          (let [label (get-label name)]
+                            (if (clojure.string/blank? label)
+                              "—"
+                              label))]])
                       (take num-results-to-show @search-results))])
                  (when (<= (count @search-results) (inc num-results-to-show))
                    [:a.profile__organization-more-results
