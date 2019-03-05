d96b5ca21e2ec5180a071ddced398b9567688264
Jani Rahkola <jani.rahkola@reaktor.com>
2019-03-05 14:40:34 +0200

    Tehokkaammat update-person-info kyselyt

--- resources/sql/person-integration-queries.sql
+++ resources/sql/person-integration-queries.sql
@@ -8,9 +8,13 @@ FROM (SELECT :preferred_name AS preferred_name,
              :last_name AS last_name,
              upper(:ssn) AS ssn,
              to_date(:dob, 'YYYY-MM-DD') AS dob) AS t
-WHERE id IN (SELECT id
-             FROM latest_applications
-             WHERE person_oid = :person_oid)
+WHERE id IN (SELECT a.id
+             FROM applications AS a
+             WHERE a.person_oid = :person_oid AND
+                   NOT EXISTS (SELECT 1
+                               FROM applications AS a2
+                               WHERE a2.key = a.key AND
+                                     a2.id > a.id))
   AND (applications.preferred_name IS DISTINCT FROM t.preferred_name
        OR applications.last_name IS DISTINCT FROM t.last_name
        OR applications.ssn IS DISTINCT FROM t.ssn
@@ -22,7 +26,7 @@ SET preferred_name = t.preferred_name,
     last_name = t.last_name,
     ssn = t.ssn,
     dob = t.dob
-FROM (SELECT id,
+FROM (SELECT a.id,
              (array_agg(answers->>'value')
               FILTER (WHERE answers->>'key' = 'preferred-name'))[1] AS preferred_name,
              (array_agg(answers->>'value')
@@ -32,9 +36,13 @@ FROM (SELECT id,
              to_date((array_agg(answers->>'value')
                       FILTER (WHERE answers->>'key' = 'birth-date'))[1],
                      'DD.MM.YYYY') AS dob
-      FROM latest_applications
-      JOIN LATERAL jsonb_array_elements(content->'answers') AS answers ON true
-      WHERE person_oid = :person_oid
+      FROM applications AS a
+      JOIN LATERAL jsonb_array_elements(a.content->'answers') AS answers ON true
+      WHERE a.person_oid = :person_oid AND
+            NOT EXISTS (SELECT 1
+                        FROM applications AS a2
+                        WHERE a2.key = a.key AND
+                              a2.id > a.id)
       GROUP BY id) AS t
 WHERE applications.id = t.id
   AND (applications.preferred_name IS DISTINCT FROM t.preferred_name
