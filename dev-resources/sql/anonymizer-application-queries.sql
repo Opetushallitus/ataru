a789362d5bfa9c6445832346dffedd409a184cce
Jani Rahkola <jani.rahkola@reaktor.com>
2019-03-19 09:02:35 +0200

    Päivitä myös anonymisoidut email ja dob sarakkeet

--- dev-resources/sql/anonymizer-application-queries.sql
+++ dev-resources/sql/anonymizer-application-queries.sql
@@ -3,5 +3,10 @@ SELECT id, person_oid, content FROM applications;
 
 -- name: sql-update-application!
 UPDATE applications
-SET preferred_name = :preferred_name, last_name = :last_name, ssn = :ssn, content = :content
+SET preferred_name = :preferred_name,
+    last_name = :last_name,
+    ssn = :ssn,
+    email = :email,
+    dob = :dob,
+    content = :content
 WHERE id = :id;
