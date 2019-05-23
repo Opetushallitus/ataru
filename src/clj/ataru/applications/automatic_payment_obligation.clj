7c9b000f1f71a66d4f614e8ae863eabf8cfa9156
Jani Rahkola <jani.rahkola@reaktor.com>
2019-05-23 14:58:24 +0300

    Automaattinen hakukelpoisuus vain kk hauille

--- src/clj/ataru/applications/automatic_payment_obligation.clj
+++ src/clj/ataru/applications/automatic_payment_obligation.clj
@@ -3,14 +3,20 @@
             [ataru.cache.cache-service :as cache]
             [ataru.koodisto.koodisto-codes :as codes]
             [ataru.person-service.person-service :as person-service]
-            [ataru.applications.application-store :as application-store]))
+            [ataru.applications.application-store :as application-store]
+            [ataru.tarjonta-service.tarjonta-protocol :as tarjonta]))
 
 (defn nationality-finland? [person]
   (some #(= codes/finland-country-code (:kansalaisuusKoodi %)) (:kansalaisuus person)))
 
+(defn- korkeakouluhaku? [tarjonta-service haku-oid]
+  (clojure.string/starts-with?
+   (:kohdejoukkoUri (tarjonta/get-haku tarjonta-service haku-oid))
+   "haunkohdejoukko_12#"))
+
 (defn automatic-payment-obligation-job-step
   [{:keys [person-oid]}
-   {:keys [person-service henkilo-cache]}]
+   {:keys [person-service tarjonta-service henkilo-cache]}]
   (cache/remove-from henkilo-cache person-oid)
   (let [person (person-service/get-person person-service person-oid)]
     (when (or (:yksiloity person)
@@ -19,11 +25,14 @@
             applications         (->> (application-store/get-application-keys-for-person-oid person-oid)
                                       (map :key)
                                       (map application-store/get-latest-application-by-key))]
-        (doseq [application applications]
+        (doseq [application applications
+                :when       (and (some? (:haku application))
+                                 (korkeakouluhaku? tarjonta-service
+                                                   (:haku application)))]
           (doseq [hakukohde-oid (:hakukohde application)]
             (application-store/save-payment-obligation-automatically-changed
-              (:key application)
-              hakukohde-oid
-              "payment-obligation"
-              (if finnish-nationality? "not-obligated" "unreviewed")))))))
+             (:key application)
+             hakukohde-oid
+             "payment-obligation"
+             (if finnish-nationality? "not-obligated" "unreviewed")))))))
   {:transition {:id :final}})
