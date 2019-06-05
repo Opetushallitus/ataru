c2292effc0eed250d400812ae28e9394fd9b3421
Jani Rahkola <jani.rahkola@reaktor.com>
2019-06-05 09:07:59 +0300

    Access logi tyypitettynä stdout:iin

--- src/clj/ataru/log/access_log.clj
+++ src/clj/ataru/log/access_log.clj
@@ -1,30 +1,36 @@
 (ns ataru.log.access-log
   (:require [ataru.config.core :refer [config]]
             [ataru.util.app-utils :as app-utils]
+            [cheshire.core :as json]
             [clj-time.core :as t]
             [clojure.string :as s]
             [environ.core :refer [env]]
             [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.core :refer [println-appender]]
             [taoensso.timbre.appenders.3rd-party.rolling :refer [rolling-appender]]))
 
 (defonce service-name
   (case (app-utils/get-app-id)
-    :virkailija "ataru_virkailija"
-    :hakija "ataru_hakija"
-    nil))
+    :virkailija "ataru-editori"
+    :hakija     "ataru-hakija"))
 
 (defonce access-log-config
   (assoc timbre/example-config
          :appenders {:file-appender
-                     (assoc (rolling-appender {:path    (str (-> config :log :virkailija-base-path)
-                                                             ; ^Virkailija and hakia paths are the same
-                                                             "/accesslog_" service-name
-                                                             ; Hostname will differentiate files in actual environments
-                                                             (when (:hostname env)
-                                                               (str "_" (:hostname env)))
-                                                             ".log")
+                     (assoc (rolling-appender {:path    (str (case (app-utils/get-app-id)
+                                                               :virkailija (-> config :log :virkailija-base-path)
+                                                               :hakija     (-> config :log :hakija-base-path))
+                                                             "/access_" service-name
+                                                             ;; Hostname will differentiate files in actual environments
+                                                             (when (:hostname env) (str "_" (:hostname env))))
                                                :pattern :daily})
-                            :output-fn (fn [{:keys [msg_]}] (force msg_)))}))
+                            :output-fn (fn [{:keys [msg_]}] (force msg_)))
+                     :stdout-appender (assoc (println-appender
+                                              {:stream :std-out})
+                                             :output-fn (fn [data]
+                                                          (json/generate-string
+                                                           {:eventType "access"
+                                                            :event     (json/parse-string (force (:msg_ data)))})))}))
 
 (defn info [str]
   (timbre/log* access-log-config :info str))
