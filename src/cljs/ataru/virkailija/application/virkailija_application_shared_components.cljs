0a967936e203207b5d102629ce07817312c8b08f
Esko Lahti <esko.lahti@reaktor.com>
2019-09-24 16:33:04 +0300

    Poistettu käyttämätön tiedosto

--- src/cljs/ataru/virkailija/application/virkailija_application_shared_components.cljs
+++
@@ -1,9 +0,0 @@
-(ns ataru.virkailija.application.virkailija-application-shared-components)
-
-(defn icon-check []
-  [:span.application-handling__review-state-selected-icon.zmdi-hc-stack.zmdi-hc-lg
-   [:i.zmdi.zmdi-check.zmdi-hc-stack-1x]])
-
-(defn icon-unselected []
-  [:span.application-handling__review-state-selected-icon.zmdi-hc-stack.zmdi-hc-lg
-   [:i.zmdi.zmdi-hc-stack-1x]])
