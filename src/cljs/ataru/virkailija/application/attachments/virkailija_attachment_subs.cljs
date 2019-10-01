bb9b41f5017d4480ba46804052dbb9ec9aa2cb87
Esko Lahti <esko.lahti@reaktor.com>
2019-10-01 10:47:17 +0300

    Lasketaan liitepyynnöt valituille hakukohteille re-framen subscriptionissa

--- src/cljs/ataru/virkailija/application/attachments/virkailija_attachment_subs.cljs
+++ src/cljs/ataru/virkailija/application/attachments/virkailija_attachment_subs.cljs
@@ -39,3 +39,31 @@
                     conj)
          (first)
          (can-display-file?))))
+
+(re-frame/reg-sub
+  :virkailija-attachments/liitepyynnot-for-selected-hakukohteet
+  (fn []
+    [(re-frame/subscribe [:state-query [:application :selected-review-hakukohde-oids]])
+     (re-frame/subscribe [:application/selected-form-fields-by-id])
+     (re-frame/subscribe [:application/selected-application])
+     (re-frame/subscribe [:state-query [:application :review :attachment-reviews]])])
+  (fn [[selected-hakukohde-oids
+        form-fields
+        application
+        liitepyynnot-for-hakukohteet]]
+    (transduce (comp (map (fn [hakukohde-oid]
+                            (when-let [liitepyynnot-for-hakukohde (-> hakukohde-oid keyword liitepyynnot-for-hakukohteet)]
+                              (map (fn [[liitepyynto-key-str liitepyynto-state]]
+                                     (let [liitepyynto-key (keyword liitepyynto-key-str)
+                                           values          (-> application :answers liitepyynto-key :values)
+                                           label           (-> form-fields liitepyynto-key :label)]
+                                       {:key           liitepyynto-key
+                                        :state         liitepyynto-state
+                                        :values        values
+                                        :label         label
+                                        :hakukohde-oid hakukohde-oid}))
+                                   liitepyynnot-for-hakukohde))))
+                     (filter (comp not nil?))
+                     (mapcat identity))
+               conj
+               selected-hakukohde-oids)))
