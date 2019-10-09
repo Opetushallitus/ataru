5daaf66921c517c874975c1ccdca21c1b8316203
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-10-09 15:18:28 +0300

    BUG-2068 : WIP - Saving.

--- src/clj/ataru/virkailija/virkailija_routes.clj
+++ src/clj/ataru/virkailija/virkailija_routes.clj
@@ -1272,6 +1272,11 @@
     (api/GET "/dashboard" []
       (selmer/render-file "templates/dashboard.html" {}))))
 
+(defn- combine-statuses [statuses]
+  (taoensso.timbre/info "-------combine-statuses--------" (pr-str statuses))
+  (clojure.pprint/pprint "-------pprint--------" (pr-str statuses))
+  statuses)
+
 (defn status-routes
   [system]
   (api/context "/status" []
@@ -1281,7 +1286,11 @@
                       :fail    s/Int
                       :error   s/Int
                       :waiting s/Int}}
-      (let [status (job/status 24)]
+      ; PITÄISKÖ TÄSSÄ HAKEE ENSIN VIIKKO, SIT PÄIVÄ JA SIT TUNTI?!?
+      ; TEHÄÄN ENSIN NIIN JA SIT VAIKKA KÄÄNNETÄÄN VALUET JOS TARTTEE.
+      (let [status (combine-statuses (apply map #(job/status %) [168, 24, 1]))]
+      ;(let [status (job/status 168)]
+      ;  (taoensso.timbre/info "---------------" status)
         (cond-> (dissoc status :ok)
                 (:ok status)       response/ok
                 (not (:ok status)) response/internal-server-error)))
