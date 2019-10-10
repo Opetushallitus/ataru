9e4528a50c4db7c457f1ea7eb921bf652a48ef1b
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-10-10 15:24:25 +0300

    BUG-2074 : Handle dots, concat filename to 250 chars.

--- src/clj/ataru/files/file_store.clj
+++ src/clj/ataru/files/file_store.clj
@@ -53,7 +53,7 @@
     (doseq [key keys]
       (if-let [file (get-file key)]
         (let [[_ filename] (re-matches #"attachment; filename=\"(.*)\"" (:content-disposition file))]
-          (.putNextEntry zout (new ZipEntry (str (apply str (butlast (str/split filename #"\."))) "_" key "." (last (str/split filename #"\.")))))
+          (.putNextEntry zout (new ZipEntry (str (subs (str (str/join "." (butlast (str/split filename #"\."))) "_" key) 0 250) "." (last (str/split filename #"\.")))))
           (with-open [fin (:body file)]
             (io/copy fin zout))
           (.closeEntry zout)
