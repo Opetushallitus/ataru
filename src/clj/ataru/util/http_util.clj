2abd010ee82c41bf433fdb58ddac2a460f2da7a9
Mikko Siukola <mikko.siukola@cybercom.com>
2019-11-13 13:27:43 +0200

    SEC-83 Add CSRF cookie to rest calls made from backend

--- src/clj/ataru/util/http_util.clj
+++ src/clj/ataru/util/http_util.clj
@@ -2,10 +2,23 @@
   (:require [org.httpkit.client :as http]
             [taoensso.timbre :as log]))
 
+(defn add-cookie [opts cookie-to-add]
+  (let [existing-cookies (get (get opts :headers) "Cookie")
+        cookies  (if existing-cookies (str existing-cookies ";" cookie-to-add) cookie-to-add)]
+    (log/info "Adding cookie (" cookie-to-add ") to existing cookies (" existing-cookies "), result:" cookies )
+    (update opts :headers merge {"Cookie" cookies})))
+
+;todo maybe only add csrf cookie to PUT POST DELETE?
+(defn enrich-with-mandatory-headers [opts method]
+  (add-cookie (update opts :headers merge
+                      {"Caller-Id" "1.2.246.562.10.00000000001.ataru-editori.backend"}
+                      {"CSRF" "ataru"})
+              "CSRF=ataru"))
+
 (defn do-request
   [{:keys [url method] :as opts}]
-  (let [opts        (update opts :headers merge {"Caller-Id" "ataru"} {"CSRF" "ataru"})
-        method-name (clojure.string/upper-case (name method))
+  (let [method-name (clojure.string/upper-case (name method))
+        opts        (enrich-with-mandatory-headers opts method-name)
         start       (System/currentTimeMillis)
         response    @(http/request opts)
         time        (- (System/currentTimeMillis) start)
