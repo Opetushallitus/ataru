c470b92fb253c93f36a5421e8a30fd33f9d049f0
Mikko Siukola <mikko.siukola@cybercom.com>
2019-11-14 15:51:13 +0200

    SEC-83 Define csrf value, cleanup

--- src/clj/ataru/util/http_util.clj
+++ src/clj/ataru/util/http_util.clj
@@ -2,23 +2,25 @@
   (:require [org.httpkit.client :as http]
             [taoensso.timbre :as log]))
 
-(defn add-cookie [opts cookie-to-add]
+
+(defn add-cookie [cookie-to-add opts]
   (let [existing-cookies (get (get opts :headers) "Cookie")
-        cookies  (if existing-cookies (str existing-cookies ";" cookie-to-add) cookie-to-add)]
-    (log/info "Adding cookie (" cookie-to-add ") to existing cookies (" existing-cookies "), result:" cookies )
+        cookies  (if existing-cookies (str existing-cookies "; " cookie-to-add) cookie-to-add)]
     (update opts :headers merge {"Cookie" cookies})))
 
+(def csrf-value "ataru")
+
 ;todo maybe only add csrf cookie to PUT POST DELETE?
-(defn enrich-with-mandatory-headers [opts method]
-  (add-cookie (update opts :headers merge
+(defn enrich-with-mandatory-headers [opts]
+  (add-cookie (str "CSRF="csrf-value)
+              (update opts :headers merge
                       {"Caller-Id" "1.2.246.562.10.00000000001.ataru-editori.backend"}
-                      {"CSRF" "ataru"})
-              "CSRF=ataru"))
+                      {"CSRF" csrf-value})))
 
 (defn do-request
   [{:keys [url method] :as opts}]
   (let [method-name (clojure.string/upper-case (name method))
-        opts        (enrich-with-mandatory-headers opts method-name)
+        opts        (enrich-with-mandatory-headers opts)
         start       (System/currentTimeMillis)
         response    @(http/request opts)
         time        (- (System/currentTimeMillis) start)
