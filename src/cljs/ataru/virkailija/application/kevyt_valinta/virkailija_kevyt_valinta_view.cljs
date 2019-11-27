81d6c453a2092529c9f12171a1d195c99029b4cf
Esko Lahti <esko.lahti@reaktor.com>
2019-11-27 13:11:27 +0200

    Kevytvalinnan tarkistusmerkki omaksi komponentikseen

--- src/cljs/ataru/virkailija/application/kevyt_valinta/virkailija_kevyt_valinta_view.cljs
+++ src/cljs/ataru/virkailija/application/kevyt_valinta/virkailija_kevyt_valinta_view.cljs
@@ -11,6 +11,38 @@
                                lang)]
     [:span.application-handling__kevyt-valinta-value valinnan-tila-i18n]))
 
+(def ^:private checked-valinnan-tilat
+  #{"HYLATTY" "PERUUNTUNUT" "VARASIJALTA_HYVAKSYTTY" "HYVAKSYTTY" "PERUNUT" "PERUUTETTU"})
+
+(defn- kevyt-valinta-checkmark [kind]
+  (let [checkmark-class (case kind
+                          :checked
+                          "application-handling__kevyt-valinta-checkmark--checked"
+
+                          :unchecked
+                          "application-handling__kevyt-valinta-checkmark--unchecked"
+
+                          :grayed-out
+                          "application-handling__kevyt-valinta-checkmark--grayed-out")
+        show-checkmark? (= kind :checked)]
+    [:div.application-handling__kevyt-valinta-checkmark
+     {:class checkmark-class}
+     (when show-checkmark?
+       [:i.zmdi.zmdi-check.application-handling__kevyt-valinta-checkmark--bold])]))
+
+(defn- kevyt-valinta-valinnan-tila-checkmark []
+  (let [application-key @(re-frame/subscribe [:state-query [:application :selected-application-and-form :application :key]])
+        valinnan-tila   @(re-frame/subscribe [:virkailija-kevyt-valinta/valinnan-tila application-key])
+        kind            (cond (checked-valinnan-tilat valinnan-tila)
+                              :checked
+
+                              (= "VARALLA" valinnan-tila)
+                              :unchecked
+
+                              :else
+                              :grayed-out)]
+    [kevyt-valinta-checkmark kind]))
+
 (defn- kevyt-valinta-selection-state-row []
   (let [lang                  @(re-frame/subscribe [:editor/virkailija-lang])
         selection-state-label (->> review-states/hakukohde-review-types
@@ -20,8 +52,7 @@
                                           label-i18n))
                                    (map lang))]
     [:div.application-handling__kevyt-valinta-row
-     [:div.application-handling__kevyt-valinta-check.application-handling__kevyt-valinta-check--checked
-      [:i.zmdi.zmdi-check.application-handling__kevyt-valinta-check--bold]]
+     [kevyt-valinta-valinnan-tila-checkmark]
      [:div.application-handling__kevyt-valinta-label
       [:span selection-state-label]
       [:div.application-handling__kevyt-valinta-hr]]
