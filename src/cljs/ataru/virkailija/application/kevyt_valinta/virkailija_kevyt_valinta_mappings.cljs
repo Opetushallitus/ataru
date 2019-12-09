a368c531012d0f23a765f9b92e23023f06404fb7
Esko Lahti <esko.lahti@reaktor.com>
2019-12-09 16:45:45 +0200

    Kevytvalinnan ja valinta-tulos-servicen väliset muunnokset omaan nimiavaruuteen

---
+++ src/cljs/ataru/virkailija/application/kevyt_valinta/virkailija_kevyt_valinta_mappings.cljs
@@ -0,0 +1,7 @@
+(ns ataru.virkailija.application.kevyt-valinta.virkailija-kevyt-valinta-mappings)
+
+(defn kevyt-valinta-property->valinta-tulos-service-property [kevyt-valinta-property]
+  (case kevyt-valinta-property
+    :kevyt-valinta/valinnan-tila :valinnantila
+    :kevyt-valinta/julkaisun-tila :julkaistavissa
+    :kevyt-valinta/vastaanotto-tila :vastaanottotila))
