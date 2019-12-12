a4a63c20496ad5c04750ec480bbdfec3f9f2229c
Esko Lahti <esko.lahti@reaktor.com>
2019-12-12 13:26:16 +0200

    Määritetään kirjautumiset yleiskäyttöisissä komponenteissa

--- src/cljs/ataru/virkailija/application/kevyt_valinta/view/virkailija_kevyt_valinta_ilmoittautumisen_tila_view.cljs
+++ src/cljs/ataru/virkailija/application/kevyt_valinta/view/virkailija_kevyt_valinta_ilmoittautumisen_tila_view.cljs
@@ -1,55 +1,11 @@
 (ns ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-ilmoittautumisen-tila-view
-  (:require [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-common-view :as common-view]
-            [ataru.virkailija.application.kevyt-valinta.virkailija-kevyt-valinta-translations :as translations]
-            [re-frame.core :as re-frame]))
+  (:require [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-common-view :as common-view]))
 
-(defn- kevyt-valinta-ilmoittautumisen-tila-selection [hakukohde-oid
-                                                      application-key
-                                                      ilmoittautumisen-tila-selection-state
-                                                      ilmoittautumisen-tila
-                                                      lang]
-  (let [ilmoittautumisen-tilat      @(re-frame/subscribe [:virkailija-kevyt-valinta/allowed-kevyt-valinta-property-values
-                                                          :kevyt-valinta/ilmoittautumisen-tila
-                                                          application-key])
-        ilmoittautumisen-tilat-i18n (map (fn [ilmoittautumisen-tila]
-                                           {:value ilmoittautumisen-tila
-                                            :label (translations/kevyt-valinta-selection-label :kevyt-valinta/ilmoittautumisen-tila
-                                                                                               ilmoittautumisen-tila
-                                                                                               lang)})
-                                         ilmoittautumisen-tilat)
-        ilmoittautumisen-tila-i18n  (->> ilmoittautumisen-tilat-i18n
-                                         (filter (comp (partial = ilmoittautumisen-tila)
-                                                       :value))
-                                         (map :label)
-                                         (first))]
-    [common-view/kevyt-valinta-dropdown-selection
-     :kevyt-valinta/ilmoittautumisen-tila
-     ilmoittautumisen-tila-selection-state
-     ilmoittautumisen-tila-i18n
-     ilmoittautumisen-tilat-i18n
-     (partial common-view/on-kevyt-valinta-property-change
-              :kevyt-valinta/ilmoittautumisen-tila
-              hakukohde-oid
-              application-key)]))
+(defn- kevyt-valinta-ilmoittautumisen-tila-selection []
+  [common-view/kevyt-valinta-dropdown-selection :kevyt-valinta/ilmoittautumisen-tila])
 
-(defn kevyt-valinta-ilmoittautumisen-tila-row [hakukohde-oid
-                                               application-key
-                                               lang]
-  (let [ilmoittautumisen-tila                 @(re-frame/subscribe [:virkailija-kevyt-valinta/kevyt-valinta-property-state
-                                                                    :kevyt-valinta/ilmoittautumisen-tila
-                                                                    application-key])
-        ilmoittautumisen-tila-label           (translations/kevyt-valinta-review-type-label :kevyt-valinta/ilmoittautumisen-tila lang)
-        ilmoittautumisen-tila-selection-state @(re-frame/subscribe [:virkailija-kevyt-valinta/kevyt-valinta-selection-state
-                                                                    :kevyt-valinta/ilmoittautumisen-tila
-                                                                    application-key])]
-    [common-view/kevyt-valinta-row
-     ilmoittautumisen-tila-selection-state
-     [common-view/kevyt-valinta-checkmark :kevyt-valinta/ilmoittautumisen-tila application-key]
-     ilmoittautumisen-tila-label
-     [kevyt-valinta-ilmoittautumisen-tila-selection
-      hakukohde-oid
-      application-key
-      ilmoittautumisen-tila-selection-state
-      ilmoittautumisen-tila
-      lang]]))
+(defn kevyt-valinta-ilmoittautumisen-tila-row []
+  [common-view/kevyt-valinta-row
+   :kevyt-valinta/ilmoittautumisen-tila
+   [kevyt-valinta-ilmoittautumisen-tila-selection]])
 
--- src/cljs/ataru/virkailija/application/kevyt_valinta/view/virkailija_kevyt_valinta_view.cljs
+++ src/cljs/ataru/virkailija/application/kevyt_valinta/view/virkailija_kevyt_valinta_view.cljs
@@ -2,29 +2,11 @@
   (:require [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-valinnan-tila-view :as valinnan-tila]
             [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-julkaisun-tila-view :as julkaisun-tila]
             [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-vastaanotto-tila-view :as vastaanotto-tila]
-            [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-ilmoittautumisen-tila-view :as ilmoittautumisen-tila]
-            [re-frame.core :as re-frame])
-  (:require-macros [cljs.core.match :refer [match]]))
+            [ataru.virkailija.application.kevyt-valinta.view.virkailija-kevyt-valinta-ilmoittautumisen-tila-view :as ilmoittautumisen-tila]))
 
 (defn kevyt-valinta []
-  (let [application-key @(re-frame/subscribe [:state-query [:application :selected-application-and-form :application :key]])
-        ;; kevytvalinta näytetään ainoastaan, kun yksi hakukohde valittuna, ks. :virkailija-kevyt-valinta/show-kevyt-valinta?
-        hakukohde-oid   (first @(re-frame/subscribe [:state-query [:application :selected-review-hakukohde-oids]]))
-        lang            @(re-frame/subscribe [:editor/virkailija-lang])]
-    [:div.application-handling__kevyt-valinta
-     [valinnan-tila/kevyt-valinta-valinnan-tila-row
-      hakukohde-oid
-      application-key
-      lang]
-     [julkaisun-tila/kevyt-valinta-julkaisun-tila-row
-      hakukohde-oid
-      application-key
-      lang]
-     [vastaanotto-tila/kevyt-valinta-vastaanotto-tila-row
-      hakukohde-oid
-      application-key
-      lang]
-     [ilmoittautumisen-tila/kevyt-valinta-ilmoittautumisen-tila-row
-      hakukohde-oid
-      application-key
-      lang]]))
+  [:div.application-handling__kevyt-valinta
+   [valinnan-tila/kevyt-valinta-valinnan-tila-row]
+   [julkaisun-tila/kevyt-valinta-julkaisun-tila-row]
+   [vastaanotto-tila/kevyt-valinta-vastaanotto-tila-row]
+   [ilmoittautumisen-tila/kevyt-valinta-ilmoittautumisen-tila-row]])
