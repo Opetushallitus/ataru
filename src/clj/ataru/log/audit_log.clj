a80ffe8eb877fc23e7acb539870e98ee23698c5e
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-03-25 17:11:57 +0200

    Käytä uudelleenkäytettävää auditlog-käärijää

--- src/clj/ataru/log/audit_log.clj
+++ src/clj/ataru/log/audit_log.clj
@@ -1,21 +1,14 @@
 (ns ataru.log.audit-log
   (:require [ataru.util.app-utils :as app-utils]
             [ataru.config.core :refer [config]]
-            [cheshire.core :as json]
-            [taoensso.timbre :as timbre]
-            [environ.core :refer [env]]
             [clojure.data :refer [diff]]
-            [taoensso.timbre.appenders.core :refer [println-appender]]
-            [taoensso.timbre.appenders.3rd-party.rolling :refer [rolling-appender]])
+            [clj-timbre-auditlog.audit-log :as cta-audit-log])
   (:import [fi.vm.sade.auditlog
             Operation
             Changes$Builder
             Target$Builder
-            Logger
-            Audit
             ApplicationType
             User]
-           java.util.TimeZone
            java.net.InetAddress
            org.ietf.jgss.Oid))
 
@@ -29,40 +22,19 @@
 (def operation-delete (create-operation "poisto"))
 (def operation-login (create-operation "kirjautuminen"))
 
-(defn- create-audit-logger []
+(def ^:private logger
   (let [service-name     (case (app-utils/get-app-id)
                            :virkailija "ataru-editori"
-                           :hakija     "ataru-hakija"
+                           :hakija "ataru-hakija"
                            nil)
         base-path        (case (app-utils/get-app-id)
                            :virkailija (-> config :log :virkailija-base-path)
-                           :hakija     (-> config :log :hakija-base-path))
-        audit-log-config (assoc timbre/example-config
-                                :appenders {:file-appender   (assoc (rolling-appender
-                                                                     {:path    (str base-path
-                                                                                    "/audit_" service-name
-                                                                                    ;; Hostname will differentiate files in actual environments
-                                                                                    (when (:hostname env) (str "_" (:hostname env))))
-                                                                      :pattern :daily})
-                                                                    :output-fn (fn [data] (force (:msg_ data))))
-                                            :stdout-appender (assoc (println-appender
-                                                                     {:stream :std-out})
-                                                                    :output-fn (fn [data]
-                                                                                 (json/generate-string
-                                                                                  {:eventType "audit"
-                                                                                   :timestamp (force (:timestamp_ data))
-                                                                                   :event     (json/parse-string (force (:msg_ data)))})))}
-                                :timestamp-opts {:pattern  "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
-                                                 :timezone (TimeZone/getTimeZone "Europe/Helsinki")})
-        logger           (proxy [Logger] [] (log [str]
-                                              (timbre/log* audit-log-config :info str)))
+                           :hakija (-> config :log :hakija-base-path))
         application-type (case (app-utils/get-app-id)
                            :virkailija ApplicationType/VIRKAILIJA
-                           :hakija     ApplicationType/OPPIJA
+                           :hakija ApplicationType/OPPIJA
                            ApplicationType/BACKEND)]
-    (new Audit logger service-name application-type)))
-
-(def ^:private logger (create-audit-logger))
+    (cta-audit-log/create-audit-logger service-name base-path application-type)))
 
 (defn- map-or-vec? [x]
   (or (map? x)
