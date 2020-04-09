50d545411223a64381665045533b1cd672a0fb12
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-04-09 10:54:14 +0300

    Parempi nimi
    Ei toiminnallisia muutoksia.

--- src/clj/ataru/util/http_util.clj
+++ src/clj/ataru/util/http_util.clj
@@ -5,7 +5,7 @@
 (def csrf-value "ataru")
 
 ;todo maybe only add csrf cookie to PUT POST DELETE?
-(defn enrich-with-mandatory-headers [opts]
+(defn enrich-with-mandatory-headers-and-common-settings [opts]
   (-> opts
       (update :connection-timeout (fnil identity 60000))
       (update :socket-timeout (fnil identity 60000))
@@ -17,7 +17,7 @@
 (defn do-request
   [{:keys [url method] :as opts}]
   (let [method-name (clojure.string/upper-case (name method))
-        opts        (enrich-with-mandatory-headers opts)
+        opts        (enrich-with-mandatory-headers-and-common-settings opts)
         start       (System/currentTimeMillis)
         response    (http-client/request opts)
         time        (- (System/currentTimeMillis) start)
