a2b6daaf91c2f441d0c774c5b2752490b2d3d063
oph-petar <petar@phz.fi>
2020-04-16 22:47:34 +0300

    Increase timeout to 300 sec

--- resources/spec/util.js
+++ resources/spec/util.js
@@ -42,7 +42,7 @@ const isRadioButton = ($e) => {
 
 const wait = {
   waitIntervalMs: 100,
-  testTimeoutDefault: 100000,
+  testTimeoutDefault: 300000,
   until: (condition, maxWaitMs, infoText) => {
     return () => {
       if (maxWaitMs == undefined) maxWaitMs = wait.testTimeoutDefault
