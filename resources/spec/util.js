70f7630947f77fe80496aab2eee66a35d98578fd
oph-petar <petar@phz.fi>
2020-04-17 10:23:23 +0300

    Set timeout to 15min

--- resources/spec/util.js
+++ resources/spec/util.js
@@ -42,7 +42,7 @@ const isRadioButton = ($e) => {
 
 const wait = {
   waitIntervalMs: 100,
-  testTimeoutDefault: 300000,
+  testTimeoutDefault: 900000,
   until: (condition, maxWaitMs, infoText) => {
     return () => {
       if (maxWaitMs == undefined) maxWaitMs = wait.testTimeoutDefault
