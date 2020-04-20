64eca3a7ddd6bf395bf57726171e596957226800
oph-petar <petar@phz.fi>
2020-04-20 08:46:29 +0300

    Wait differently for navigation completion

--- resources/spec/virkailijaApplicationHandlingSpec.js
+++ resources/spec/virkailijaApplicationHandlingSpec.js
@@ -1056,7 +1056,7 @@
       describe('filter by hakukohde and then open hakukohde details by pressing candidate name', () => {
         before(
           navigateToApplicationHandlingForHaku,
-          wait.until(hakukohdeRajausToggleButtonExists()),
+          wait.until(hakukohdeRajausToggleButtonExists),
           wait.until(() => {
             return hakukohdeRajausToggleButton().is(':visible')
           }),
