f968178ea8f9caf60487ba35c9f84caf00882889
Mikko Suonio <mikko.suonio@reaktor.com>
2020-05-06 21:34:57 +0300

    Käytä when if-lauseen sijaan

--- src/cljs/ataru/cljs_util.cljs
+++ src/cljs/ataru/cljs_util.cljs
@@ -24,7 +24,7 @@
   ([f timeout]
    (let [id (atom nil)]
      (fn [& args]
-       (if (not (nil? @id))
+       (when (not (nil? @id))
          (js/clearTimeout @id))
        (reset! id (js/setTimeout
                     (apply partial f args)
