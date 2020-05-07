a0eef78909ebef63d6044fbf68aafb877881fbc1
Esko Lahti <esko.lahti@reaktor.com>
2020-05-07 10:05:04 +0300

    Suomennettu muuttujien nimet

--- cypress/reitit.ts
+++ cypress/reitit.ts
@@ -1,21 +1,22 @@
 export const virkailija = {
-  haeLomakkeenEsikatseluOsoite: (formKey: string) =>
-    `/lomake-editori/api/preview/form/${formKey}?lang=fi`,
+  haeLomakkeenEsikatseluOsoite: (lomakkeenAvain: string) =>
+    `/lomake-editori/api/preview/form/${lomakkeenAvain}?lang=fi`,
 
   haeHakemuksenMuokkauksenKirjautumisenOsoite: () =>
     `/lomake-editori/auth/cas?ticket=DEVELOPER`,
 
   haeUudenLomakkeenLahettamisenOsoite: () => `/lomake-editori/api/forms`,
 
-  haeLomakkeenMuuttamisenOsoite: (formId: number) =>
-    `/lomake-editori/api/forms/${formId}`,
+  haeLomakkeenMuuttamisenOsoite: (lomakkeenId: number) =>
+    `/lomake-editori/api/forms/${lomakkeenId}`,
 }
 
 export const hakija = {
-  haeHakijanNakymanOsoite: (formKey: string) => `/hakemus/${formKey}`,
+  haeHakijanNakymanOsoite: (lomakkeenAvain: string) =>
+    `/hakemus/${lomakkeenAvain}`,
 
-  haeLomakkeenHaunOsoite: (formKey: string) =>
-    `/hakemus/api/form/${formKey}?role=hakija`,
+  haeLomakkeenHaunOsoite: (lomakkeenAvain: string) =>
+    `/hakemus/api/form/${lomakkeenAvain}?role=hakija`,
 }
 
 export const cypress = {
