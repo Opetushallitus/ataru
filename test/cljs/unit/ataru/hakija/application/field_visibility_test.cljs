73a4505b80034dab1b5e695e943c15b6561fe873
Mikko Suonio <mikko.suonio@reaktor.com>
2020-05-15 17:10:06 +0300

    HLE-1192 : Tarkasta testissä vain tarpeellinen osa db:stä

--- test/cljs/unit/ataru/hakija/application/field_visibility_test.cljs
+++ test/cljs/unit/ataru/hakija/application/field_visibility_test.cljs
@@ -2,9 +2,21 @@
   (:require [clojure.test :refer [deftest is testing]]
             [ataru.hakija.application.field-visibility :as field-visibility]))
 
+(defn- ui-of [db]
+  (get-in db [:application :ui]))
+
 (deftest set-field-visibility-for-plain-field-test
-  (is (= {:application {:ui {:kysymys {:visible? true}}}}
-         (field-visibility/set-field-visibility {} {:id "kysymys"}))))
+  (testing "answers stays intact:"
+    (is (= {:application {:answers {:kysymys {:value "0"}}}}
+           (update-in
+             (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
+                                                    {:id "kysymys"})
+             [:application]
+             select-keys [:answers]))))
+  (testing "field visibility:"
+    (is (= {:kysymys {:visible? true}}
+           (ui-of
+             (field-visibility/set-field-visibility {} {:id "kysymys"}))))))
 
 (deftest set-field-visibility-for-options-test
   (testing "single option:"
@@ -16,48 +28,48 @@
                                                    :fieldType "singleChoice"
                                                    :options   [{:value "0"}]}))))
   (testing "multiple options:"
-    (is (= {:application {:answers {:kysymys {:value "1"}}
-                          :ui      {:kysymys {0         {:visible? true}
-                                              1         {:visible? true}
-                                              :visible? true}}}}
-           (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "1"}}}}
-                                                  {:id        "kysymys"
-                                                   :fieldType "singleChoice"
-                                                   :options   [{:value "0"}
-                                                               {:value "1"}]})))))
+    (is (= {:kysymys {0         {:visible? true}
+                      1         {:visible? true}
+                      :visible? true}}
+           (ui-of
+             (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "1"}}}}
+                                                    {:id        "kysymys"
+                                                     :fieldType "singleChoice"
+                                                     :options   [{:value "0"}
+                                                                 {:value "1"}]}))))))
 
 (deftest set-field-visibility-for-option-followups-test
   (testing "single option, option selected, single followup:"
-    (is (= {:application {:answers {:kysymys {:value "0"}}
-                          :ui      {:kysymys      {0         {:visible? true}
-                                                   :visible? true}
-                                    :jatkokysymys {:visible? true}}}}
-           (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
-                                                  {:id        "kysymys"
-                                                   :fieldType "singleChoice"
-                                                   :options   [{:value     "0"
-                                                                :followups [{:id "jatkokysymys"}]}]}))))
+    (is (= {:kysymys      {0         {:visible? true}
+                           :visible? true}
+            :jatkokysymys {:visible? true}}
+           (ui-of
+             (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
+                                                    {:id        "kysymys"
+                                                     :fieldType "singleChoice"
+                                                     :options   [{:value     "0"
+                                                                  :followups [{:id "jatkokysymys"}]}]})))))
   (testing "multiple options, option not selected, single followup:"
-    (is (= {:application {:answers {:kysymys {:value "0"}}
-                          :ui      {:kysymys      {0         {:visible? true}
-                                                   1         {:visible? true}
-                                                   :visible? true}
-                                    :jatkokysymys {:visible? false}}}}
-           (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
-                                                  {:id        "kysymys"
-                                                   :fieldType "singleChoice"
-                                                   :options   [{:value "0"}
-                                                               {:value     "1"
-                                                                :followups [{:id "jatkokysymys"}]}]}))))
+    (is (= {:kysymys      {0         {:visible? true}
+                           1         {:visible? true}
+                           :visible? true}
+            :jatkokysymys {:visible? false}}
+           (ui-of
+             (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
+                                                    {:id        "kysymys"
+                                                     :fieldType "singleChoice"
+                                                     :options   [{:value "0"}
+                                                                 {:value     "1"
+                                                                  :followups [{:id "jatkokysymys"}]}]})))))
   (testing "single option, option selected, multiple followups:"
-    (is (= {:application {:answers {:kysymys {:value "0"}}
-                          :ui      {:kysymys   {0         {:visible? true}
-                                                :visible? true}
-                                    :jatkokysymys-1 {:visible? true}
-                                    :jatkokysymys-2 {:visible? true}}}}
-           (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
-                                                  {:id        "kysymys"
-                                                   :fieldType "singleChoice"
-                                                   :options   [{:value     "0"
-                                                                :followups [{:id "jatkokysymys-1"}
-                                                                            {:id "jatkokysymys-2"}]}]})))))
+    (is (= {:kysymys        {0         {:visible? true}
+                             :visible? true}
+            :jatkokysymys-1 {:visible? true}
+            :jatkokysymys-2 {:visible? true}}
+           (ui-of
+             (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
+                                                    {:id        "kysymys"
+                                                     :fieldType "singleChoice"
+                                                     :options   [{:value     "0"
+                                                                  :followups [{:id "jatkokysymys-1"}
+                                                                              {:id "jatkokysymys-2"}]}]}))))))
