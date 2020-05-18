a82e394385800001f7a6f63f74d518957acc78d8
Mikko Suonio <mikko.suonio@reaktor.com>
2020-05-18 11:44:36 +0300

    HLE-1192 : Testaa relevantit kysymystyypit

--- test/cljs/unit/ataru/hakija/application/field_visibility_test.cljs
+++ test/cljs/unit/ataru/hakija/application/field_visibility_test.cljs
@@ -37,7 +37,7 @@
                                                                {:value "1"}]}))))))
 
 (deftest set-field-visibility-for-option-followups-test
-  (testing "single option, option selected, single followup: all field types:"
+  (testing "single option, option selected, single followup: relevant field types:"
     (are [field-type]
       (= {:kysymys      {0         {:visible? true}
                          :visible? true}
@@ -48,9 +48,9 @@
                                                    :fieldType field-type
                                                    :options   [{:value     "0"
                                                                 :followups [{:id "jatkokysymys"}]}]})))
-      "singleChoice"
       "dropdown"
-      "multipleChoice"))
+      "multipleChoice"
+      "singleChoice"))
   (testing "multiple options, option not selected, single followup:"
     (is (= {:kysymys      {0         {:visible? true}
                            1         {:visible? true}
@@ -63,15 +63,19 @@
                                                      :options   [{:value "0"}
                                                                  {:value     "1"
                                                                   :followups [{:id "jatkokysymys"}]}]})))))
-  (testing "single option, option selected, multiple followups:"
-    (is (= {:kysymys        {0         {:visible? true}
-                             :visible? true}
-            :jatkokysymys-1 {:visible? true}
-            :jatkokysymys-2 {:visible? true}}
-           (ui-of
-             (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
-                                                    {:id        "kysymys"
-                                                     :fieldType "singleChoice"
-                                                     :options   [{:value     "0"
-                                                                  :followups [{:id "jatkokysymys-1"}
-                                                                              {:id "jatkokysymys-2"}]}]}))))))
+  (testing "single option, option selected, multiple followups: relevant field types:"
+    (are [fieldType]
+      (= {:kysymys        {0         {:visible? true}
+                           :visible? true}
+          :jatkokysymys-1 {:visible? true}
+          :jatkokysymys-2 {:visible? true}}
+         (ui-of
+           (field-visibility/set-field-visibility {:application {:answers {:kysymys {:value "0"}}}}
+                                                  {:id        "kysymys"
+                                                   :fieldType fieldType
+                                                   :options   [{:value     "0"
+                                                                :followups [{:id "jatkokysymys-1"}
+                                                                            {:id "jatkokysymys-2"}]}]})))
+      "dropdown"
+      "multipleChoice"
+      "singleChoice")))
