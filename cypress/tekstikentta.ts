a2b93e956311869fc2e988679010259cf9a9e44d
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-06-26 11:46:49 +0300

    Tarkista jokainen syötetty merkki
    Tämä toivottavasti poistaa race conditionin hitaasti päivittyvien
    Reactin kontrolloimien tekstikenttien täyttämisestä testeissä. (Ongelma
    koskee lähinnä virkailijakäyttöliittymää.)

--- cypress/tekstikentta.ts
+++ cypress/tekstikentta.ts
@@ -1,9 +1,21 @@
-import * as asetukset from './asetukset'
-
 import Chainable = Cypress.Chainable
 
 export const syotaTeksti = <T>(
   elementti: Chainable<T>,
   teksti: string
-): Chainable<T> =>
-  elementti.clear().type(teksti, { delay: asetukset.tekstikentanSyotonViive })
+): Chainable<T> => {
+  return elementti.clear().then((tyhjennettyElementti) => {
+    const merkit = [...teksti]
+    return Cypress.Promise.all(
+      merkit.map((m, i) => {
+        elementti
+          .type(m, {
+            delay: 0,
+          })
+          .then(() => {
+            elementti.should('have.value', teksti.substring(0, i + 1))
+          })
+      })
+    ).then(() => tyhjennettyElementti)
+  })
+}
