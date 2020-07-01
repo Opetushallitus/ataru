158214fa9dee307575dc080b7a4f20babb4c91f4
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-07-01 10:19:36 +0300

    Odota, että kenttä tyhjenee
    Tämän pitäisi korjata tällainen satunnainen virhe:
    
    AssertionError: expected '<input.editor-form__form-name-input>' to have value 'T', but the value was 'Uusi lomakeT'

--- cypress/tekstikentta.ts
+++ cypress/tekstikentta.ts
@@ -24,18 +24,23 @@ export const syotaTeksti = <T>(
   elementti: Chainable<T>,
   teksti: string
 ): Chainable<T> => {
-  return elementti.clear().then((tyhjennettyElementti) => {
-    const merkit = [...teksti]
-    return Cypress.Promise.all(
-      merkit.map((m, i) => {
-        elementti
-          .type(m, {
-            delay: 0,
-          })
-          .then(() => {
-            elementti.should('have.value', teksti.substring(0, i + 1))
-          })
-      })
-    ).then(() => tyhjennettyElementti)
-  })
+  return elementti
+    .clear()
+    .then(() => {
+      elementti.should('have.value', '')
+    })
+    .then((tyhjennettyElementti) => {
+      const merkit = [...teksti]
+      return Cypress.Promise.all(
+        merkit.map((m, i) => {
+          elementti
+            .type(m, {
+              delay: 0,
+            })
+            .then(() => {
+              elementti.should('have.value', teksti.substring(0, i + 1))
+            })
+        })
+      ).then(() => tyhjennettyElementti)
+    })
 }
