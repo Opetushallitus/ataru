d7b76f5d52f4d9af6fea33bb2a63e5bd6d0f51cb
Mikko Suonio <mikko.suonio@reaktor.com>
2020-07-15 10:37:51 +0300

    HLE-1192 : Siirrä testin tiedostot alihakemistoon

--- cypress/integration/tekstikentanLisakysymysSpec.ts
+++ cypress/integration/tekstikentanLisakysymysSpec.ts
@@ -1,6 +1,6 @@
 import kirjautuminenVirkailijanNakymaan from '../testit/kirjautuminenVirkailijanNakymaan'
 import lomakkeenLuonti from '../testit/lomakkeenLuonti'
-import tekstikentanLisakysymyksenLisays from '../testit/komponentit/tekstikentta/tekstikentanLisakysymyksenLisays'
+import tekstikentanLisakysymyksenLisays from '../testit/lomake-elementit/tekstikentta/lomakkeet/tekstikentanLisakysymyksenLisays'
 
 describe('Tekstikentän lisäkysymys', () => {
   kirjautuminenVirkailijanNakymaan(() => {
