cca5d229365d5116f6232a009b3c53ad68640c65
Mikko Suonio <mikko.suonio@reaktor.com>
2020-07-28 16:42:45 +0300

    HLE-1207 : Salli suodatus liitepyynnöllä tai kysymyksellä

--- src/cljs/ataru/virkailija/application/application_list/virkailija_application_list_handlers.cljs
+++ src/cljs/ataru/virkailija/application/application_list/virkailija_application_list_handlers.cljs
@@ -1,5 +1,6 @@
 (ns ataru.virkailija.application.application-list.virkailija-application-list-handlers
   (:require [ataru.cljs-util :as cljs-util]
+            [ataru.util :as util]
             [ataru.virkailija.db :as initial-db]
             [re-frame.core :refer [reg-event-db reg-event-fx]]))
 
@@ -15,7 +16,8 @@
                    (assoc-in [:application :filters] (get-in db [:application :filters-checkboxes]))
                    (assoc-in [:application :ensisijaisesti?] (get-in db [:application :ensisijaisesti?-checkbox]))
                    (assoc-in [:application :rajaus-hakukohteella] (get-in db [:application :rajaus-hakukohteella-value]))
-                   (assoc-in [:application :attachment-review-states] (get-in db [:application :question-answer-filtering-options-value])))
+                   (assoc-in [:application :attachment-review-states] (get-in db [:application :attachment-review-states-value]))
+                   (assoc-in [:application :question-answer-filtering-options] (get-in db [:application :question-answer-filtering-options-value])))
      :dispatch [:application/reload-applications]}))
 
 (reg-event-fx
@@ -29,6 +31,8 @@
                    (assoc-in [:application :rajaus-hakukohteella] nil)
                    (assoc-in [:application :rajaus-hakukohteella-value] nil)
                    (assoc-in [:application :attachment-review-states] {})
+                   (assoc-in [:application :attachment-review-states-value] {})
+                   (assoc-in [:application :question-answer-filtering-options] {})
                    (assoc-in [:application :question-answer-filtering-options-value] {}))
      :dispatch [:application/reload-applications]}))
 
@@ -58,7 +62,8 @@
       (assoc-in [:application :filters-checkboxes] (get-in db [:application :filters]))
       (set-ensisijaisesti (get-in db [:application :ensisijaisesti?]))
       (set-rajaus-hakukohteella (get-in db [:application :rajaus-hakukohteella]))
-      (assoc-in [:application :question-answer-filtering-options-value] (get-in db [:application :attachment-review-states]))))
+      (assoc-in [:application :attachment-review-states-value] (get-in db [:application :attachment-review-states]))
+      (assoc-in [:application :question-answer-filtering-options-value] (get-in db [:application :question-answer-filtering-options]))))
 
 (reg-event-db
   :application/undo-filters
@@ -80,12 +85,37 @@
                              (assoc % :order-by column-id)))
      :dispatch [:application/reload-applications]}))
 
+(defn- init-question-answer-filtering-options [field]
+  (->> (:options field)
+       (map :value)
+       (mapv (fn [v] [v false]))
+       (into {})))
+
+(defn- form-field [db form-key field-id]
+  (-> db :forms (get form-key) util/form-fields-by-id (get (keyword field-id))))  ; TODO: Tämän voi varmasti tehdä fiksummin.
+
 (reg-event-db
-  :application/remove-filter-attachment
-  (fn [db [_ field-id]]
-    (update-in db [:application :question-answer-filtering-options-value] dissoc field-id)))
+  :application/add-question-filter
+  (fn [db [_ form-key field-id]]
+    (let [field (form-field db form-key field-id)]
+      (if (= (:fieldType field) "attachment")
+        (assoc-in db [:application :attachment-review-states-value field-id] initial-db/default-attachment-review-states)
+        (assoc-in db [:application :question-answer-filtering-options-value field-id] (init-question-answer-filtering-options field))))))
+
+(reg-event-db
+  :application/remove-question-filter
+  (fn [db [_ form-key field-id]]
+    (let [field (form-field db form-key field-id)]
+      (if (= (:fieldType field) "attachment")
+        (update-in db [:application :attachment-review-states-value] dissoc field-id)
+        (update-in db [:application :question-answer-filtering-options-value] dissoc field-id)))))
 
 (reg-event-db
   :application/set-filter-attachment-state
   (fn [db [_ field-id state value]]
-    (assoc-in db [:application :question-answer-filtering-options-value field-id state] value)))
+    (assoc-in db [:application :attachment-review-states-value field-id state] value)))
+
+(reg-event-db
+  :application/set-question-answer-filtering-options
+  (fn [db [_ field-id option value]]
+    (assoc-in db [:application :question-answer-filtering-options-value field-id option] value)))
--- src/cljs/ataru/virkailija/db.cljs
+++ src/cljs/ataru/virkailija/db.cljs
@@ -52,30 +52,31 @@
                                 :organizations       {:org-select-organizations    true
                                                       :org-select-hakukohde-groups true
                                                       :results-page                0}}
-                                        ; Initial active panel on page load.
+   ; Initial active panel on page load.
    :active-panel               :editor
-   :application                {:applications               []
-                                :applications-to-render     25
-                                :review-state-counts        (into {} (map #(vector (first %) 0) review-states/application-hakukohde-processing-states))
-                                :selection-state-counts     (into {} (map #(vector (first %) 0) review-states/application-hakukohde-selection-states))
-                                :attachment-state-counts    (into {} (map #(vector (first %) 0) review-states/attachment-hakukohde-review-types-with-no-requirements))
-                                :review                     {}
-                                :attachment-state-filter    (set (mapv first review-states/attachment-hakukohde-review-types-with-no-requirements))
-                                :processing-state-filter    (set (mapv first review-states/application-hakukohde-processing-states))
-                                :selection-state-filter     (set (mapv first review-states/application-hakukohde-selection-states))
-                                :attachment-review-states   {}  ; TODO : Tämä nimi kannattaisi muuttaa kuvaamaan, että kyse on filtteröintiehdosta. Tai koota filterit omaan alidokumenttiinsa
-                                :fetching-applications?     false
-                                :sort                       {:order-by "applicant-name"
-                                                             :order    "asc"}
-                                :selected-time-column       "created-time"
-                                :application-list-expanded? true
-                                :mass-information-request   {:form-status :disabled}
-                                :filters                    default-filters
-                                :filters-checkboxes         default-filters
-                                :ensisijaisesti?            false
-                                :ensisijaisesti?-checkbox   false
-                                :rajaus-hakukohteella       nil
-                                :rajaus-hakukohteella-value nil}
+   :application                {:applications                      []
+                                :applications-to-render            25
+                                :review-state-counts               (into {} (map #(vector (first %) 0) review-states/application-hakukohde-processing-states))
+                                :selection-state-counts            (into {} (map #(vector (first %) 0) review-states/application-hakukohde-selection-states))
+                                :attachment-state-counts           (into {} (map #(vector (first %) 0) review-states/attachment-hakukohde-review-types-with-no-requirements))
+                                :review                            {}
+                                :attachment-state-filter           (set (mapv first review-states/attachment-hakukohde-review-types-with-no-requirements))
+                                :processing-state-filter           (set (mapv first review-states/application-hakukohde-processing-states))
+                                :selection-state-filter            (set (mapv first review-states/application-hakukohde-selection-states))
+                                :attachment-review-states          {} ; TODO : Tämä nimi kannattaisi muuttaa kuvaamaan, että kyse on filtteröintiehdosta. Tai koota filterit omaan alidokumenttiinsa
+                                :question-answer-filtering-options {}
+                                :fetching-applications?            false
+                                :sort                              {:order-by "applicant-name"
+                                                                    :order    "asc"}
+                                :selected-time-column              "created-time"
+                                :application-list-expanded?        true
+                                :mass-information-request          {:form-status :disabled}
+                                :filters                           default-filters
+                                :filters-checkboxes                default-filters
+                                :ensisijaisesti?                   false
+                                :ensisijaisesti?-checkbox          false
+                                :rajaus-hakukohteella              nil
+                                :rajaus-hakukohteella-value        nil}
    :haut                       {}
    :hakukohteet                {}
    :fetching-haut              0
