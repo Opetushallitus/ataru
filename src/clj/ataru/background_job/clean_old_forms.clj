ffb6195e5e6242b6abd6f72a343c9324bbe3d58e
Mikko Siukola <mikko.siukola@cybercom.com>
2020-07-28 15:06:12 +0300

    OY-341 set daily limit to 1000 forms cleaned

--- src/clj/ataru/background_job/clean_old_forms.clj
+++ src/clj/ataru/background_job/clean_old_forms.clj
@@ -10,7 +10,7 @@
 (defn clean-old-forms-job-step [state _]
   (let [now (time/now)
         next-activation (time/plus (time/with-time-at-start-of-day now) (time/hours 26))
-        result (db/exec :db yesql-clean-up-old-forms! {:limit 500})]
+        result (db/exec :db yesql-clean-up-old-forms! {:limit 1000})]
     (log/info (str "clean-old-forms-job: total of " result " forms cleaned. Next activation at " next-activation))
     {:transition      {:id :to-next :step :initial}
      :updated-state   {:last-run-long (coerce/to-long now)}
