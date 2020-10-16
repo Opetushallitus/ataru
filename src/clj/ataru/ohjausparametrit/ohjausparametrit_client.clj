48c3f7418241b001f1eb4da53db45675977aa75a
Jani Rahkola <jani.rahkola@reaktor.com>
2020-10-16 11:19:07 +0300

    Poikkeus jos ohjausparametrit kutsu epäonnistuu

--- src/clj/ataru/ohjausparametrit/ohjausparametrit_client.clj
+++ src/clj/ataru/ohjausparametrit/ohjausparametrit_client.clj
@@ -1,12 +1,14 @@
 (ns ataru.ohjausparametrit.ohjausparametrit-client
   (:require [ataru.util.http-util :as hu]
             [ataru.config.url-helper :as url-helper]
-            [cheshire.core :as json]
-            [clj-time.coerce :as t]))
+            [cheshire.core :as json]))
 
 (defn get-ohjausparametrit [haku-oid]
-  (let [{:keys [status body]} (-> :ohjausparametrit-service.parametri
-                                  (url-helper/resolve-url haku-oid)
-                                  (hu/do-get))]
-    (when (= 200 status)
-      (json/parse-string body true))))
+  (let [url                   (url-helper/resolve-url :ohjausparametrit-service.parametri haku-oid)
+        {:keys [status body]} (hu/do-get url)]
+    (case status
+      200 (json/parse-string body true)
+      404 nil
+      (throw (new RuntimeException (str "Could not get " url ", "
+                                        "status: " status ", "
+                                        "body: " body))))))
