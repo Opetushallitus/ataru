fec9964ca393d52a0c805cdf3a0bb305cfc7fcb1
Jani Rahkola <jani.rahkola@reaktor.com>
2020-10-21 12:41:38 +0300

    Päivitä carmine

--- src/clj/ataru/redis.clj
+++ src/clj/ataru/redis.clj
@@ -1,12 +1,16 @@
 (ns ataru.redis
   (:require [com.stuartsierra.component :as component]
             [taoensso.carmine.connections :as carcon]
+            [taoensso.nippy :refer [*thaw-serializable-allowlist*]]
             [taoensso.timbre :as log]
             [ataru.config.core :refer [config]]))
 
 (defrecord Redis [connection-opts]
   component/Lifecycle
   (start [this]
+    (alter-var-root
+     #'*thaw-serializable-allowlist*
+     (constantly #{"*"}))
     (if (nil? connection-opts)
       (let [uri (get-in config [:redis :uri])]
         (log/info "Redis" uri)
