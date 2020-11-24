3450d94cb92c8b9c1b954049262c3984c5da1db3
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-11-24 15:06:45 +0200

    fake oids -> realistic looking oids (to pass oid validation during tests)

--- spec/ataru/virkailija/authentication/virkailija_edit_spec.clj
+++ spec/ataru/virkailija/authentication/virkailija_edit_spec.clj
@@ -1,9 +1,10 @@
 (ns ataru.virkailija.authentication.virkailija-edit-spec
-  (:require [speclj.core :refer :all]
+  (:require [speclj.core :refer [before tags describe it should=]]
             [ataru.virkailija.authentication.virkailija-edit :as virkailija-edit]
             [yesql.core :as sql]
             [ataru.db.db :as db]))
 
+(declare yesql-upsert-virkailija<!)
 (sql/defqueries "sql/virkailija-queries.sql")
 
 (describe "virkailija edit"
@@ -11,12 +12,12 @@
 
   ; We can do this here, since in reality we can assume that auth/login has been made
   ; for every logged in user, which upserts virkailija.
-  (before (db/exec :db yesql-upsert-virkailija<! {:oid        "1213"
+  (before (db/exec :db yesql-upsert-virkailija<! {:oid        "1.2.246.562.24.00000001213"
                                                   :first_name "Hemuli"
                                                   :last_name  "Hemuli?"}))
 
   (it "creates virkailija credentials"
-    (let [secret (virkailija-edit/create-virkailija-update-secret {:identity {:oid        "1213"
+    (let [secret (virkailija-edit/create-virkailija-update-secret {:identity {:oid        "1.2.246.562.24.00000001213"
                                                                               :username   "hhemuli"
                                                                               :first-name "Hemuli"
                                                                               :last-name  "Hemuli?"}}
@@ -24,7 +25,7 @@
       (should= true (virkailija-edit/virkailija-update-secret-valid? secret))))
 
   (it "should invalidate credentials"
-    (let [secret (virkailija-edit/create-virkailija-update-secret {:identity {:oid        "1213"
+    (let [secret (virkailija-edit/create-virkailija-update-secret {:identity {:oid        "1.2.246.562.24.00000001213"
                                                                               :username   "hhemuli"
                                                                               :first-name "Hemuli"
                                                                               :last-name  "Hemuli?"}}
