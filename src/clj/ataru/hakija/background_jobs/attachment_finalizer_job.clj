71d572c3be63d0f6b39afb80c8c6ecdc8e383fca
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-12-01 13:07:53 +0200

    liiteri finalize with cas

--- src/clj/ataru/hakija/background_jobs/attachment_finalizer_job.clj
+++ src/clj/ataru/hakija/background_jobs/attachment_finalizer_job.clj
@@ -1,12 +1,11 @@
 (ns ataru.hakija.background-jobs.attachment-finalizer-job
   (:require [ataru.applications.application-store :as application-store]
-            [ataru.config.core :refer [config]]
+            [ataru.cas.client :as cas]
             [ataru.config.url-helper :refer [resolve-url]]
-            [ataru.util.http-util :as http-util]
             [cheshire.core :as json]
             [taoensso.timbre :as log]))
 
-(defn finalize-attachments [{:keys [application-id]} _]
+(defn finalize-attachments [{:keys [application-id]} {:keys [liiteri-cas-client]}]
   (let [application    (application-store/get-application application-id)
         attachment-ids (->> application
                             :answers
@@ -15,9 +14,9 @@
                             flatten
                             (remove nil?))]
     (when (> (count attachment-ids) 0)
-      (let [response (http-util/do-post (resolve-url :liiteri.finalize)
-                                        {:headers {"Content-Type" "application/json"}
-                                         :body    (json/generate-string {:keys attachment-ids})})]
+      (let [response (cas/cas-authenticated-post liiteri-cas-client
+                                                 (resolve-url :liiteri.finalize)
+                                                 (json/generate-string {:keys attachment-ids}))]
         (when (not= 200 (:status response))
           (throw (Exception. (str "Could not finalize attachments for application " application-id))))
         (log/info (str "Finalized attachments for application " application-id))))
