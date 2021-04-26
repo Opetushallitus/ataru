b05a4c8f8861de0a8920ef6e7a4b43af58a8b3b9
Risto Salama <risto.salama@gofore.com>
2021-04-26 10:51:16 +0300

    Increase logging

--- src/clj/ataru/haku/haku_service.clj
+++ src/clj/ataru/haku/haku_service.clj
@@ -112,27 +112,40 @@
    get-haut-cache
    session
    show-hakukierros-paattynyt?]
-  (let [tarjonta-haut (get-tarjonta-haut ohjausparametrit-service
-                                         organization-service
-                                         tarjonta-service
-                                         get-haut-cache
-                                         session
-                                         show-hakukierros-paattynyt?)]
-    {:tarjonta-haut    tarjonta-haut
-     :direct-form-haut (get-direct-form-haut organization-service get-haut-cache session)
-     :haut             (->> (keys tarjonta-haut)
-                            (keep #(tarjonta/get-haku tarjonta-service %))
-                            (util/group-by-first :oid))
-     :hakukohteet      (->> (keys tarjonta-haut)
+  (let [startedHaku (System/currentTimeMillis)]
+    (log/info (str "!!!!Started haku at "  (quot startedHaku 1000) " s"))
+    (let [tarjonta-haut (get-tarjonta-haut ohjausparametrit-service
+                                           organization-service
+                                           tarjonta-service
+                                           get-haut-cache
+                                           session
+                                           show-hakukierros-paattynyt?)]
+      (log/info (str "!!!!Time passed after fetching tarjonta-haut: " (quot (- (System/currentTimeMillis) startedHaku) 1000)  " s, amount " (count tarjonta-haut)))
+      (let [direct-form-haut (get-direct-form-haut organization-service get-haut-cache session)]
+        (log/info (str "!!!!Time passed after fetching direct-form-haut: " (quot (- (System/currentTimeMillis) startedHaku) 1000)  " s, amount " (count direct-form-haut)))
+        (let [haut (->> (keys tarjonta-haut)
+                     (keep #(tarjonta/get-haku tarjonta-service %))
+                     (util/group-by-first :oid))]
+          (log/info (str "!!!!Time passed after fetching haut: " (quot (- (System/currentTimeMillis) startedHaku) 1000)  " s, amount " (count haut)))
+          (let [hakukohteet (->> (keys tarjonta-haut)
                             (mapcat #(tarjonta/hakukohde-search
                                        tarjonta-service
                                        %
                                        nil))
-                            (map (fn [hakukohde]
+                            (map (fn [{hakukohde-oid :oid :as hakukohde}]
                                    (assoc
                                      hakukohde
-                                     :selection-state-used false)))
-                            (util/group-by-first :oid))
-     :hakukohderyhmat  (util/group-by-first
-                         :oid
-                         (filter :active? (organization-service/get-hakukohde-groups organization-service)))}))
+                                     :selection-state-used
+                                     (hakukohde-store/selection-state-used-in-hakukohde? hakukohde-oid)
+                                     )))
+                            (util/group-by-first :oid))]
+            (log/info (str "!!!!Time passed after fetching hakukohteet: " (quot (- (System/currentTimeMillis) startedHaku) 1000)  " s, amount " (count hakukohteet)))
+            (let [hakukohderyhmat (util/group-by-first
+                             :oid
+                             (filter :active? (organization-service/get-hakukohde-groups organization-service)))]
+              (log/info (str "!!!!Time passed after fetching hakukohderyhmat: " (quot (- (System/currentTimeMillis) startedHaku) 1000)  " s, amount " (count hakukohderyhmat)))
+              {:tarjonta-haut    tarjonta-haut
+               :direct-form-haut direct-form-haut
+               :haut             haut
+               :hakukohteet      hakukohteet
+               :hakukohderyhmat  hakukohderyhmat})))))))
