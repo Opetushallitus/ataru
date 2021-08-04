d96e8533c424aeca593297294f510785d1f6e72c
Topi Penttinen <topi.penttinen@reaktor.com>
2021-08-04 12:19:29 +0300

    HLE-1319 Luo rajapinta koulutustyypeille

--- src/clj/ataru/tarjonta_service/tarjonta_parser.clj
+++ src/clj/ataru/tarjonta_service/tarjonta_parser.clj
@@ -1,6 +1,6 @@
 (ns ataru.tarjonta-service.tarjonta-parser
   (:require [clj-time.core :as t]
-            [taoensso.timbre :as log]
+            [clojure.string]
             [ataru.tarjonta-service.hakuaika :as hakuaika]
             [ataru.koodisto.koodisto :refer [get-koodisto-options]]
             [ataru.organization-service.organization-service :as organization-service]
@@ -8,7 +8,7 @@
             [ataru.ohjausparametrit.ohjausparametrit-protocol :as ohjausparametrit-protocol]))
 
 (defn- parse-hakukohde
-  [tarjonta-service
+  [_
    now
    hakukohderyhmat
    haku
@@ -29,6 +29,7 @@
      :koulutukset                                                 (mapv #(or (get tarjonta-koulutukset %)
                                                                              (throw (new RuntimeException (str "Koulutus " % " not found"))))
                                                                         (:koulutus-oids hakukohde))
+     :koulutustyypit                                              (:koulutustyypit hakukohde)
      :hakuaika                                                    (hakuaika/hakukohteen-hakuaika now haku ohjausparametrit hakukohde)
      :applicable-base-educations                                  (mapcat pohjakoulutukset-by-vaatimus (:hakukelpoisuusvaatimus-uris hakukohde))
      :jos-ylioppilastutkinto-ei-muita-pohjakoulutusliitepyyntoja? (boolean (:jos-ylioppilastutkinto-ei-muita-pohjakoulutusliitepyyntoja? hakukohde))}))
@@ -88,6 +89,7 @@
            :max-hakukohteet                  (:max-hakukohteet haku)
            :hakuaika                         (hakuaika/haun-hakuaika now haku ohjausparametrit)
            :can-submit-multiple-applications (:can-submit-multiple-applications haku)
+           :kohdejoukko-uri                  (:kohdejoukko-uri haku)
            :yhteishaku                       (:yhteishaku haku)}}))))
   ([koodisto-cache tarjonta-service organization-service ohjausparametrit-service haku-oid]
    (when haku-oid
