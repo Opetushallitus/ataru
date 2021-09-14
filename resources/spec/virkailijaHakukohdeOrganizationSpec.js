944c03ce43c91f127f0802620de690fcde7b6be0
Jaakko Ketola <jaakko.ketola@rebase.fi>
2021-09-14 14:52:15 +0300

    Fix virkailija spec tests

--- resources/spec/virkailijaHakukohdeOrganizationSpec.js
+++ resources/spec/virkailijaHakukohdeOrganizationSpec.js
@@ -29,26 +29,6 @@
     )
   }
 
-  const formComponents = () => {
-    return (
-      testFrame()
-        .find('.editor-form__component-wrapper')
-        // exclude followup question components
-        .not(
-          '.editor-form__followup-question-overlay .editor-form__component-wrapper'
-        )
-        // exclude hakukohteet
-        .not(
-          (i, node) => $(node).find("header:contains('Hakukohteet')").length > 0
-        )
-        // exclude henkilötiedot
-        .not(
-          (i, node) =>
-            $(node).find("header:contains('Henkilötiedot')").length > 0
-        )
-    )
-  }
-
   const formSections = () => {
     return testFrame()
       .find('.editor-form__component-wrapper')
