ae71417e3a55dff6c2b308cdc2500367230ed87c
Jaakko Ketola <jaakko.ketola@rebase.fi>
2021-10-18 13:12:37 +0300

    HLE-1453 Handle followups per hakukohde

--- src/cljs/ataru/hakija/handlers_util.cljs
+++ src/cljs/ataru/hakija/handlers_util.cljs
@@ -8,9 +8,30 @@
              selected-hakukohde (some #(when (= (:oid %) hakukohde-oid) %) tarjonta-hakukohteet)]
          (some is-ryhma-in-hakukohderyhmat (:hakukohderyhmat selected-hakukohde))))
 
+(declare change-followups-for-question)
+
+(defn- change-followup-id
+  [followup hakukohde-oid]
+  (-> followup
+    (assoc :id (str (:id followup) "_" hakukohde-oid))
+    (change-followups-for-question hakukohde-oid)))
+
+(defn- change-followups-for-option
+  [option hakukohde-oid]
+  (if-let [followups (seq (:followups option))]
+    (assoc option :followups (map #(change-followup-id % hakukohde-oid) followups))
+    option))
+
+(defn- change-followups-for-question
+  [question hakukohde-oid]
+  (if-let [options (seq (:options question))]
+    (assoc question :options (map #(change-followups-for-option % hakukohde-oid) options))
+    question))
+
 (defn- create-duplicate-question
   [hakukohde-oid question]
   (-> question
+      (change-followups-for-question hakukohde-oid)
       (dissoc :per-hakukohde)
       (assoc :id (str (:id question) "_" hakukohde-oid)
              :duplikoitu-kysymys-hakukohde-oid hakukohde-oid
