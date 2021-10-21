a2b064fd74b6ee1f1b39aaed7ce2ab47de68fbf1
Mikko Siukola <mikko.siukola@cybercom.com>
2021-10-21 15:07:58 +0300

    OY-3112 Also track errors for whole hakemus case

--- src/cljs/ataru/virkailija/application/hyvaksynnan_ehto/subs.cljs
+++ src/cljs/ataru/virkailija/application/hyvaksynnan_ehto/subs.cljs
@@ -31,8 +31,9 @@
     [(re-frame/subscribe [:state-query [:hyvaksynnan-ehto application-key]])
      (re-frame/subscribe [:state-query [:application :selected-review-hakukohde-oids]])])
   (fn [[hyvaksynnan-ehdot hakukohde-oids]]
+      (js/console.log (str "hyvaksynnan-ehto/errors " hyvaksynnan-ehdot ": " (conj hakukohde-oids "kaikki-hakukohteet")))
     (into []
-          (comp (hx/filter-hyvaksynnan-ehdot-for-correct-hakukohde hakukohde-oids)
+          (comp (hx/filter-hyvaksynnan-ehdot-for-correct-hakukohde (conj hakukohde-oids "kaikki-hakukohteet"))
                 (map hyvaksynnan-ehto-error)
                 (filter (comp not nil?)))
           hyvaksynnan-ehdot)))
