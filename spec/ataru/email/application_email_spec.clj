ec963aa2a17f3ba90d2ac659c095f2e0ed5bcdba
Risto Salama <risto.salama@gofore.com>
2021-12-15 16:34:58 +0200

    HLE-1498: Fix attachment request not seen in confirmation email

--- spec/ataru/email/application_email_spec.clj
+++ spec/ataru/email/application_email_spec.clj
@@ -29,7 +29,7 @@
         (should= true (str/includes? body "Hius- ja kauneudenhoitoalan perustutkinto - Stadin ammatti- ja aikuisopisto, Sturenkadun toimipaikka"))
         (should= "no-reply@opintopolku.fi" (:from email))))
 
-  (it "creates email with attachment notifications"
+  (it "creates email with per-hakukohde attachment notifications"
       (let [template-name-fn (constantly "templates/email_submit_confirmation_template_fi.html")
             application-attachment-reviews []
             get-attachment-type (fn [attachment-type] {:fi attachment-type
@@ -51,4 +51,45 @@
         (should= true (str/includes? body "Palautettava viimeistään 31.1.2022 klo 12:00"))
         (should= true (str/includes? body "Palautettava viimeistään 30.5.2022 klo 13:05"))
         (should= true (str/includes? body "Tai käytä: <a href='https://hampaat-liitteena.fi'>https://hampaat-liitteena.fi</a>"))
-      )))
+      ))
+
+  (it "creates email with regular attachment notifications"
+      (let [template-name-fn (constantly "templates/email_submit_confirmation_template_fi.html")
+            application-attachment-reviews [{:attachment-key "9b00783c-5f4e-4ef9-bca4-c2e57b443d3c"}
+                                            {:attachment-key "98655824-bb9d-4f4a-a1e5-4e39bd0f61f0"}]
+            get-attachment-type (fn [attachment-type] {:fi attachment-type
+                                                       :sv attachment-type
+                                                       :en attachment-type})
+            [email] (email/create-emails email/edit-email-subjects
+                                         template-name-fn
+                                         fixtures/application
+                                         fixtures/tarjonta-info
+                                         fixtures/form
+                                         application-attachment-reviews
+                                         fixtures/email-template
+                                         get-attachment-type
+                                         false)
+            body    (:body email)]
+          (should= true (str/includes? body "Upload liite"))
+          (should= true (str/includes? body "Perinteinen liitepyyntö"))))
+
+  (it "does not ask for attachment notification if attachment key is provided"
+    (let [template-name-fn (constantly "templates/email_submit_confirmation_template_fi.html")
+          application-attachment-reviews [{:attachment-key "9b00783c-5f4e-4ef9-bca4-c2e57b443d3c"}
+                                          {:attachment-key "98655824-bb9d-4f4a-a1e5-4e39bd0f61f0"}]
+          get-attachment-type (fn [attachment-type] {:fi attachment-type
+                                                     :sv attachment-type
+                                                     :en attachment-type})
+          application (assoc-in fixtures/application [:answers 31 :value] "liite upattu")
+          [email] (email/create-emails email/edit-email-subjects
+                                       template-name-fn
+                                       application
+                                       fixtures/tarjonta-info
+                                       fixtures/form
+                                       application-attachment-reviews
+                                       fixtures/email-template
+                                       get-attachment-type
+                                       false)
+          body    (:body email)]
+      (should= false (str/includes? body "Upload liite"))
+      (should= true (str/includes? body "Perinteinen liitepyyntö")))))
