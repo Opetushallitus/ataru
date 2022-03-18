07203ae1c78ca0c62bb3b5d5b1b831777829b520
Risto Salama <risto.salama@gofore.com>
2022-03-18 22:28:26 +0200

    Add spec for question-util

---
+++ spec/ataru/applications/question_util_spec.clj
@@ -0,0 +1,53 @@
+(ns ataru.applications.question-util-spec
+  (:require [speclj.core :refer [it describe tags should=]]
+            [ataru.translations.texts :refer [base-education-2nd-module-texts]]
+            [ataru.applications.question-util :as qu :refer [kiinnostunut-oppisopimuskoulutuksesta-wrapper-label
+                                                             amm-kaksoistutkinto-wrapper-label
+                                                             lukio-kaksoistutkinto-wrapper-label
+                                                             sora-question-wrapper-label]]
+            [ataru.component-data.base-education-module-2nd :refer [base-education-choice-key base-education-wrapper-key]]))
+
+(def form-2nd-aste {"content" [{"id" "oppikeywrapper"
+                                "label" kiinnostunut-oppisopimuskoulutuksesta-wrapper-label
+                                "children" [{"id" "oppikey"}]}
+                               {"id" "ammkaksoistutkintowrapper"
+                                "label" amm-kaksoistutkinto-wrapper-label
+                                "children" [{"id" "amm2tutkintokey"}]}
+                               {"id" "lukkaksoistutkintowrapper"
+                                "label" lukio-kaksoistutkinto-wrapper-label
+                                "children" [{"id" "lukio2tutkintokey"}]}
+                               {"id" "sorawrapper"
+                                "label" sora-question-wrapper-label
+                                "children" [{"id" "terveys-key"}
+                                            {"id" "aiempi-key"}]}
+                               {"id" base-education-wrapper-key
+                                "children" [{"id" base-education-choice-key
+                                             "options" [{"followups" [{"id" "year-key1"
+                                                                       "label" (:year-of-graduation-question base-education-2nd-module-texts)}
+                                                                      {"id" "language-key1"
+                                                                       "label" (:study-language base-education-2nd-module-texts)}]}
+                                                        {"followups" [{"id" "year-key2"
+                                                                       "label" (:year-of-graduation-question base-education-2nd-module-texts)}
+                                                                      {"id" "language-key2"
+                                                                       "label" (:study-language base-education-2nd-module-texts)}]}]}]}]})
+
+(describe "question-util"
+          (tags :unit)
+          (describe "get-hakurekisteri-toinenaste-specific-questions"
+                    (it "returns oppisopimuskoulutus-key"
+                        (should= :oppikey (:oppisopimuskoulutus-key (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))
+
+                    (it "returns kaksoistutkinto keys"
+                        (should= ["amm2tutkintokey" "lukio2tutkintokey"] (:kaksoistutkinto-keys (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))
+
+                    (it "returns tutkintovuosi keys"
+                        (should= [:year-key1 :year-key2] (:tutkintovuosi-keys (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))
+
+                    (it "returns tutkintokieli keys"
+                        (should= [:language-key1 :language-key2] (:tutkintokieli-keys (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))
+
+                    (it "returns sora-aiempi key"
+                        (should= "aiempi-key" (:sora-aiempi-key (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))
+
+                    (it "returns sora-terveys key"
+                        (should= "terveys-key" (:sora-terveys-key (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))))
