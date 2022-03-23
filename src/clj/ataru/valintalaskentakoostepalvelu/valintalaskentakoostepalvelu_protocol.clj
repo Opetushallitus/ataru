1bcb8994d69734b6c34f967c0218b050d69f49a1
Risto Salama <risto.salama@gofore.com>
2022-03-23 14:34:57 +0200

    WIP get harkinnanvaraisuus from valintalaskentakoostepalvelu

--- src/clj/ataru/valintalaskentakoostepalvelu/valintalaskentakoostepalvelu_protocol.clj
+++ src/clj/ataru/valintalaskentakoostepalvelu/valintalaskentakoostepalvelu_protocol.clj
@@ -2,4 +2,5 @@
 
 (defprotocol ValintalaskentakoostepalveluService
   (hakukohde-uses-valintalaskenta? [this hakukohde-oid])
-  (opiskelijan-suoritukset [this haku-oid hakemus-oid]))
+  (opiskelijan-suoritukset [this haku-oid hakemus-oid])
+  (hakemusten-harkinnanvaraisuus-valintalaskennasta [this hakemukset-with-harkinnanvaraisuus]))
