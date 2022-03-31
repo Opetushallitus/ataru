f9c704fb37400d7f04e4686ad072b515fc9827ed
Risto Salama <risto.salama@gofore.com>
2022-03-31 14:45:26 +0300

    Display pohjakoulutus harkinnanvarainen on ui

--- src/cljs/ataru/virkailija/application/pohjakoulutus_toinen_aste/pohjakoulutus_toinen_aste_handlers.cljs
+++ src/cljs/ataru/virkailija/application/pohjakoulutus_toinen_aste/pohjakoulutus_toinen_aste_handlers.cljs
@@ -1,6 +1,8 @@
 (ns ataru.virkailija.application.pohjakoulutus-toinen-aste.pohjakoulutus-toinen-aste-handlers
   (:require [re-frame.core :as re-frame]
-            [ataru.tarjonta.haku :as haku]))
+            [ataru.tarjonta.haku :as haku]
+            [ataru.application.harkinnanvaraisuus-types :refer [harkinnanvaraisuus-yksilollistetty-matikka-aikka-types
+                                                                pohjakoulutus-harkinnanvarainen-types]]))
 
 (re-frame/reg-event-fx
   :application/fetch-applicant-pohjakoulutus
@@ -24,7 +26,36 @@
     (-> db
       (assoc-in [:application :pohjakoulutus-by-application-key application-key :error] true))))
 
+(re-frame/reg-event-fx
+  :application/fetch-applicant-harkinnanvaraisuus
+  (fn [_ [_ application-key]]
+    {:http {:method              :get
+            :path                (str "/lomake-editori/api/valintalaskentakoostepalvelu/harkinnanvaraisuus/hakemus/" application-key)
+            :handler-or-dispatch :application/handle-fetch-applicant-harkinnanvaraisuus-response
+            :handler-args        application-key
+            :id                  :fetch-applicant-harkinnanvaraisuus}}))
+
+(re-frame/reg-event-db
+  :application/handle-fetch-applicant-harkinnanvaraisuus-response
+  (fn [db [_ response application-key]]
+    (let [has-harkinnanvaraisuus-reason-in-group (fn [resp group]
+                                                   (->> resp
+                                                        (map :harkinnanvaraisuudenSyy)
+                                                        (some (fn [harkinnanvaraisuus]
+                                                                (some #{harkinnanvaraisuus} group)) )
+                                                        boolean))
+          yksilollistetty-matikka-aikka? (has-harkinnanvaraisuus-reason-in-group
+                                           response harkinnanvaraisuus-yksilollistetty-matikka-aikka-types)
+          harkinnanvarainen-pohjakoulutus? (has-harkinnanvaraisuus-reason-in-group
+                                             response pohjakoulutus-harkinnanvarainen-types)]
+    (-> db
+        (assoc-in [:application :harkinnanvarainen-pohjakoulutus-by-application-key application-key]
+                  harkinnanvarainen-pohjakoulutus?)
+        (assoc-in [:application :yksilollistetty-matikka-aikka-by-application-key application-key]
+                  yksilollistetty-matikka-aikka?)))))
+
 (defn create-fetch-applicant-pohjakoulutus-event-if-toisen-asteen-yhteishaku
   [application]
   (when (haku/toisen-asteen-yhteishaku? (:tarjonta application))
-    [:application/fetch-applicant-pohjakoulutus (:haku application) (:key application)]))
+    [:application/fetch-applicant-pohjakoulutus (:haku application) (:key application)]
+    [:application/fetch-applicant-harkinnanvaraisuus (:key application)]))
