11734c748d824afcda5e8c96fd8b73a8dc3e238e
Jaakko Ketola <jaakko.ketola@rebase.fi>
2022-04-14 14:36:48 +0300

    Fix tests

--- spec/ataru/applications/harkinnanvaraisuus/harkinnanvaraisuus_filter_spec.clj
+++ spec/ataru/applications/harkinnanvaraisuus/harkinnanvaraisuus_filter_spec.clj
@@ -21,77 +21,82 @@
   (it "returns empty vector when given empty vector"
     (let [input  []
           result (hf/filter-applications-by-harkinnanvaraisuus
-                   (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta [])
+                   (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta {})
                    input
                    harkinnanvaraisuus-only-filters)]
       (should= [] result)))
 
   (it "returns all applications if :only-harkinnanvaraiset flag is false"
-    (let [input  [{:id "application-1-id"}]
+    (let [input  [{:key "application-1-id"}]
           result (hf/filter-applications-by-harkinnanvaraisuus
-                   (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta [])
+                   (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta {})
                    input
                    {:harkinnanvaraisuus {:only-harkinnanvaraiset false}})]
       (should= input result)))
 
   (it "does not return application if it has no application-wide reason for harkinnanvaraisuus"
-    (let [input  [{:id "application-1-id"}]
+    (let [input  [{:key "application-1-id"}]
           result (hf/filter-applications-by-harkinnanvaraisuus
                    (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta
-                     [{:hakemusOid  "application-1-id"
+                     {"application-1-id"
+                      {:hakemusOid  "application-1-id"
                        :hakutoiveet [{:hakukohdeOid            "hakukohde-oid-1"
-                                      :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}])
+                                      :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}})
                    input
                    harkinnanvaraisuus-only-filters)]
       (should= [] result)))
 
   (it "returns application if it has an application-wide reason for harkinnanvaraisuus"
-    (let [input  [{:id "application-1-id"}]
+    (let [input  [{:key "application-1-id"}]
           result (hf/filter-applications-by-harkinnanvaraisuus
                    (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta
-                     [{:hakemusOid  "application-1-id"
+                     {"application-1-id"
+                      {:hakemusOid  "application-1-id"
                        :hakutoiveet [{:hakukohdeOid            "hakukohde-oid-1"
-                                      :harkinnanvaraisuudenSyy "ATARU_ULKOMAILLA_OPISKELTU"}]}])
+                                      :harkinnanvaraisuudenSyy "ATARU_ULKOMAILLA_OPISKELTU"}]}})
                    input
                    harkinnanvaraisuus-only-filters)]
       (should= input result)))
 
   (it "returns application if it has a harkinnanvaraisuus reason for some hakukohde"
-    (let [input  [{:id "application-1-id" :hakukohde ["hakukohde-oid-1" "hakukohde-oid-2"]}]
+    (let [input  [{:key "application-1-id" :hakukohde ["hakukohde-oid-1" "hakukohde-oid-2"]}]
           result (hf/filter-applications-by-harkinnanvaraisuus
                    (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta
-                     [{:hakemusOid  "application-1-id"
+                     {"application-1-id"
+                      {:hakemusOid  "application-1-id"
                        :hakutoiveet [{:hakukohdeOid            "hakukohde-oid-1"
                                       :harkinnanvaraisuudenSyy "ATARU_SOSIAALISET_SYYT"}
                                      {:hakukohdeOid            "hakukohde-oid-2"
-                                      :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}])
+                                      :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}})
                    input
                    harkinnanvaraisuus-only-filters)]
       (should= input result)))
 
   (describe "when filtering by hakukohteet as well"
     (it "returns application if it has a harkinnanvaraisuus reason for given hakukohde"
-      (let [input  [{:id "application-1-id" :hakukohde ["hakukohde-oid-1" "hakukohde-oid-2"]}]
+      (let [input  [{:key "application-1-id" :hakukohde ["hakukohde-oid-1" "hakukohde-oid-2"]}]
             result (hf/filter-applications-by-harkinnanvaraisuus
                      (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta
-                       [{:hakemusOid  "application-1-id"
+                       {"application-1-id"
+                        {:hakemusOid  "application-1-id"
                          :hakutoiveet [{:hakukohdeOid            "hakukohde-oid-1"
                                         :harkinnanvaraisuudenSyy "ATARU_SOSIAALISET_SYYT"}
                                        {:hakukohdeOid            "hakukohde-oid-2"
-                                        :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}])
+                                        :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}})
                      input
                      (assoc harkinnanvaraisuus-only-filters :selected-hakukohteet #{"hakukohde-oid-1"}))]
         (should= input result)))
 
     (it "does not return application if it has a harkinnanvaraisuus reason for another hakukohde"
-      (let [input  [{:id "application-1-id" :hakukohde ["hakukohde-oid-1" "hakukohde-oid-2"]}]
+      (let [input  [{:key "application-1-id" :hakukohde ["hakukohde-oid-1" "hakukohde-oid-2"]}]
             result (hf/filter-applications-by-harkinnanvaraisuus
                      (fake-hakemusten-harkinnanvaraisuus-valintalaskennasta
-                       [{:hakemusOid  "application-1-id"
+                       {"application-1-id"
+                        {:hakemusOid  "application-1-id"
                          :hakutoiveet [{:hakukohdeOid            "hakukohde-oid-1"
                                         :harkinnanvaraisuudenSyy "ATARU_SOSIAALISET_SYYT"}
                                        {:hakukohdeOid            "hakukohde-oid-2"
-                                        :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}])
+                                        :harkinnanvaraisuudenSyy "EI_HARKINNANVARAINEN"}]}})
                      input
                      (assoc harkinnanvaraisuus-only-filters :selected-hakukohteet #{"hakukohde-oid-2"}))]
         (should= [] result)))))
