e5e204b7492e36d0cb43530af5874f66207d5a68
Mikko Siukola <mikko.siukola@cybercom.com>
2022-05-04 15:59:17 +0300

    OY-3431 Optimizations for toisen asteen tietos

--- src/clj/ataru/valintalaskentakoostepalvelu/valintalaskentakoostepalvelu_protocol.clj
+++ src/clj/ataru/valintalaskentakoostepalvelu/valintalaskentakoostepalvelu_protocol.clj
@@ -3,4 +3,5 @@
 (defprotocol ValintalaskentakoostepalveluService
   (hakukohde-uses-valintalaskenta? [this hakukohde-oid])
   (opiskelijan-suoritukset [this haku-oid hakemus-oid])
+  (opiskelijoiden-suoritukset [this haku-oid hakemus-oids])
   (hakemusten-harkinnanvaraisuus-valintalaskennasta [this hakemus-oids]))
