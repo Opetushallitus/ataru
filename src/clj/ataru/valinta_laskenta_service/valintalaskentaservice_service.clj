73f2cc9b032c67adbfe9e239722a50a57ddc2bcd
Risto Salama <risto.salama@gofore.com>
2022-05-12 13:44:24 +0300

    Try to parse tulos and pisteet

--- src/clj/ataru/valinta_laskenta_service/valintalaskentaservice_service.clj
+++ src/clj/ataru/valinta_laskenta_service/valintalaskentaservice_service.clj
@@ -3,14 +3,35 @@
             [ataru.valinta-tulos-service.valintatulosservice-protocol :as vts]
             [ataru.valinta-laskenta-service.valintalaskentaservice-client :as client]))
 
+(defn- parse-pisteet
+  [pisteet hakukohde-oid]
+  (let [jarjestyskriteerit (->> (:hakukohteet pisteet)
+                                (filter #(= hakukohde-oid (:oid %)))
+                                first
+                                :valinnanvaihe
+                                (mapcat :valintatapajonot)
+                                (mapcat :jonosijat)
+                                (mapcat :jarjestyskriteerit))]
+    jarjestyskriteerit))
+
+(defn- parse-tulos
+  [tulos pisteet]
+  (let [hakutoiveet (->> (:hakutoiveet tulos)
+                         (map (fn [toive]
+                                {:oid (:hakukohdeOid toive)
+                                 :name (str (:hakukohdeNimi toive) " - " (:tarjoajaNimi toive))
+                                 :valintatila (:valintatila toive)
+                                 :vastaanottotila (:vastaanottotila toive)
+                                 :ilmoittautumistila (get-in toive [:ilmoittautumistila :ilmoittautumistila])
+                                 :pisteet (parse-pisteet pisteet (:hakukohdeOid toive))})))]
+    hakutoiveet))
+
 (defn- get-valinnan-ja-laskennan-tulos-hakemukselle
   [cas-client valinta-tulos-service haku-oid hakemus-oid]
-  (let [pisteet (future
-                (client/hakemuksen-laskennan-tiedot cas-client haku-oid hakemus-oid))
-        tulos (vts/valinnan-tulos-hakemukselle valinta-tulos-service haku-oid hakemus-oid)]
-    (prn tulos)
-    (prn @pisteet)
-    (apply merge {:pisteet @pisteet} {:tulos tulos})))
+  (let [tulos (future (vts/valinnan-tulos-hakemukselle valinta-tulos-service haku-oid hakemus-oid))
+        pisteet (client/hakemuksen-laskennan-tiedot cas-client haku-oid hakemus-oid)
+        parsed-tulos (parse-tulos @tulos pisteet)]
+    parsed-tulos))
 
 (defrecord RemoteValintaLaskentaService [cas-client valinta-tulos-service]
   ValintaLaskentaService
