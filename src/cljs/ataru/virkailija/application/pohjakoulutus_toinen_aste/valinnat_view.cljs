115e41911a8c73429b7c42e5a0fd6634b75a1f45
Risto Salama <risto.salama@gofore.com>
2022-05-16 14:15:11 +0300

    Styles for valinnat

--- src/cljs/ataru/virkailija/application/pohjakoulutus_toinen_aste/valinnat_view.cljs
+++ src/cljs/ataru/virkailija/application/pohjakoulutus_toinen_aste/valinnat_view.cljs
@@ -2,16 +2,17 @@
   (:require [re-frame.core :refer [subscribe]]
             [ataru.virkailija.application.pohjakoulutus-toinen-aste.pohjakoulutus-toinen-aste-view :refer [loading-indicator not-found error-loading]]))
 
-(defn- pisteet [lang hakukohde-oid pisteet]
+(defn- pisteet [lang hakukohde-oid nimet? pisteet]
   [:<>
     [:div.grade @(subscribe [:editor/virkailija-translation :scores])]
       (for [piste pisteet]
         ^{:key (str hakukohde-oid "-" (:tunniste piste))}
-        [:div.grade
-          [:span.grade__subject (lang (:nimi piste))]
-          (if (:localize-arvo piste)
-            [:span.grade__value @(subscribe [:editor/virkailija-translation (keyword (:arvo piste))])]
-            [:span.grade__value (:arvo piste)])])])
+        [:div.tulos
+          (if nimet?
+            [:span.tulos__subject (lang (:nimi piste))]
+            (if (:localize-arvo piste)
+              [:span.tulos__value @(subscribe [:editor/virkailija-translation (keyword (:arvo piste))])]
+              [:span.tulos__value (:arvo piste)]))])])
 
 (defn- valinnat-loaded []
   (let [valinnat (subscribe [:application/application-valinnat])
@@ -21,32 +22,41 @@
        (doall
          (map-indexed (fn [idx hakukohde]
            ^{:key (:oid hakukohde)}
-           [:<>
-             [:div.grade
-              [:span.grade__subject (str (+ idx 1) ". " (:name hakukohde))]]
-            (when (not (nil? (:kokonaispisteet hakukohde)))
-              [:div.grade
-               [:span.grade__subject "Valinnan kokonaispisteet"]
-               [:span.grade__value (:kokonaispisteet hakukohde)]])
-             [:div.grade
-              [:span.grade__subject "Sijoittelun tulos"]
-              [:span.grade__value @(subscribe [:editor/virkailija-translation (keyword (:valintatila hakukohde))])]]
-             [:div.grade
-              [:span.grade__subject "Vastaanottotieto"]
-              [:span.grade__value @(subscribe [:editor/virkailija-translation (keyword (:vastaanottotila hakukohde))])]]
-             [:div.grade
-              [:span.grade__subject "Ilmoittautumistila"]
-              [:span.grade__value @(subscribe [:editor/virkailija-translation (keyword (:ilmoittautumistila hakukohde))])]]
-            (when (> (count (:pisteet hakukohde)) 0)
-              [pisteet @lang (:oid hakukohde) (:pisteet hakukohde)])
-            [:hr]]) @valinnat))])))
+           [:div.valinnat__tulokset-container
+             [:div.valinnat__hakukohde
+              [:span (str (+ idx 1) ". " (:name hakukohde))]]
+            [:div.valinnat__tulokset__nimet
+              (when (not (nil? (:kokonaispisteet hakukohde)))
+                [:div.tulos
+                 [:span.tulos__subject "Valinnan kokonaispisteet"]])
+               [:div.tulos
+                [:span.tulos__subject "Sijoittelun tulos"]]
+               [:div.tulos
+                [:span.tulos__subject "Vastaanottotieto"]]
+               [:div.tulos
+                [:span.tulos__subject "Ilmoittautumistila"]]
+              (when (> (count (:pisteet hakukohde)) 0)
+                [pisteet @lang (:oid hakukohde) true (:pisteet hakukohde)])]
+            [:div.valinnat__tulokset__arvot
+              (when (not (nil? (:kokonaispisteet hakukohde)))
+                [:div.tulos
+                  [:span.tulos__value (:kokonaispisteet hakukohde)]])
+              [:div.tulos
+                [:span.tulos__value @(subscribe [:editor/virkailija-translation (keyword (:valintatila hakukohde))])]]
+              [:div.tulos
+                [:span.tulos__value @(subscribe [:editor/virkailija-translation (keyword (:vastaanottotila hakukohde))])]]
+              [:div.tulos
+                [:span.tulos__value @(subscribe [:editor/virkailija-translation (keyword (:ilmoittautumistila hakukohde))])]]
+              (when (> (count (:pisteet hakukohde)) 0)
+                [pisteet @lang (:oid hakukohde) false (:pisteet hakukohde)])]
+            [:div.valinnat__divider]]) @valinnat))])))
 
 (defn valinnat []
   (let [valinnat-loading-state @(subscribe [:application/application-valinnat-loading-state])]
-    [:div.grades
-     [:div.grades__left-panel
+    [:div.valinnat
+     [:div.valinnat__left-panel
       [:h2 @(subscribe [:editor/virkailija-translation :valinnat])]]
-     [:div.grades__right-panel
+     [:div.valinnat__right-panel
       (case valinnat-loading-state
         :loading [loading-indicator]
         :loaded [valinnat-loaded]
