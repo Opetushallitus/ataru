773e5d8bb3a2c12b6305c0bf9cb37fbe8c2946ae
Mikko Siukola <mikko.siukola@cybercom.com>
2022-10-19 12:39:43 +0300

    OY-3820 Add tests

--- spec/ataru/forms/form_access_control_spec.clj
+++ spec/ataru/forms/form_access_control_spec.clj
@@ -23,6 +23,18 @@
   {:key              "form-access-control-test-yhteishaku-form"
    :organization-oid "no-access-to-this-org"})
 
+(def field-id-test-form
+  {:key              "test-field-id-change-form"
+   :organization-oid "1.2.246.562.10.12345"
+   :created_by       "Test-user"
+   :content          [{:fieldClass "formField"
+                       :id         "vanha-id"}
+                      {:fieldClass "formField"
+                       :id         "jo-olemassaoleva-id"}]
+   :name             {:fi "Testilomake"}
+   :locked           nil
+   :locked-by        nil})
+
 (describe
   "get-forms-for-editor"
   (tags :unit)
@@ -55,4 +67,51 @@
             superuser-session    (update session :identity assoc :superuser true)
             {forms :forms}       (fac/get-forms-for-editor superuser-session tarjonta-service organization-service nil)]
         (println superuser-session)
-        (should= 1 (count forms))))))
+        (should= 1 (count forms)))))
+
+  (it "Should not be able to update existing form field id as a non-superuser"
+      (with-redefs [form-store/fetch-by-key (fn [_] field-id-test-form)]
+        (let [tarjonta-service (mts/->MockTarjontaService)
+              organization-service (os/->FakeOrganizationService)
+              non-superuser-session (update session :identity assoc :superuser false)
+              result (try (fac/update-field-id-in-form "test-field-id-change-form" "vanha-id" "uusi-id"
+                                                       non-superuser-session tarjonta-service organization-service nil)
+                          (catch Throwable e
+                            (.getMessage e)))]
+          (should= "Ei oikeuksia muokata lomakkeen kentän id:tä." result))))
+
+  (it "Should fail to update existing form field if such field does not exist"
+      (with-redefs [form-store/fetch-by-key (fn [_] field-id-test-form)]
+        (let [tarjonta-service (mts/->MockTarjontaService)
+              organization-service (os/->FakeOrganizationService)
+              superuser-session (update session :identity assoc :superuser true)
+              failure-reason (try (fac/update-field-id-in-form "test-field-id-change-form" "tuntematon-id" "uusi-id"
+                                                               superuser-session tarjonta-service organization-service nil)
+                                  (catch Throwable e
+                                    (.getMessage e)))]
+          (should= "Lomakkeelta ei löytynyt kenttää vanhalla id:llä tuntematon-id" failure-reason))))
+
+  (it "Should fail to update existing form field if field with target id already exists"
+      (with-redefs [form-store/fetch-by-key (fn [_] field-id-test-form)]
+        (let [tarjonta-service (mts/->MockTarjontaService)
+              organization-service (os/->FakeOrganizationService)
+              superuser-session (update session :identity assoc :superuser true)
+              failure-reason (try (fac/update-field-id-in-form "test-field-id-change-form" "vanha-id" "jo-olemassaoleva-id"
+                                                               superuser-session tarjonta-service organization-service nil)
+                                  (catch Throwable e
+                                    (.getMessage e)))]
+          (should= "Lomakkeelta löytyi jo kenttä uudella id:llä jo-olemassaoleva-id" failure-reason))))
+
+
+  (it "Should fail to update existing form field if form already has applications"
+      (with-redefs [form-store/fetch-by-key (fn [_] field-id-test-form)
+                    form-store/form-has-applications (fn [form-key]
+                                                       (= "test-field-id-change-form" form-key))]
+        (let [tarjonta-service (mts/->MockTarjontaService)
+              organization-service (os/->FakeOrganizationService)
+              superuser-session (update session :identity assoc :superuser true)
+              failure-reason (try (fac/update-field-id-in-form "test-field-id-change-form" "vanha" "uusi-id"
+                                                               superuser-session tarjonta-service organization-service nil)
+                                  (catch Throwable e
+                                    (.getMessage e)))]
+          (should= "Lomakkeella test-field-id-change-form on hakemuksia." failure-reason)))))
