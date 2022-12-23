26217040eb9f17b8163b0b0d4e4c80cab0d51a2e
Tomi Katajanheimo <tomi@katajanheimo.com>
2022-12-23 16:56:44 +0200

    Move karma config to project.clj

--- project.clj
+++ project.clj
@@ -136,7 +136,13 @@
             [speclj "3.3.2"]]
 
   :doo {:debug true
-        :paths {:karma "./node_modules/karma/bin/karma"}}
+        :paths {:karma "./node_modules/karma/bin/karma"}
+        :karma {:config
+                {"browsers" ["ChromeWithoutGpu"]
+                 "customLaunchers"
+                 {"ChromeWithoutGpu"
+                  {"base" "ChromeHeadless"
+                   "flags" ["--disable-gpu" "--disable-software-rasterizer"]}}}}}
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "resources/public/css"
