0e4806b8a03733a6107d84be19256a37770aecb3
Risto Salama <risto.salama@gofore.com>
2023-02-13 14:33:50 +0200

    Display only mail attachments in answers

--- src/cljs/ataru/virkailija/application/attachments/virkailija_attachment_subs.cljs
+++ src/cljs/ataru/virkailija/application/attachments/virkailija_attachment_subs.cljs
@@ -1,7 +1,8 @@
 (ns ataru.virkailija.application.attachments.virkailija-attachment-subs
   (:require [ataru.util :as util]
             [re-frame.core :as re-frame]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.set :as set]))
 
 (defn attachment-preview-pages-to-display []
          (get (js->clj js/config) "attachment-preview-pages-to-display" 15))
@@ -94,7 +95,7 @@
   liitepyynnot-for-selected-hakukohteet)
 
 (defn- to-liitteet-with-hakukohde
-  [hakutoiveet liitekoodisto]
+  [attachment-answers hakutoiveet liitekoodisto]
   (let [hakukohteen-tiedot-fn (fn [hk] {:oid (:oid hk)
                                         :name (:name hk)
                                         :tarjoaja (:tarjoaja-name hk)})
@@ -106,27 +107,72 @@
         get-koodi-fn (fn [liite] (->> liitekoodisto
                                       (filter #(string/includes? (:tyyppi liite) (:uri %)))
                                       (first)))
-        toiveet-to-liitteet (->> hakutoiveet
-                                 (map toive-to-liitteet-fn)
-                                 flatten
-                                 (map #(assoc % :tyyppi-label (get (get-koodi-fn %) :label (:tyyppi %))))
-                                 (group-by :tyyppi))]
-    toiveet-to-liitteet))
+        is-in-answers (fn [liite]
+                        (let [liitetyyppi (:tyyppi liite)
+                              hakukohde (get-in liite [:hakukohde :oid])
+                              matching-answers-by-tyyppi (->> attachment-answers
+                                                              (filter #(string/includes? liitetyyppi (:attachment-type %))))
+                              matching-answers-with-no-duplication (->> matching-answers-by-tyyppi
+                                                                        (filter #(not (or (:original-followup %) (:original-question %)))))
+                              matching-answers-with-duplication (set/difference matching-answers-by-tyyppi matching-answers-with-no-duplication)
+                              answer-has-hakukohde (fn [answer]
+                                                     (or (= hakukohde (:duplikoitu-kysymys-hakukohde-oid answer))
+                                                         (= hakukohde (:duplikoitu-followup-hakukohde-oid answer))))]
+                          (cond
+                            (< 0 (count matching-answers-with-no-duplication))
+                            true
+
+                            (< 0 (count (filter #(answer-has-hakukohde %) matching-answers-with-duplication)))
+                            true
+
+                            :else
+                            false)))]
+    (->> hakutoiveet
+         (map toive-to-liitteet-fn)
+         flatten
+         (filter is-in-answers)
+         (map #(assoc % :tyyppi-label (get (get-koodi-fn %) :label (:tyyppi %))))
+         (group-by :tyyppi))))
+
+(defn- answers-with-attachments
+  [answers attachment-fields]
+  (let [answer-with-attachment-field (fn [answer]
+                                         (let [answer-key (:key answer)
+                                               answer-original (:original-question answer)
+                                               answer-followup (:original-followup answer)
+                                               matches-fn (fn [field-id]
+                                                            (or (= field-id answer-key)
+                                                                (and answer-original (string/includes? answer-original field-id))
+                                                                (and answer-followup (string/includes? answer-followup field-id))))
+                                               matching-attachment-field (->> attachment-fields
+                                                                              (filter #(matches-fn (:id %)))
+                                                                              first)]
+                                           (when matching-attachment-field
+                                             (assoc answer :attachment-type (get-in matching-attachment-field [:params :attachment-type])))))
+        answers-with-attachments (->> answers
+                                      (map answer-with-attachment-field)
+                                      (remove nil?))]
+    answers-with-attachments))
 
 (re-frame/reg-sub
   :virkailija-attachments/liitepyynnot-hakemuksen-hakutoiveille
   (fn []
     [(re-frame/subscribe [:application/valitun-hakemuksen-hakukohteet])
      (re-frame/subscribe [:application/hakukohteet])
-     (re-frame/subscribe [:editor/get-attachment-types-koodisto])])
-  (fn [[hakemuksen-hakutoiveet hakukohteet liitekoodisto]]
+     (re-frame/subscribe [:editor/get-attachment-types-koodisto])
+     (re-frame/subscribe [:application/selected-application-answers])
+     (re-frame/subscribe [:application/selected-form])
+     (re-frame/subscribe [:application/forms])])
+  (fn [[hakemuksen-hakutoiveet hakukohteet liitekoodisto answers form forms]]
     (let [hakutoiveet (->> hakemuksen-hakutoiveet
-                           (map #(get hakukohteet %)))]
-      (prn "TÄÄLLÄ")
-      (prn liitekoodisto)
-      (prn hakutoiveet)
-      (prn (to-liitteet-with-hakukohde hakutoiveet liitekoodisto))
-      (to-liitteet-with-hakukohde hakutoiveet liitekoodisto))))
+                           (map #(get hakukohteet %)))
+          fields-with-attachments (->> form
+                                       :key
+                                       (get forms)
+                                       :flat-form-fields
+                                       (filter #(true? (get-in % [:params :mail-attachment?]))))
+          answers-with-attachments (answers-with-attachments (vals answers) fields-with-attachments)]
+      (to-liitteet-with-hakukohde answers-with-attachments hakutoiveet liitekoodisto))))
 
 (re-frame/reg-sub
   :virkailija-attachments/selected-attachment-and-liitepyynto
