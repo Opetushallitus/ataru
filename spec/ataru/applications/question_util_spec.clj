b9fef1079ec1c76a39866a1fb68f3045ddb78730
Risto Salama <risto.salama@gofore.com>
2023-03-21 10:23:58 +0200

    Add spec for lukio sport questions

--- spec/ataru/applications/question_util_spec.clj
+++ spec/ataru/applications/question_util_spec.clj
@@ -109,6 +109,45 @@
                                         :liitto                      "2b2ede36-e520-4727-8151-93115d26ef7f"}]
                           (should= sport-questions expected)))
 
+                    (it "returns urheilija gymnasium extra questions"
+                        (let [sport-questions (:urheilijan-lisakysymys-keys (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))
+                              expected {:keskiarvo                   "urheilija-2nd-keskiarvo"
+                                        :peruskoulu                  "urheilija-2nd-peruskoulu"
+                                        :tamakausi                   "urheilija-2nd-tamakausi"
+                                        :viimekausi                  "urheilija-2nd-viimekausi"
+                                        :toissakausi                 "urheilija-2nd-toissakausi"
+                                        :sivulaji                    "urheilija-2nd-sivulaji"
+                                        :valmentaja_nimi             "urheilija-2nd-valmentaja-nimi"
+                                        :valmentaja_email            "urheilija-2nd-valmentaja-email"
+                                        :valmentaja_puh              "urheilija-2nd-valmentaja-puh"
+                                        :valmennusryhma_seurajoukkue "urheilija-2nd-valmennus-seurajoukkue"
+                                        :valmennusryhma_piirijoukkue "urheilija-2nd-valmennus-piirijoukkue"
+                                        :valmennusryhma_maajoukkue   "urheilija-2nd-valmennus-maajoukkue"
+                                        :valmennusryhmatParent       "84cd8829-ee39-437f-b730-9d68f0f07555"
+                                        :paalaji-dropdown            "urheilija-2nd-lajivalinta-dropdown"
+                                        :seura                       "urheilija-2nd-seura"
+                                        :liitto                      "urheilija-2nd-liitto"}]
+                          (should= sport-questions expected)))
+
+                    (it "returns urheilija gymnasium extra questions for specific haku"
+                        (let [sport-questions (:urheilijan-lisakysymys-keys (qu/get-hakurekisteri-toinenaste-specific-questions
+                                                                                  form-2nd-aste
+                                                                                  "1.2.246.562.29.00000000000000005368"))
+                              expected {:keskiarvo                   "7b88594a-c308-41f8-bac3-2d3779ea4443"
+                                        :peruskoulu                  "9a4de985-9a70-4de6-bfa7-0a5c2f18cb8c"
+                                        :tamakausi                   "f944c9c3-c1f8-43c7-a27e-49d89d4e8eec"
+                                        :viimekausi                  "e3e8b5ef-f8d9-4256-8ef6-1a52d562a370"
+                                        :toissakausi                 "95b565ee-f64e-4805-b319-55b99bbce1a8"
+                                        :sivulaji                    "dbfc1215-896a-47d4-bc07-b9f1494658f4"
+                                        :valmentaja_nimi             "a1f1147a-d466-4d98-9a62-079a42dd4089"
+                                        :valmentaja_email            "625fe96d-a5ff-4b3a-8ace-e36524215d1c"
+                                        :valmentaja_puh              "f1c5986c-bea8-44f7-8324-d1cac179e6f4"
+                                        :valmennusryhma_seurajoukkue "92d579fb-dafa-4edc-9e05-8f493badc4f3"
+                                        :valmennusryhma_piirijoukkue "58125631-762a-499b-a402-717778bf8233"
+                                        :valmennusryhma_maajoukkue   "261d7ffc-54a7-4c5c-ab80-82f7de49f648"
+                                        :paalajiSeuraLiittoParent    "98951abd-fdd5-46a0-8427-78fe9706d286"}]
+                          (should= sport-questions expected)))
+
                     (it "returns sora-aiempi key"
                         (should= "aiempi-key" (:sora-aiempi-key (qu/get-hakurekisteri-toinenaste-specific-questions form-2nd-aste))))
 
