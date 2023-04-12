a6365a5c9e19f2013603bc7aa721bf81ef76c872
Risto Salama <risto.salama@gofore.com>
2023-04-12 15:48:38 +0300

    Check that parent actually exists in new answers

--- spec/ataru/hakija/hakija_application_service_spec.clj
+++ spec/ataru/hakija/hakija_application_service_spec.clj
@@ -3,6 +3,22 @@
             [speclj.core :refer [describe it should-be should-not-be should-contain should=]]))
 
 (def edited-cannot-edit-questions #'hakija-application-service/edited-cannot-edit-questions)
+(def form-with-followup {:content [{:id      "id"
+                                    :options [{:value 0 :followups [{:id          "followup-id"
+                                                                     :cannot-view true
+                                                                     :followup-of "id"
+                                                                     :option-value 0}]}
+                                              {:value 1}]}]})
+
+(def followup-answer {:key                               "followup-id_1"
+                      :value                             "old-value"
+                      :original-followup                 "followup-id"
+                      :duplikoitu-followup-hakukohde-oid "1"})
+(defn- create-per-hakukohde-answer [id hakukohde answer-value]
+  {:key (str id "_" hakukohde)
+   :original-question id
+   :duplikoitu-kysymys-hakukohde-oid hakukohde
+   :value answer-value})
 
 (describe "edited-cannot-edit-questions"
   (describe "old application does not have an answer"
@@ -73,55 +89,34 @@
 
     (describe "per-hakukohde followup"
       (it "removes answer when hakukohde is removed in new application"
-        (let [form            {:content [{:id      "id"
-                                          :options [{:followups [{:id          "followup-id"
-                                                                  :cannot-view true}]}]}]}
-              old-application {:answers   [{:key                               "followup-id_1"
-                                            :value                             "old-value"
-                                            :original-followup                 "followup-id"
-                                            :duplikoitu-followup-hakukohde-oid "1"}]
+        (let [old-application {:answers   [followup-answer]
                                :hakukohde ["1"]}
               new-application {:answers   []
                                :hakukohde []}
-              answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form))]
+              answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
           (should= 0 (count answers))))
 
       (it "copies answer value from old application when new application doesn't have answer"
-        (let [form            {:content [{:id      "id"
-                                          :options [{:followups [{:id          "followup-id"
-                                                                  :cannot-view true}]}]}]}
-              old-application {:answers   [{:key                               "followup-id_1"
-                                            :value                             "old-value"
-                                            :original-followup                 "followup-id"
-                                            :duplikoitu-followup-hakukohde-oid "1"}]
+        (let [old-application {:answers   [followup-answer]
                                :hakukohde ["1"]}
               new-application {:answers   [{:key                               "followup-id_1"
                                             :value                             nil
                                             :original-followup                 "followup-id"
                                             :duplikoitu-followup-hakukohde-oid "1"}]
                                :hakukohde ["1"]}
-              answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form))]
+              answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
           (should= 1 (count answers))
-          (should-contain {:key                               "followup-id_1"
-                           :value                             "old-value"
-                           :original-followup                 "followup-id"
-                           :duplikoitu-followup-hakukohde-oid "1"} answers)))
+          (should-contain followup-answer answers)))
 
       (it "replaces old answer value with value from new application"
-        (let [form            {:content [{:id      "id"
-                                          :options [{:followups [{:id          "followup-id"
-                                                                  :cannot-view true}]}]}]}
-              old-application {:answers   [{:key                               "followup-id_1"
-                                            :value                             "old-value"
-                                            :original-followup                 "followup-id"
-                                            :duplikoitu-followup-hakukohde-oid "1"}]
+        (let [old-application {:answers   [followup-answer]
                                :hakukohde ["1"]}
               new-application {:answers   [{:key                               "followup-id_1"
                                             :value                             "new-value"
                                             :original-followup                 "followup-id"
                                             :duplikoitu-followup-hakukohde-oid "1"}]
                                :hakukohde ["1"]}
-              answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form))]
+              answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
           (should= 1 (count answers))
           (should-contain {:key                               "followup-id_1"
                            :value                             "new-value"
@@ -129,24 +124,46 @@
                            :duplikoitu-followup-hakukohde-oid "1"} answers)))
 
       (it "does not copy followup answer value from old application when new application has answered different option parent of followup"
-          (let [form            {:content [{:id      "id"
-                                            :options [{:value 0 :followups [{:id          "followup-id"
-                                                                             :cannot-view true}]}
-                                                      {:value 1}]}]}
-                old-application {:answers   [{:key                               "followup-id_1"
-                                              :value                             "old-value"
-                                              :original-followup                 "followup-id"
-                                              :duplikoitu-followup-hakukohde-oid "1"}
-                                             {:key "id_1"
-                                              :original-question "id"
-                                              :value 0}]
+          (let [old-application {:answers   [followup-answer
+                                             (create-per-hakukohde-answer "id" "1" 0)]
                                  :hakukohde ["1"]}
-                new-application {:answers   [{:key "id_1"
-                                              :original-question "id"
-                                              :value 1}]
+                new-application {:answers   [(create-per-hakukohde-answer "id" "1" 1)]
                                  :hakukohde ["1"]}
-                answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form))]
+                answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
             (should= 1 (count answers))
-            (should-contain {:key "id_1"
-                             :original-question "id"
-                             :value 1} answers)))        )))
+            (should-contain (create-per-hakukohde-answer "id" "1" 1) answers)))
+
+      (it "copies followup answer value from old application when new application has multiple parents with different values but with same application target"
+          (let [old-application {:answers   [followup-answer
+                                             (create-per-hakukohde-answer "id" "1" 0)]
+                                 :hakukohde ["1"]}
+                new-application {:answers   [(create-per-hakukohde-answer "id" "2" 1)
+                                             (create-per-hakukohde-answer "id" "1" 0)]
+                                 :hakukohde ["1" "2"]}
+                answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
+            (should= 3 (count answers))
+            (should-contain followup-answer answers)))
+
+      (it "does not copy followup answer value from old application when new application has multiple parents with different values but with same application target"
+          (let [old-application {:answers   [followup-answer
+                                             (create-per-hakukohde-answer "id" "1" 0)]
+                                 :hakukohde ["1"]}
+                new-application {:answers   [(create-per-hakukohde-answer "id" "2" 0)
+                                             (create-per-hakukohde-answer "id" "1" 1)]
+                                 :hakukohde ["1" "2"]}
+                answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
+            (should= 2 (count answers))
+            (should-contain (create-per-hakukohde-answer "id" "1" 1) answers)
+            (should-contain (create-per-hakukohde-answer "id" "2" 0) answers)))
+
+      (it "does not copy followup answer value from old application when new application has multiple parents with different values but not same application target"
+          (let [old-application {:answers   [followup-answer
+                                             (create-per-hakukohde-answer "id" "1" 0)]
+                                 :hakukohde ["1"]}
+                new-application {:answers   [(create-per-hakukohde-answer "id" "2" 0)
+                                             (create-per-hakukohde-answer "id" "3" 1)]
+                                 :hakukohde ["1" "2" "3"]}
+                answers         (:answers (hakija-application-service/merge-unviewable-answers-from-previous new-application old-application form-with-followup))]
+            (should= 2 (count answers))
+            (should-contain (create-per-hakukohde-answer "id" "3" 1) answers)
+            (should-contain (create-per-hakukohde-answer "id" "2" 0) answers))))))
