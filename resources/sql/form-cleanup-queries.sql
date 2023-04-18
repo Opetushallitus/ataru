ab80ece458020e39559d45c282494f186f061f40
Marja Kari <marja.kari@amban.fi>
2023-04-18 14:49:07 +0300

    OY-4223 lisätarkistus lomakkeiden poistoon

--- resources/sql/form-cleanup-queries.sql
+++ resources/sql/form-cleanup-queries.sql
@@ -2,5 +2,6 @@
 DELETE FROM forms WHERE id IN (SELECT id FROM forms f
                                WHERE created_time < (NOW() - interval '1 month')
                                  AND f.id NOT IN (SELECT form_id FROM applications)
+                                 AND f.id NOT IN (SELECT form_id FROM application_feedback)
                                  AND f.id NOT IN (SELECT id FROM forms f2 WHERE f2.key = f.key ORDER BY f2.id DESC LIMIT 5)
                                LIMIT :limit);
