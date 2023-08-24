b6570194a0be93337c0f11c5685eb30da2aa4508
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2023-08-24 16:27:23 +0300

    drag-n-drop-spacer cleanup

--- src/cljs/ataru/virkailija/editor/components/drag_n_drop_spacer.cljs
+++ src/cljs/ataru/virkailija/editor/components/drag_n_drop_spacer.cljs
@@ -2,27 +2,22 @@
   (:require [re-frame.core :as re-frame]
             [reagent.core :as r]))
 
-(defn drag-n-drop-spacer [path]
+(defn drag-n-drop-spacer [_]
   (let [expanded?         (r/atom false)
-        copy-component    (re-frame/subscribe [:editor/copy-component])
-        selected-form-key (re-frame/subscribe [:editor/selected-form-key])]
+        copy-component    (re-frame/subscribe [:editor/copy-component])]
     (fn [path]
       [:div.editor-form__drag_n_drop_spacer_container_for_component
        {:on-mouse-over (fn [_] (when (some? @copy-component)
                                  (reset! expanded? true)))
         :on-mouse-out  (fn [_] (reset! expanded? false))}
        (when @expanded?
-         (let [{form-key :copy-component-form-key
-                cut?     :copy-component-cut?} @copy-component
-               same-form? (= @selected-form-key form-key)]
-           [:div.editor-form__drag_n_drop_spacer--dashbox
-            (if @(re-frame/subscribe [:editor/can-copy-or-paste?])
-              [:button.editor-form__component-button
-               {
-                :on-click (fn [_] (when (and @expanded?)
-                                    (reset! expanded? false)
-                                    (re-frame/dispatch [:editor/paste-component @copy-component path])))}
-               @(re-frame/subscribe [:editor/virkailija-translation :paste-element])]
-              [:button.editor-form__component-button
-               {:disabled true}
-               @(re-frame/subscribe [:editor/virkailija-translation :paste-element])])]))])))
+         [:div.editor-form__drag_n_drop_spacer--dashbox
+          (if @(re-frame/subscribe [:editor/can-copy-or-paste?])
+            [:button.editor-form__component-button
+             {:on-click (fn [_] (when @expanded?
+                                  (reset! expanded? false)
+                                  (re-frame/dispatch [:editor/paste-component @copy-component path])))}
+             @(re-frame/subscribe [:editor/virkailija-translation :paste-element])]
+            [:button.editor-form__component-button
+             {:disabled true}
+             @(re-frame/subscribe [:editor/virkailija-translation :paste-element])])])])))
