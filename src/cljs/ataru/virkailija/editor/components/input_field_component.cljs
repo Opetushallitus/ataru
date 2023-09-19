9919233a21ced4093db59dbfa98bcf7e520d25a6
Joona Haavisto <joona.haavisto@gofore.com>
2023-09-19 15:39:44 +0300

    Update reagent to 1.0.0 and fix namespace changes

--- src/cljs/ataru/virkailija/editor/components/input_field_component.cljs
+++ src/cljs/ataru/virkailija/editor/components/input_field_component.cljs
@@ -1,6 +1,7 @@
 (ns ataru.virkailija.editor.components.input-field-component
   (:require [re-frame.core :refer [subscribe]]
             [reagent.core :as r]
+            [reagent.dom :as r-dom]
             [reagent.ratom :refer-macros [reaction]]))
 
 (defn- prevent-default
@@ -29,7 +30,7 @@
                               (when (cond-> @focus?
                                             (> (count @languages) 1)
                                             (and (= (first @languages) lang)))
-                                (let [dom-node (r/dom-node component)]
+                                (let [dom-node (r-dom/dom-node component)]
                                   (.focus dom-node))))
        :reagent-render      (fn [_ _ _ _]
                               [tag
