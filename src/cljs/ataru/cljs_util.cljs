b6b9fd749e576858c96417fd28f88a341858da62
Joona Haavisto <joona.haavisto@gofore.com>
2023-09-19 15:39:44 +0300

    Update reagent to 1.0.0 and fix namespace changes

--- src/cljs/ataru/cljs_util.cljs
+++ src/cljs/ataru/cljs_util.cljs
@@ -7,6 +7,7 @@
             [cljs-uuid-utils.core :as uuid]
             [re-frame.core :refer [dispatch] :as re-frame]
             [reagent.core :as r]
+            [reagent.dom :as r-dom]
             [cemerick.url :as url]
             [camel-snake-kebab.core :refer [->kebab-case-keyword]]
             [camel-snake-kebab.extras :refer [transform-keys]]
@@ -25,7 +26,7 @@
       warning-label)))
 
 (def wrap-scroll-to
-  (with-meta identity {:component-did-mount #(let [node (r/dom-node %)]
+  (with-meta identity {:component-did-mount #(let [node (r-dom/dom-node %)]
                                               (if (.-scrollIntoViewIfNeeded node)
                                                 (.scrollIntoViewIfNeeded node)
                                                 (.scrollIntoView node)))}))
