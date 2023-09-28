8c39f8d10718fe53e3a5c5fca4d310d5045ddce6
Joona Haavisto <joona.haavisto@gofore.com>
2023-09-28 13:23:56 +0300

    Updated node packages and replaced lein-less with node less

---
+++ bin/compile-less.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm resources/public/css/compiled/*.css
+
+for FILE in resources/less/*.less; do
+  NAME=${FILE##*/}
+  BASE=${NAME%.less}
+  node_modules/less/bin/lessc $FILE resources/public/css/compiled/${BASE}.css;
+done
