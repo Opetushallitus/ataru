65a62781e5f97b400288b3efcb18f889b790d683
Marja Kari <marja.kari@amban.fi>
2023-10-12 18:57:34 +0300

    OY-4012 cypress-testit läpi

--- cypress/integration/virkailijanMassatoiminnotSpec.ts
+++ cypress/integration/virkailijanMassatoiminnotSpec.ts
@@ -5,7 +5,7 @@ describe('Hakemusten massatoiminnot ei-rekisterinpitäjälle', () => {
     'OPINTO-OHJAAJA',
     'hakemusten käsittelyä varten',
     () => {
-      before(() => {
+      beforeEach(() => {
         cy.fixture('hakemuksetmassatoiminnoille.json').as('hakemuslistaus')
         cy.fixture('hautmassatoiminnoille.json').as('haut')
 
@@ -23,57 +23,51 @@ describe('Hakemusten massatoiminnot ei-rekisterinpitäjälle', () => {
           '/lomake-editori/api/applications/list',
           '@hakemuslistaus'
         ).as('listApplications')
-        cy.route('GET', '/lomake-editori/api/haut*', '@haut')
         cy.route('GET', '/lomake-editori/api/haku*', '@haut')
+        cy.route('GET', '/lomake-editori/api/haut*', '@haut')
       })
 
-      after(() => {
+      afterEach(() => {
         cy.server({ enable: false })
       })
 
-      describe('Haun hakemuslistauksessa', () => {
-        it('Massaviestipainike on näkyvissä ja massaviesti-ikkuna latautuu oikeilla teksteillä', () => {
-          cy.avaaHaunHakemuksetVirkailijanNakymassa(
-            '1.2.246.562.29.00000000000000018308'
-          )
-          cy.get('[data-test-id="show-results"]').should('be.visible')
-          cy.get('[data-test-id="show-results"]').click()
-          cy.get('.application-handling__mass-information-request-link').should(
-            'be.visible'
-          )
-          cy.get('.application-handling__mass-information-request-link').click()
-          cy.get(
-            '.application-handling__mass-information-request-popup'
-          ).should('be.visible')
-          cy.get(
-            '.application-handling__mass-edit-review-states-title-container > h4'
-          ).contains('Massaviesti')
-          cy.get('p').contains('Lähetä sähköposti 2 hakijalle').should('exist')
-        })
+      it('Massaviestipainike on näkyvissä ja massaviesti-ikkuna latautuu oikeilla teksteillä', () => {
+        cy.avaaHaunHakemuksetVirkailijanNakymassa(
+          '1.2.246.562.29.00000000000000018308'
+        )
+        cy.get('[data-test-id="show-results"]').should('be.visible')
+        cy.get('[data-test-id="show-results"]').click()
+        cy.get('.application-handling__mass-information-request-link').should(
+          'be.visible'
+        )
+        cy.get('.application-handling__mass-information-request-link').click()
+        cy.get('.application-handling__mass-information-request-popup').should(
+          'be.visible'
+        )
+        cy.get(
+          '.application-handling__mass-edit-review-states-title-container > h4'
+        ).contains('Massaviesti')
+        cy.get('p').contains('Lähetä sähköposti 2 hakijalle').should('exist')
+      })
 
-        it('Toisen asteen yhteishaussa ei näy massamuistiinpanotoimintoa', () => {
-          cy.avaaHaunHakemuksetVirkailijanNakymassa(
-            '1.2.246.562.29.10000000001'
-          )
-          cy.get('[data-test-id="show-results"]').should('be.visible')
-          cy.get('[data-test-id="show-results"]').click()
-          cy.get('.application-handling__mass-information-request-link').should(
-            'be.visible'
-          )
-          cy.get('[data-test-id="mass-review-notes-button"]').should(
-            'not.be.visible'
-          )
-        })
+      it('Toisen asteen yhteishaussa ei näy massamuistiinpanotoimintoa', () => {
+        cy.avaaHaunHakemuksetVirkailijanNakymassa('1.2.246.562.29.10000000001')
+        cy.get('[data-test-id="show-results"]').should('be.visible')
+        cy.get('[data-test-id="show-results"]').click()
+        cy.get('.application-handling__mass-information-request-link').should(
+          'be.visible'
+        )
+        cy.get('[data-test-id="mass-review-notes-button"]').should(
+          'not.be.visible'
+        )
+      })
 
-        it('Ei-yhteishaulle massamuistiinpanotoiminto näkyy', () => {
-          cy.avaaHaunHakemuksetVirkailijanNakymassa(
-            '1.2.246.562.29.00000000000000018308'
-          )
-          cy.get('[data-test-id="show-results"]').click()
-          cy.get('[data-test-id="mass-review-notes-button"]').should(
-            'be.visible'
-          )
-        })
+      it('Ei-yhteishaulle massamuistiinpanotoiminto näkyy', () => {
+        cy.avaaHaunHakemuksetVirkailijanNakymassa(
+          '1.2.246.562.29.00000000000000018308'
+        )
+        cy.get('[data-test-id="show-results"]').click()
+        cy.get('[data-test-id="mass-review-notes-button"]').should('be.visible')
       })
     }
   )
