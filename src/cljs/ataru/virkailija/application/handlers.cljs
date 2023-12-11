2fb96f49559dc22d079a00cb7bfa5899cf4e0df4
Marja Kari <marja.kari@amban.fi>
2023-12-11 10:47:31 +0200

    OY-4309 ladataan hakemus uudelleen ja nollataan muutokset jos ei oikeuksia tallentaa

--- src/cljs/ataru/virkailija/application/handlers.cljs
+++ src/cljs/ataru/virkailija/application/handlers.cljs
@@ -545,6 +545,7 @@
                              :changed-predicate review-autosave-predicate
                              :handler (fn [current _]
                                         (let [selected-review-hakukohde-oids @(subscribe [:state-query [:application :selected-review-hakukohde-oids]])
+                                              current-application @(subscribe [:state-query [:application :selected-key]])
                                               filtered-hakukohde-reviews (select-keys (current :hakukohde-reviews) (map keyword selected-review-hakukohde-oids))]
                                         (ajax/http
                                           :put
@@ -556,7 +557,12 @@
                                                                                         :score
                                                                                         :state
                                                                                         :attachment-reviews])
-                                                                         (hash-map :hakukohde-reviews filtered-hakukohde-reviews))})))})))
+                                                                         (hash-map :hakukohde-reviews filtered-hakukohde-reviews))
+                                                          :error-handler (fn [response]
+                                                                           (when (= (count (str (:status response))) 3)
+                                                                             (dispatch [:add-toast-message (-> response :response :error)]))
+                                                                           (when current-application
+                                                                             (dispatch [:application/fetch-application current-application])))})))})))
 
 (reg-event-db
   :application/handle-fetch-application-attachment-metadata
