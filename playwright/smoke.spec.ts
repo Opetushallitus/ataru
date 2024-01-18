7c46f788d95364d7597694f2b27b288460b1e062
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-01-18 15:30:36 +0200

    Poistettu savutesti

--- playwright/smoke.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-import { kirjauduVirkailijanNakymaan } from './playwright-utils'
-
-test('Smoke', async ({ page }) => {
-  await kirjauduVirkailijanNakymaan(page)
-  await page.goto('/lomake-editori')
-  await expect(page.getByRole('heading', { name: 'Lomakkeet' })).toBeVisible()
-})
