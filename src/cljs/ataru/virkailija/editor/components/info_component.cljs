198398dbab68a66b2b41c30c11e8f56452521a88
Risto Salama <risto.salama@gofore.com>
2024-01-29 11:37:10 +0200

    Refactor

--- src/cljs/ataru/virkailija/editor/components/info_component.cljs
+++ src/cljs/ataru/virkailija/editor/components/info_component.cljs
@@ -8,12 +8,30 @@
             [ataru.virkailija.editor.components.input-field-component :as input-field-component]
             [ataru.virkailija.editor.components.markdown-help-component :as markdown-help-component]))
 
+(defn- info-checkbox
+  [path component-locked? param translation-key]
+  (let [checked? (subscribe [:editor/get-component-value path :params param])]
+    [:div.editor-form__checkbox-wrapper
+     (let [input-id (util/new-uuid)]
+       [:div.editor-form__checkbox-container
+        [:input.editor-form__checkbox {:type      "checkbox"
+                                       :id        input-id
+                                       :checked   (boolean @checked?)
+                                       :disabled  @component-locked?
+                                       :on-change (fn [event]
+                                                    (dispatch [:editor/set-component-value
+                                                               (-> event .-target .-checked)
+                                                               path :params param]))}]
+        [:label.editor-form__checkbox-label
+         {:for   input-id
+          :class (when @component-locked? "editor-form__checkbox-label--disabled")}
+         @(subscribe [:editor/virkailija-translation translation-key])]])])
+  )
+
 (defn info-element
   "Info text which is a standalone component"
   [_ path]
   (let [languages        (subscribe [:editor/languages])
-        collapse-checked (subscribe [:editor/get-component-value path :params :info-text-collapse])
-        show-identified-checked (subscribe [:editor/get-component-value path :params :show-for-identified])
         sub-header       (subscribe [:editor/get-component-value path :label])
         component-locked?     (subscribe [:editor/component-locked? path])]
     (fn [initial-content path]
@@ -52,34 +70,6 @@
                         (into field [[:div.editor-form__markdown-anchor
                                       (markdown-help-component/markdown-help)]])))
                  doall)]]
-          [:div.editor-form__checkbox-wrapper
-           (let [collapsed-id (util/new-uuid)]
-             [:div.editor-form__checkbox-container
-              [:input.editor-form__checkbox {:type      "checkbox"
-                                             :id        collapsed-id
-                                             :checked   (boolean @collapse-checked)
-                                             :disabled  @component-locked?
-                                             :on-change (fn [event]
-                                                          (dispatch [:editor/set-component-value
-                                                                     (-> event .-target .-checked)
-                                                                     path :params :info-text-collapse]))}]
-              [:label.editor-form__checkbox-label
-               {:for   collapsed-id
-                :class (when @component-locked? "editor-form__checkbox-label--disabled")}
-               @(subscribe [:editor/virkailija-translation :collapse-info-text])]])]
-          [:div.editor-form__checkbox-wrapper
-           (let [show-identified-id (util/new-uuid)]
-             [:div.editor-form__checkbox-container
-              [:input.editor-form__checkbox {:type      "checkbox"
-                                             :id        show-identified-id
-                                             :checked   (boolean @show-identified-checked)
-                                             :disabled  @component-locked?
-                                             :on-change (fn [event]
-                                                          (dispatch [:editor/set-component-value
-                                                                     (-> event .-target .-checked)
-                                                                     path :params :show-for-identified]))}]
-              [:label.editor-form__checkbox-label
-               {:for   show-identified-id
-                :class (when @component-locked? "editor-form__checkbox-label--disabled")}
-               @(subscribe [:editor/virkailija-translation :show-for-identified-info-text])]])]
+          [info-checkbox path component-locked? :info-text-collapse :collapse-info-text]
+          [info-checkbox path component-locked? :show-for-identified :show-for-identified-info-text]
           [belongs-to-hakukohteet-component/belongs-to-hakukohteet path initial-content]]]]])))
