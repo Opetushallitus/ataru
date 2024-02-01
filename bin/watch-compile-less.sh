97e585bb9c02192c7c2d59f582783746a2d44b3d
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-02-01 12:05:44 +0200

    Käytetään PM2:n watch-optiota less-tiedostojen muutosten tarkkailuun "less-watch-compiler"-kirjaston sijaan

--- bin/watch-compile-less.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-node_modules/.bin/less-watch-compiler resources/less resources/public/css/compiled virkailija-site.less &
-node_modules/.bin/less-watch-compiler resources/less resources/public/css/compiled hakija-site.less &
-wait
