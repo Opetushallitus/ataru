32ccada6419da9a96c39b83b12c37b0e49180bc1
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-02-16 18:59:59 +0200

    Siivottu suurin osa event-handlereiden sisäisistä re-frame/subscribe-kutsuista pois
    - Korvattu selektoreilla, koska "db" on event handlereissa saatavilla
    - Muutamaan hankalampaan kohtaan lisätty TODO-kommentti ja ignoroitu clj-kondon varoitus asiasta

---
+++ src/cljs/ataru/virkailija/editor/editor_selectors.cljs
@@ -0,0 +1,11 @@
+(ns ataru.virkailija.editor.editor-selectors)
+
+(defn get-virkailija-lang [db]
+  (or (-> db :editor :user-info :lang keyword) :fi))
+
+(defn get-email-template [db]
+  (get-in db [:editor :email-template (get-in db [:editor :selected-form-key])]))
+
+(defn db-all-organizations-have-only-opinto-ohjaaja-rights? [db]
+  (let [user-info (-> db :editor :user-info)]
+    (every? (fn [org] (every? #(= "opinto-ohjaaja" %) (:rights org))) (:organizations user-info))))
