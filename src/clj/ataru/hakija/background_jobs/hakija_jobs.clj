0609162987ddafc6f493a036f32d353794b1f92b
Santeri Korri <santeri.korri@knowit.fi>
2024-03-07 14:17:13 +0200

    OY-3657 Vaihdettu background-jobit käyttämään Proletarian-kirjastoa

--- src/clj/ataru/hakija/background_jobs/hakija_jobs.clj
+++ src/clj/ataru/hakija/background_jobs/hakija_jobs.clj
@@ -9,16 +9,19 @@
 (def job-definitions
   {(:type email-job/job-definition)                    email-job/job-definition
    person-integration/job-type                         person-integration/job-definition
-   "update-person-info-job"                            {:steps {:initial person-integration/update-person-info-job-step}
-                                                        :type  "update-person-info-job"}
-   "automatic-payment-obligation-job"                  {:steps {:initial automatic-payment-obligation/automatic-payment-obligation-job-step}
-                                                        :type  "automatic-payment-obligation-job"}
+   "update-person-info-job"                            {:handler person-integration/update-person-info-job-handler
+                                                        :type    "update-person-info-job"}
+   "automatic-payment-obligation-job"                  {:handler automatic-payment-obligation/automatic-payment-obligation-job-handler
+                                                        :type    "automatic-payment-obligation-job"}
    (:type attachment-finalizer-job/job-definition)     attachment-finalizer-job/job-definition
-   "automatic-eligibility-if-ylioppilas-job"           {:steps {:initial automatic-eligibility/automatic-eligibility-if-ylioppilas-job-step}
-                                                        :type  "automatic-eligibility-if-ylioppilas-job"}
-   "start-automatic-eligibility-if-ylioppilas-job-job" {:steps {:initial automatic-eligibility/start-automatic-eligibility-if-ylioppilas-job-job-step}
-                                                        :type  "start-automatic-eligibility-if-ylioppilas-job-job"}
-   "tutkintojen-tunnustaminen-submit-job"              {:steps {:initial tutkintojen-tunnustaminen/tutkintojen-tunnustaminen-submit-job-step}
-                                                        :type  "tutkintojen-tunnustaminen-submit-job"}
-   "tutkintojen-tunnustaminen-edit-job"                {:steps {:initial tutkintojen-tunnustaminen/tutkintojen-tunnustaminen-edit-job-step}
-                                                        :type  "tutkintojen-tunnustaminen-edit-job"}})
+   "automatic-eligibility-if-ylioppilas-job"           {:handler automatic-eligibility/automatic-eligibility-if-ylioppilas-job-handler
+                                                        :type    "automatic-eligibility-if-ylioppilas-job"
+                                                        :queue {:proletarian/retry-strategy-fn
+                                                                (fn [_ _] {:retries 3
+                                                                           :delays [10000]})}}
+   "start-automatic-eligibility-if-ylioppilas-job-job" {:handler automatic-eligibility/start-automatic-eligibility-if-ylioppilas-job-job-handler
+                                                        :type    "start-automatic-eligibility-if-ylioppilas-job-job"}
+   "tutkintojen-tunnustaminen-submit-job"              {:handler tutkintojen-tunnustaminen/tutkintojen-tunnustaminen-submit-job-handler
+                                                        :type    "tutkintojen-tunnustaminen-submit-job"}
+   "tutkintojen-tunnustaminen-edit-job"                {:handler tutkintojen-tunnustaminen/tutkintojen-tunnustaminen-edit-job-handler
+                                                        :type    "tutkintojen-tunnustaminen-edit-job"}})
