aeb8edf5d5411ec2e78712951f0b1e86519a6ba3
Risto Salama <risto.salama@gofore.com>
2024-03-22 11:19:16 +0200

    Pad empty question group answers in application load

--- src/cljs/ataru/hakija/application_handlers.cljs
+++ src/cljs/ataru/hakija/application_handlers.cljs
@@ -475,7 +475,7 @@
           answers-with-values-ids (set (->> question-group-answers-with-values
                                        (map #(keyword (:key %)))))
           question-group-answers-without-values (->> question-group-form-fields
-                                                     (map #(get-in db [:application :answers (keyword (:id %))]))
+                                                     (map #(assoc (get-in db [:application :answers (keyword (:id %))]) :id (:id %)))
                                                      (remove nil?)
                                                      (remove #(contains? answers-with-values-ids (keyword (:id %)))))
           ;_ (prn question-group-field-ids)
@@ -499,21 +499,26 @@
                                 (let [matching-field (->> question-group-form-fields
                                                           (filter #(= (:id %) (:id answer)))
                                                           first)
-                                      _ (prn "map-matching-amount")
-                                      _ (prn matching-field)
                                       amount (->> (get-in matching-field [:params :question-group-id])
                                                   keyword
-                                                  (get question-group-ids-with-amounts))
-                                      _ (prn amount)
-                                      padded-value (pad amount (:value answer) nil)
-                                      padded-values (pad amount (:values answer) nil)]
+                                                  (get question-group-ids-with-amounts))]
                                   (when amount
-                                    (update-in answer :value padded-value :values padded-values))))
+                                    (-> answer
+                                        ((fn[a]
+                                          (prn a)
+                                          a))
+                                        (assoc :value (pad amount (:value answer) nil))
+                                        (assoc :values (pad amount (:values answer) nil))))))
+          _ (prn (first question-group-answers-without-values))
           updated-answers (->> question-group-answers-without-values
-                               (map map-matching-amount-if-found)
-                               (remove nil?))
+                                        (map map-matching-amount-if-found)
+                                        (remove nil?))
           _ (prn updated-answers)]
-      (update-in db [:application :answers] merge updated-answers)
+      (-> (reduce (fn [db answer]
+                    (let [id (keyword (:id answer))
+                          answer-without-id (dissoc answer :id)]
+                      (assoc-in db [:application :answers id] answer-without-id)))
+                  db updated-answers))
     )))
 
 (defn- merge-submitted-answers [db submitted-answers flat-form-content]
@@ -560,7 +565,7 @@
         (populate-hakukohde-answers-if-necessary)
         (set-have-finnish-ssn flat-form-content)
         (original-values->answers)
-        (reinitialize-question-group-empty-answers submitted-answers flat-form-content)
+        ;(reinitialize-question-group-empty-answers submitted-answers flat-form-content)
         (rules/run-all-rules flat-form-content)
         (set-question-group-row-amounts))))
 
@@ -638,8 +643,7 @@
         questions-with-duplicates  (handlers-util/duplicate-questions-for-hakukohteet-during-form-load (get-in form [:tarjonta :hakukohteet]) hakukohde-oids-to-duplicate questions)
         flat-form-content          (autil/flatten-form-fields questions-with-duplicates)
         selected-language          (:selected-language form)
-        initial-answers            (create-initial-answers flat-form-content preselected-hakukohde-oids selected-language)
-        _ (prn "HERERERERE")]
+        initial-answers            (create-initial-answers flat-form-content preselected-hakukohde-oids selected-language)]
     (-> db
         (update :form (fn [{:keys [selected-language]}]
                         (cond-> form
@@ -760,7 +764,6 @@
         [secret-kwd secret-val]           (if-not (clojure.string/blank? secret)
                                             [:secret secret]
                                             [:virkailija-secret virkailija-secret])]
-    (prn "handle-get-application")
     (util/set-query-param "application-key" (:key application))
     {:db       (-> db
                    (assoc-in [:application :application-identifier] (:application-identifier application))
