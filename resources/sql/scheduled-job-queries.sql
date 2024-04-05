c087b65cc15f0de4cfa7b3050cdff8233e1784a6
Santeri Korri <santeri.korri@knowit.fi>
2024-04-05 16:58:07 +0300

    OY-3657 Lisätty raportointi

--- resources/sql/scheduled-job-queries.sql
+++ resources/sql/scheduled-job-queries.sql
@@ -9,3 +9,6 @@ SELECT job_type, enabled FROM job_types;
 
 -- name: yesql-update-job-type!
 UPDATE job_types SET enabled=:enabled WHERE job_type=:job_type;
+
+-- name: yesql-get-queue-lengths
+SELECT job_type, (SELECT count(1) FROM proletarian_jobs WHERE queue=job_types.job_type) AS length FROM job_types;
