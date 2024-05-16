2414e23cb1d91f80fef788795b457498342646fe
August Kilponen <august.kilponen@gmail.com>
2024-05-16 12:58:27 +0300

    Siivottu pois turha require

--- src/clj/ataru/virkailija/virkailija_routes.clj
+++ src/clj/ataru/virkailija/virkailija_routes.clj
@@ -86,7 +86,6 @@
             [ataru.applications.suoritus-filter :as suoritus-filter]
             [ataru.valintalaskentakoostepalvelu.pohjakoulutus-toinen-aste :as pohjakoulutus-toinen-aste]
             [cuerdas.core :as str]
-            [clj-time.format :as f]
             [ataru.virkailija.virkailija-application-service :as virkailija-application-service]
             [ataru.background-job.job :as job])
 
