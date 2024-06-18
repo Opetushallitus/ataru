c66ead5caf51a0d6e3b56425a86618c38040c83c
Väinö Ala-Härkönen <vae@wonna.fi>
2024-06-18 15:02:58 +0300

    OK-553 basic tests

--- spec/ataru/kk_application_payment/kk_application_payment_store_spec.clj
+++ spec/ataru/kk_application_payment/kk_application_payment_store_spec.clj
@@ -1,16 +1,27 @@
 (ns ataru.kk-application-payment.kk-application-payment-store-spec
-  (:require [speclj.core :refer [describe tags it should-not-be-nil should= should-not]]
-            [ataru.kk-application-payment.kk-application-payment-store :as store]))
+  (:require [speclj.core :refer [describe tags it should-not-be-nil should= should-not before-all]]
+            [ataru.kk-application-payment.kk-application-payment-store :as store]
+            [ataru.db.db :as db]
+            [clojure.java.jdbc :as jdbc]))
 
 (def test-person-oid "1.2.3.4.5.6")
 (def test-term-spring "kausi_k")
 (def test-term-fall "kausi_s")
 (def test-year "2025")
+(def test-year-2 "2026")
 (def test-state-pending "kk-application-payment-pending")
 (def test-state-paid "kk-application-payment-paid")
 
+(defn- delete-states-and-events! []
+  (jdbc/with-db-transaction [conn {:datasource (db/get-datasource :db)}]
+                            (jdbc/delete! conn :kk_application_payment_events [])
+                            (jdbc/delete! conn :kk_application_payment_states [])))
+
 (describe "kk application payment states"
           (tags :unit :kk-application-payment)
+
+          (before-all
+            (delete-states-and-events!))
 
           (it "should store payment states for person and terms"
               (let [spring-state (store/create-or-update-kk-application-payment-state!
@@ -43,4 +54,20 @@
                 (should= expected-fall-state (dissoc fall-state :created_time :modified_time)))))
 
 (describe "kk application payment events"
-          (tags :unit :kk-application-payment))
+          (tags :unit :kk-application-payment)
+
+          (before-all
+            (delete-states-and-events!))
+
+          (it "should store and retrieve payment events for payment state"
+              (let [state-id (:id (store/create-or-update-kk-application-payment-state!
+                                    test-person-oid test-term-spring test-year-2 test-state-pending))
+                    event-1-id (store/create-kk-application-payment-event!
+                              state-id test-state-paid "state-updated" nil nil)
+                    event-2-id (store/create-kk-application-payment-event!
+                              state-id nil "comment" nil "Kommentti")
+                    events (store/get-kk-application-payment-events [state-id])]
+                (should-not-be-nil event-1-id)
+                (should-not-be-nil event-2-id)
+                (should-not (= event-1-id event-2-id))
+                (should= 2 (count events)))))
