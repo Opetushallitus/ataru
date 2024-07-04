377f82435041fcc14a6d9763c82837a83e92c936
Joona Haavisto <joona.haavisto@gofore.com>
2024-07-04 09:28:08 +0300

    Testi korjauksia

--- spec/ataru/forms/form_payment_info_spec.clj
+++ spec/ataru/forms/form_payment_info_spec.clj
@@ -8,9 +8,9 @@
     [ataru.tarjonta-service.mock-tarjonta-service :as mts]))
 
 (def expected-payment-info
-  {:type "payment-type-kk",
-   :processing-fee (str payment-info/kk-processing-fee),
-   :decision-fee nil})
+  {:payment {:type "payment-type-kk",
+             :processing-fee (str payment-info/kk-processing-fee),
+             :decision-fee nil}})
 
 (def test-kk-form
   {:key              "payment-info-test-kk-form"
@@ -29,7 +29,7 @@
    :organization-oid "1.2.246.562.10.1234334543"})
 
 (def test-payment-info
-  {:type :payment-type-tutu :processing-fee 100 :decision-fee nil})
+  {:payment {:type :payment-type-tutu :processing-fee 100 :decision-fee nil}})
 
 (def test-kk-form-with-existing-payment-info
   (merge test-kk-form {:properties test-payment-info}))
