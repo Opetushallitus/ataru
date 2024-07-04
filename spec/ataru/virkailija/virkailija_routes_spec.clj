a0768e9eb8fa84d8f35214661d8d8234cfe835cf
Joona Haavisto <joona.haavisto@gofore.com>
2024-07-04 15:53:32 +0300

    Maksutyyppi radio-buttonit ja testikorjauksia

--- spec/ataru/virkailija/virkailija_routes_spec.clj
+++ spec/ataru/virkailija/virkailija_routes_spec.clj
@@ -555,10 +555,10 @@
                 {:paymentType :payment-type-astu :decisionFee "bird"}
                 {} 400))
 
-          (it "should set TUTU payment information, forcing a hardcoded processing fee"
+          (it "should set TUTU payment information"
               (update-and-check
                 {:paymentType :payment-type-tutu :processingFee "100.00"}
-                {:payment {:type "payment-type-tutu" :processing-fee "70.00" :decision-fee nil}}
+                {:payment {:type "payment-type-tutu" :processing-fee "100.00" :decision-fee nil}}
                 200))
 
           (it "should fail when trying to set a fixed decision fee for TUTU"
@@ -566,15 +566,14 @@
                 {:paymentType :payment-type-tutu :processingFee "100.00" :decisionFee "100.00"}
                 {} 400))
 
-          (it "should set ASTU payment information"
-              (update-and-check
-                {:paymentType :payment-type-astu :decisionFee "150.00"}
-                {:payment {:type "payment-type-astu" :processing-fee nil :decision-fee "150.00"}}
-                200))
-
           (it "should fail when trying to set a processing fee for ASTU"
               (update-and-check
                 {:paymentType :payment-type-astu :processingFee "100.00" :decisionFee "100.00"}
+                {} 400))
+
+          (it "should fail when trying to set a fixed decision fee for ASTU"
+              (update-and-check
+                {:paymentType :payment-type-astu :decisionFee "150.00"}
                 {} 400))
 
           (it "should not allow setting hakemusmaksu / kk payment information manually"
