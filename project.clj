c2e9b20b36be79eb42e02e0edf7a7e7f18e2c49e
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-08-14 10:26:46 +0300

    Poistettu jsch

--- project.clj
+++ project.clj
@@ -102,7 +102,6 @@
                  [com.amazonaws/aws-java-sdk-cloudwatch "1.12.558"]
                  [com.github.ben-manes.caffeine/caffeine "3.1.8"]
                  [org.clojure/data.xml "0.0.8"]
-                 [com.jcraft/jsch "0.1.55"]
                  [fi.vm.sade.dokumenttipalvelu/dokumenttipalvelu "6.12-SNAPSHOT"]
                  ; these two deps are for routing all other logging frameworks' output to timbre by first piping them to SLF4J and then timbre
                  [com.fzakaria/slf4j-timbre "0.4.0" :exclusions [io.aviso/pretty]]
