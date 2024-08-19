437446aee3ed88127968de81fcc119580ceeeba4
Mikko Siukola <mikko.siukola@cybercom.com>
2024-08-19 16:57:35 +0300

    OK-568 Add test, alter migration, fine-tune siirtotiedosto functionality

--- resources/sql/siirtotiedosto-queries.sql
+++ resources/sql/siirtotiedosto-queries.sql
@@ -1,14 +1,14 @@
 -- name: latest-siirtotiedosto-data
 -- Returns latest successful siirtotiedosto-operation data
-select id, window_start, window_end, run_start, run_end, info, success, error_message from siirtotiedostot
-where success order by run_start desc limit 1;
+select id, uuid, window_start, window_end, run_start::text, run_end::text, info, success, error_message from siirtotiedosto
+where success order by run_end desc limit 1;
 
 -- name: upsert-siirtotiedosto-data!
 -- Upserts siirtotiedosto-operation data
-insert into siirtotiedostot (id, window_start, window_end, run_start, run_end, info, success, error_message)
-values (:id::uuid, :window_start, :window_end, :run_start::timestamptz, :run_end::timestamptz,
+insert into siirtotiedosto (id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
+values (nextval('siirtotiedosto_id_seq'), :id::uuid, :window_start, :window_end, :run_start::timestamptz, :run_end::timestamptz,
         :info::jsonb, :success, :error_message)
-on conflict on constraint siirtotiedostot_pkey do update
+on conflict on constraint siirtotiedosto_pkey do update
     set window_start = :window_start,
         window_end = :window_end,
         run_start = :run_start::timestamptz,
--- src/clj/ataru/siirtotiedosto_app.clj
+++ src/clj/ataru/siirtotiedosto_app.clj
@@ -8,7 +8,7 @@
             [cheshire.core :as json]
             [ataru.config.core :refer [config]]
             [ataru.siirtotiedosto.siirtotiedosto-store :as siirtotiedosto-store]
-            [clj-time.jdbc] ; for java.sql.Timestamp / org.joda.time.DateTime coercion
+            [clj-time.jdbc]                                 ; for java.sql.Timestamp / org.joda.time.DateTime coercion
             )
   (:import java.time.ZonedDateTime
            java.time.format.DateTimeFormatter
@@ -26,20 +26,20 @@
                  (.format d DateTimeFormatter/ISO_OFFSET_DATE_TIME))))
 
 (defn create-siirtotiedosto-client [config]
-  (let [region   (-> config :siirtotiedostot :aws-region)
-        bucket   (-> config :siirtotiedostot :s3-bucket)
+  (let [region (-> config :siirtotiedostot :aws-region)
+        bucket (-> config :siirtotiedostot :s3-bucket)
         role-arn (-> config :siirtotiedostot :transferFileTargetRoleArn)
-        client   (new SiirtotiedostoPalvelu
-                      region
-                      bucket
-                      role-arn)]
-    (log/info "Created siirtotiedosto-client" client "with params" {:region region
-                                                                    :bucket bucket
+        client (new SiirtotiedostoPalvelu
+                    region
+                    bucket
+                    role-arn)]
+    (log/info "Created siirtotiedosto-client" client "with params" {:region   region
+                                                                    :bucket   bucket
                                                                     :role-arn role-arn})
     client))
 
 
-(defn- siirtotiedosto-data [last-siirtotiedosto-data execution-id current-date-str]
+(defn siirtotiedosto-data [last-siirtotiedosto-data execution-id current-date-str]
   (let [base-data {:id execution-id :window_end current-date-str :run_start current-date-str
                    :window_start (.format
                                    (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssZZZ")
@@ -50,10 +50,10 @@
         base-data)
       (merge base-data {:window_start (:window_end last-siirtotiedosto-data)}))))
 
-(defn- update-siirtotiedosto-data [orig-data operation-results]
+(defn update-siirtotiedosto-data [orig-data operation-results]
   (let [base-data (merge orig-data {:run_end (t/now)})]
     (if (= true (:success operation-results))
-      (merge base-data {:success true :info {:count (:total operation-results)}})
+      (merge base-data {:success true :info (:info operation-results)})
       (merge base-data {:success false :error-message (:error-msg operation-results)}))))
 
 (defn- run-migrations! []
@@ -73,9 +73,16 @@
         new-siirtotiedosto-data (siirtotiedosto-data last-siirtotiedosto-data execution-id current-datetime)]
     (log/info (str "Launching siirtotiedosto operation " execution-id ". Previous data: " last-siirtotiedosto-data ", new data " new-siirtotiedosto-data))
     (upsert-data-fn new-siirtotiedosto-data)
-    (let [siirtotiedosto-data-after-operation (->> (siirtotiedosto-service/siirtotiedosto-everything siirtotiedosto-service new-siirtotiedosto-data)
-                                                   (update-siirtotiedosto-data new-siirtotiedosto-data))]
-      (if (= true (:success siirtotiedosto-data-after-operation))
-        (log/info (str "Created siirtotiedostot " (json/generate-string siirtotiedosto-data-after-operation)))
-        (log/error (str "Siirtotiedosto operation failed; " (json/generate-string siirtotiedosto-data-after-operation))))
-      (upsert-data-fn siirtotiedosto-data-after-operation))))
+    (try
+      (let [siirtotiedosto-data-after-operation (->> (siirtotiedosto-service/siirtotiedosto-everything siirtotiedosto-service new-siirtotiedosto-data)
+                                                     (update-siirtotiedosto-data new-siirtotiedosto-data))]
+        (if (= true (:success siirtotiedosto-data-after-operation))
+          (log/info (str "Created siirtotiedostot " (json/generate-string siirtotiedosto-data-after-operation)))
+          (log/error (str "Siirtotiedosto operation failed: " (json/generate-string siirtotiedosto-data-after-operation))))
+        (upsert-data-fn siirtotiedosto-data-after-operation))
+      (catch Exception e
+        (let [failed-data (-> new-siirtotiedosto-data
+                              (assoc :success false)
+                              (assoc :error-message (.getMessage e)))]
+          (log/error "Siirtotiedosto operation failed: " e)
+          (upsert-data-fn failed-data))))))
