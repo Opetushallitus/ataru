e253cb1cd86668381607a438a3fe56a53dcf9f5f
Mikko Siukola <mikko.siukola@cybercom.com>
2024-08-26 13:39:42 +0300

    OK-568 Refactor next-siirtotiedosto-logic, fine-tune build

--- resources/sql/siirtotiedosto-queries.sql
+++ resources/sql/siirtotiedosto-queries.sql
@@ -1,18 +1,20 @@
 -- name: latest-siirtotiedosto-data
 -- Returns latest successful siirtotiedosto-operation data
-select id, uuid, window_start, window_end, run_start::text, run_end::text, info, success, error_message from siirtotiedosto
-where success order by run_end desc limit 1;
+-- There should always be at least one, as per siirtotiedosto table migration.
+select id, execution_uuid, window_start, window_end, run_start::text, run_end::text, info, success, error_message from siirtotiedosto
+where success order by id desc limit 1;
+
+-- name: insert-new-siirtotiedosto-operation!<
+-- Inserts new siirtotiedosto-operation data
+insert into siirtotiedosto (id, execution_uuid, window_start, window_end, run_start, run_end, info, success, error_message)
+values (nextval('siirtotiedosto_id_seq'), :execution_uuid::uuid, :window_start, now(), now(), null,
+        '{}'::jsonb, null, null) returning id, execution_uuid, window_start, window_end, info, success, error_message;
 
 -- name: upsert-siirtotiedosto-data!
 -- Upserts siirtotiedosto-operation data
-insert into siirtotiedosto (id, uuid, window_start, window_end, run_start, run_end, info, success, error_message)
-values (nextval('siirtotiedosto_id_seq'), :id::uuid, :window_start, :window_end, :run_start::timestamptz, :run_end::timestamptz,
-        :info::jsonb, :success, :error_message)
-on conflict on constraint siirtotiedosto_pkey do update
-    set window_start = :window_start,
-        window_end = :window_end,
-        run_start = :run_start::timestamptz,
-        run_end = :run_end::timestamptz,
-        info = :info,
-        success = :success,
-        error_message = :error_message;
+update siirtotiedosto
+    set run_end = now(),
+        info = :info::jsonb,
+        success = :success::boolean,
+        error_message = :error_message
+where id = :id;
--- src/clj/ataru/siirtotiedosto_app.clj
+++ src/clj/ataru/siirtotiedosto_app.clj
@@ -2,21 +2,13 @@
   (:require [ataru.db.flyway-migration :as migration]
             [ataru.log.audit-log :as audit-log]
             [ataru.siirtotiedosto-service :as siirtotiedosto-service]
-            [clj-time.core :as t]
             [cheshire.generate :refer [add-encoder]]
             [clojure.tools.logging :as log]
-            [cheshire.core :as json]
-            [ataru.config.core :refer [config]]
-            [ataru.siirtotiedosto.siirtotiedosto-store :as siirtotiedosto-store]
-            [clj-time.jdbc]                                 ; for java.sql.Timestamp / org.joda.time.DateTime coercion
-            )
+            [ataru.config.core :refer [config]])
   (:import java.time.ZonedDateTime
            java.time.format.DateTimeFormatter
            (ataru.siirtotiedosto_service CommonSiirtotiedostoService)
-           (fi.vm.sade.valinta.dokumenttipalvelu SiirtotiedostoPalvelu)
-           (java.text SimpleDateFormat)
-           (java.util Date)
-           (java.util UUID))
+           (fi.vm.sade.valinta.dokumenttipalvelu SiirtotiedostoPalvelu))
   (:gen-class))
 
 (add-encoder ZonedDateTime
@@ -38,51 +30,14 @@
                                                                     :role-arn role-arn})
     client))
 
-
-(defn siirtotiedosto-data [last-siirtotiedosto-data execution-id current-date-str]
-  (let [base-data {:id execution-id :window_end current-date-str :run_start current-date-str
-                   :window_start (.format
-                                   (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssZZZ")
-                                   (Date. 0)) :run_end nil :success nil :info nil :error_message nil}]
-    (if (not (boolean (:success last-siirtotiedosto-data)))
-      (do
-        (log/info "No successful siirtotiedosto data found, using base-data " base-data)
-        base-data)
-      (merge base-data {:window_start (:window_end last-siirtotiedosto-data)}))))
-
-(defn update-siirtotiedosto-data [orig-data operation-results]
-  (let [base-data (merge orig-data {:run_end (t/now)})]
-    (if (= true (:success operation-results))
-      (merge base-data {:success true :info (:info operation-results)})
-      (merge base-data {:success false :error-message (:error-msg operation-results)}))))
-
 (defn- run-migrations! []
   (let [audit-logger (audit-log/new-audit-logger "ataru-editori")]
     (migration/migrate audit-logger)))
 
 (defn -main [& _]
   (run-migrations!)
+  (log/info "Ovara-ataru up, creating client and service")
   (let [siirtotiedosto-client (create-siirtotiedosto-client config)
         siirtotiedosto-service (CommonSiirtotiedostoService. siirtotiedosto-client)
-        execution-id (str (UUID/randomUUID))
-        current-datetime (.format
-                           (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssZZZ")
-                           (Date.))
-        last-siirtotiedosto-data (siirtotiedosto-store/get-latest-data)
-        upsert-data-fn (fn [data] (siirtotiedosto-store/persist-siirtotiedosto-data data))
-        new-siirtotiedosto-data (siirtotiedosto-data last-siirtotiedosto-data execution-id current-datetime)]
-    (log/info (str "Launching siirtotiedosto operation " execution-id ". Previous data: " last-siirtotiedosto-data ", new data " new-siirtotiedosto-data))
-    (upsert-data-fn new-siirtotiedosto-data)
-    (try
-      (let [siirtotiedosto-data-after-operation (->> (siirtotiedosto-service/siirtotiedosto-everything siirtotiedosto-service new-siirtotiedosto-data)
-                                                     (update-siirtotiedosto-data new-siirtotiedosto-data))]
-        (if (= true (:success siirtotiedosto-data-after-operation))
-          (log/info (str "Created siirtotiedostot " (json/generate-string siirtotiedosto-data-after-operation)))
-          (log/error (str "Siirtotiedosto operation failed: " (json/generate-string siirtotiedosto-data-after-operation))))
-        (upsert-data-fn siirtotiedosto-data-after-operation))
-      (catch Exception e
-        (let [failed-data (-> new-siirtotiedosto-data
-                              (assoc :success false)
-                              (assoc :error-message (.getMessage e)))]
-          (log/error "Siirtotiedosto operation failed: " e)
-          (upsert-data-fn failed-data))))))
+        result (siirtotiedosto-service/form-next-siirtotiedosto siirtotiedosto-service)]
+    (log/info "ready!" result)))
