576b531895eb410a26b61edfa50ce41ca8d2d0e7
Risto Salama <risto.salama@gofore.com>
2024-11-04 16:33:55 +0200

    Add logging, spec and fix payment-module structure

---
+++ spec/ataru/component_data/kk_application_payment_module_spec.clj
@@ -0,0 +1,31 @@
+(ns ataru.component-data.kk-application-payment-module-spec
+  (:require [ataru.component-data.kk-application-payment-module :as payment-module]
+            [ataru.util :as util]
+            [speclj.core :refer :all]))
+
+
+(describe "kk-application-payment-module"
+          (tags :unit :attachments)
+
+          (it "should contain all the specified ids"
+              (let [keys-generated (->> (payment-module/kk-application-payment-module)
+                                        :children
+                                        util/flatten-form-fields
+                                        (map (comp name :id))
+                                        set)
+                    keys-to-check ["kk-application-payment-option"
+                                   "asiakasnumero-migri"
+                                   "passport-attachment"
+                                   "eu-blue-card-attachment"
+                                   "eu-passport-attachment"
+                                   "continuous-residence-permit-front"
+                                   "continuous-residence-permit-back"
+                                   "continuous-residence-passport-attachment"
+                                   "permanent-residence-permit"
+                                   "permanent-residence-passport-attachment"
+                                   "eu-family-member-permit"
+                                   "eu-family-passport-attachment"
+                                   "temporary-protection-ukraine-permit"
+                                   "none-passport-attachment"]]
+                (doseq [id keys-to-check]
+                  (should-contain id keys-generated)))))
