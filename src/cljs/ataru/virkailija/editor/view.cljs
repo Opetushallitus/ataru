15fe6ec2ed88b985a983e6cbda3af019324f252d
Risto Salama <risto.salama@gofore.com>
2024-12-02 14:42:02 +0200

    Fix merge error

--- src/cljs/ataru/virkailija/editor/view.cljs
+++ src/cljs/ataru/virkailija/editor/view.cljs
@@ -456,30 +456,6 @@
         [:div.editor-form__checkbox-with-label
          [:input.editor-form__plain-radio
           {:type      "radio"
-           :value     "payment-type-tutu"
-           :checked   (= (:type maksutiedot) "payment-type-tutu")
-           :id        "maksutyyppi-tutu-radio"
-           :disabled  disabled?
-           :on-change #(dispatch [:editor/change-maksutyyppi "payment-type-tutu"])
-           :data-test-id "maksutyyppi-tutu-radio"}]
-         [:label.editor-form__checkbox-label
-          {:for   "maksutyyppi-tutu-radio"}
-          @(subscribe [:editor/virkailija-translation :maksutyyppi-tutu-radio])]]
-        (when (= (:type maksutiedot) "payment-type-tutu")
-          [:div.editor-form__payment-properties-wrapper
-           [:div.editor-form__text-field-wrapper
-            [:label.editor-form__component-item-header
-             @(subscribe [:editor/virkailija-translation :kasittelymaksu-input])]
-            [:input.editor-form__text-field
-             {:data-test-id "tutu-processing-fee-input"
-              :type         "number"
-              :value        (:processing-fee maksutiedot)
-              :required     true
-              :disabled     disabled?
-              :on-change    #(dispatch [:editor/change-processing-fee (.-value (.-target %))])}]]])
-        [:div.editor-form__checkbox-with-label
-         [:input.editor-form__plain-radio
-          {:type      "radio"
            :value     "payment-type-astu"
            :checked   (= (:type maksutiedot) "payment-type-astu")
            :id        "maksutyyppi-astu-radio"
