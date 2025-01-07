6b8509020f2b768f1f3ae0477c006075f8d70afa
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2025-01-07 14:19:34 +0200

    OK-774: Peruutusmahdollisuus itse syötetyille tutkinnoille, tutkinnot excel -listauksiin

--- src/clj/ataru/applications/application_service.clj
+++ src/clj/ataru/applications/application_service.clj
@@ -525,8 +525,7 @@
             koski-tutkinnot       (future (some->> (when requested-tutkinto-levels (:person-oid application))
                                                    (koski/get-tutkinnot-for-oppija koski-service)
                                                    :opiskeluoikeudet
-                                                   (parse-koski-tutkinnot
-                                                     (str/split requested-tutkinto-levels #","))))]
+                                                   (parse-koski-tutkinnot requested-tutkinto-levels)))]
         (util/remove-nil-values {:application           (-> application
                                                             (dissoc :person-oid)
                                                             (assoc :person (get-person this application))
@@ -587,7 +586,8 @@
                                                                        tarjonta-service
                                                                        koodisto-cache
                                                                        organization-service
-                                                                       ohjausparametrit-service))]
+                                                                       ohjausparametrit-service
+                                                                       koski-service))]
           xls
           (throw (new RuntimeException "Excelin muodostaminen ei onnistunut"))))))
 
