10d553767f005a4ade04e0b67da257f2e774166c
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-04-29 08:40:58 +0300

    OPHTUTU-18: Testaamiseen clj-http client

--- src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_send_job.clj
+++ src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_send_job.clj
@@ -1,27 +1,29 @@
 (ns ataru.tutkintojen-tunnustaminen.tutkintojen-tunnustaminen-send-job
-  (:require [ataru.cas.client :as cas]
-            [ataru.config.url-helper :refer [resolve-url]]
-            [taoensso.timbre :as log]))
+  (:require
+    ;[ataru.cas.client :as cas]
+    [ataru.config.url-helper :refer [resolve-url]]
+    [clojure.data.json :as json]
+    [taoensso.timbre :as log]
+    [clj-http.client :as client]
+    ))
 
 
-(defn handle-send [{:keys [application-id]} {:keys [tutu-cas-client]}]
+(defn tutkintojen-tunnustaminen-send-handler [{:keys [application-id]} {:keys [tutu-cas-client]}]
   (log/info "STARTING sending application" application-id)
-  (let [response (cas/cas-authenticated-post tutu-cas-client
-                                             (resolve-url :tutu-service.hakemus)
-                                             {:hakemusOid application-id})]
-    (log/info response)
+  (log/info tutu-cas-client)
+  (let [url (resolve-url :tutu-service.hakemus)
+        req {:hakemusOid application-id}
+        ;TODO: Switch to tutu-cas-client
+        ;response (cas/cas-authenticated-post tutu-cas-client url req)
+        response (client/post url {:as           :auto
+                                   :coerce       :always
+                                   :content-type :application/json
+                                   :body         (json/write-str req)})
+        ]
+    (log/info "Response" response)
     (when (not= 200 (:status response))
       (throw (Exception. (str "Sending application " application-id " to Tutu failed"))))
     (log/info (str "Application " application-id " successfully sent to Tutu"))))
 
-
-
-(defn tutkintojen-tunnustaminen-send-handler [{:keys [application-id]} job-runner]
-  (log/info "Handling send for application" application-id)
-  (try (handle-send application-id job-runner)
-       (catch Exception e (log/error e (str "Error while sending application " application-id " to Tutu")))))
-
-
 (def job-definition {:handler tutkintojen-tunnustaminen-send-handler
-                     :type    (-> *ns* ns-name str)
-                     :schedule "* * * * *"})
+                     :type    (-> *ns* ns-name str)})
