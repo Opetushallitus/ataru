caf8852c015dddc2a6f6d41514df501d0c48efeb
Dmitry Marokhonov <dmitry.marokhonov@knowit.fi>
2025-05-09 14:56:12 +0300

    OY-4946: unit test fix

--- test/cljs/unit/ataru/hakija/application_hakukohde_handlers_test.cljs
+++ test/cljs/unit/ataru/hakija/application_hakukohde_handlers_test.cljs
@@ -1,16 +1,14 @@
 (ns ataru.hakija.application-hakukohde-handlers-test
-  (:require
-   [ataru.hakija.application-hakukohde-handlers :refer [filter-take-hakukohteet-and-ryhmat
-                                                        ;collect-root-ids-related-to-removable-hakukohde
-                                                        ;filter-by-children-id
-                                                        ;prepare-hakukohteet-data
-                                                        ]]
-   [cljs.test :refer-macros [deftest testing is]]))
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [ataru.hakija.application-hakukohde-handlers :refer [filter-take-hakukohteet-and-ryhmat
+                                                    ;collect-root-ids-related-to-removable-hakukohde
+                                                    ;filter-by-children-id
+                                                    ;prepare-hakukohteet-data
+                                                    ]]))
 
 (deftest test-filter-take-hakukohteet-and-ryhmat
-  (testing "inncludes only entries with belongs-to-hakukohteet or belongs-to-hakukohderyhma"
+  (testing "includes only entries with belongs-to-hakukohteet or belongs-to-hakukohderyhma"
     (let [data [{:foo 1}
                 {:belongs-to-hakukohteet ["123"]}
                 {:options [{:belongs-to-hakukohderyhma ["ryhma-1"]}]}]]
-      (is (= 2 [count (filter-take-hakukohteet-and-ryhmat data)] )))))
-
+      (is (= 2 (count (filter-take-hakukohteet-and-ryhmat data)))))))
