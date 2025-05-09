ea1f662ae717ceb4c4355d5f4b0723e20c66eb68
Dmitry Marokhonov <dmitry.marokhonov@knowit.fi>
2025-05-09 14:00:01 +0300

    OY-4946: Unit tests added + helper function moved to hakukohde-utils

--- src/cljs/ataru/hakija/application_hakukohde_util.cljs
+++ src/cljs/ataru/hakija/application_hakukohde_util.cljs
@@ -1,6 +1,7 @@
 (ns ataru.hakija.application-hakukohde-util
   (:require [ataru.util :as util]
             [clojure.string :as string]
+            [clojure.set :as s]
             [medley.core :refer [index-by map-vals]]))
 
 (defn opetuskielet-to-lang-codes [opetuskielet]
@@ -49,3 +50,104 @@
     {:hakukohde-hits  hakukohde-hits
      :rest-results    rest-results
      :hakukohde-query hakukohde-query}))
+
+(defn filter-take-hakukohteet-and-ryhmat [data]
+  (filter (fn [v] (or
+                   (contains? v :belongs-to-hakukohteet)
+                   (contains? v :belongs-to-hakukohderyhma)
+                   (some (fn [opt]
+                           (or (contains? opt :belongs-to-hakukohteet)
+                               (contains? opt :belongs-to-hakukohderyhma)))
+                         (:options v))))
+          data))
+
+(defn collect-root-ids-related-to-removable-hakukohde
+  [hakukohde-oid flat-content selected-hakukohteet]
+  (let [removable-ryhmat (:removable-ryhmat selected-hakukohteet)]
+    (->> flat-content
+         (filter
+          (fn [{:keys [belongs-to-hakukohteet belongs-to-hakukohderyhma options]}]
+            (or
+             (some #{hakukohde-oid} belongs-to-hakukohteet)
+             (seq (s/intersection removable-ryhmat (set belongs-to-hakukohderyhma)))
+             (some #(some #{hakukohde-oid} (:belongs-to-hakukohteet %)) options))))
+         (map :id)
+         set)))
+
+(defn filter-by-children-id
+  [questions id-set]
+  (filter (fn [item]
+            (or (contains? id-set (:id item))
+                (some (fn [child]
+                        (contains? id-set (:id child)))
+                      (:children item))))
+          questions))
+
+
+(defn prepare-hakukohteet-data [hakukohde-oid hakukohteet selected-hakukohteet-oids]
+  (let [removable-hakukohteet (->> hakukohteet
+                                   (filter #(= (:oid %) hakukohde-oid))
+                                   (map #(select-keys % [:hakukohderyhmat :oid :name])))
+        selected-hakukohteet  (->> hakukohteet
+                                   (filter #(some (fn [oid] (= (:oid %) oid)) selected-hakukohteet-oids))
+                                   (map #(select-keys % [:hakukohderyhmat :oid :name])))
+        selected-ryhmat       (->> selected-hakukohteet (mapcat :hakukohderyhmat)
+                                   set)
+        removable-ryhmat      (->> removable-hakukohteet
+                                   (mapcat :hakukohderyhmat)
+                                   (filter #(not (contains? selected-ryhmat %)))
+                                   set)]
+    {:selected-ryhmat selected-ryhmat
+     :removable-hakukohteet removable-hakukohteet
+     :removable-ryhmat removable-ryhmat}))
+
+(defn removable-ids-from-answers
+  [hakukohde-oid form-content-filtered hakukohteet-form-data root-ids]
+  (->> (util/flatten-form-fields form-content-filtered)
+       (filter #(contains? root-ids (:id %)))
+       (mapcat
+        (fn [child]
+          (let [options (:options child)
+                valid-option-ids
+                (->> options
+                     (filter
+                      (fn [opt]
+                        (let [belongs (set (:belongs-to-hakukohderyhma opt))
+                              belongs-to-hakukohteet (:belongs-to-hakukohteet opt)]
+                          (or
+                           (= [hakukohde-oid] belongs-to-hakukohteet)
+                           (and (not (empty? (s/intersection belongs (:removable-ryhmat hakukohteet-form-data))))
+                                (empty? (s/intersection belongs (:selected-ryhmat hakukohteet-form-data))))))))
+                     (map
+                      (fn [opt]
+                        (or (:id opt) (:id child)))))] ;fallback to parent :id
+            (when (seq valid-option-ids)
+              valid-option-ids))))
+       (remove nil?)
+       set))
+
+
+
+
+(defn invalidate-answers [answers keys root-keys]
+  (reduce
+   (fn [acc k]
+     (if-let [entry (get acc k)]
+       (let [original-value (:value entry)
+             cleared-value (cond
+                             (sequential? original-value) []
+                             (string? original-value) ""
+                             :else nil)
+             updated-entry (-> entry
+                               (assoc :valid (not-any? #(= % k) root-keys))
+                               (assoc :value cleared-value)
+                               (update :values
+                                       (fn [v]
+                                         (cond
+                                           (and (map? v) (contains? v :valid)) (assoc v :valid false)
+                                           (sequential? v) []
+                                           :else v))))]
+         (assoc acc k updated-entry))
+       acc))
+   answers
+   keys))
