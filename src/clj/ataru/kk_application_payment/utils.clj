9a4f921fa6b28a23d72434b83cd86cba38813537
Tomi Katajanheimo <tomi@katajanheimo.com>
2025-05-12 16:28:41 +0300

    OY-5083 oikeanmuotoiset maskatut urlit

--- src/clj/ataru/kk_application_payment/utils.clj
+++ src/clj/ataru/kk_application_payment/utils.clj
@@ -52,7 +52,7 @@
        :body       body
        :masks      (if-let [url (:payment-url data)]
                      [{:secret url
-                       :mask "<payment link redacted>"}]
+                       :mask   "https://maksulinkki-poistettu.opintopolku.fi/"}]
                      [])})))
 
 (defn get-application-language
