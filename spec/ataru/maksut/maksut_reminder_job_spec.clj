1e55fb66a6e30dd4458d675ed13db5513d9b6b20
Joona Haavisto <joona.haavisto@gofore.com>
2025-07-10 17:30:25 +0300

    Korjailuja

--- spec/ataru/maksut/maksut_reminder_job_spec.clj
+++ spec/ataru/maksut/maksut_reminder_job_spec.clj
@@ -6,23 +6,17 @@
             [ataru.maksut.maksut-store :as maksut-store]
             [clj-time.core :as c]
             [clojure.java.jdbc :as jdbc]
-            [ataru.fixtures.db.unit-test-db :as unit-test-db]
-            [ataru.fixtures.form :refer [minimal-form]]
             [ataru.background-job.job :as job]
             [com.stuartsierra.component :as component]
             [ataru.log.audit-log :as audit-log]
-            [speclj.core :refer [describe tags it should= before-all after around with-stubs stub should-have-invoked should-not-have-invoked]]
-            [yesql.core :as sql]))
+            [speclj.core :refer [describe tags it should= after around with-stubs stub should-have-invoked should-not-have-invoked]]))
 
 (declare conn)
 (declare spec)
 
 (defn- clear! []
   (jdbc/with-db-transaction [conn {:datasource (db/get-datasource :db)}]
-                            (jdbc/delete! conn :payment-reminders [])))
-
-(declare yesql-upsert-virkailija<!)
-(sql/defqueries "sql/virkailija-queries.sql")
+                            (jdbc/delete! conn :payment_reminders [])))
 
 (defn start-runner-job [_ _ _ _])
 
@@ -36,24 +30,9 @@
 (def runner
   (map->FakeJobRunner {}))
 
-(def application-key (atom nil))
-
 (describe "maksut-reminder-job"
           (tags :unit :maksut-reminder)
           (with-stubs)
-
-          (before-all
-            (db/exec :db yesql-upsert-virkailija<! {:oid        "1.2.246.562.11.11111111111"
-                                                    :first_name "Testi"
-                                                    :last_name  "Täydennyspyytäjä"})
-            (let [id (unit-test-db/init-db-fixture
-                       minimal-form
-                       {:form       (:id minimal-form),
-                        :lang       "fi"
-                        :person-oid "1.2.3.4.5.6"
-                        :answers [{:key "first-name" :value "Aku Petteri" :fieldType "textField" :label {:fi "Etunimet" :sv "Förnamn"}}]}
-                       nil)]
-              (reset! application-key (:key (application-store/get-application id)))))
 
           (after
             (clear!))
@@ -74,13 +53,14 @@
                                                                                 :status :active
                                                                                 :due_date (str (c/today))
                                                                                 :origin "tutu"
-                                                                                :reference @application-key}]})
-                            maksut-reminder-job/start-email-job (stub :start-email-job)]
-                (maksut-store/add-payment-reminder {:application-key @application-key
+                                                                                :reference "testihakemus"}]})
+                            maksut-reminder-job/start-email-job (stub :start-email-job)
+                            application-store/add-application-event-in-tx (stub :add-application-event)]
+                (maksut-store/add-payment-reminder {:application-key "testihakemus"
                                                     :message ""
                                                     :lang ""
-                                                    :send_reminder_time (c/today-at 10 0)
-                                                    :order_id "TTUtesti-2"})
+                                                    :send-reminder-time (c/today-at 10 0)
+                                                    :order-id "TTUtesti-2"})
                 (maksut-reminder-job/handler nil runner)
                 (should= 0 (count (maksut-store/get-payment-reminders)))
                 (should-have-invoked :start-email-job {:times 1})))
@@ -96,7 +76,14 @@
                                                                                 :status :paid
                                                                                 :due_date (str (c/today))
                                                                                 :origin "tutu"
-                                                                                :reference @application-key}]})
-                            maksut-reminder-job/start-email-job (stub :start-email-job)]
+                                                                                :reference "testihakemus"}]})
+                            maksut-reminder-job/start-email-job (stub :start-email-job)
+                            maksut-store/set-reminder-handled (stub :set-reminder-handled)]
+                (maksut-store/add-payment-reminder {:application-key "testihakemus"
+                                                    :message ""
+                                                    :lang ""
+                                                    :send-reminder-time (c/today-at 10 0)
+                                                    :order-id "TTUtesti-2"})
                 (maksut-reminder-job/handler nil runner)
+                (should-have-invoked :set-reminder-handled {:with [:* "paid"]})
                 (should-not-have-invoked :start-email-job))))
