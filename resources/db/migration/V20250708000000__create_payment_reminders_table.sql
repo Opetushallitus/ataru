40a1810d9725ab038ee8cc552c22cef5ffd1998f
Joona Haavisto <joona.haavisto@gofore.com>
2025-07-11 13:49:58 +0300

    Review korjauksia

--- resources/db/migration/V20250708000000__create_payment_reminders_table.sql
+++ resources/db/migration/V20250708000000__create_payment_reminders_table.sql
@@ -1,10 +1,12 @@
+CREATE TYPE payment_reminder_status AS ENUM ('sent', 'paid', 'overdue', 'invalidated');
+
 CREATE TABLE IF NOT EXISTS payment_reminders (
     id                 BIGSERIAL PRIMARY KEY,
     application_key    VARCHAR(256) NOT NULL,
     order_id           VARCHAR(256) NOT NULL UNIQUE,
-    message            TEXT,
-    lang               TEXT,
+    message            TEXT DEFAULT '',
+    lang               VARCHAR(2) NOT NULL,
     send_reminder_time TIMESTAMP WITH TIME ZONE NOT NULL,
-    status             TEXT,
+    status             payment_reminder_status,
     handled_at         TIMESTAMP WITH TIME ZONE
 );
