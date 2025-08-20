d8cc9dac8c319732a6b1032a908b43fd6a12fe43
Tomi Katajanheimo <tomi@katajanheimo.com>
2025-08-20 11:33:48 +0300

    OK-996 fix cron syntax

--- src/clj/ataru/kk_application_payment/kk_application_payment_status_updater_job.clj
+++ src/clj/ataru/kk_application_payment/kk_application_payment_status_updater_job.clj
@@ -313,4 +313,4 @@
 
 (def scheduler-job-definition {:handler  update-kk-payment-status-for-all-handler
                                :type     "kk-application-payment-status-update-scheduler-job"
-                               :schedule "0 6/12 * * *"})
+                               :schedule "0 6,18 * * *"})
