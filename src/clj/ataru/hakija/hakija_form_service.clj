b6cfde8e6662f1d641ad0c95a463b38557fa911a
Marja Kari <marja.kari@amban.fi>
2025-09-03 17:15:07 +0300

    OY-5195 yleiskäyttöinen util synteettisen hakemuksen form-id:n hakuun

--- src/clj/ataru/hakija/hakija_form_service.clj
+++ src/clj/ataru/hakija/hakija_form_service.clj
@@ -7,7 +7,6 @@
             [ataru.hakija.person-info-fields :refer [viewing-forbidden-person-info-field-ids
                                                      editing-forbidden-person-info-field-ids
                                                      editing-allowed-person-info-field-ids]]
-            [ataru.ohjausparametrit.ohjausparametrit-protocol :as ohjausparametrit]
             [ataru.tarjonta-service.tarjonta-parser :as tarjonta-parser]
             [ataru.tarjonta-service.tarjonta-protocol :as tarjonta]
             [ataru.tarjonta-service.hakukohde :refer [populate-hakukohde-answer-options populate-attachment-deadlines]]
@@ -27,7 +26,8 @@
             [taoensso.timbre :as log]
             [ataru.demo-config :as demo]
             [ataru.hakija.toisen-asteen-yhteishaku-logic :as toisen-asteen-yhteishaku-logic]
-            [ataru.kk-application-payment.utils :refer [has-payment-module?]]))
+            [ataru.kk-application-payment.utils :refer [has-payment-module?]]
+            [ataru.ohjausparametrit.utils :as ohjausparametrit-utils]))
 
 (defn- set-can-submit-multiple-applications-and-yhteishaku
   [multiple? yhteishaku? haku-oid field]
@@ -275,10 +275,6 @@
   [form hakuajat now]
   (assoc form :demo-allowed (is-demo-allowed? form hakuajat now)))
 
-(defn- synthetic-application-form-key
-  [ohjausparametrit-service haku-oid]
-  (get (ohjausparametrit/get-parametri ohjausparametrit-service haku-oid) :synteettisetLomakeavain))
-
 (s/defn ^:always-validate fetch-form-by-id :- s/Any
   ([id :- s/Any
    roles :- [form-role/FormRole]
@@ -392,7 +388,7 @@
                                                      is-rewrite-secret-used?
                                                      haku)
         form-key (or (:ataru-form-key haku)
-                     (synthetic-application-form-key ohjausparametrit-service haku-oid))
+                     (ohjausparametrit-utils/synthetic-application-form-key ohjausparametrit-service haku-oid))
         latest-id (some-> form-key form-store/latest-id-by-key)]
     (when latest-id
       (fetch-form-by-haku-oid-and-id form-by-id-cache
--- src/clj/ataru/virkailija/virkailija_routes.clj
+++ src/clj/ataru/virkailija/virkailija_routes.clj
@@ -29,7 +29,6 @@
             [clj-ring-db-session.session.session-client :as session-client]
             [ataru.maksut.maksut-protocol :as maksut-protocol]
             [ataru.middleware.user-feedback :as user-feedback]
-            [ataru.ohjausparametrit.ohjausparametrit-protocol :as ohjausparametrit]
             [ataru.schema.form-schema :as ataru-schema]
             [ataru.schema.form-element-schema :as form-schema]
             [ataru.schema.maksut-schema :as maksut-schema]
@@ -52,6 +51,7 @@
             [ataru.valinta-laskenta-service.valintalaskentaservice-protocol :as vls]
             [ataru.valinta-tulos-service.valintatulosservice-protocol :as vts]
             [ataru.virkailija.authentication.auth-utils :as auth-utils]
+            [ataru.ohjausparametrit.utils :as ohjausparametrit-utils]
             [clj-ring-db-session.authentication.auth-middleware :as crdsa-auth-middleware]
             [cheshire.core :as json]
             [cheshire.generate :refer [add-encoder]]
@@ -181,10 +181,6 @@
                                 :cookie-attrs {:secure (not (:dev? env))}
                                 :store        session-store})))
 
-(defn- synthetic-application-form-key
-  [ohjausparametrit-service haku-oid]
-  (get (ohjausparametrit/get-parametri ohjausparametrit-service haku-oid) :synteettisetLomakeavain))
-
 (api/defroutes test-routes
   (api/undocumented
     (api/GET "/virkailija-test.html" []
@@ -294,7 +290,7 @@
       (if-let [key (:ataru-form-key (tarjonta/get-haku tarjonta-service haku-oid))]
         (ok (->> (form-store/fetch-by-key key)
                  (koodisto/populate-form-koodisto-fields koodisto-cache)))
-        (if-let [synthetic-key (synthetic-application-form-key ohjausparametrit-service haku-oid)]
+        (if-let [synthetic-key (ohjausparametrit-utils/synthetic-application-form-key ohjausparametrit-service haku-oid)]
           (do
             (log/info "Fetching synthetic application form with key" synthetic-key)
             (ok (->> (form-store/fetch-by-key synthetic-key)
