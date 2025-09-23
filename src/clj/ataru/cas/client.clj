cce0dfcebd937eed7c1299ebb8517aff9c39e0f6
Marja Kari <marja.kari@amban.fi>
2025-09-23 16:53:31 +0300

    OY-5245 korjattu kouta-internalin cas-url, cas client get oletus-retryllä, lisäheaderit myös cas-authenticated-delete funktioon

--- src/clj/ataru/cas/client.clj
+++ src/clj/ataru/cas/client.clj
@@ -2,7 +2,7 @@
   (:require [ataru.config.url-helper :refer [resolve-url]]
             [ataru.config.core :refer [config]]
             [taoensso.timbre :as log])
-  (:import [fi.vm.sade.javautils.nio.cas CasConfig CasConfig$CasConfigBuilder CasClientBuilder]
+  (:import [fi.vm.sade.javautils.nio.cas CasConfig$CasConfigBuilder CasClientBuilder]
            [org.asynchttpclient RequestBuilder]))
 
 
@@ -37,20 +37,33 @@
                       (.setMethod "GET")
                       (.setUrl url)
                       .build)]
-      (.executeAndRetryWithCleanSessionOnStatusCodes client request #{401 302}))
+      ;(.executeAndRetryWithCleanSessionOnStatusCodes client request #{401 302}))
+      (.execute client request))
     (catch Exception e
       (do
         (log/error "Error during CAS authenticated GET: " (.getMessage e))
         (throw e)))))
 
+(defn apply-extra-opts [base-request extra-opts]
+  (reduce (fn [req [k v]]
+            (case k
+              :headers (reduce (fn [r [header-key header-value]]
+                                 (.addHeader r header-key header-value))
+                               req v)
+              ;; future opts can be added here
+              req))
+          base-request
+          extra-opts))
+
 (defn cas-authenticated-delete [client url & [opts-fn]]
-  (let [request (-> (RequestBuilder.)
-                    (.setMethod "DELETE")
-                    (.setUrl url)
-                    (.addHeader "Content-type" "application/json")
-                    (.addHeader "Accept" "application/json")
-                    .build)]
-    (.executeAndRetryWithCleanSessionOnStatusCodes client request #{401 302})))
+  (let [base-request (-> (RequestBuilder.)
+                         (.setMethod "DELETE")
+                         (.setUrl url)
+                         (.addHeader "Content-type" "application/json")
+                         (.addHeader "Accept" "application/json"))
+        extra-opts   (if opts-fn (opts-fn) {})
+        request      (apply-extra-opts base-request extra-opts)]
+    (.executeAndRetryWithCleanSessionOnStatusCodes client (.build request) #{401 302})))
 
 (defn cas-authenticated-post [client url body & [opts-fn]]
   (log/debug "Performing CAS authenticated POST to URL:" url)
@@ -61,15 +74,36 @@
                          (.addHeader "Accept" "application/json")
                          (.setBody body))
         extra-opts   (if opts-fn (opts-fn) {})
-        request      (reduce (fn [req [k v]]
-                               (case k
-                                 :headers (reduce (fn [r [header-key header-value]]
-                                                    (.addHeader r header-key header-value))
-                                                  req v)
-                                 req))
-                             base-request
-                             extra-opts)]
+        request      (apply-extra-opts base-request extra-opts)]
     (.executeAndRetryWithCleanSessionOnStatusCodes client (.build request) #{401 302})))
+
+;(defn cas-authenticated-delete [client url & [opts-fn]]
+;  (let [request (-> (RequestBuilder.)
+;                    (.setMethod "DELETE")
+;                    (.setUrl url)
+;                    (.addHeader "Content-type" "application/json")
+;                    (.addHeader "Accept" "application/json")
+;                    .build)]
+;    (.executeAndRetryWithCleanSessionOnStatusCodes client request #{401 302})))
+;
+;(defn cas-authenticated-post [client url body & [opts-fn]]
+;  (log/debug "Performing CAS authenticated POST to URL:" url)
+;  (let [base-request (-> (RequestBuilder.)
+;                         (.setMethod "POST")
+;                         (.setUrl url)
+;                         (.addHeader "Content-type" "application/json")
+;                         (.addHeader "Accept" "application/json")
+;                         (.setBody body))
+;        extra-opts   (if opts-fn (opts-fn) {})
+;        request      (reduce (fn [req [k v]]
+;                               (case k
+;                                 :headers (reduce (fn [r [header-key header-value]]
+;                                                    (.addHeader r header-key header-value))
+;                                                  req v)
+;                                 req))
+;                             base-request
+;                             extra-opts)]
+;    (.executeAndRetryWithCleanSessionOnStatusCodes client (.build request) #{401 302})))
 
 (defn cas-authenticated-multipart-post [client url body & [opts-fn]]
   (let [base-request (-> (RequestBuilder.)
@@ -95,7 +129,8 @@
                     (.setUrl url)
                     (.addHeader "Accept" "application/octet-stream")
                     .build)]
-    (.executeAndRetryWithCleanSessionOnStatusCodes cas-client request #{401 302})))
+;    (.executeAndRetryWithCleanSessionOnStatusCodes cas-client request #{401 302})))
+    (.execute cas-client request)))
 
 (defn cas-authenticated-patch [client url body & [opts-fn]]
   (let [base-request (-> (RequestBuilder.)
