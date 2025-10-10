8a4a3d18c14ce5c65710fa2b8d7c8d4704891e2a
Tomi Katajanheimo <tomi@katajanheimo.com>
2025-10-10 11:06:59 +0300

    OK-1064 fix get from field-deadlines

--- src/clj/ataru/kk_application_payment/kk_application_payment.clj
+++ src/clj/ataru/kk_application_payment/kk_application_payment.clj
@@ -279,10 +279,7 @@
 (defn- keep-if-deadline-passed
   [attachment-deadline-service field-deadlines form haku now application-submitted review]
   (let [id             (:attachment-key review)
-        field-deadline (some->> field-deadlines
-                                (filter #(= id (:field-id %)))
-                                first
-                                :deadline)
+        field-deadline (get field-deadlines [id :deadline])
         field          (get (util/form-fields-by-id form) (keyword id))
         passed         (cond
                          (some? field-deadline)
