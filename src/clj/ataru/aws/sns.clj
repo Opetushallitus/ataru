81782d209608127849d3b53ec668df4f4025f80d
Topias Heinonen <topias.heinonen@gofore.com>
2025-11-06 11:58:31 +0200

    OY-5245 AWS SDK päivitys (#1771)
    Päivitetään CloudWatch, S3 ja SQS -kirjastot uusimpaan versioon. Näissä
    on lähinnä teknisiä muutoksia, kun uusi rajapinta on erilainen kun
    aikasempi. Samat toiminnallisuudet kuitenkin löytyvät.
    
    AWS:n Java SDK:n versioon 2 ei ole toteutettu vanhan SnsMessageManager
    luokan mukaista toiminnallisuutta. Onneksi sitä ei tarvitakaan.
    
    Ataruun tulee SNS-viestejä vain SQS:stä. Aiemmin viesteille tehtiin
    kuitenkin tarkastuksia, jotka ovat tarpeellisia kun viestit otetaan
    vastaan HTTPS-rajapinnan kautta. Allekirjoituksen tarkistaminen ei ole
    hyödyllistä, kun tiedämme, ettei SQS:ään voi kirjoittaa ulkopuoliset.
    
    Samaten SnsSubscriptionConfirmation ja SnsUnsubscribeConfirmation
    liittyvät vain HTTPS-endpontteihin, joten niiden käsittelyä ei tarvitse
    ottaa huomioon.
    
    Varmuuden vuoksi tarkistetaan kuitenkin, että vastaanotettu viesti on
    Notification-tyyppiä.
    
    Ilman näitä tarkistuksia, ei tarvita mitään SnsMessageManagerin
    toimintoja, eikä siis koko SNS-SDK:takaan.

--- src/clj/ataru/aws/sns.clj
+++ src/clj/ataru/aws/sns.clj
@@ -1,51 +1,11 @@
 (ns ataru.aws.sns
-  (:require [ataru.config.core :refer [config]]
-            [com.stuartsierra.component :as component]
-            [taoensso.timbre :as log])
-  (:import [com.amazonaws.services.sns.message
-            SnsMessageManager
-            SnsNotification
-            SnsSubscriptionConfirmation
-            SnsUnknownMessage
-            SnsUnsubscribeConfirmation]
-           [java.io InputStream ByteArrayInputStream]))
+  (:require [cheshire.core :as json]
+            [taoensso.timbre :as log]))
 
-(defrecord SNSMessageManager [sns-message-manager]
-  component/Lifecycle
-  (start [this]
-    (if (nil? sns-message-manager)
-      (assoc this :sns-message-manager (new SnsMessageManager
-                                            (:region (:aws config))))
-      this))
-  (stop [this]
-    (assoc this :sns-message-manager nil)))
-
-(defmulti handle-message (fn [_ s] (class s)))
-
-(defmethod handle-message InputStream [sns-message-manager input-stream]
-  (->> input-stream
-       (.parseMessage (:sns-message-manager sns-message-manager))
-       (handle-message sns-message-manager)))
-
-(defmethod handle-message String [sns-message-manager string]
-  (->> (.getBytes string "UTF-8")
-       (new ByteArrayInputStream)
-       (handle-message sns-message-manager)))
-
-(defmethod handle-message SnsNotification
-  [_ message]
-  message)
-
-(defmethod handle-message SnsSubscriptionConfirmation
-  [_ message]
-  (log/info "Confirming subscription to" (.getTopicArn message))
-  (.confirmSubscription message)
-  (log/info "Subscription to" (.getTopicArn message) "confirmed"))
-
-(defmethod handle-message SnsUnknownMessage
-  [_ message]
-  (log/error "Unknown SNS message" message))
-
-(defmethod handle-message SnsUnsubscribeConfirmation
-  [_ message]
-  (log/warn "Unsubscribed from" (.getTopicArn message)))
+(defn handle-message [string]
+  (let [message (json/parse-string string true)]
+    (if-let [type (:Type message)]
+      (if (= "Notification" type)
+        (:Message message)
+        (log/error "Unknown SNS message type" type "in" string))
+      (log/error "Unknown SNS message" string))))
--- src/clj/ataru/temp_file_storage/s3_client.clj
+++ src/clj/ataru/temp_file_storage/s3_client.clj
@@ -1,25 +1,25 @@
 (ns ataru.temp-file-storage.s3-client
   (:require [com.stuartsierra.component :as component]
             [ataru.config.core :refer [config]])
-  (:import [com.amazonaws.services.s3 AmazonS3Client]
-           [com.amazonaws.regions Regions]))
+  (:import software.amazon.awssdk.services.s3.S3Client
+           software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+           software.amazon.awssdk.regions.Region))
 
-(defrecord S3Client [credentials-provider s3-client]
+(defrecord AmazonS3Client [credentials-provider s3-client]
   component/Lifecycle
 
   (start [this]
     (if (nil? s3-client)
-      (assoc this :s3-client (-> (AmazonS3Client/builder)
-                                 (.withRegion (Regions/fromName (get-in config [:aws :region] "eu-west-1")))
-                                 (.withCredentials
-                                  (:credentials-provider credentials-provider))
+      (assoc this :s3-client (-> (S3Client/builder)
+                                 (.region (Region/of (get-in config [:aws :region] "eu-west-1")))
+                                 (.credentialsProvider ^AwsCredentialsProvider (:credentials-provider credentials-provider))
                                  (.build)))
       this))
 
   (stop [this]
-    (when-let [client (:s3-client this)]
-      (.shutdown client))
+    (when-let [^S3Client client (:s3-client this)]
+      (.close client))
     (assoc this :s3-client nil)))
 
 (defn new-client []
-  (map->S3Client {}))
+  (map->AmazonS3Client {}))
