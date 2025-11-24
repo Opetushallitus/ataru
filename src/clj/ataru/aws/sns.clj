b78eb79a0ba436503ec9c6a7fcf7a80e1527d0da
Marja Kari <marja.kari@amban.fi>
2025-11-24 15:33:49 +0200

    rebase fix

--- src/clj/ataru/aws/sns.clj
+++ src/clj/ataru/aws/sns.clj
@@ -7,9 +7,6 @@
   (let [message (json/parse-string string true)]
     (if-let [type (:Type message)]
       (if (= "Notification" type)
-        (-> message
-            (:Message)
-            (s/replace "\\" "")
-            (json/parse-string true))
+         (s/replace (:Message message) "\\" "")
         (log/error "Unknown SNS message type" type "in" string))
       (log/error "Unknown SNS message" string))))
