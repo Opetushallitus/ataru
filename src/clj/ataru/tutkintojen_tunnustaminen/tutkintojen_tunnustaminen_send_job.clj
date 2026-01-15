884be5d56a76edaf0ff81916821e83d7c812419b
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2026-01-15 14:48:22 +0200

    OPHTUTU-69: Hakemuksen lähettäminen tutuun oletetaan onnistuneeksi jos http-koodi on 200 tai 204

--- src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_send_job.clj
+++ src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_send_job.clj
@@ -10,10 +10,11 @@
   (let [url (resolve-url :tutu-service.hakemus)
         req {:hakemusOid    key
              :hakemusKoskee (or apply-reason apply-reason-lopullinen-paatos)} ;; Default to Lopullinen päätös if apply-reason is nil
-        response (cas/cas-authenticated-post tutu-cas-client url req)]
+        response (cas/cas-authenticated-post tutu-cas-client url req)
+        status (:status response)]
 
     (log/info "Response" response)
-    (when (not= 200 (:status response))
+    (when (and (not= 200 status)(not= 204 status))
       (throw (Exception. (str "Sending application " (:application-key key) " to Tutu failed"))))
     (log/info (str "Application " (:application-key key) " successfully sent to Tutu")))
   )
