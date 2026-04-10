cb51d713ee0e7fb6eaca0a7d52f4fbfcda1dcf2d
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-04-10 19:05:18 +0300

    Ajetaan CI:ssä mocha-testit makella ja eri jobissa kuin muut clojure-testit

--- bin/cibuild.sh
+++ bin/cibuild.sh
@@ -24,27 +24,6 @@ pnpm-dependencies() {
     export CHROME_BIN=$(node -e "console.log(require('puppeteer').executablePath());")
 }
 
-type_check() {
-  echo "Running TypeScript type check"
-  time pnpm run tsc:type-check
-}
-
-eslint() {
-    echo "Running ESLint"
-    pnpm run lint:js
-}
-
-clj_kondo() {
-    echo "Running clj-kondo"
-    pnpm run lint:clj
-}
-
-lint() {
-    type_check
-    eslint
-    clj_kondo
-}
-
 start_fake_deps_server() {
   ./bin/fake-deps-server.sh start
 }
@@ -58,37 +37,6 @@ build-clojurescript() {
     time ./bin/lein cljsbuild once virkailija-min hakija-min
 }
 
-test-clojure() {
-    echo "Running clojure tests"
-    start_fake_deps_server
-    time ./bin/lein spec -t ~ui
-    stop_fake_deps_server
-}
-
-test-clojurescript() {
-    echo "Testing clojurescript"
-    time ./bin/lein doo chrome test once
-}
-
-test-browser-mocha() {
-  start_fake_deps_server
-  time ./bin/lein spec -t ui
-
-  stop_fake_deps_server
-}
-
-run-migrations() {
-    echo "Running migrations"
-    start_fake_deps_server
-    time ./bin/lein with-profile test run -m ataru.db.flyway-migration/migrate "use dummy-audit-logger!"
-    stop_fake_deps_server
-}
-
-nuke-test-db() {
-    echo "Nuking test database"
-    time ./bin/lein with-profile test run -m ataru.fixtures.db.unit-test-db/clear-database
-}
-
 create-both-uberjars() {
     clean
     build-clojurescript
@@ -96,26 +44,6 @@ create-both-uberjars() {
     compile-less
     echo "Creating uberjar"
     time ./bin/lein with-profile ataru-main:ovara uberjar
-}
-
-run-spec-and-mocha-tests() {
-    export CONFIG=${CONFIG:-config/test.edn}
-    clean
-    pnpm-dependencies
-    lint
-    echo "Starting Docker containers for Mocha tests"
-    docker compose up -d ataru-test-db ataru-test-redis ataru-test-ftpd
-    echo "Starting spec and mocha test run"
-    test-clojurescript
-    nuke-test-db
-    run-migrations
-    test-clojure
-    compile-less
-    build-clojurescript
-    test-browser-mocha
-    echo "Stopping Docker containers used by Mocha tests"
-    docker compose kill ataru-test-db ataru-test-redis ataru-test-ftpd
-    docker compose rm -f ataru-test-db ataru-test-redis ataru-test-ftpd
 }
 
 run-browser-tests-integration() {
@@ -135,9 +63,6 @@ case "$command" in
         ;;
     "run-browser-tests-integration" )
         run-browser-tests-integration
-        ;;
-    "run-spec-and-mocha-tests" )
-        run-spec-and-mocha-tests
         ;;
     *)
         echo "Unknown command $command. Available commands:
