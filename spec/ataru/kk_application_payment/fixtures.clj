d385cb54fc9a4c4d2fb68617072e9a94d2bfb4ff
Topias Heinonen <topias.heinonen@gofore.com>
2026-08-07 13:34:07 +0300

    OPHHAMA-21 Vapautus hakemusmaksusta ETA + Sveitsi
    Vaihdetaan hakeuksesta vapauttaminen käyttämään määritelmän mukaisesti
    ETA-valtioryhmää ja Sveitsiä. Aiempi EU ja EFTA oli vähän hack.

--- spec/ataru/kk_application_payment/fixtures.clj
+++ spec/ataru/kk_application_payment/fixtures.clj
@@ -6,6 +6,12 @@
     :value "EU"
     :label {}
     :valid { :start "2015-09-03T00:00:00+03:00" }
+    :within {}}
+   {:uri "valtioryhmat_2"
+    :version 1
+    :value "ETA"
+    :label {}
+    :valid { :start "2015-09-03T00:00:00+03:00" }
     :within [{:uri "maatjavaltiot2_246"
               :version 1
               :value "246"}
@@ -20,30 +26,10 @@
               :value "233"}
              {:uri "maatjavaltiot2_056"
               :version 1
-              :value "056"}]}
-   {:uri "valtioryhmat_2"
-    :version 1
-    :value "ETA"
-    :label {}
-    :valid { :start "2015-09-03T00:00:00+03:00" }
-    :within {}}
-   {:uri "valtioryhmat_2"
-    :version 1
-    :value "EFTA"
-    :label {}
-    :valid { :start "2015-09-03T00:00:00+03:00" }
-    :within [{:uri "maatjavaltiot2_352"
-              :version 1
-              :value "352"}
-             {:uri "maatjavaltiot2_438"
-              :version 1
-              :value "438"}
+              :value "056"}
              {:uri "maatjavaltiot2_578"
               :version 1
-              :value "578"}
-             {:uri "maatjavaltiot2_756"
-              :version 1
-              :value "756"}]}])
+              :value "578"}]}])
 
 (defn haku-with-hakuajat
   [hakuaika-start hakuaika-end]
