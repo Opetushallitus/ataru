7ae5c0b758ff2093294bd1ccd09ebcfef90b46f1
Marja Kari <marja.kari@amban.fi>
2026-09-08 15:23:12 +0300

    OY-5756 kuollutta tilastokeskus-apin käyttämää koodia opiskelijoiden-suoritukset pois koostepalvelu-apista

--- src/clj/ataru/valintalaskentakoostepalvelu/valintalaskentakoostepalvelu_client.clj
+++ src/clj/ataru/valintalaskentakoostepalvelu/valintalaskentakoostepalvelu_client.clj
@@ -70,14 +70,6 @@
                            "response body: "
                            (:body result))))))
 
-(defn opiskelijoiden-suoritukset-by-personoid
-  [valintalaskentakoostepalvelu-cas-client haku-oid hakemus-oids]
-  (let [url          (url/resolve-url :valintalaskentakoostepalvelu-service.opiskelijan-suoritukset haku-oid)
-        request-body hakemus-oids
-        result       (cas/cas-authenticated-post
-                       valintalaskentakoostepalvelu-cas-client
-                       url
-                       request-body)]
     (match result
            {:status 200 :body response-body}
            (json/parse-string response-body true)
