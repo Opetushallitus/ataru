0c870753e52cd767f6b0eedb0c5afa3b46039a7f
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2026-09-16 16:13:49 +0300

    OPHTUTU-388: Parannettu virhelogitusta

--- src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_information_request_notify_job.clj
+++ src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_information_request_notify_job.clj
@@ -22,5 +22,5 @@
                  :latestVersionCreated (:created tutu-application)}
             response (cas/cas-authenticated-put tutu-cas-client url req)]
         (when (not (<= 200 (:status response) 299))
-          (throw (Exception. (str "Sending information-request notification for application " application-key " to Tutu failed"))))
+          (throw (Exception. (str "Sending information-request notification for application " application-key " to Tutu failed, status: " (:status response) ", body: " (:body response)))))
         (log/info (str "Information-request notification for application " application-key " successfully sent to Tutu"))))))
