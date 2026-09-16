eb93f65306f4876384be7580ead823ecc20fece0
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2026-09-16 13:16:30 +0300

    OPHTUTU-388: Muutettu tutu edit jobi välittämään hakemuksen tiedot PUT:illa

--- src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_edit_notify_job.clj
+++ src/clj/ataru/tutkintojen_tunnustaminen/tutkintojen_tunnustaminen_edit_notify_job.clj
@@ -6,13 +6,16 @@
     [ataru.tutkintojen-tunnustaminen.tutkintojen-tunnustaminen-utils :refer [get-form tutu-form?]]
     [taoensso.timbre :as log]))
 
+(def forwarded-application-fields [:form_id :content :created :modified :submitted :application-hakukohde-reviews :information-request-timestamp])
+
 (defn tutkintojen-tunnustaminen-edit-handler [{:keys [application-key]} {:keys [form-by-id-cache koodisto-cache attachment-deadline-service tutu-cas-client]}]
   (let [tutu-application (application-store/get-tutu-application application-key)
         form (get-form form-by-id-cache koodisto-cache attachment-deadline-service
                        (assoc tutu-application :form-id (:form_id tutu-application)))]
     (when (tutu-form? form)
       (let [url (resolve-url :tutu-service.hakemus-update application-key)
-            response (cas/cas-authenticated-get tutu-cas-client url)]
+            req (select-keys tutu-application forwarded-application-fields)
+            response (cas/cas-authenticated-put tutu-cas-client url req)]
         (when (not (<= 200 (:status response) 299))
           (throw (Exception. (str "Sending edit notification for application " application-key " to Tutu failed"))))
         (log/info (str "Sending edit notification for application " application-key " successfully sent to Tutu"))))))
