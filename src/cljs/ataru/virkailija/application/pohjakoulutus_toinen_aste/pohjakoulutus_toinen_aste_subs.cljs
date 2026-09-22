f0844530bbf7fdf49a563766afd4e3b1be1155ec
Tomi Katajanheimo <tomi@katajanheimo.com>
2026-09-22 09:49:20 +0300

    OY-5690 Add PK_ET to grade-order
    Elämänkatsomustieto (PK_ET) puuttui grade-orderista, katselmoijan
    huomiona lisätty uskonnon (PK_KT) viereen.

--- src/cljs/ataru/virkailija/application/pohjakoulutus_toinen_aste/pohjakoulutus_toinen_aste_subs.cljs
+++ src/cljs/ataru/virkailija/application/pohjakoulutus_toinen_aste/pohjakoulutus_toinen_aste_subs.cljs
@@ -86,7 +86,7 @@
 (def grade-order ["PK_AI" "PK_AOM" "PK_A1" "PK_A12" "PK_A2" "PK_A22"
                  "PK_B1" "PK_B2" "PK_B22" "PK_B23"
                  "PK_MA" "PK_BI" "PK_GE" "PK_FY" "PK_KE" "PK_YL"
-                 "PK_TE" "PK_KT" "PK_HI" "PK_YH" "PK_MU"
+                 "PK_TE" "PK_KT" "PK_ET" "PK_HI" "PK_YH" "PK_MU"
                  "PK_KU" "PK_KS" "PK_LI" "PK_KO" "PK_OP" "PK_OPA"])
 
 (re-frame/reg-sub
