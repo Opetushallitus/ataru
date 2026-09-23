848b033297e1f142dfabc0315f9ad30227bfc0f2
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-09-23 12:43:32 +0300

    OPHHAMA-18: Pieniä saavutettavuusparannuksia dropdown-komponenttiin

--- src/cljs/ataru/application_common/components/dropdown_keyboard.cljs
+++ src/cljs/ataru/application_common/components/dropdown_keyboard.cljs
@@ -1,10 +1,12 @@
 (ns ataru.application-common.components.dropdown-keyboard
   "Näppäimistönavigointi pudotusvalikon syötekentässä."
-  (:require [ataru.application-common.components.dropdown-actions :as actions]))
+  (:require [clojure.string :as string]
+            [ataru.application-common.components.dropdown-actions :as actions]))
 
 (defn make-on-input-key-down
   [{:keys [dropdown-id expanded? active-index selected-index last-option-index
-           active-option open-popup move-active-to set-active-index on-option-click]}]
+           active-option open-popup move-active-to set-active-index on-option-click
+           clearable? selected-value clear-value]}]
   (fn on-input-key-down [e]
     (cond
       (= "Escape" (.-key e))
@@ -16,6 +18,13 @@
         (.preventDefault e)
         (open-popup)
         (move-active-to (or selected-index 0)))
+      
+      (and (= "Backspace" (.-key e))
+           clearable?
+           (not (string/blank? selected-value))
+           (string/blank? (.. e -target -value)))
+      (do (.preventDefault e)
+          (clear-value))
 
       (< last-option-index 0)
       nil
