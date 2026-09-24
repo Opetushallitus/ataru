bea88da4b6bfb14e0e34776273325ccb236fd87e
August Kilponen <august.kilponen@gmail.com>
2026-09-24 09:36:31 +0300

    Lisätty lokitus sessioiden siivous -jobin aloittamisesta

--- src/clj/ataru/background_job/clean_old_sessions.clj
+++ src/clj/ataru/background_job/clean_old_sessions.clj
@@ -7,5 +7,6 @@
 (defqueries "sql/session-cleanup-queries.sql")
 
 (defn clean-old-sessions-job-step [_ _]
+  (log/info "clean-old-sessions-job: starting cleanup of old sessions.")
   (let [result (db/exec :db yesql-clean-up-old-sessions! {})]
     (log/info (str "clean-old-sessions-job: total of " result " sessions cleaned."))))
